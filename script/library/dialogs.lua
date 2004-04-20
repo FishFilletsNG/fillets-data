a9f89063d033118292ed5e98674234a2edc951b5
fidlej <fidlej>
2004-04-20 04:25:16 +0000

    Updated dialogs, 'cs' and 'en' are names for lang.

--- script/library/dialogs.lua
+++ script/library/dialogs.lua
@@ -1,113 +1,112 @@
-dialog_addFont("M", "font/small.png")
-dialog_addFont("V", "font/big.png")
+dialog_addFont("font_small", "font/small.png")
+dialog_addFont("font_big", "font/big.png")
 
 
-dialog_addDialog("vrak-v-vraky0", "english", "", "V", "These shipwrecs grow more and more arrogant.")
-dialog_addDialog("vrak-v-vraky0", "czech", "sound/"..codename.."/vrak-v-vraky0.ogg", "V", "Ty vraky lodí jsou èím dál drzej¹í.")
+dialog_addDialog("vrak-v-vraky0", "en", "", "font_big", "These shipwrecs grow more and more arrogant.")
+dialog_addDialog("vrak-v-vraky0", "cs", "sound/"..codename.."/cs/vrak-v-vraky0.ogg", "font_big", "Ty vraky lodí jsou èím dál drzej¹í.")
 
-dialog_addDialog("vrak-v-vraky1", "english", "", "V", "These sunken ships really irritate me.")
-dialog_addDialog("vrak-v-vraky1", "czech", "sound/"..codename.."/vrak-v-vraky1.ogg", "V", "Ty potopené lodì u¾ mi jdou na nervy.")
+dialog_addDialog("vrak-v-vraky1", "en", "", "font_big", "These sunken ships really irritate me.")
+dialog_addDialog("vrak-v-vraky1", "cs", "sound/"..codename.."/cs/vrak-v-vraky1.ogg", "font_big", "Ty potopené lodì u¾ mi jdou na nervy.")
 
-dialog_addDialog("vrak-v-vraky2", "english", "", "V", "Oh my, another ship sank. It really makes me itchy.")
-dialog_addDialog("vrak-v-vraky2", "czech", "sound/"..codename.."/vrak-v-vraky2.ogg", "V", "Ach jo, zase se nìkde potopila loï. U¾ mám tìch vrakù dost!")
+dialog_addDialog("vrak-v-vraky2", "en", "", "font_big", "Oh my, another ship sank. It really makes me itchy.")
+dialog_addDialog("vrak-v-vraky2", "cs", "sound/"..codename.."/cs/vrak-v-vraky2.ogg", "font_big", "Ach jo, zase se nìkde potopila loï. U¾ mám tìch vrakù dost!")
 
-dialog_addDialog("vrak-m-vrak0", "english", "", "M", "Look, it broke our bookcase!")
-dialog_addDialog("vrak-m-vrak0", "czech", "sound/"..codename.."/vrak-m-vrak0.ogg", "M", "Podívej, jak nám ponièily knihovnièku!")
+dialog_addDialog("vrak-m-vrak0", "en", "", "font_small", "Look, it broke our bookcase!")
+dialog_addDialog("vrak-m-vrak0", "cs", "sound/"..codename.."/cs/vrak-m-vrak0.ogg", "font_small", "Podívej, jak nám ponièily knihovnièku!")
 
-dialog_addDialog("vrak-m-vrak1", "english", "", "M", "Look, it smashed our shelves.")
-dialog_addDialog("vrak-m-vrak1", "czech", "sound/"..codename.."/vrak-m-vrak1.ogg", "M", "Podívej, rozbily nám polièky.")
+dialog_addDialog("vrak-m-vrak1", "en", "", "font_small", "Look, it smashed our shelves.")
+dialog_addDialog("vrak-m-vrak1", "cs", "sound/"..codename.."/cs/vrak-m-vrak1.ogg", "font_small", "Podívej, rozbily nám polièky.")
 
-dialog_addDialog("vrak-m-vrak2", "english", "", "M", "Look, it invaded our "..codename..".")
-dialog_addDialog("vrak-m-vrak2", "czech", "sound/"..codename.."/vrak-m-vrak2.ogg", "M", "Podívej, vlezly nám a¾ do knihovny.")
+dialog_addDialog("vrak-m-vrak2", "en", "", "font_small", "Look, it invaded our library.")
+dialog_addDialog("vrak-m-vrak2", "cs", "sound/"..codename.."/cs/vrak-m-vrak2.ogg", "font_small", "Podívej, vlezly nám a¾ do knihovny.")
 
-dialog_addDialog("vrak-m-kupovat0", "english", "", "M", "See? You shouldn`t have bought so many books.")
-dialog_addDialog("vrak-m-kupovat0", "czech", "sound/"..codename.."/vrak-m-kupovat0.ogg", "M", "To má¹ z toho, ¾e kupuje¹ tolik kní¾ek.")
+dialog_addDialog("vrak-m-kupovat0", "en", "", "font_small", "See? You shouldn`t have bought so many books.")
+dialog_addDialog("vrak-m-kupovat0", "cs", "sound/"..codename.."/cs/vrak-m-kupovat0.ogg", "font_small", "To má¹ z toho, ¾e kupuje¹ tolik kní¾ek.")
 
-dialog_addDialog("vrak-m-kupovat1", "english", "", "M", "You buy every book you can lay your hand on and now you have problems.")
-dialog_addDialog("vrak-m-kupovat1", "czech", "sound/"..codename.."/vrak-m-kupovat1.ogg", "M", "To má¹ z toho, ¾e koupí¹ ka¾dou kní¾ku, kterou vidí¹.")
+dialog_addDialog("vrak-m-kupovat1", "en", "", "font_small", "You buy every book you can lay your hand on and now you have problems.")
+dialog_addDialog("vrak-m-kupovat1", "cs", "sound/"..codename.."/cs/vrak-m-kupovat1.ogg", "font_small", "To má¹ z toho, ¾e koupí¹ ka¾dou kní¾ku, kterou vidí¹.")
 
-dialog_addDialog("vrak-m-naco", "english", "", "M", "Why do you have to have so many of them?")
-dialog_addDialog("vrak-m-naco", "czech", "sound/"..codename.."/vrak-m-naco.ogg", "M", "Na co ti v¹echny jsou?")
+dialog_addDialog("vrak-m-naco", "en", "", "font_small", "Why do you have to have so many of them?")
+dialog_addDialog("vrak-m-naco", "cs", "sound/"..codename.."/cs/vrak-m-naco.ogg", "font_small", "Na co ti v¹echny jsou?")
 
-dialog_addDialog("vrak-m-knihy0", "english", "", "M", "ESP")
-dialog_addDialog("vrak-m-knihy0", "czech", "sound/"..codename.."/vrak-m-knihy0.ogg", "M", "Paranormální jevy.")
+dialog_addDialog("vrak-m-knihy0", "en", "", "font_small", "ESP")
+dialog_addDialog("vrak-m-knihy0", "cs", "sound/"..codename.."/cs/vrak-m-knihy0.ogg", "font_small", "Paranormální jevy.")
 
-dialog_addDialog("vrak-m-knihy1", "english", "", "M", "Life after life.")
-dialog_addDialog("vrak-m-knihy1", "czech", "sound/"..codename.."/vrak-m-knihy1.ogg", "M", "®ivot po ¾ivotì.")
+dialog_addDialog("vrak-m-knihy1", "en", "", "font_small", "Life after life.")
+dialog_addDialog("vrak-m-knihy1", "cs", "sound/"..codename.."/cs/vrak-m-knihy1.ogg", "font_small", "®ivot po ¾ivotì.")
 
-dialog_addDialog("vrak-m-knihy2", "english", "", "M", "Memories of the future.")
-dialog_addDialog("vrak-m-knihy2", "czech", "sound/"..codename.."/vrak-m-knihy2.ogg", "M", "Vzpomínky na budoucnost.")
+dialog_addDialog("vrak-m-knihy2", "en", "", "font_small", "Memories of the future.")
+dialog_addDialog("vrak-m-knihy2", "cs", "sound/"..codename.."/cs/vrak-m-knihy2.ogg", "font_small", "Vzpomínky na budoucnost.")
 
-dialog_addDialog("vrak-m-knihy3", "english", "", "M", "Holistic methods.")
-dialog_addDialog("vrak-m-knihy3", "czech", "sound/"..codename.."/vrak-m-knihy3.ogg", "M", "Holistické metody.")
+dialog_addDialog("vrak-m-knihy3", "en", "", "font_small", "Holistic methods.")
+dialog_addDialog("vrak-m-knihy3", "cs", "sound/"..codename.."/cs/vrak-m-knihy3.ogg", "font_small", "Holistické metody.")
 
-dialog_addDialog("vrak-m-knihy4", "english", "", "M", "Teach yourself telepathy.")
-dialog_addDialog("vrak-m-knihy4", "czech", "sound/"..codename.."/vrak-m-knihy4.ogg", "M", "Telepatem snadno a rychle.")
+dialog_addDialog("vrak-m-knihy4", "en", "", "font_small", "Teach yourself telepathy.")
+dialog_addDialog("vrak-m-knihy4", "cs", "sound/"..codename.."/cs/vrak-m-knihy4.ogg", "font_small", "Telepatem snadno a rychle.")
 
-dialog_addDialog("vrak-m-knihy5", "english", "", "M", "Unwilling clairvoyant.")
-dialog_addDialog("vrak-m-knihy5", "czech", "sound/"..codename.."/vrak-m-knihy5.ogg", "M", "Jasnovidcem proti své vùli.")
+dialog_addDialog("vrak-m-knihy5", "en", "", "font_small", "Unwilling clairvoyant.")
+dialog_addDialog("vrak-m-knihy5", "cs", "sound/"..codename.."/cs/vrak-m-knihy5.ogg", "font_small", "Jasnovidcem proti své vùli.")
 
-dialog_addDialog("vrak-m-knihy6", "english", "", "M", "Outreaching Daniken.")
-dialog_addDialog("vrak-m-knihy6", "czech", "sound/"..codename.."/vrak-m-knihy6.ogg", "M", "Co Daniken netu¹il.")
+dialog_addDialog("vrak-m-knihy6", "en", "", "font_small", "Outreaching Daniken.")
+dialog_addDialog("vrak-m-knihy6", "cs", "sound/"..codename.."/cs/vrak-m-knihy6.ogg", "font_small", "Co Daniken netu¹il.")
 
-dialog_addDialog("vrak-v-vyhodit", "english", "", "V", "I`d throw out these, for example.")
-dialog_addDialog("vrak-v-vyhodit", "czech", "sound/"..codename.."/vrak-v-vyhodit.ogg", "V", "Já bych vyhodil tøeba támhlety.")
+dialog_addDialog("vrak-v-vyhodit", "en", "", "font_big", "I`d throw out these, for example.")
+dialog_addDialog("vrak-v-vyhodit", "cs", "sound/"..codename.."/cs/vrak-v-vyhodit.ogg", "font_big", "Já bych vyhodil tøeba támhlety.")
 
-dialog_addDialog("vrak-v-knihy0", "english", "", "V", "The Three Little Shrimp")
-dialog_addDialog("vrak-v-knihy0", "czech", "sound/"..codename.."/vrak-v-knihy0.ogg", "V", "Jak Frk a Brk putovali za sumý¹kem.")
+dialog_addDialog("vrak-v-knihy0", "en", "", "font_big", "The Three Little Shrimp")
+dialog_addDialog("vrak-v-knihy0", "cs", "sound/"..codename.."/cs/vrak-v-knihy0.ogg", "font_big", "Jak Frk a Brk putovali za sumý¹kem.")
 
-dialog_addDialog("vrak-v-knihy1", "english", "", "V", "The Gingerbread Submarine")
-dialog_addDialog("vrak-v-knihy1", "czech", "sound/"..codename.."/vrak-v-knihy1.ogg", "V", "O perníkové ponorce.")
+dialog_addDialog("vrak-v-knihy1", "en", "", "font_big", "The Gingerbread Submarine")
+dialog_addDialog("vrak-v-knihy1", "cs", "sound/"..codename.."/cs/vrak-v-knihy1.ogg", "font_big", "O perníkové ponorce.")
 
-dialog_addDialog("vrak-v-knihy2", "english", "", "V", "Goldilocks and the Three Lobsters")
-dialog_addDialog("vrak-v-knihy2", "czech", "sound/"..codename.."/vrak-v-knihy2.ogg", "V", "Dlouhý, ©iroký a Hluboký.")
+dialog_addDialog("vrak-v-knihy2", "en", "", "font_big", "Goldilocks and the Three Lobsters")
+dialog_addDialog("vrak-v-knihy2", "cs", "sound/"..codename.."/cs/vrak-v-knihy2.ogg", "font_big", "Dlouhý, ©iroký a Hluboký.")
 
-dialog_addDialog("vrak-v-knihy3", "english", "", "V", "Sharkerella")
-dialog_addDialog("vrak-v-knihy3", "czech", "sound/"..codename.."/vrak-v-knihy3.ogg", "V", "O rybce se zlatou mu¹lí na èele.")
+dialog_addDialog("vrak-v-knihy3", "en", "", "font_big", "Sharkerella")
+dialog_addDialog("vrak-v-knihy3", "cs", "sound/"..codename.."/cs/vrak-v-knihy3.ogg", "font_big", "O rybce se zlatou mu¹lí na èele.")
 
-dialog_addDialog("vrak-v-knihy4", "english", "", "V", "Slimy Squid and the Seven Dwarfs")
-dialog_addDialog("vrak-v-knihy4", "czech", "sound/"..codename.."/vrak-v-knihy4.ogg", "V", "Sépijka a sedm trpaslíkù.")
+dialog_addDialog("vrak-v-knihy4", "en", "", "font_big", "Slimy Squid and the Seven Dwarfs")
+dialog_addDialog("vrak-v-knihy4", "cs", "sound/"..codename.."/cs/vrak-v-knihy4.ogg", "font_big", "Sépijka a sedm trpaslíkù.")
 
-dialog_addDialog("vrak-m-pohadky", "english", "", "M", "Never! My lovely fairy-tales.")
-dialog_addDialog("vrak-m-pohadky", "czech", "sound/"..codename.."/vrak-m-pohadky.ogg", "M", "Nikdy! Moje oblíbené pohádky!")
+dialog_addDialog("vrak-m-pohadky", "en", "", "font_small", "Never! My lovely fairy-tales.")
+dialog_addDialog("vrak-m-pohadky", "cs", "sound/"..codename.."/cs/vrak-m-pohadky.ogg", "font_small", "Nikdy! Moje oblíbené pohádky!")
 
-dialog_addDialog("vrak-v-nevejdu0", "english", "", "V", "I can`t hide myself here.")
-dialog_addDialog("vrak-v-nevejdu0", "czech", "sound/"..codename.."/vrak-v-nevejdu0.ogg", "V", "Sem se neschovám.")
+dialog_addDialog("vrak-v-nevejdu0", "en", "", "font_big", "I can`t hide myself here.")
+dialog_addDialog("vrak-v-nevejdu0", "cs", "sound/"..codename.."/cs/vrak-v-nevejdu0.ogg", "font_big", "Sem se neschovám.")
 
-dialog_addDialog("vrak-v-nevejdu1", "english", "", "V", "I can`t fit in here.")
-dialog_addDialog("vrak-v-nevejdu1", "czech", "sound/"..codename.."/vrak-v-nevejdu1.ogg", "V", "Sem se nevejdu.")
+dialog_addDialog("vrak-v-nevejdu1", "en", "", "font_big", "I can`t fit in here.")
+dialog_addDialog("vrak-v-nevejdu1", "cs", "sound/"..codename.."/cs/vrak-v-nevejdu1.ogg", "font_big", "Sem se nevejdu.")
 
-dialog_addDialog("vrak-m-cteni0", "english", "", "M", "Why do we have so many books if we cannot pull them out anyway?")
-dialog_addDialog("vrak-m-cteni0", "czech", "sound/"..codename.."/vrak-m-cteni0.ogg", "M", "K èemu je nám tolik kní¾ek, kdy¾ stejnì nejdou vytáhnout z polièek?")
+dialog_addDialog("vrak-m-cteni0", "en", "", "font_small", "Why do we have so many books if we cannot pull them out anyway?")
+dialog_addDialog("vrak-m-cteni0", "cs", "sound/"..codename.."/cs/vrak-m-cteni0.ogg", "font_small", "K èemu je nám tolik kní¾ek, kdy¾ stejnì nejdou vytáhnout z polièek?")
 
-dialog_addDialog("vrak-m-cteni1", "english", "", "M", "If you did some work-outs instead of reading about silly affairs you could fit yourself in.")
-dialog_addDialog("vrak-m-cteni1", "czech", "sound/"..codename.."/vrak-m-cteni1.ogg", "M", "Kdybys místo ètení o nesmyslech trochu sportoval, ve¹el by ses tam.")
+dialog_addDialog("vrak-m-cteni1", "en", "", "font_small", "If you did some work-outs instead of reading about silly affairs you could fit yourself in.")
+dialog_addDialog("vrak-m-cteni1", "cs", "sound/"..codename.."/cs/vrak-m-cteni1.ogg", "font_small", "Kdybys místo ètení o nesmyslech trochu sportoval, ve¹el by ses tam.")
 
-dialog_addDialog("vrak-m-cteni2", "english", "", "M", "I told you not to buy that Compleate Dictionary of Abnormal.")
-dialog_addDialog("vrak-m-cteni2", "czech", "sound/"..codename.."/vrak-m-cteni2.ogg", "M", "Øíkala jsem ti, a» nekupuje¹ ten Velký slovník abnormálních výrazù.")
+dialog_addDialog("vrak-m-cteni2", "en", "", "font_small", "I told you not to buy that Compleate Dictionary of Abnormal.")
+dialog_addDialog("vrak-m-cteni2", "cs", "sound/"..codename.."/cs/vrak-m-cteni2.ogg", "font_small", "Øíkala jsem ti, a» nekupuje¹ ten Velký slovník abnormálních výrazù.")
 
-dialog_addDialog("vrak-v-policky", "english", "", "V", "Let`s think. We can`t get these books off the shelf.")
-dialog_addDialog("vrak-v-policky", "czech", "sound/"..codename.."/vrak-v-policky.ogg", "V", "Zamysleme se. Ty knihy nedostaneme z polièek.")
+dialog_addDialog("vrak-v-policky", "en", "", "font_big", "Let`s think. We can`t get these books off the shelf.")
+dialog_addDialog("vrak-v-policky", "cs", "sound/"..codename.."/cs/vrak-v-policky.ogg", "font_big", "Zamysleme se. Ty knihy nedostaneme z polièek.")
 
-dialog_addDialog("vrak-m-predmety", "english", "", "M", "There are only two objects here which we can move with any results.")
-dialog_addDialog("vrak-m-predmety", "czech", "sound/"..codename.."/vrak-m-predmety.ogg", "M", "Jsou tu jen dva pøedmìty, se kterými má smysl hýbat.")
+dialog_addDialog("vrak-m-predmety", "en", "", "font_small", "There are only two objects here which we can move with any results.")
+dialog_addDialog("vrak-m-predmety", "cs", "sound/"..codename.."/cs/vrak-m-predmety.ogg", "font_small", "Jsou tu jen dva pøedmìty, se kterými má smysl hýbat.")
 
-dialog_addDialog("vrak-m-ostnatec", "english", "", "M", "How did that prickler get here?")
-dialog_addDialog("vrak-m-ostnatec", "czech", "sound/"..codename.."/vrak-m-ostnatec.ogg", "M", "Kde se tady vzal ten ostnatec?")
+dialog_addDialog("vrak-m-ostnatec", "en", "", "font_small", "How did that prickler get here?")
+dialog_addDialog("vrak-m-ostnatec", "cs", "sound/"..codename.."/cs/vrak-m-ostnatec.ogg", "font_small", "Kde se tady vzal ten ostnatec?")
 
-dialog_addDialog("vrak-m-zivocich", "english", "", "M", "You are in the way, strange life form!")
-dialog_addDialog("vrak-m-zivocich", "czech", "sound/"..codename.."/vrak-m-zivocich.ogg", "M", "®ivoèichu, zavazí¹!")
+dialog_addDialog("vrak-m-zivocich", "en", "", "font_small", "You are in the way, strange life form!")
+dialog_addDialog("vrak-m-zivocich", "cs", "sound/"..codename.."/cs/vrak-m-zivocich.ogg", "font_small", "®ivoèichu, zavazí¹!")
 
-dialog_addDialog("vrak-v-potvurka", "english", "", "V", "You are an obstacle, strange creature!")
-dialog_addDialog("vrak-v-potvurka", "czech", "sound/"..codename.."/vrak-v-potvurka.ogg", "V", "Zavazí¹, potvùrko!")
+dialog_addDialog("vrak-v-potvurka", "en", "", "font_big", "You are an obstacle, strange creature!")
+dialog_addDialog("vrak-v-potvurka", "cs", "sound/"..codename.."/cs/vrak-v-potvurka.ogg", "font_big", "Zavazí¹, potvùrko!")
 
-dialog_addDialog("vrak-v-snek", "english", "", "V", "Can`t you push that snail to me?")
-dialog_addDialog("vrak-v-snek", "czech", "sound/"..codename.."/vrak-v-snek.ogg", "V", "Nemù¾e¹ toho ¹neka posunout sem ke mnì?")
+dialog_addDialog("vrak-v-snek", "en", "", "font_big", "Can`t you push that snail to me?")
+dialog_addDialog("vrak-v-snek", "cs", "sound/"..codename.."/cs/vrak-v-snek.ogg", "font_big", "Nemù¾e¹ toho ¹neka posunout sem ke mnì?")
 
-dialog_addDialog("vrak-m-ocel", "english", "", "M", "How am I to get there through that steel?!")
-dialog_addDialog("vrak-m-ocel", "czech", "sound/"..codename.."/vrak-m-ocel.ogg", "M", "Jak se tam mám asi dostat, pøes tu ocel?!")
+dialog_addDialog("vrak-m-ocel", "en", "", "font_small", "How am I to get there through that steel?!")
+dialog_addDialog("vrak-m-ocel", "cs", "sound/"..codename.."/cs/vrak-m-ocel.ogg", "font_small", "Jak se tam mám asi dostat, pøes tu ocel?!")
 
-dialog_addDialog("vrak-m-restart", "english", "", "M", "What about restarting and trying it again from the scratch?")
-dialog_addDialog("vrak-m-restart", "czech", "sound/"..codename.."/vrak-m-restart.ogg", "M", "Co takhle restartovat a zkusit to znovu?")
-
+dialog_addDialog("vrak-m-restart", "en", "", "font_small", "What about restarting and trying it again from the scratch?")
+dialog_addDialog("vrak-m-restart", "cs", "sound/"..codename.."/cs/vrak-m-restart.ogg", "font_small", "Co takhle restartovat a zkusit to znovu?")
 
