7b46537c3fc5d4ecd12110c1fba677882b7847e3
mrpaja <mrpaja>
2004-04-25 21:57:51 +0000

    level noground - scripts

---
+++ script/noground/dialogs.lua
@@ -0,0 +1,19 @@
+dialog_addFont("font_small", "font/small.png")
+dialog_addFont("font_big", "font/big.png")
+
+
+dialog_addDialog("nog-v-zvlastni", "en", "", "font_big", "This is a very strange room.")
+dialog_addDialog("nog-v-zvlastni", "cs", "sound/"..codename.."/cs/nog-v-zvlastni.ogg", "font_big", "Tohle je ale zvlá¹tní místnost.")
+
+dialog_addDialog("nog-m-uvedom0", "en", "", "font_small", "You need to realize that the steel cylinder surrounding us is only an object.")
+dialog_addDialog("nog-m-uvedom0", "cs", "sound/"..codename.."/cs/nog-m-uvedom0.ogg", "font_small", "Je tøeba si uvìdomit, ¾e ta trubka kolem je jen pøedmìt.")
+
+dialog_addDialog("nog-m-uvedom1", "en", "", "font_small", "All in all there are only four squares of wall here.")
+dialog_addDialog("nog-m-uvedom1", "cs", "sound/"..codename.."/cs/nog-m-uvedom1.ogg", "font_small", "Jsou tu jen ètyøi políèka zdi.")
+
+dialog_addDialog("nog-v-smetiste0", "en", "", "font_big", "This trash heap is weird...")
+dialog_addDialog("nog-v-smetiste0", "cs", "sound/"..codename.."/cs/nog-v-smetiste0.ogg", "font_big", "Tohle smeti¹tì je divný...")
+
+dialog_addDialog("nog-v-smetiste1", "en", "", "font_big", "This garbage heap still looks kind of strange...")
+dialog_addDialog("nog-v-smetiste1", "cs", "sound/"..codename.."/cs/nog-v-smetiste1.ogg", "font_big", "Tohle smeti¹tì je stejnì ale divný...")
+
