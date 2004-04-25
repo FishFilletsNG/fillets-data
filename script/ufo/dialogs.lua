ee86695d26767ade6e8b68ce55f986d8f446a242
fidlej <fidlej>
2004-04-25 13:43:58 +0000

    Converted levels 'ufo' and 'computer'.

---
+++ script/ufo/dialogs.lua
@@ -0,0 +1,52 @@
+dialog_addFont("font_small", "font/small.png")
+dialog_addFont("font_big", "font/big.png")
+
+
+dialog_addDialog("ufo-v-znicilo", "en", "", "font_big", "We must be getting near the end of our mission. This is surely the UFO that destroyed our house.")
+dialog_addDialog("ufo-v-znicilo", "cs", "sound/"..codename.."/cs/ufo-v-znicilo.ogg", "font_big", "Blí¾íme se konci na¹í cesty: tohle je jistì to UFO, co nám znièilo domeèek.")
+
+dialog_addDialog("ufo-m-osmy", "en", "", "font_small", "Are you sure? This is but the eighth level.")
+dialog_addDialog("ufo-m-osmy", "cs", "sound/"..codename.."/cs/ufo-m-osmy.ogg", "font_small", "Myslí¹? V¾dy» je to teprve osmý level od zaèátku.")
+
+dialog_addDialog("ufo-m-valce", "en", "", "font_small", "These steel cylinders are pretty obnoxious.")
+dialog_addDialog("ufo-m-valce", "cs", "sound/"..codename.."/cs/ufo-m-valce.ogg", "font_small", "Ty ocelové válce mi jdou pìknì na nervy.")
+
+dialog_addDialog("ufo-m-moc", "en", "", "font_small", "There is just too much steel here.")
+dialog_addDialog("ufo-m-moc", "cs", "sound/"..codename.."/cs/ufo-m-moc.ogg", "font_small", "Oceli je tu tentokrát trochu moc.")
+
+dialog_addDialog("ufo-v-hur", "en", "", "font_big", "It could be worse. I for one can imagine myself a room where there is nothing but steel.")
+dialog_addDialog("ufo-v-hur", "cs", "sound/"..codename.."/cs/ufo-v-hur.ogg", "font_big", "Mù¾e být i hùø. Já si tøeba dovedu pøedstavit místo, kde nebude nic ne¾ ocel.")
+
+dialog_addDialog("ufo-m-ne", "en", "", "font_small", "My goodness? What should I do in such a level?")
+dialog_addDialog("ufo-m-ne", "cs", "sound/"..codename.."/cs/ufo-m-ne.ogg", "font_small", "To snad ne. Co bych v takové místnosti dìlala?")
+
+dialog_addDialog("ufo-v-vpredu", "en", "", "font_big", "You would let me go in front, for a change.")
+dialog_addDialog("ufo-v-vpredu", "cs", "sound/"..codename.."/cs/ufo-v-vpredu.ogg", "font_big", "Koneènì bys mì nechala jít vpøedu.")
+
+dialog_addDialog("ufo-m-zvlastni", "en", "", "font_small", "It`s interesting that this tangled steel forestalls our entry into that UFO more effectively than any security system.")
+dialog_addDialog("ufo-m-zvlastni", "cs", "sound/"..codename.."/cs/ufo-m-zvlastni.ogg", "font_small", "Zvlá¹tní je, ¾e ta zmì» oceli funguje jako úèinnìj¹í pøeká¾ka ne¾ bezpeènostní systém toho UFA.")
+
+dialog_addDialog("ufo-v-rikam", "en", "", "font_big", "I keep telling you that our life is controlled by some higher, strange consciousness.")
+dialog_addDialog("ufo-v-rikam", "cs", "sound/"..codename.."/cs/ufo-v-rikam.ogg", "font_big", "Poøád ti øíkám, ¾e ná¹ ¾ivot je øízen jakousi vy¹¹í, cizí inteligencí.")
+
+dialog_addDialog("ufo-m-vidim", "en", "", "font_small", "I can see that, too. But I am not that sure it`s higher.")
+dialog_addDialog("ufo-m-vidim", "cs", "sound/"..codename.."/cs/ufo-m-vidim.ogg", "font_small", "To vidím taky. Jenom si nejsem jistá, ¾e je vy¹¹í.")
+
+dialog_addDialog("ufo-v-dovnitr", "en", "", "font_big", "What can we find inside, once we manage to get there?")
+dialog_addDialog("ufo-v-dovnitr", "cs", "sound/"..codename.."/cs/ufo-v-dovnitr.ogg", "font_big", "Co tam tak mù¾eme najít, jestli se dostaneme dovnitø?")
+
+dialog_addDialog("ufo-m-tajemstvi", "en", "", "font_small", "Secrets of another civilization, new technologies, new sources of energy...")
+dialog_addDialog("ufo-m-tajemstvi", "cs", "sound/"..codename.."/cs/ufo-m-tajemstvi.ogg", "font_small", "Tajemství cizí civilizace, nové technologie, nové zdroje energie...")
+
+dialog_addDialog("ufo-v-zjistit0", "en", "", "font_big", "Most importantly, we should try to find out why they came at all.")
+dialog_addDialog("ufo-v-zjistit0", "cs", "sound/"..codename.."/cs/ufo-v-zjistit0.ogg", "font_big", "Hlavnì bychom mìli zjistit, proè sem vlastnì pøiletìli.")
+
+dialog_addDialog("ufo-v-zjistit1", "en", "", "font_big", "You know, most of all I wonder why have they crashed just here.")
+dialog_addDialog("ufo-v-zjistit1", "cs", "sound/"..codename.."/cs/ufo-v-zjistit1.ogg", "font_big", "Ví¹, nejvíc by mì zajímalo, proè ztroskotali právì tady.")
+
+dialog_addDialog("ufo-m-tady", "en", "", "font_small", "Didn`t you see it? Everything happens just here.")
+dialog_addDialog("ufo-m-tady", "cs", "sound/"..codename.."/cs/ufo-m-tady.ogg", "font_small", "V¹ak jsi to vidìl. V¹echno se dìje právì tady.")
+
+dialog_addDialog("ufo-m-nevim", "en", "", "font_small", "I don`t know why the UFOs keep crashing at our house, either.")
+dialog_addDialog("ufo-m-nevim", "cs", "sound/"..codename.."/cs/ufo-m-nevim.ogg", "font_small", "Taky nevím, proè ka¾dé UFO padá právì na ná¹ domeèek.")
+
