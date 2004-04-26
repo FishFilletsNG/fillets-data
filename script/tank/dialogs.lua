37a0c610e0044bb3d0f60ac98940abe1af0e82ad
mrpaja <mrpaja>
2004-04-26 11:00:37 +0000

    level tank - scripts

---
+++ script/tank/dialogs.lua
@@ -0,0 +1,52 @@
+dialog_addFont("font_small", "font/small.png")
+dialog_addFont("font_big", "font/big.png")
+
+
+dialog_addDialog("sv-m-pomohli", "en", "", "font_small", "This is some improvement. From bathyscaph to tank, this is like from line to the net.")
+dialog_addDialog("sv-m-pomohli", "cs", "sound/"..codename.."/cs/sv-m-pomohli.ogg", "font_small", "To jsme si pomohli. Z batyskafu do tanku, to je jako ze sítì do podbìráku.")
+
+dialog_addDialog("sv-v-bezsneku", "en", "", "font_big", "At least, there are no oh-so-obliging snails here.")
+dialog_addDialog("sv-v-bezsneku", "cs", "sound/"..codename.."/cs/sv-v-bezsneku.ogg", "font_big", "Aspoò tu nejsou ¾ádní pøeochotní ¹neci.")
+
+dialog_addDialog("sv-m-utecha", "en", "", "font_small", "This is but little consolation.")
+dialog_addDialog("sv-m-utecha", "cs", "sound/"..codename.."/cs/sv-m-utecha.ogg", "font_small", "To je jen slabá útìcha.")
+
+dialog_addDialog("sv-v-chtel", "en", "", "font_big", "Thinking about it, I always wanted to see the tank from within.")
+dialog_addDialog("sv-v-chtel", "cs", "sound/"..codename.."/cs/sv-v-chtel.ogg", "font_big", "Vlasntì jsem si v¾dycky chtìl prohlédnout tank zevnitø.")
+
+dialog_addDialog("sv-m-doscasu", "en", "", "font_small", "It seems you will have plenty of opportunity to do this.")
+dialog_addDialog("sv-m-doscasu", "cs", "sound/"..codename.."/cs/sv-m-doscasu.ogg", "font_small", "Na to teï bude¹ mít èasu víc ne¾ dost.")
+
+dialog_addDialog("sv-m-tank", "en", "", "font_small", "How did this tank got into the sea?")
+dialog_addDialog("sv-m-tank", "cs", "sound/"..codename.."/cs/sv-m-tank.ogg", "font_small", "Jak se asi dostal ten tank do moøe?")
+
+dialog_addDialog("sv-v-obojzivelny", "en", "", "font_big", "Maybe it was an amphibious tank.")
+dialog_addDialog("sv-v-obojzivelny", "cs", "sound/"..codename.."/cs/sv-v-obojzivelny.ogg", "font_big", "Mo¾ná to byl oboj¾ivelný tank.")
+
+dialog_addDialog("sv-m-kecy", "en", "", "font_small", "Sea amphibious tank? Just imagine: On the moonless night, the uncomprehending defenders bewilderedly watch the forest of snorkels emerging from the surf, searching in vain for the landing craft...")
+dialog_addDialog("sv-m-kecy", "cs", "sound/"..codename.."/cs/sv-m-kecy.ogg", "font_small", "Moøský oboj¾ivelný tank? Úplnì to vidím pøed sebou: za bezmìsíèné noci se k pobøe¾í omývanému vlnami ¹írého oceánu blí¾í les ¹norchlù... a na píseèné plá¾e vyjí¾dìjí první obrnìnci... konsternovaní obránci nechápavì hledají loï, která je tu vysadila...")
+
+dialog_addDialog("sv-v-proc", "en", "", "font_big", "So why are you asking, if you are so darned clever?!")
+dialog_addDialog("sv-v-proc", "cs", "sound/"..codename.."/cs/sv-v-proc.ogg", "font_big", "Tak proè se ptá¹, kdy¾ jsi tak chytrá?!")
+
+dialog_addDialog("sv-v-potopena", "en", "", "font_big", "Maybe, there is sunken landing craft nearby.")
+dialog_addDialog("sv-v-potopena", "cs", "sound/"..codename.."/cs/sv-v-potopena.ogg", "font_big", "Mo¾ná je tu potopená výsadková loï.")
+
+dialog_addDialog("sv-m-pravdepodob", "en", "", "font_small", "It is possible.")
+dialog_addDialog("sv-m-pravdepodob", "cs", "sound/"..codename.."/cs/sv-m-pravdepodob.ogg", "font_small", "To je pravdìpodobné.")
+
+dialog_addDialog("sv-m-munice", "en", "", "font_small", "Do you think that this ammunition could harm us?")
+dialog_addDialog("sv-m-munice", "cs", "sound/"..codename.."/cs/sv-m-munice.ogg", "font_small", "Myslí¹, ¾e ta munice je je¹tì funkèní?")
+
+dialog_addDialog("sv-v-nevim", "en", "", "font_big", "I don`t know. But surely, I`ll try to keep my distance from it.")
+dialog_addDialog("sv-v-nevim", "cs", "sound/"..codename.."/cs/sv-v-nevim.ogg", "font_big", "Nevím. Ka¾dopádnì se od ní budu dr¾et rad¹i dál.")
+
+dialog_addDialog("sv-v-zebrik", "en", "", "font_big", "I think we are going to need that ladder.")
+dialog_addDialog("sv-v-zebrik", "cs", "sound/"..codename.."/cs/sv-v-zebrik.ogg", "font_big", "Myslím, ¾e budeme potøebovat ten ¾ebøík.")
+
+dialog_addDialog("sv-m-ven", "en", "", "font_small", "To climb out? But we have only fins.")
+dialog_addDialog("sv-m-ven", "cs", "sound/"..codename.."/cs/sv-m-ven.ogg", "font_small", "Abychom po nìm vylezli ven? V¾dy» máme jen ploutve.")
+
+dialog_addDialog("sv-v-ucpat", "en", "", "font_big", "No. To plug that hole.")
+dialog_addDialog("sv-v-ucpat", "cs", "sound/"..codename.."/cs/sv-v-ucpat.ogg", "font_big", "Abychom s ním ucpali tu díru.")
+
