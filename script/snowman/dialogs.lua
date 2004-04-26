464855bc297e781329ec128c32627f94bd4a57a2
mrpaja <mrpaja>
2004-04-26 11:45:31 +0000

    level snowman - scripts

---
+++ script/snowman/dialogs.lua
@@ -0,0 +1,37 @@
+dialog_addFont("font_small", "font/small.png")
+dialog_addFont("font_big", "font/big.png")
+
+
+dialog_addDialog("tr-m-chlad1", "en", "", "font_small", "I`m kind of cold.")
+dialog_addDialog("tr-m-chlad1", "cs", "sound/"..codename.."/cs/tr-m-chlad1.ogg", "font_small", "Je mi nìjak chladno.")
+
+dialog_addDialog("tr-v-jid1", "en", "", "font_big", "No wonder. This is the Winter Mess Hall.")
+dialog_addDialog("tr-v-jid1", "cs", "sound/"..codename.."/cs/tr-v-jid1.ogg", "font_big", "Není divu, v¾dy» tohle je Zimní jídelna.")
+
+dialog_addDialog("tr-m-chlad2", "en", "", "font_small", "I`m cold.")
+dialog_addDialog("tr-m-chlad2", "cs", "sound/"..codename.."/cs/tr-m-chlad2.ogg", "font_small", "Je mi zima.")
+
+dialog_addDialog("tr-v-jid2", "en", "", "font_big", "Well that`s what you would expect in a Winter Mess Hall, right?")
+dialog_addDialog("tr-v-jid2", "cs", "sound/"..codename.."/cs/tr-v-jid2.ogg", "font_big", "To je snad v Zimní jídelnì normální, ne?")
+
+dialog_addDialog("tr-v-prezil", "en", "", "font_big", "At least the snowman has survived.")
+dialog_addDialog("tr-v-prezil", "cs", "sound/"..codename.."/cs/tr-v-prezil.ogg", "font_big", "Aspoò ¾e to pøe¾il ten snìhulák.")
+
+dialog_addDialog("tr-m-cvicit", "en", "", "font_small", "Mabye you should start thinking about how to manipulate the tables.")
+dialog_addDialog("tr-m-cvicit", "cs", "sound/"..codename.."/cs/tr-m-cvicit.ogg", "font_small", "Mìl by sis cvièit manipulaci se stoly.")
+
+dialog_addDialog("tr-m-ztuhl", "en", "", "font_small", "Everything is so frozen here...")
+dialog_addDialog("tr-m-ztuhl", "cs", "sound/"..codename.."/cs/tr-m-ztuhl.ogg", "font_small", "V¹echno je tu tak ztuhlé...")
+
+dialog_addDialog("tr-m-au1", "en", "", "font_small", "Ow!")
+dialog_addDialog("tr-m-au1", "cs", "sound/"..codename.."/cs/tr-m-au1.ogg", "font_small", "Au!")
+
+dialog_addDialog("tr-m-au2", "en", "", "font_small", "Ouch!")
+dialog_addDialog("tr-m-au2", "cs", "sound/"..codename.."/cs/tr-m-au2.ogg", "font_small", "Auvajs.")
+
+dialog_addDialog("tr-v-agres", "en", "", "font_big", "I`ve never seen such an agressive snowman.")
+dialog_addDialog("tr-v-agres", "cs", "sound/"..codename.."/cs/tr-v-agres.ogg", "font_big", "Tak agresívního snìhuláka jsem je¹tì nevidìl.")
+
+dialog_addDialog("tr-x-koste", "en", "")
+dialog_addDialog("tr-x-koste", "cs", "sound/"..codename.."/cs/tr-x-koste.ogg")
+
