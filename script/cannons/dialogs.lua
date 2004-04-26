723b3c202a4e52efb250c451c8d751fe1d14ffdc
mrpaja <mrpaja>
2004-04-26 14:55:49 +0000

    level cannons - scripts

---
+++ script/cannons/dialogs.lua
@@ -0,0 +1,31 @@
+dialog_addFont("font_small", "font/small.png")
+dialog_addFont("font_big", "font/big.png")
+
+
+dialog_addDialog("del-v-dve", "en", "", "font_big", "See? This ship had two gun decks. It must have been a man-of-war. The acrid smell of gunpowder still seems to linger in the air...")
+dialog_addDialog("del-v-dve", "cs", "sound/"..codename.."/cs/del-v-dve.ogg", "font_big", "Vidí¹? Tahle loï mìla dvì dìlové paluby. To byla jistì øadová loï. Jako by se tu ve vzduchu je¹tì vzná¹ela vùnì støelného prachu.")
+
+dialog_addDialog("del-m-voda", "en", "", "font_small", "You mean, in the water.")
+dialog_addDialog("del-m-voda", "cs", "sound/"..codename.."/cs/del-m-voda.ogg", "font_small", "Chce¹ øíct ve vodì.")
+
+dialog_addDialog("del-m-ci", "en", "", "font_small", "Whose ship was she? Maybe she served under admiral Nelson... or captain Hornblower.")
+dialog_addDialog("del-m-ci", "cs", "sound/"..codename.."/cs/del-m-ci.ogg", "font_small", "Èípak to byla asi loï? Mo¾ná se na ní plavil admirál Nelson... nebo kapitán Hornblower.")
+
+dialog_addDialog("del-v-splet", "en", "", "font_big", "No, I think you`re about a hundred years off.")
+dialog_addDialog("del-v-splet", "cs", "sound/"..codename.."/cs/del-v-splet.ogg", "font_big", "V tom pøípadì jsme se spletli nejménì o sto let.")
+
+dialog_addDialog("del-v-mec", "en", "", "font_big", "I have a feeling we`re going to need that sword.")
+dialog_addDialog("del-v-mec", "cs", "sound/"..codename.."/cs/del-v-mec.ogg", "font_big", "Nìco mi øíká, ¾e budeme potøebovat ten meè.")
+
+dialog_addDialog("del-m-tus", "en", "", "font_small", "You and your 'feelings`. Besides, we`re not supposed to be giving any hints.")
+dialog_addDialog("del-m-tus", "cs", "sound/"..codename.."/cs/del-m-tus.ogg", "font_small", "Ty tvoje tu¹ení. A vùbec, nenapovídej.")
+
+dialog_addDialog("del-m-jedn0", "en", "", "font_small", "That was a little too simple, wasn`t it?")
+dialog_addDialog("del-m-jedn0", "cs", "sound/"..codename.."/cs/del-m-jedn0.ogg", "font_small", "To bylo nìjak jednoduché, ne?")
+
+dialog_addDialog("del-m-jedn1", "en", "", "font_small", "Could it possibly be so simple?")
+dialog_addDialog("del-m-jedn1", "cs", "sound/"..codename.."/cs/del-m-jedn1.ogg", "font_small", "®e by to bylo tak jednoduchý?")
+
+dialog_addDialog("del-m-jedn2", "en", "", "font_small", "There must be some kind of mistake.")
+dialog_addDialog("del-m-jedn2", "cs", "sound/"..codename.."/cs/del-m-jedn2.ogg", "font_small", "To se autoøi asi spletli.")
+
