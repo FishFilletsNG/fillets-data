74a29ad4bb0ca2b31c0f242b0bb94e652918876a
fidlej <fidlej>
2004-05-08 00:23:10 +0000

    Added files for level description.

---
+++ script/worlddesc.lua
@@ -0,0 +1,11 @@
+
+-- worldmap_addDesc(codename, lang, levelname, desc)
+worldmap_addDesc("menu", "en", "Game menu", "Fish Fillets - Next Generation")
+worldmap_addDesc("menu", "cs", "Herní menu", "Fish Fillets - Next Generation")
+
+worldmap_addDesc("start", "en", "How It All Started", "Fish House")
+worldmap_addDesc("start", "cs", "Jak to v¹echno zaèalo", "Rybí domeèek")
+
+worldmap_addDesc("briefcase", "en", "Briefcase Message", "Fish House")
+worldmap_addDesc("briefcase", "cs", "Podivná zásilka", "Rybí domeèek")
+
