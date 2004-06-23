0687c3f904f36e9ff6c481bde5537a232aaf8464
mrpaja <mrpaja>
2004-06-23 23:43:49 +0000

    new levels - airplane, wreck

---
+++ script/airplane/dialogs.lua
@@ -0,0 +1,28 @@
+dialog_addFont("font_big", "font/font_big.png")
+dialog_addFont("font_small", "font/font_small.png")
+
+dialog_addDialog("let-m-divna", "en", "", "font_small", "What kind of strange ship is that?")
+dialog_addDialog("let-m-divna", "cs", "sound/"..codename.."/cs/let-m-divna.ogg", "font_small", "Co je to za divnou loï?")
+
+dialog_addDialog("let-v-vrak0", "en", "", "font_big", "This is a wreck of civilian airplane LC-10 Lemura.")
+dialog_addDialog("let-v-vrak0", "cs", "sound/"..codename.."/cs/let-v-vrak0.ogg", "font_big", "To je vrak dopravního letadla LC-10 Lemura.")
+
+dialog_addDialog("let-v-vrak1", "en", "", "font_big", "This is a wreck of civilian airplane Atlantobus.")
+dialog_addDialog("let-v-vrak1", "cs", "sound/"..codename.."/cs/let-v-vrak1.ogg", "font_big", "To je vrak dopravního letadla Atlantobus.")
+
+dialog_addDialog("let-v-vrak2", "en", "", "font_big", "This is a wreck of civilian airplane Poseidon 737.")
+dialog_addDialog("let-v-vrak2", "cs", "sound/"..codename.."/cs/let-v-vrak2.ogg", "font_big", "To je vrak dopravního letadla Poseidon 737.")
+
+dialog_addDialog("let-v-oko", "en", "", "font_big", "Can you see that eye? The silent witness to the tragedy... Somebody trusted this airplane - and the glass eye is everything he left behind.")
+dialog_addDialog("let-v-oko", "cs", "sound/"..codename.."/cs/let-v-oko.ogg", "font_big", "Vidí¹ to oko? Nìmý svìdek tragédie... Nìkdo v dùvìøe usedl do letadla - a zùstalo z nìho jen sklenìné oko.")
+
+dialog_addDialog("let-m-oko", "en", "", "font_small", "This is not a glass eye but gyroscope. At least in this level.")
+dialog_addDialog("let-m-oko", "cs", "sound/"..codename.."/cs/let-m-oko.ogg", "font_small", "To není sklenìné oko, ale gyroskop. Aspoò v této místnosti.")
+
+dialog_addDialog("let-m-sedadlo", "en", "", "font_small", "Seats. Why is there so many seats here?")
+dialog_addDialog("let-m-sedadlo", "cs", "sound/"..codename.."/cs/let-m-sedadlo.ogg", "font_small", "Sedadla. Proè jsou tu v¹ude sedadla?")
+
+dialog_addDialog("let-v-budrada", "en", "", "font_big", "Be thankful. Could you get out without them?")
+dialog_addDialog("let-v-budrada", "cs", "sound/"..codename.."/cs/let-v-budrada.ogg", "font_big", "Buï ráda. Jak by ses jinak dostala ven?")
+
+
---
+++ script/wreck/dialogs.lua
@@ -0,0 +1,79 @@
+dialog_addFont("font_big", "font/font_big.png")
+dialog_addFont("font_small", "font/font_small.png")
+
+dialog_addDialog("pot-v-lod", "en", "", "font_big", "Could this be Captain Silver`s ship?")
+dialog_addDialog("pot-v-lod", "cs", "sound/"..codename.."/cs/pot-v-lod.ogg", "font_big", "®e by to byla ta Silverova loï?")
+
+dialog_addDialog("pot-m-soud", "en", "", "font_small", "What makes you think so?")
+dialog_addDialog("pot-m-soud", "cs", "sound/"..codename.."/cs/pot-m-soud.ogg", "font_small", "Z èeho tak soudí¹?")
+
+dialog_addDialog("pot-v-jmeno", "en", "", "font_big", "The title of this level.")
+dialog_addDialog("pot-v-jmeno", "cs", "sound/"..codename.."/cs/pot-v-jmeno.ogg", "font_big", "Ze jména této místnosti.")
+
+dialog_addDialog("pot-v-slus", "en", "", "font_big", "It suits you well.")
+dialog_addDialog("pot-v-slus", "cs", "sound/"..codename.."/cs/pot-v-slus.ogg", "font_big", "Moc ti to slu¹í.")
+
+dialog_addDialog("pot-m-dik", "en", "", "font_small", "Thanks, you too.")
+dialog_addDialog("pot-m-dik", "cs", "sound/"..codename.."/cs/pot-m-dik.ogg", "font_small", "Díky, tobì taky.")
+
+dialog_addDialog("pot-v-cepic", "en", "", "font_big", "A very nice hat.")
+dialog_addDialog("pot-v-cepic", "cs", "sound/"..codename.."/cs/pot-v-cepic.ogg", "font_big", "Moc slu¹ivá èepièka.")
+
+dialog_addDialog("pot-m-klob", "en", "", "font_small", "Your stetson is quite good, too.")
+dialog_addDialog("pot-m-klob", "cs", "sound/"..codename.."/cs/pot-m-klob.ogg", "font_small", "Tvùj klobouk taky ujde.")
+
+dialog_addDialog("pot-v-hlave", "en", "", "font_big", "What is that on your head?")
+dialog_addDialog("pot-v-hlave", "cs", "sound/"..codename.."/cs/pot-v-hlave.ogg", "font_big", "Co to má¹ na hlavì?")
+
+dialog_addDialog("pot-m-zima", "en", "", "font_small", "My ears were freezing.")
+dialog_addDialog("pot-m-zima", "cs", "sound/"..codename.."/cs/pot-m-zima.ogg", "font_small", "Byla mi zima na u¹i...")
+
+dialog_addDialog("pot-m-pujc", "en", "", "font_small", "That`s some hat! Can I borrow it?")
+dialog_addDialog("pot-m-pujc", "cs", "sound/"..codename.."/cs/pot-m-pujc.ogg", "font_small", "Ty má¹ ale klobouk! Pùjèí¹ mi ho?")
+
+dialog_addDialog("pot-v-leda", "en", "", "font_big", "If you lend me yours.")
+dialog_addDialog("pot-v-leda", "cs", "sound/"..codename.."/cs/pot-v-leda.ogg", "font_big", "Leda za èepièku.")
+
+dialog_addDialog("pot-m-velik", "en", "", "font_small", "This sure is a big stetson.")
+dialog_addDialog("pot-m-velik", "cs", "sound/"..codename.."/cs/pot-m-velik.ogg", "font_small", "To je ale veliký klobouk.")
+
+dialog_addDialog("pot-v-kras", "en", "", "font_big", "And a beatiful one, too.")
+dialog_addDialog("pot-v-kras", "cs", "sound/"..codename.."/cs/pot-v-kras.ogg", "font_big", "A taky krásný.")
+
+dialog_addDialog("pot-v-nehnu", "en", "", "font_big", "I can`t move this.")
+dialog_addDialog("pot-v-nehnu", "cs", "sound/"..codename.."/cs/pot-v-nehnu.ogg", "font_big", "S tím ani nehnu.")
+
+dialog_addDialog("pot-v-trub", "en", "", "font_big", "I can`t move that cylinder.")
+dialog_addDialog("pot-v-trub", "cs", "sound/"..codename.."/cs/pot-v-trub.ogg", "font_big", "S tou trubkou nepohnu.")
+
+dialog_addDialog("pot-m-nezb", "en", "", "font_small", "I think we`re going to have to go inside that ship.")
+dialog_addDialog("pot-m-nezb", "cs", "sound/"..codename.."/cs/pot-m-nezb.ogg", "font_small", "Asi nám nezbude ne¾ vlézt do té lodi.")
+
+dialog_addDialog("pot-m-dovn", "en", "", "font_small", "It looks like we have to go inside.")
+dialog_addDialog("pot-m-dovn", "cs", "sound/"..codename.."/cs/pot-m-dovn.ogg", "font_small", "Asi budeme muset dovnitø.")
+
+dialog_addDialog("pot-m-zatuch", "en", "", "font_small", "The air in here smells so stale.")
+dialog_addDialog("pot-m-zatuch", "cs", "sound/"..codename.."/cs/pot-m-zatuch.ogg", "font_small", "Páchne to tu zatuchlinou.")
+
+dialog_addDialog("pot-v-nikdo", "en", "", "font_big", "It seems that nobody`s here.")
+dialog_addDialog("pot-v-nikdo", "cs", "sound/"..codename.."/cs/pot-v-nikdo.ogg", "font_big", "Zdá se, ¾e tu nikdo není.")
+
+dialog_addDialog("pot-m-moc", "en", "", "font_small", "I don`t like it here.")
+dialog_addDialog("pot-m-moc", "cs", "sound/"..codename.."/cs/pot-m-moc.ogg", "font_small", "Moc se mi tu nelíbí.")
+
+dialog_addDialog("pot-v-plav", "en", "", "font_big", "Stop talking and swim. Old ship interiors are highly interesting places.")
+dialog_addDialog("pot-v-plav", "cs", "sound/"..codename.."/cs/pot-v-plav.ogg", "font_big", "Nemluv a plav, interiéry starých lodí jsou nadmíru zajímavá místa.")
+
+dialog_addDialog("pot-v-ponur", "en", "", "font_big", "Can you sense that atmosphere of doom and decay?")
+dialog_addDialog("pot-v-ponur", "cs", "sound/"..codename.."/cs/pot-v-ponur.ogg", "font_big", "Cítí¹ tu ponurou atmosféru zániku a pomíjivosti?")
+
+dialog_addDialog("pot-m-hnil", "en", "", "font_small", "No, just the mold.")
+dialog_addDialog("pot-m-hnil", "cs", "sound/"..codename.."/cs/pot-m-hnil.ogg", "font_small", "Cítím akorát hnilobu.")
+
+dialog_addDialog("pot-m-vidis", "en", "", "font_small", "What do you see?")
+dialog_addDialog("pot-m-vidis", "cs", "sound/"..codename.."/cs/pot-m-vidis.ogg", "font_small", "Co tam vidí¹?")
+
+dialog_addDialog("pot-v-vidim", "en", "", "font_big", "I can see many interesting levels which we`ll have to solve.")
+dialog_addDialog("pot-v-vidim", "cs", "sound/"..codename.."/cs/pot-v-vidim.ogg", "font_big", "Vidím spoustu zajímavých místností, které budeme muset øe¹it.")
+
+
