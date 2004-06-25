f88c8ff21dce25dee587c8da37f692cfc883444e
fidlej <fidlej>
2004-06-25 15:38:22 +0000

    Added 'submarine' level. Note the mirror.

---
+++ script/submarine/dialogs.lua
@@ -0,0 +1,53 @@
+dialog_addFont("font_big", "font/font_big.png")
+dialog_addFont("font_white", "font/font_white.png")
+dialog_addFont("font_small", "font/font_small.png")
+
+dialog_addDialog("zr-x-nabito", "en", "", "font_white", "FIRST TORPEDO BAY LOADED...")
+dialog_addDialog("zr-x-nabito", "cs", "sound/"..codename.."/cs/zr-x-nabito.ogg", "font_white", "PRVNÍ NÁBOJOVÁ KOMORA NABITA...")
+
+dialog_addDialog("zr-m-obliceje", "en", "", "font_small", "Stop making faces and come help me.")
+dialog_addDialog("zr-m-obliceje", "cs", "sound/"..codename.."/cs/zr-m-obliceje.ogg", "font_small", "Nedìlej na sebe oblièeje a pomoz mi.")
+
+dialog_addDialog("zr-m-prestan", "en", "", "font_small", "Stop looking at yourself in the mirror and make yourself useful.")
+dialog_addDialog("zr-m-prestan", "cs", "sound/"..codename.."/cs/zr-m-prestan.ogg", "font_small", "Pøestaò se prohlí¾et v zrcadle a rad¹i nìco dìlej.")
+
+dialog_addDialog("zr-m-takfajn", "en", "", "font_small", "That`s great! He just goes on his merry little way, but what about me?")
+dialog_addDialog("zr-m-takfajn", "cs", "sound/"..codename.."/cs/zr-m-takfajn.ogg", "font_small", "Tak fajn, on si odjede - a co já?")
+
+dialog_addDialog("zr-m-tojeon", "en", "", "font_small", "As usual. He just leaves me behind.")
+dialog_addDialog("zr-m-tojeon", "cs", "sound/"..codename.."/cs/zr-m-tojeon.ogg", "font_small", "To je celej on. Nechat mì v bryndì...")
+
+dialog_addDialog("zr-m-pockej", "en", "", "font_small", "Wait! What about me? I want to get out of here, too!")
+dialog_addDialog("zr-m-pockej", "cs", "sound/"..codename.."/cs/zr-m-pockej.ogg", "font_small", "Poèkej, a co já? Já chci taky ven!")
+
+dialog_addDialog("zr-v-hej", "en", "", "font_big", "Hey, is anybody down there?")
+dialog_addDialog("zr-v-hej", "cs", "sound/"..codename.."/cs/zr-v-hej.ogg", "font_big", "Hej, jsi tam dole?")
+
+dialog_addDialog("zr-v-halo", "en", "", "font_big", "Hello, can you hear me in there?")
+dialog_addDialog("zr-v-halo", "cs", "sound/"..codename.."/cs/zr-v-halo.ogg", "font_big", "Haló, sly¹í¹ mì tam uvnitø?")
+
+dialog_addDialog("zr-v-jetunekdo", "en", "", "font_big", "Is anybody here?")
+dialog_addDialog("zr-v-jetunekdo", "cs", "sound/"..codename.."/cs/zr-v-jetunekdo.ogg", "font_big", "Je tu nìkdóóó?")
+
+dialog_addDialog("zr-m-nervi", "en", "", "font_small", "Stop shouting and just get me out of here.")
+dialog_addDialog("zr-m-nervi", "cs", "sound/"..codename.."/cs/zr-m-nervi.ogg", "font_small", "Neøvi a rad¹i mi nìjak pomoz ven.")
+
+dialog_addDialog("zr-m-nepovykuj", "en", "", "font_small", "Stop your screaming and help me get out of here.")
+dialog_addDialog("zr-m-nepovykuj", "cs", "sound/"..codename.."/cs/zr-m-nepovykuj.ogg", "font_small", "Nech toho povykování a dostaò mì odsud.")
+
+dialog_addDialog("zr-m-tadyjsem", "en", "", "font_small", "Here I am. I can`t get out.")
+dialog_addDialog("zr-m-tadyjsem", "cs", "sound/"..codename.."/cs/zr-m-tadyjsem.ogg", "font_small", "Tady jsem. Nemù¾u se dostat ven.")
+
+dialog_addDialog("zr-m-komu", "en", "", "font_small", "Whose eyes are those?")
+dialog_addDialog("zr-m-komu", "cs", "sound/"..codename.."/cs/zr-m-komu.ogg", "font_small", "Komu mù¾ou patøit ty oèi?")
+
+dialog_addDialog("zr-v-nevim", "en", "", "font_big", "I don`t know. Maybe the periscope is connected to some other dimension.")
+dialog_addDialog("zr-v-nevim", "cs", "sound/"..codename.."/cs/zr-v-nevim.ogg", "font_big", "Nevím, snad ten periskop vede do jiné dimenze...")
+
+dialog_addDialog("zr-v-opatrne", "en", "", "font_big", "Careful...")
+dialog_addDialog("zr-v-opatrne", "cs", "sound/"..codename.."/cs/zr-v-opatrne.ogg", "font_big", "Opatrnì...")
+
+dialog_addDialog("zr-v-vzdyt", "en", "", "font_big", "Don`t say I didn`t warn you!")
+dialog_addDialog("zr-v-vzdyt", "cs", "sound/"..codename.."/cs/zr-v-vzdyt.ogg", "font_big", "V¾dy» jsem ti to øíkal!")
+
+
