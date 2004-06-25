f18483cacd591187242eebcee9c8604e7a092d40
mrpaja <mrpaja>
2004-06-25 18:37:23 +0000

    french translation

--- script/worlddesc.lua
+++ script/worlddesc.lua
@@ -2,220 +2,292 @@
 -- worldmap_addDesc(codename, lang, levelname, desc)
 worldmap_addDesc("menu", "en", "Game menu", "Fish Fillets - Next Generation")
 worldmap_addDesc("menu", "cs", "Herní menu", "Fish Fillets - Next Generation")
+worldmap_addDesc("menu", "fr", "Menu", "Fish Fillets - Nouvelle Génération")
 
 worldmap_addDesc("start", "en", "How It All Started", "Fish House")
 worldmap_addDesc("start", "cs", "Jak to v¹echno zaèalo", "Rybí domeèek")
+worldmap_addDesc("start", "fr", "Comment tout a commancé", "Maison Poisson")
 
 worldmap_addDesc("briefcase", "en", "Briefcase Message", "Fish House")
 worldmap_addDesc("briefcase", "cs", "Podivná zásilka", "Rybí domeèek")
+worldmap_addDesc("briefcase", "fr", "La Valise-Message", "Maison Poisson")
 
 worldmap_addDesc("cellar", "en", "Rehearsal in Cellar", "Fish House")
 worldmap_addDesc("cellar", "cs", "Zkou¹ka ve sklepì", "Rybí domeèek")
+worldmap_addDesc("cellar", "fr", "Répétition dans la cave", "Maison Poisson")
 
 worldmap_addDesc("library", "en", "Library Flotsam", "Fish House")
 worldmap_addDesc("library", "cs", "Co zbylo z knihovny", "Rybí domeèek")
+worldmap_addDesc("library", "fr", "La bibliothèque", "Maison Poisson")
 
 worldmap_addDesc("stairs", "en", "Plants on the Stairs", "Fish House")
 worldmap_addDesc("stairs", "cs", "Zeleò na schodi¹ti", "Rybí domeèek")
+worldmap_addDesc("stairs", "fr", "Des plantes dans les escaliers", "Maison Poisson")
 
 worldmap_addDesc("broom", "en", "A Mess in the Boiler Room", "Fish House")
 worldmap_addDesc("broom", "cs", "Nepoøádek v kotelnì", "Rybí domeèek")
+worldmap_addDesc("broom", "fr", "Réunion dans la chaudière", "Maison Poisson")
 
 worldmap_addDesc("reef", "en", "Under the Reef", "Fish House")
 worldmap_addDesc("reef", "cs", "Pod útesem", "Rybí domeèek")
+worldmap_addDesc("reef", "fr", "Sous le récif", "Maison Poisson")
 
 worldmap_addDesc("wc", "en", "Closed in the Closet", "Fish House")
 worldmap_addDesc("wc", "cs", "Zamèeni na WC", "Rybí domeèek")
+worldmap_addDesc("wc", "fr", "Bloquués dans les toilettes", "Maison Poisson")
 
 worldmap_addDesc("submarine", "en", "Drowned Submarine", "Ship Wrecks")
 worldmap_addDesc("submarine", "cs", "Utonulá ponorka", "Vrakovi¹tì")
+worldmap_addDesc("submarine", "fr", "Le sous-marin englouti", "Les épaves")
 
 worldmap_addDesc("party1", "en", "Picnic Boat", "Ship Wrecks")
 worldmap_addDesc("party1", "cs", "Výletní loï", "Vrakovi¹tì")
+worldmap_addDesc("party1", "fr", "le bateau pique-nique", "Les épaves")
 
 worldmap_addDesc("elk", "en", "Great War", "Ship Wrecks")
 worldmap_addDesc("elk", "cs", "Velká vlastenecká válka", "Vrakovi¹tì")
+worldmap_addDesc("elk", "fr", "La grande guerre", "Les épaves")
 
 worldmap_addDesc("wreck", "en", "The Ship of Captain Silver", "Ship Wrecks")
 worldmap_addDesc("wreck", "cs", "Loï kapitána Silvera", "Vrakovi¹tì")
+worldmap_addDesc("wreck", "fr", "Le navire du Capitaine Silver", "Les épaves")
 
 worldmap_addDesc("viking1", "en", "The Last Voyage", "Ship Wrecks")
 worldmap_addDesc("viking1", "cs", "Poslední plavba", "Vrakovi¹tì")
+worldmap_addDesc("viking1", "fr", "Le dernier voyage", "Les épaves")
 
 worldmap_addDesc("airplane", "en", "Altitude: Minus 9111 Feet", "Ship Wrecks")
 worldmap_addDesc("airplane", "cs", "Vý¹ka: -9111 stop", "Vrakovi¹tì")
+worldmap_addDesc("airplane", "fr", "Altitude: moins 9111 pieds", "Les épaves")
 
 worldmap_addDesc("bathyscaph", "en", "Bathyscaph", "Ship Wrecks")
 worldmap_addDesc("bathyscaph", "cs", "Batyskaf", "Vrakovi¹tì")
+worldmap_addDesc("bathyscaph", "fr", "Bathyscaphe", "Les épaves")
 
 worldmap_addDesc("tank", "en", "Amphibious Tank", "Ship Wrecks")
 worldmap_addDesc("tank", "cs", "Oboj¾ivelný tank", "Vrakovi¹tì")
+worldmap_addDesc("tank", "fr", "Le char amphibi", "Les épaves")
 
 worldmap_addDesc("viking2", "en", "Eight Vikings in a Boat", "Ship Wrecks")
 worldmap_addDesc("viking2", "cs", "Osm vikingù a pes", "Vrakovi¹tì")
+worldmap_addDesc("viking2", "fr", "Huit vikings dans un bateau", "Les épaves")
 
 worldmap_addDesc("party2", "en", "Return from the Party", "Ship Wrecks")
 worldmap_addDesc("party2", "cs", "Návrat z výletu", "Vrakovi¹tì")
+worldmap_addDesc("party2", "fr", "Retour de la fête", "Les épaves")
 
 worldmap_addDesc("gods", "en", "The Gods Must Be Mad", "Ship Wrecks")
 worldmap_addDesc("gods", "cs", "Bohové musí být ¹ílení", "Vrakovi¹tì")
+worldmap_addDesc("gods", "fr", "Les dieux doivent être fou", "Les épaves")
 
 worldmap_addDesc("elevator1", "en", "House with an Elevator", "City In the Deep")
 worldmap_addDesc("elevator1", "cs", "Dùm s výtahem", "Mìsto v hlubinách")
+worldmap_addDesc("elevator1", "fr", "Maison avec ascenceur", "Citée des profondeurs")
 
 worldmap_addDesc("city", "en", "Welcome To Our City", "City In the Deep")
 worldmap_addDesc("city", "cs", "Vítejte ve mìstì!", "Mìsto v hlubinách")
+worldmap_addDesc("city", "fr", "Bienvenue dans notre cité", "Citée des profondeurs")
 
 worldmap_addDesc("ufo", "en", "Independence Day", "City In the Deep")
 worldmap_addDesc("ufo", "cs", "Den nezávislosti", "Mìsto v hlubinách")
+worldmap_addDesc("ufo", "fr", "Jour d'indépendance", "Citée des profondeurs")
 
 worldmap_addDesc("columns", "en", "The Columns", "City In the Deep")
 worldmap_addDesc("columns", "cs", "Na kolonádì", "Mìsto v hlubinách")
+worldmap_addDesc("columns", "fr", "Les colonnes", "Citée des profondeurs")
 
 worldmap_addDesc("pavement", "en", "Uneven Pavement", "City In the Deep")
 worldmap_addDesc("pavement", "cs", "Nerovná dla¾ba", "Mìsto v hlubinách")
+worldmap_addDesc("pavement", "fr", "Chaussée abimée", "Citée des profondeurs")
 
 worldmap_addDesc("pyramid", "en", "Mr. Cheops` House", "City In the Deep")
 worldmap_addDesc("pyramid", "cs", "Dùm pana Cheopse", "Mìsto v hlubinách")
+worldmap_addDesc("pyramid", "fr", "Chez Mr. Cheops", "Citée des profondeurs")
 
 worldmap_addDesc("music", "en", "A Bit of Music", "City In the Deep")
 worldmap_addDesc("music", "cs", "Pro potì¹ení", "Mìsto v hlubinách")
+worldmap_addDesc("music", "fr", "Une note de musique", "Citée des profondeurs")
 
 worldmap_addDesc("crabshow", "en", "Crab Freak Show", "City In the Deep")
 worldmap_addDesc("crabshow", "cs", "Paniptikum korý¹ù", "Mìsto v hlubinách")
+worldmap_addDesc("crabshow", "fr", "La danse des crabes", "Citée des profondeurs")
 
 worldmap_addDesc("elevator2", "en", "Another elevator", "City In the Deep")
 worldmap_addDesc("elevator2", "cs", "Druhý dùm s výtahem", "Mìsto v hlubinách")
+worldmap_addDesc("elevator2", "fr", "Un autre ascenceur", "Citée des profondeurs")
 
 worldmap_addDesc("atlantis", "en", "And How It Was", "City In the Deep")
 worldmap_addDesc("atlantis", "cs", "Tak takhle to bylo", "Mìsto v hlubinách")
+worldmap_addDesc("atlantis", "fr", "Comment ce fut",  "Citée des profondeurs")
 
 worldmap_addDesc("corals", "en", "First Bizarre Things", "Coral reef")
 worldmap_addDesc("corals", "cs", "První bizarnosti", "Korálový útes")
+worldmap_addDesc("corals", "fr", "Première chose bizarre", "Récif de corail")
 
 worldmap_addDesc("labyrinth", "en", "Labyrinth", "Coral reef")
 worldmap_addDesc("labyrinth", "cs", "Bludi¹tì", "Korálový útes")
+worldmap_addDesc("labyrinth", "fr", "Labyrinthe", "Récif de corail")
 
 worldmap_addDesc("imprisoned", "en", "Imprisoned", "Coral reef")
 worldmap_addDesc("imprisoned", "cs", "Uvìznìni", "Korálový útes")
+worldmap_addDesc("imprisoned", "fr", "En prison", "Récif de corail")
 
 worldmap_addDesc("society", "en", "Closed Society", "Coral reef")
 worldmap_addDesc("society", "cs", "Uzavøená spoleènost", "Korálový útes")
+worldmap_addDesc("society", "fr", "Société fermée", "Récif de corail")
 
 worldmap_addDesc("creatures", "en", "Sleeping Creatures", "Coral reef")
 worldmap_addDesc("creatures", "cs", "Spící tvoreèkové", "Korálový útes")
+worldmap_addDesc("creatures", "fr", "Créatures dormantes", "Récif de corail")
 
 worldmap_addDesc("cancan", "en", "Cancan Crabs", "Coral reef")
 worldmap_addDesc("cancan", "cs", "Kankánoví krabíci", "Korálový útes")
+worldmap_addDesc("cancan", "fr", "Le cancan des crabes", "Récif de corail")
 
 worldmap_addDesc("pearls", "en", "One More Pearl; Please!", "Coral reef")
 worldmap_addDesc("pearls", "cs", "Je¹tì jednu perlièku!", "Korálový útes")
+worldmap_addDesc("pearls", "fr", "Une perle de plus; s'il vous plaît !", "Récif de corail")
 
 worldmap_addDesc("turtle", "en", "Telepathic Devil", "Coral reef")
 worldmap_addDesc("turtle", "cs", "Telepatická mrcha", "Korálový útes")
+worldmap_addDesc("turtle", "fr", "Le diable télépathique", "Récif de corail")
 
 worldmap_addDesc("computer", "en", "The Deep Server", "Dump")
 worldmap_addDesc("computer", "cs", "Hlubinný server", "Smeti¹tì")
+worldmap_addDesc("computer", "fr", "Le serveur des profondeurs", "le dépotoir")
 
 worldmap_addDesc("noground", "en", "Almost No Wall", "Dump")
 worldmap_addDesc("noground", "cs", "Skoro ¾ádná zeï", "Smeti¹tì")
+worldmap_addDesc("noground", "fr", "Presque pas de mur", "le dépotoir")
 
 worldmap_addDesc("bathroom", "en", "Plumbman`s Refuse", "Dump")
 worldmap_addDesc("bathroom", "cs", "Instalatérský odpad", "Smeti¹tì")
+worldmap_addDesc("bathroom", "fr", "Le refus du bonhomme de plomb", "le dépotoir")
 
 worldmap_addDesc("duckie", "en", "Adventure with Pink Duckie", "Dump")
 worldmap_addDesc("duckie", "cs", "Dobrodru¾ství s kachnièkou", "Smeti¹tì")
+worldmap_addDesc("duckie", "fr", "Aventure avec le canard rose", "le dépotoir")
 
 worldmap_addDesc("puzzle", "en", "Shredded Stickman", "Dump")
 worldmap_addDesc("puzzle", "cs", "Rozstøíhaná èmáranice", "Smeti¹tì")
+worldmap_addDesc("puzzle", "fr", "Eparpillement", "le dépotoir")
 
 worldmap_addDesc("dump", "en", "Real Chaos", "Dump")
 worldmap_addDesc("dump", "cs", "Opravdový chaos", "Smeti¹tì")
+worldmap_addDesc("dump", "fr", "Le vrai chaos", "le dépotoir")
 
 worldmap_addDesc("barrel", "en", "Outraged Greenpeace", "Dump")
 worldmap_addDesc("barrel", "cs", "Greenpeace by zuøili", "Smeti¹tì")
+worldmap_addDesc("barrel", "fr", "Greenpeace scandalisé", "le dépotoir")
 
 worldmap_addDesc("cabin1", "en", "The First Mate`s Cabin", "Silver's Ship")
 worldmap_addDesc("cabin1", "cs", "Kajuta prvního dùstojníka", "Silverova loï")
+worldmap_addDesc("cabin1", "fr", "La cabine du premier matelot", "Le navire de Silver")
 
 worldmap_addDesc("snowman", "en", "The Winter Mess Hall", "Silver's Ship")
 worldmap_addDesc("snowman", "cs", "Zimní jídelna", "Silverova loï")
+worldmap_addDesc("snowman", "fr", "Le mess des officiers", "Le navire de Silver")
 
 worldmap_addDesc("cannons", "en", "Fire!", "Silver's Ship")
 worldmap_addDesc("cannons", "cs", "Pal!", "Silverova loï")
+worldmap_addDesc("cannons", "fr", "Feu !", "Le navire de Silver")
 
 worldmap_addDesc("kitchen", "en", "Ship Kitchen", "Silver's Ship")
 worldmap_addDesc("kitchen", "cs", "Lodní kuchynì", "Silverova loï")
+worldmap_addDesc("kitchen", "fr", "La cuisine du navire", "Le navire de Silver")
 
 worldmap_addDesc("cabin2", "en", "Second Mate`s Cabin", "Silver's Ship")
 worldmap_addDesc("cabin2", "cs", "Kajuta druhého dùstojníka", "Silverova loï")
+worldmap_addDesc("cabin2", "fr", "La cabine du second", "Le navire de Silver")
 
 worldmap_addDesc("captain", "en", "Captain`s Cabin", "Silver's Ship")
 worldmap_addDesc("captain", "cs", "Kapitánova kajuta", "Silverova loï")
+worldmap_addDesc("captain", "fr", "La cabine du Capitaine", "Le navire de Silver")
 
 worldmap_addDesc("map", "en", "Silver`s Hideout", "Silver's Ship")
 worldmap_addDesc("map", "cs", "Silverova tajná skrý¹", "Silverova loï")
+worldmap_addDesc("map", "fr", "La cache de Silver", "Le navire de Silver")
 
 worldmap_addDesc("reactor", "en", "Power Plant", "UFO")
 worldmap_addDesc("reactor", "cs", "Reaktor", "Ztroskotané UFO")
+worldmap_addDesc("reactor", "fr", "Centrale électrique", "ovni")
 
 worldmap_addDesc("magnet", "en", "Strange Forces", "UFO")
 worldmap_addDesc("magnet", "cs", "Neznámé síly", "Ztroskotané UFO")
+worldmap_addDesc("magnet", "fr", "Forces étranges", "ovni")
 
 worldmap_addDesc("engine", "en", "Brm... Brm...", "UFO")
 worldmap_addDesc("engine", "cs", "Brm brm...", "Ztroskotané UFO")
+worldmap_addDesc("engine", "fr", "Brm... Brm...", "ovni")
 
 worldmap_addDesc("steel", "en", "Nothing but steel", "UFO")
 worldmap_addDesc("steel", "cs", "Ocel; samá ocel", "Ztroskotané UFO")
+worldmap_addDesc("steel", "fr", "Rien que du métal", "ovni")
 
 worldmap_addDesc("corridor", "en", "Guarded Corridor", "UFO")
 worldmap_addDesc("corridor", "cs", "Pøísnì støe¾ená chodba", "Ztroskotané UFO")
+worldmap_addDesc("corridor", "fr", "Couloir gardé", "ovni")
 
 worldmap_addDesc("experiments", "en", "Biological Experiments", "UFO")
 worldmap_addDesc("experiments", "cs", "Biologické experimenty", "Ztroskotané UFO")
+worldmap_addDesc("experiments", "fr", "Expérience biologique", "ovni")
 
 worldmap_addDesc("propulsion", "en", "The Real Propulsion", "UFO")
 worldmap_addDesc("propulsion", "cs", "Skuteèný pohon", "Ztroskotané UFO")
+worldmap_addDesc("propulsion", "fr", "La vraie propulsion", "ovni")
 
 worldmap_addDesc("aztec", "en", "Aztec Art Hall", "Treasure Cave")
 worldmap_addDesc("aztec", "cs", "Sál aztéckého umìní", "Jeskynì pokladù")
+worldmap_addDesc("aztec", "fr", "La salle d'art Aztec", "La grotte au trésor")
 
 worldmap_addDesc("gems", "en", "Shiny Cave-in", "Treasure Cave")
 worldmap_addDesc("gems", "cs", "Blýskavý zával", "Jeskynì pokladù")
+worldmap_addDesc("gems", "fr", "La grotte lumineuse", "La grotte au trésor")
 
 worldmap_addDesc("chest", "en", "Giant`s Chest", "Treasure Cave")
 worldmap_addDesc("chest", "cs", "Truhlièka obra Kolodìje", "Jeskynì pokladù")
+worldmap_addDesc("chest", "fr", "Le coffre géant", "La grotte au trésor")
 
 worldmap_addDesc("alibaba", "en", "The Hall of Ali-baba", "Treasure Cave")
 worldmap_addDesc("alibaba", "cs", "Alibabùv sál", "Jeskynì pokladù")
+worldmap_addDesc("alibaba", "fr", "La caverne d'Ali-baba", "La grotte au trésor")
 
 worldmap_addDesc("cave", "en", "The Deepest Cave", "Treasure Cave")
 worldmap_addDesc("cave", "cs", "Nejhlub¹í jeskynì", "Jeskynì pokladù")
+worldmap_addDesc("cave", "fr", "La cave la plus profonde", "La grotte au trésor")
 
 worldmap_addDesc("grail", "en", "What Would King Arthur Say?", "Treasure Cave")
 worldmap_addDesc("grail", "cs", "Artu¹ by se divil", "Jeskynì pokladù")
+worldmap_addDesc("grail", "fr", "Qu'aurait dit le roi Arthur ?", "La grotte au trésor")
 
 worldmap_addDesc("tetris", "en", "TETRIS", "Secret Computer")
 worldmap_addDesc("tetris", "cs", "TETRIS", "Tajný poèítaè")
+worldmap_addDesc("tetris", "fr", "TETRIS", "l'ordinateur secret")
 
 worldmap_addDesc("emulator", "en", "Emulator", "Secret Computer")
 worldmap_addDesc("emulator", "cs", "Emulátor", "Tajný poèítaè")
+worldmap_addDesc("emulator", "fr", "Emulateur", "l'ordinateur secret")
 
 worldmap_addDesc("warcraft", "en", "Garden of War", "Secret Computer")
 worldmap_addDesc("warcraft", "cs", "Garden of War", "Tajný poèítaè")
+worldmap_addDesc("warcraft", "fr", "Jardin de guerre", "l'ordinateur secret")
 
 worldmap_addDesc("windoze", "en", "Favorites", "Secret Computer")
 worldmap_addDesc("windoze", "cs", "Oblíbené polo¾ky", "Tajný poèítaè")
+worldmap_addDesc("windoze", "fr", "Favoris", "l'ordinateur secret")
 
 worldmap_addDesc("hardware", "en", "A Hardware Problem", "Secret Computer")
 worldmap_addDesc("hardware", "cs", "Hardwarový problém", "Tajný poèítaè")
+worldmap_addDesc("hardware", "fr", "Problème matériel", "l'ordinateur secret")
 
 worldmap_addDesc("floppy", "en", "Read Only", "Secret Computer")
 worldmap_addDesc("floppy", "cs", "Read only", "Tajný poèítaè")
+worldmap_addDesc("floppy", "fr", "En lecture seul", "l'ordinateur secret")
 
 worldmap_addDesc("ending", "en", "At Home", "")
 worldmap_addDesc("ending", "cs", "Doma", "")
+worldmap_addDesc("ending", "fr", "A la maison", "")
 
 worldmap_addDesc("score", "en", "Special Score", "")
 worldmap_addDesc("score", "cs", "Special Score", "")
-
+worldmap_addDesc("score", "fr", "Spécial Score", "")
