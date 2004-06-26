5ec6adb0dc6883b866d91ba780450df1a72d3bd8
fidlej <fidlej>
2004-06-26 13:52:24 +0000

    Added 'elk' level.

---
+++ script/elk/dialogs.lua
@@ -0,0 +1,84 @@
+dialog_addFont("font_big", "font/font_big.png")
+dialog_addFont("font_elk", "font/font_elk.png")
+dialog_addFont("font_small", "font/font_small.png")
+dialog_addFont("font_parrot", "font/font_parrot.png")
+
+dialog_addDialog("deu-m-valka", "en", "", "font_small", "The war goes on even here, on the ocean floor.")
+dialog_addDialog("deu-m-valka", "cs", "sound/"..codename.."/cs/deu-m-valka.ogg", "font_small", "Válka pokraèuje i v hlubinách.")
+
+dialog_addDialog("deu-v-nepratelstvi", "en", "", "font_big", "The hostility will never ebb.")
+dialog_addDialog("deu-v-nepratelstvi", "cs", "sound/"..codename.."/cs/deu-v-nepratelstvi.ogg", "font_big", "Nepøátelství nikdy neutichne.")
+
+dialog_addDialog("deu-m-bojovat", "en", "", "font_small", "They will fight to the last... parrot.")
+dialog_addDialog("deu-m-bojovat", "cs", "sound/"..codename.."/cs/deu-m-bojovat.ogg", "font_small", "Budou bojovat a¾ do posledního... papou¹ka.")
+
+dialog_addDialog("deu-v-losa", "en", "", "font_big", "And elk.")
+dialog_addDialog("deu-v-losa", "cs", "sound/"..codename.."/cs/deu-v-losa.ogg", "font_big", "A losa.")
+
+dialog_addDialog("deu-m-zvlastni", "en", "", "font_small", "Now, this is strange. I thought that elks only live in Sweden.")
+dialog_addDialog("deu-m-zvlastni", "cs", "sound/"..codename.."/cs/deu-m-zvlastni.ogg", "font_small", "To je zvlá¹tní. Myslela jsem, ¾e losové ¾ijí jen ve ©védsku.")
+
+dialog_addDialog("deu-v-slysel", "en", "", "font_big", "Yes. I too have heard that they bite tourists there.")
+dialog_addDialog("deu-v-slysel", "cs", "sound/"..codename.."/cs/deu-v-slysel.ogg", "font_big", "Taky jsem sly¹el, ¾e tam hry¾ou turisty.")
+
+dialog_addDialog("deu-l-los", "en", "", "font_elk", "Ya rusky los.")
+dialog_addDialog("deu-l-los", "cs", "sound/"..codename.."/cs/deu-l-los.ogg", "font_elk", "Ja ruskij los.")
+
+dialog_addDialog("deu-v-radsi", "en", "", "font_big", "I`d like this elk much more if it wasn`t blocking our way so much.")
+dialog_addDialog("deu-v-radsi", "cs", "sound/"..codename.."/cs/deu-v-radsi.ogg", "font_big", "Toho losa bych mìl mnohem rad¹i, kdyby tu tolik nepøeká¾el.")
+
+dialog_addDialog("deu-l-pozalsta", "en", "", "font_elk", "Pozhalsta, izvinitye.")
+dialog_addDialog("deu-l-pozalsta", "cs", "sound/"..codename.."/cs/deu-l-pozalsta.ogg", "font_elk", "Po¾alsta, izvinitì.")
+
+dialog_addDialog("deu-p-schnell", "en", "", "font_parrot", "Schnell, schnell.")
+dialog_addDialog("deu-p-schnell", "cs", "sound/"..codename.."/cs/deu-p-schnell.ogg", "font_parrot", "Schnell, schnell.")
+
+dialog_addDialog("deu-p-los", "en", "", "font_parrot", "Was is den los?")
+dialog_addDialog("deu-p-los", "cs", "sound/"..codename.."/cs/deu-p-los.ogg", "font_parrot", "Was is den los?")
+
+dialog_addDialog("deu-l-los0", "en", "", "font_elk", "Ja los!")
+dialog_addDialog("deu-l-los0", "cs", "sound/"..codename.."/cs/deu-l-los0.ogg", "font_elk", "Ja los!")
+
+dialog_addDialog("deu-l-los1", "en", "", "font_elk", "Chto sluchilos?")
+dialog_addDialog("deu-l-los1", "cs", "sound/"..codename.."/cs/deu-l-los1.ogg", "font_elk", "Èto sluèilos?")
+
+dialog_addDialog("deu-p-jawohl", "en", "", "font_parrot", "Jawohl, mein Herr!")
+dialog_addDialog("deu-p-jawohl", "cs", "sound/"..codename.."/cs/deu-p-jawohl.ogg", "font_parrot", "Jawohl, mein Herr!")
+
+dialog_addDialog("deu-p-stimmt", "en", "", "font_parrot", "Das stimmt, mein Herr!")
+dialog_addDialog("deu-p-stimmt", "cs", "sound/"..codename.."/cs/deu-p-stimmt.ogg", "font_parrot", "Das stimmt, mein Herr!")
+
+dialog_addDialog("deu-p-streng", "en", "", "font_parrot", "Streng geheim!")
+dialog_addDialog("deu-p-streng", "cs", "sound/"..codename.."/cs/deu-p-streng.ogg", "font_parrot", "Streng geheim!")
+
+dialog_addDialog("deu-p-ordnung", "en", "", "font_parrot", "Das ist ein Ordnung, Herr Oberleitnant!")
+dialog_addDialog("deu-p-ordnung", "cs", "sound/"..codename.."/cs/deu-p-ordnung.ogg", "font_parrot", "Das ist ein Ordnung, Herr Oberleitnant!")
+
+dialog_addDialog("deu-p-skoll", "en", "", "font_parrot", "Skoll!")
+dialog_addDialog("deu-p-skoll", "cs", "sound/"..codename.."/cs/deu-p-skoll.ogg", "font_parrot", "Skoll!")
+
+dialog_addDialog("deu-p-trinken0", "en", "", "font_parrot", "uten Morgen, mein Herr! Wollen Sie noch ein Tee haben?")
+dialog_addDialog("deu-p-trinken0", "cs", "sound/"..codename.."/cs/deu-p-trinken0.ogg", "font_parrot", "uten Morgen, mein Herr! Wollen Sie noch ein Tee haben?")
+
+dialog_addDialog("deu-p-trinken1", "en", "", "font_parrot", "uten Tag, mein Herr! Wollen Sie etwas zum trinken?")
+dialog_addDialog("deu-p-trinken1", "cs", "sound/"..codename.."/cs/deu-p-trinken1.ogg", "font_parrot", "uten Tag, mein Herr! Wollen Sie etwas zum trinken?")
+
+dialog_addDialog("deu-l-ja", "en", "", "font_elk", "Ya los...")
+dialog_addDialog("deu-l-ja", "cs", "sound/"..codename.."/cs/deu-l-ja.ogg", "font_elk", "Ja los...")
+
+dialog_addDialog("deu-l-neznaju", "en", "", "font_elk", "Ya nichevo nyeznayu.")
+dialog_addDialog("deu-l-neznaju", "cs", "sound/"..codename.."/cs/deu-l-neznaju.ogg", "font_elk", "Ja nièevo neznaju.")
+
+dialog_addDialog("deu-l-nesmotrel", "en", "", "font_elk", "Ya nichevo nyesmotrel.")
+dialog_addDialog("deu-l-nesmotrel", "cs", "sound/"..codename.."/cs/deu-l-nesmotrel.ogg", "font_elk", "Ja nièevo nesmotrel.")
+
+dialog_addDialog("deu-l-zivjot", "en", "", "font_elk", "Pustye zhivyot moguchy Sovietsky Soyuz!")
+dialog_addDialog("deu-l-zivjot", "cs", "sound/"..codename.."/cs/deu-l-zivjot.ogg", "font_elk", "Pus» ¾ivjot moguèij Sovìtskij Sojuz!")
+
+dialog_addDialog("deu-l-necital", "en", "", "font_elk", "Ja nichego nechital.")
+dialog_addDialog("deu-l-necital", "cs", "sound/"..codename.."/cs/deu-l-necital.ogg", "font_elk", "Ja nièevo neèital.")
+
+dialog_addDialog("deu-l-tovarisci", "en", "", "font_elk", "Tovarishchi, bratya i sestri, dorogiye druzya...")
+dialog_addDialog("deu-l-tovarisci", "cs", "sound/"..codename.."/cs/deu-l-tovarisci.ogg", "font_elk", "Tovari¹èi, bra»ja i sestri, dorogije druzja...")
+
+
