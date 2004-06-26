5bb4db773e37ad4ced7517a8752081927714b441
fidlej <fidlej>
2004-06-26 19:10:12 +0000

    Added 'viking1' level.

---
+++ script/viking1/dialogs.lua
@@ -0,0 +1,86 @@
+dialog_addFont("font_big", "font/font_big.png")
+dialog_addFont("font_viking4", "font/font_viking4.png")
+dialog_addFont("font_viking5", "font/font_viking5.png")
+dialog_addFont("font_viking1", "font/font_viking1.png")
+dialog_addFont("font_viking2", "font/font_viking2.png")
+dialog_addFont("font_viking3", "font/font_viking3.png")
+dialog_addFont("font_small", "font/font_small.png")
+
+dialog_addDialog("d1-z-b1", "en", "sound/"..codename.."/en/d1-z-b1.ogg")
+
+dialog_addDialog("d1-z-b2", "en", "sound/"..codename.."/en/d1-z-b2.ogg")
+
+dialog_addDialog("d1-z-b3", "en", "sound/"..codename.."/en/d1-z-b3.ogg")
+
+dialog_addDialog("d1-z-p1", "en", "sound/"..codename.."/en/d1-z-p1.ogg")
+
+dialog_addDialog("d1-z-p2", "en", "sound/"..codename.."/en/d1-z-p2.ogg")
+
+dialog_addDialog("d1-z-v0", "en", "sound/"..codename.."/en/d1-z-v0.ogg")
+
+dialog_addDialog("d1-z-v1", "en", "sound/"..codename.."/en/d1-z-v1.ogg")
+
+dialog_addDialog("d1-z-v2", "en", "sound/"..codename.."/en/d1-z-v2.ogg")
+
+dialog_addDialog("dr-m-tojesnad", "en", "", "font_small", "This must be a Viking ship!")
+dialog_addDialog("dr-m-tojesnad", "cs", "sound/"..codename.."/cs/dr-m-tojesnad.ogg", "font_small", "To je snad vikingská loï!")
+
+dialog_addDialog("dr-v-jiste", "en", "", "font_big", "Sure, it`s a drakkar.")
+dialog_addDialog("dr-v-jiste", "cs", "sound/"..codename.."/cs/dr-v-jiste.ogg", "font_big", "Jistì, je to drakkar.")
+
+dialog_addDialog("dr-m-musela", "en", "", "font_small", "She must have sunk fast. The oarsmen are still in their places.")
+dialog_addDialog("dr-m-musela", "cs", "sound/"..codename.."/cs/dr-m-musela.ogg", "font_small", "Musela se potopit hroznì rychle. Veslaøi je¹tì sedí na svých místech.")
+
+dialog_addDialog("dr-v-mozna", "en", "", "font_big", "Maybe they sank her on purpose. Warriors would always accompany their leader on his way to Valhalla.")
+dialog_addDialog("dr-v-mozna", "cs", "sound/"..codename.."/cs/dr-v-mozna.ogg", "font_big", "Mo¾ná, ¾e ji potopili schválnì. Bojovníci doprovázejí svého velitele do Valhaly...")
+
+dialog_addDialog("dr-m-hruza", "en", "", "font_small", "That's awful. They look like they're still alive.")
+dialog_addDialog("dr-m-hruza", "cs", "sound/"..codename.."/cs/dr-m-hruza.ogg", "font_small", "Hrùza... sedí tu jako ¾iví...")
+
+dialog_addDialog("d1-1-hudba0", "en", "", "font_viking1", "Music is gone!")
+dialog_addDialog("d1-1-hudba0", "cs", "sound/"..codename.."/cs/d1-1-hudba0.ogg", "font_viking1", "Hudba je v tahu!")
+
+dialog_addDialog("d1-1-hudba1", "en", "", "font_viking1", "They stopped playing!")
+dialog_addDialog("d1-1-hudba1", "cs", "sound/"..codename.."/cs/d1-1-hudba1.ogg", "font_viking1", "Pøestali hrát!")
+
+dialog_addDialog("d1-1-hudba2", "en", "", "font_viking1", "The music stopped again!")
+dialog_addDialog("d1-1-hudba2", "cs", "sound/"..codename.."/cs/d1-1-hudba2.ogg", "font_viking1", "Zase vypadla hudba!")
+
+dialog_addDialog("d1-2-brb0", "en", "", "font_viking2", "Oh, no!")
+dialog_addDialog("d1-2-brb0", "cs", "sound/"..codename.."/cs/d1-2-brb0.ogg", "font_viking2", "To snad ne!")
+
+dialog_addDialog("d1-2-brb1", "en", "", "font_viking2", "Again?")
+dialog_addDialog("d1-2-brb1", "cs", "sound/"..codename.."/cs/d1-2-brb1.ogg", "font_viking2", "U¾ zase?")
+
+dialog_addDialog("d1-2-brb2", "en", "", "font_viking2", "Oh, not again!")
+dialog_addDialog("d1-2-brb2", "cs", "sound/"..codename.."/cs/d1-2-brb2.ogg", "font_viking2", "U¾ se to stalo zase?")
+
+dialog_addDialog("d1-3-brb0", "en", "", "font_viking3", "What happened?")
+dialog_addDialog("d1-3-brb0", "cs", "sound/"..codename.."/cs/d1-3-brb0.ogg", "font_viking3", "Co se dìje?")
+
+dialog_addDialog("d1-3-brb1", "en", "", "font_viking3", "Again?")
+dialog_addDialog("d1-3-brb1", "cs", "sound/"..codename.."/cs/d1-3-brb1.ogg", "font_viking3", "U¾ zase?")
+
+dialog_addDialog("d1-3-brb2", "en", "", "font_viking3", "Oh, no!")
+dialog_addDialog("d1-3-brb2", "cs", "sound/"..codename.."/cs/d1-3-brb2.ogg", "font_viking3", "To snad ne!")
+
+dialog_addDialog("d1-4-brb0", "en", "", "font_viking4", "The bloody music stopped!")
+dialog_addDialog("d1-4-brb0", "cs", "sound/"..codename.."/cs/d1-4-brb0.ogg", "font_viking4", "To snad není mo¾ný!")
+
+dialog_addDialog("d1-4-brb1", "en", "", "font_viking4", "What are they doing up there?")
+dialog_addDialog("d1-4-brb1", "cs", "sound/"..codename.."/cs/d1-4-brb1.ogg", "font_viking4", "Co tam nahoøe dìlají?")
+
+dialog_addDialog("d1-4-brb2", "en", "", "font_viking4", "How could it be?")
+dialog_addDialog("d1-4-brb2", "cs", "sound/"..codename.."/cs/d1-4-brb2.ogg", "font_viking4", "Jak je tohle mo¾ný?")
+
+dialog_addDialog("d1-5-nevadi0", "en", "", "font_viking5", "Take it easy, boys. Solo for Olaf!")
+dialog_addDialog("d1-5-nevadi0", "cs", "sound/"..codename.."/cs/d1-5-nevadi0.ogg", "font_viking5", "Nevadí, chapci. Olaf sólo!")
+
+dialog_addDialog("d1-5-nevadi1", "en", "", "font_viking5", "Never mind that, boys. We can sing ourself!")
+dialog_addDialog("d1-5-nevadi1", "cs", "sound/"..codename.."/cs/d1-5-nevadi1.ogg", "font_viking5", "Nevadí, chapci. Zazpíváme si sami!")
+
+dialog_addDialog("d1-5-nevadi2", "en", "", "font_viking5", "Never mind, boys. We can sing ourself, like in the old times!")
+dialog_addDialog("d1-5-nevadi2", "cs", "sound/"..codename.."/cs/d1-5-nevadi2.ogg", "font_viking5", "To nevadí. Zazpíváme si sami, jako za mlada!")
+
+
+
