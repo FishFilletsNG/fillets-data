f4c3823996b94af4b12e80e76a71e23cff88b14c
mrpaja <mrpaja>
2004-06-29 17:39:38 +0000

    french translation update

--- script/bathyscaph/dialogs.lua
+++ script/bathyscaph/dialogs.lua
@@ -1,62 +1,82 @@
 dialog_addDialog("bat-m-tohle", "en", "", "font_small", "So this is an exploratory bathyscaph.")
 dialog_addDialog("bat-m-tohle", "cs", "sound/"..codename.."/cs/bat-m-tohle.ogg", "font_small", "Tohle vypadá na výzkumnický batyskaf.")
+dialog_addDialog("bat-m-tohle", "fr", "", "font_small", "Donc c'est un bathyscaphe d'exploration.")
 
 dialog_addDialog("bat-m-mikro", "en", "", "font_small", "A nice microscope.")
 dialog_addDialog("bat-m-mikro", "cs", "sound/"..codename.."/cs/bat-m-mikro.ogg", "font_small", "Pěkný mikroskop.")
+dialog_addDialog("bat-m-mikro", "fr", "", "font_small", "Joli microscope.")
 
 dialog_addDialog("bat-v-zved0", "en", "", "font_big", "Shouldn`t I lift this?")
 dialog_addDialog("bat-v-zved0", "cs", "sound/"..codename.."/cs/bat-v-zved0.ogg", "font_big", "Nemám to zvednout?")
+dialog_addDialog("bat-v-zved0", "fr", "", "font_big", "Ne devrai-je pas soulever ceci ?")
 
 dialog_addDialog("bat-v-zved1", "en", "", "font_big", "Do you want me to lift this up?")
 dialog_addDialog("bat-v-zved1", "cs", "sound/"..codename.."/cs/bat-v-zved1.ogg", "font_big", "Nechceš to zvednout?")
+dialog_addDialog("bat-v-zved1", "fr", "", "font_big", "Veux-tu que soulève ça ?")
 
 dialog_addDialog("bat-v-vyp", "en", "", "font_big", "Shouldn`t I turn that off?")
 dialog_addDialog("bat-v-vyp", "cs", "sound/"..codename.."/cs/bat-v-vyp.ogg", "font_big", "Nemám to vypnout?")
+dialog_addDialog("bat-v-vyp", "fr", "", "font_big", "Ne devrai-je pas éteindre ce truc ?")
 
 dialog_addDialog("bat-v-klid", "en", "", "font_big", "Now it will be quiet here.")
 dialog_addDialog("bat-v-klid", "cs", "sound/"..codename.."/cs/bat-v-klid.ogg", "font_big", "Teď už bude klid.")
+dialog_addDialog("bat-v-klid", "fr", "", "font_big", "Maintenant c'est calme ici.")
 
 dialog_addDialog("bat-m-sluch", "en", "", "font_small", "We can`t get the receiver from there.")
 dialog_addDialog("bat-m-sluch", "cs", "sound/"..codename.."/cs/bat-m-sluch.ogg", "font_small", "Odsud to sluchátko už nedostaneme.")
+dialog_addDialog("bat-m-sluch", "fr", "", "font_small", "Nous ne pouvons pas atteindre le récepteur d'ici.")
 
 dialog_addDialog("bat-s-prome0", "en", "", "font_snail", "This is going to be for me...")
 dialog_addDialog("bat-s-prome0", "cs", "sound/"..codename.."/cs/bat-s-prome0.ogg", "font_snail", "To bude asi pro mě...")
+dialog_addDialog("bat-s-prome0", "fr", "", "font_snail", "Ca va être pour ma pomme...")
 
 dialog_addDialog("bat-s-prome1", "en", "", "font_snail", "This is going to be my brother...")
 dialog_addDialog("bat-s-prome1", "cs", "sound/"..codename.."/cs/bat-s-prome1.ogg", "font_snail", "To asi volá můj brácha...")
+dialog_addDialog("bat-s-prome1", "fr", "", "font_snail", "C'est pour mon frère...")
 
 dialog_addDialog("bat-s-prome2", "en", "", "font_snail", "Could you handle me that call, please?")
 dialog_addDialog("bat-s-prome2", "cs", "sound/"..codename.."/cs/bat-s-prome2.ogg", "font_snail", "Mohli byste zvednout ten telefon?")
+dialog_addDialog("bat-s-prome2", "fr", "", "font_snail", "Pouvez-vous prendre cet appel pour moi, s'il vous plaît ?")
 
 dialog_addDialog("bat-s-snek0", "en", "", "font_snail", "I hope I am not in the way here.")
 dialog_addDialog("bat-s-snek0", "cs", "sound/"..codename.."/cs/bat-s-snek0.ogg", "font_snail", "Doufám, že vám tady moc nezavazím.")
+dialog_addDialog("bat-s-snek0", "fr", "", "font_snail", "J'espère que je ne suis pas sur le chemin ici.")
 
 dialog_addDialog("bat-s-snek1", "en", "", "font_snail", "Really, I can easily crawl away. Just say a word.")
 dialog_addDialog("bat-s-snek1", "cs", "sound/"..codename.."/cs/bat-s-snek1.ogg", "font_snail", "Opravdu, můžu klidně popolézt. Stačí říct.")
+dialog_addDialog("bat-s-snek1", "fr", "", "font_snail", "Vraiment, je peux nager plus loin. Dis le.")
 
 dialog_addDialog("bat-s-snek2", "en", "", "font_snail", "I really don`t want to be an obstacle.")
 dialog_addDialog("bat-s-snek2", "cs", "sound/"..codename.."/cs/bat-s-snek2.ogg", "font_snail", "Nerad bych nějak překážel.")
+dialog_addDialog("bat-s-snek2", "fr", "", "font_snail", "Je ne souhaite pas être un obstacle.")
 
 dialog_addDialog("bat-s-snek3", "en", "", "font_snail", "Just tell me if you want me to move away.")
 dialog_addDialog("bat-s-snek3", "cs", "sound/"..codename.."/cs/bat-s-snek3.ogg", "font_snail", "Klidně řekněte, jestli mám uhnout.")
+dialog_addDialog("bat-s-snek3", "fr", "", "font_snail", "Vous n'avez qu'à me dire si je dois bouger.")
 
 dialog_addDialog("bat-p-0", "en", "", "font_pizla", "Hello.")
 dialog_addDialog("bat-p-0", "cs", "sound/"..codename.."/cs/bat-p-0.ogg", "font_pizla", "Haló.")
+dialog_addDialog("bat-p-0", "fr", "", "font_pizla", "Salut.")
 
 dialog_addDialog("bat-p-1", "en", "", "font_pizla", "Is somebody there?")
 dialog_addDialog("bat-p-1", "cs", "sound/"..codename.."/cs/bat-p-1.ogg", "font_pizla", "Je tam někdo?")
+dialog_addDialog("bat-p-1", "fr", "", "font_pizla", "Il y a quelqu'un ?")
 
 dialog_addDialog("bat-p-2", "en", "", "font_pizla", "Are you there, brother?")
 dialog_addDialog("bat-p-2", "cs", "sound/"..codename.."/cs/bat-p-2.ogg", "font_pizla", "Brácho, seš tam?")
+dialog_addDialog("bat-p-2", "fr", "", "font_pizla", "Tu es là, frangin ?")
 
 dialog_addDialog("bat-p-3", "en", "", "font_pizla", "The snail Escargot speaking.")
 dialog_addDialog("bat-p-3", "cs", "sound/"..codename.."/cs/bat-p-3.ogg", "font_pizla", "Tady šnek Pižla.")
+dialog_addDialog("bat-p-3", "fr", "", "font_pizla", "L'escargot Snail à l'appareil.")
 
-dialog_addDialog("bat-p-4", "en", "", "font_pizla", "I can`t here you.")
+dialog_addDialog("bat-p-4", "en", "", "font_pizla", "I can`t hear you.")
 dialog_addDialog("bat-p-4", "cs", "sound/"..codename.."/cs/bat-p-4.ogg", "font_pizla", "Nic neslyším.")
+dialog_addDialog("bat-p-4", "fr", "", "font_pizla", "Je ne peux pas t'entendre.")
 
 dialog_addDialog("bat-p-5", "en", "", "font_pizla", "Hello, hello!")
 dialog_addDialog("bat-p-5", "cs", "sound/"..codename.."/cs/bat-p-5.ogg", "font_pizla", "Haló haló!")
+dialog_addDialog("bat-p-5", "fr", "", "font_pizla", "Allo, allo !")
 
 dialog_addDialog("bat-t-phone0", "en", "sound/"..codename.."/en/bat-t-phone0.ogg")
 
@@ -66,8 +86,10 @@ dialog_addDialog("bat-t-budik", "en", "sound/"..codename.."/en/bat-t-budik.ogg")
 
 dialog_addDialog("bat-p-zhov0", "en", "", "font_statue", "Hello, the chief inspector Clouseau speaking. I am looking for the dangerous shellfish, who... Oh no, not know, Kato, you yellow ape!")
 dialog_addDialog("bat-p-zhov0", "cs", "sound/"..codename.."/cs/bat-p-zhov0.ogg", "font_statue", "Haló, tady policejní šéfinspektor Clouseau. Hledám nebezpečné měkkýše, kteří... Kato, teď ne, ty žlutá opice!")
+dialog_addDialog("bat-p-zhov0", "fr", "", "font_statue", "Allo, chef inspecteur Clouseau à l'appareil. Je suis à la recherche du dangereux crustacé, qui ... Oh non, pas maintenant, Kato, tu vas rire jaune.")
 
 dialog_addDialog("bat-p-zhov1", "en", "", "font_green", "Hello, you have reached the number 4202-21913271. Nobody from the family of Roundfish is presently present. Please, leave the message after the beep. What happened?")
 dialog_addDialog("bat-p-zhov1", "cs", "sound/"..codename.."/cs/bat-p-zhov1.ogg", "font_green", "Dobrý den. Dovolali jste se na číslo 02 21913271. Z rodiny Okružáků nikdo není doma. Dokonce ani velkej brácha Eduard. Jestli chcete, můžete nám zanechat vzkaz, až pípne signál. Co je s tím signálem?")
+dialog_addDialog("bat-p-zhov1", "fr", "", "font_green", "Allo, vous appelez le 4202-21913271. Toute la famille Roundfish est actuellement absente. S'il vous plaît, veuillez laissez un message après le beep. Que se passe-t-il ?")
 
 
--- script/cannons/dialogs.lua
+++ script/cannons/dialogs.lua
@@ -1,27 +1,36 @@
 dialog_addDialog("del-v-dve", "en", "", "font_big", "See? This ship had two gun decks. It must have been a man-of-war. The acrid smell of gunpowder still seems to linger in the air...")
 dialog_addDialog("del-v-dve", "cs", "sound/"..codename.."/cs/del-v-dve.ogg", "font_big", "Vidíš? Tahle loď měla dvě dělové paluby. To byla jistě řadová loď. Jako by se tu ve vzduchu ještě vznášela vůně střelného prachu.")
+dialog_addDialog("del-v-dve", "fr", "", "font_big", "Tu vois ? Ce navire possède plusieurs canons. Ce devait être un navire de guerre. L'odeur acre de la poudre semble flotter dans l'air...")
 
 dialog_addDialog("del-m-voda", "en", "", "font_small", "You mean, in the water.")
 dialog_addDialog("del-m-voda", "cs", "sound/"..codename.."/cs/del-m-voda.ogg", "font_small", "Chceš říct ve vodě.")
+dialog_addDialog("del-m-voda", "fr", "", "font_small", "Tu veux dire dans l'eau.")
 
 dialog_addDialog("del-m-ci", "en", "", "font_small", "Whose ship was she? Maybe she served under admiral Nelson... or captain Hornblower.")
 dialog_addDialog("del-m-ci", "cs", "sound/"..codename.."/cs/del-m-ci.ogg", "font_small", "Čípak to byla asi loď? Možná se na ní plavil admirál Nelson... nebo kapitán Hornblower.")
+dialog_addDialog("del-m-ci", "fr", "", "font_small", "Quel navire était-elle ? Peut-être servait-elle l'amiral Nelson... ou le capitaine Hornblower.")
 
 dialog_addDialog("del-v-splet", "en", "", "font_big", "No, I think you`re about a hundred years off.")
 dialog_addDialog("del-v-splet", "cs", "sound/"..codename.."/cs/del-v-splet.ogg", "font_big", "V tom případě jsme se spletli nejméně o sto let.")
+dialog_addDialog("del-v-splet", "fr", "", "font_big", "Non, je pense que tu te trompes de siècle.")
 
 dialog_addDialog("del-v-mec", "en", "", "font_big", "I have a feeling we`re going to need that sword.")
 dialog_addDialog("del-v-mec", "cs", "sound/"..codename.."/cs/del-v-mec.ogg", "font_big", "Něco mi říká, že budeme potřebovat ten meč.")
+dialog_addDialog("del-v-mec", "fr", "", "font_big", "Je sens que nous allons avoir besion de cette épée.")
 
 dialog_addDialog("del-m-tus", "en", "", "font_small", "You and your 'feelings`. Besides, we`re not supposed to be giving any hints.")
 dialog_addDialog("del-m-tus", "cs", "sound/"..codename.."/cs/del-m-tus.ogg", "font_small", "Ty tvoje tušení. A vůbec, nenapovídej.")
+dialog_addDialog("del-m-tus", "fr", "", "font_small", "Toi et tes intuitions. De plus, nous sommes pas sensés donner des indices.")
 
 dialog_addDialog("del-m-jedn0", "en", "", "font_small", "That was a little too simple, wasn`t it?")
 dialog_addDialog("del-m-jedn0", "cs", "sound/"..codename.."/cs/del-m-jedn0.ogg", "font_small", "To bylo nějak jednoduché, ne?")
+dialog_addDialog("del-m-jedn0", "fr", "", "font_small", "Un petit tout simple, non ?")
 
 dialog_addDialog("del-m-jedn1", "en", "", "font_small", "Could it possibly be so simple?")
 dialog_addDialog("del-m-jedn1", "cs", "sound/"..codename.."/cs/del-m-jedn1.ogg", "font_small", "Že by to bylo tak jednoduchý?")
+dialog_addDialog("del-m-jedn1", "fr", "", "font_small", "Est-il possible que ce soit si simple")
 
 dialog_addDialog("del-m-jedn2", "en", "", "font_small", "There must be some kind of mistake.")
 dialog_addDialog("del-m-jedn2", "cs", "sound/"..codename.."/cs/del-m-jedn2.ogg", "font_small", "To se autoři asi spletli.")
+dialog_addDialog("del-m-jedn2", "fr", "", "font_small", "Il y a comme une erreur.")
 
