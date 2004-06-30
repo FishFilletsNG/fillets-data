864ef85978c9ab6431c1b090d90dbb3044321ccd
mrpaja <mrpaja>
2004-06-30 18:01:09 +0000

    french dialogs update

--- script/computer/dialogs.lua
+++ script/computer/dialogs.lua
@@ -1,69 +1,92 @@
 dialog_addDialog("poc-m-lezt0", "en", "", "font_small", "That`s another fine mess.")
 dialog_addDialog("poc-m-lezt0", "cs", "sound/"..codename.."/cs/poc-m-lezt0.ogg", "font_small", "Kam jsme to zase vlezli?")
+dialog_addDialog("poc-m-lezt0", "fr", "", "font_small", "Un autre joli bordel.")
 
 dialog_addDialog("poc-m-lezt1", "en", "", "font_small", "What kind of place is this?")
 dialog_addDialog("poc-m-lezt1", "cs", "sound/"..codename.."/cs/poc-m-lezt1.ogg", "font_small", "A co je zase tohle?")
+dialog_addDialog("poc-m-lezt1", "fr", "", "font_small", "Quel sorte de lieu est-ce ?")
 
 dialog_addDialog("poc-m-lezt2", "en", "", "font_small", "Wow, that`s a place.")
 dialog_addDialog("poc-m-lezt2", "cs", "sound/"..codename.."/cs/poc-m-lezt2.ogg", "font_small", "To je teda místo.")
+dialog_addDialog("poc-m-lezt2", "fr", "", "font_small", "Wow, ça c'est du lieu.")
 
 dialog_addDialog("poc-v-kam0", "en", "", "font_big", "Where can man - I mean fish - get by climbing into toilet?")
 dialog_addDialog("poc-v-kam0", "cs", "sound/"..codename.."/cs/poc-v-kam0.ogg", "font_big", "Kam tak člověk - chci říci ryba - může vlézt záchodem?")
+dialog_addDialog("poc-v-kam0", "fr", "", "font_big", "Par où les hommes - je veux dire poisson - grimpent aux toillettes ?")
 
 dialog_addDialog("poc-v-kam1", "en", "", "font_big", "It looks like a cozy little dump.")
 dialog_addDialog("poc-v-kam1", "cs", "sound/"..codename.."/cs/poc-v-kam1.ogg", "font_big", "Vypadá to na malé úhledné smetiště.")
+dialog_addDialog("poc-v-kam1", "fr", "", "font_big", "Ca ressemble à un douillet petit dépotoir.")
 
 dialog_addDialog("poc-v-kam2", "en", "", "font_big", "Look, what kind of things have the people flushed down the toilet.")
 dialog_addDialog("poc-v-kam2", "cs", "sound/"..codename.."/cs/poc-v-kam2.ogg", "font_big", "Podívej, co ty lidi nenaházeli do záchodu.")
+dialog_addDialog("poc-v-kam2", "fr", "", "font_big", "Regarde, qu'est-ce les gens on bien put jeter dans les WC.")
 
 dialog_addDialog("poc-v-kam3", "en", "", "font_big", "I thought it would be worse when I was climbing in.")
 dialog_addDialog("poc-v-kam3", "cs", "sound/"..codename.."/cs/poc-v-kam3.ogg", "font_big", "Když jsem sem lezl, tak jsem to čekal horší.")
+dialog_addDialog("poc-v-kam3", "fr", "", "font_big", "J'imagine que ça va être pire quand je passerai dedans.")
 
 dialog_addDialog("poc-v-nenajde", "en", "", "font_big", "You can find many strange things in such a dump.")
 dialog_addDialog("poc-v-nenajde", "cs", "sound/"..codename.."/cs/poc-v-nenajde.ogg", "font_big", "Co všechno se na takovém smetišti nenajde.")
+dialog_addDialog("poc-v-nenajde", "fr", "", "font_big", "Tu peux trouver beaucoup de chose bizarre dans un tel dépotoir.")
 
 dialog_addDialog("poc-m-myslis", "en", "", "font_small", "Do you think this is the computer that`s in stake in this game?")
 dialog_addDialog("poc-m-myslis", "cs", "sound/"..codename.."/cs/poc-m-myslis.ogg", "font_small", "Myslíš, že tohle je ten počítač, o který se tu hraje?")
+dialog_addDialog("poc-m-myslis", "fr", "", "font_small", "Penses-tu que c'est l'ordinateur dont il est question dans ce jeu ?")
 
 dialog_addDialog("poc-v-multimed", "en", "", "font_big", "Surely not! This is no powerful multimedia computer. This is but XT machine with twelve inch display.")
 dialog_addDialog("poc-v-multimed", "cs", "sound/"..codename.."/cs/poc-v-multimed.ogg", "font_big", "No dovol! Tohle přece není výkoný multimediální počítač. To je XTéčko s dvanáctipalcovým monitorem.")
+dialog_addDialog("poc-v-multimed", "fr", "", "font_big", "Surement pas ! Ce n'est pas un ordinateur multimedia puissant. Ce n'est qu'un XT avec écran 12 pouces.")
 
 dialog_addDialog("poc-v-vyresil", "en", "", "font_big", "The one who will solve, I mean who have solved, will receive, I mean received, MMX based machine with 3Dfx card, plenty of RAM, huge hard disk...")
 dialog_addDialog("poc-v-vyresil", "cs", "sound/"..codename.."/cs/poc-v-vyresil.ogg", "font_big", "Ten, kdo první vyřeší, chci říci vyřešil, všechny zbývající levely, vyhraje, chci říci vyhrál, počítač s technologií MMX, 3Dfx kartou, spoustou paměti, olbřímím diskem...")
+dialog_addDialog("poc-v-vyresil", "fr", "", "font_big", "Celui qui résoudra, je veux dire a résolu, recevra, je veux dire a reçu, un ordinateur MMX avec une carte video 3Dfx, beaucoup de RAM, un énorme disque...")
 
 dialog_addDialog("poc-m-kcemu", "en", "", "font_small", "And what is it for? I for one know plenty of games almost as good as this one that could easily run on this XT machine.")
 dialog_addDialog("poc-m-kcemu", "cs", "sound/"..codename.."/cs/poc-m-kcemu.ogg", "font_small", "No a k čemu to je? Já třeba znám spoustu her, skroro stejně dobrých, jako je tahle, a ty by klidně běžely i na tomhle XTéčku.")
+dialog_addDialog("poc-m-kcemu", "fr", "", "font_small", "Et pourquoi faire ? Je connais plein de jeu aussi bon que celui-la qui peux tourner sur ce XT.")
 
 dialog_addDialog("poc-v-pssst", "en", "", "font_big", "Shhhh!")
 dialog_addDialog("poc-v-pssst", "cs", "sound/"..codename.."/cs/poc-v-pssst.ogg", "font_big", "Pssst!")
+dialog_addDialog("poc-v-pssst", "fr", "", "font_big", "Pssst !")
 
 dialog_addDialog("poc-v-napad", "en", "", "font_big", "Hey, look, just an idea: Could this be the computer we are looking for?")
 dialog_addDialog("poc-v-napad", "cs", "sound/"..codename.."/cs/poc-v-napad.ogg", "font_big", "Hele, jen takový nápad: Co když je to ten počítač, který máme hledat?")
+dialog_addDialog("poc-v-napad", "fr", "", "font_big", "Une idée : Se pourrait-il qu'il soit l'ordinateur que nous recherchons ?")
 
 dialog_addDialog("poc-m-mohlby", "en", "", "font_small", "This could be it.")
 dialog_addDialog("poc-m-mohlby", "cs", "sound/"..codename.."/cs/poc-m-mohlby.ogg", "font_small", "To by mohl být on.")
+dialog_addDialog("poc-m-mohlby", "fr", "", "font_small", "Ca se peut.")
 
 dialog_addDialog("poc-v-stahni", "en", "", "font_big", "So what are we waiting for? Put that suspicious data on the diskette and off we go.")
 dialog_addDialog("poc-v-stahni", "cs", "sound/"..codename.."/cs/poc-v-stahni.ogg", "font_big", "Tak na co čekáme? Stáhni na disketu ta podezřelá data a můžeme jít.")
+dialog_addDialog("poc-v-stahni", "fr", "", "font_big", "Qu'attendons-nous alors ? Récupérons les données sur la disquette et partons.")
 
 dialog_addDialog("poc-m-ukryta", "en", "", "font_small", "It won`t be as easy as that. The data are surely going to be hidden somewhere. We have to go in.")
 dialog_addDialog("poc-m-ukryta", "cs", "sound/"..codename.."/cs/poc-m-ukryta.ogg", "font_small", "To nebude tak jednoduché. Data v něm budou určitě dobře ukryta. Budu se muset dostat dovnitř.")
+dialog_addDialog("poc-m-ukryta", "fr", "", "font_small", "Ce n'est pas aussi simple que ça. Les données sont surement cachées. Introduisons nous à l'intérieur.")
 
 dialog_addDialog("poc-v-dira", "en", "", "font_big", "And what about getting in through that opening?")
 dialog_addDialog("poc-v-dira", "cs", "sound/"..codename.."/cs/poc-v-dira.ogg", "font_big", "Co tam zkusit vlézt třeba támhletou dírou?")
+dialog_addDialog("poc-v-dira", "fr", "", "font_big", "Que penses-tu d'utiliser cette ouverture ?")
 
 dialog_addDialog("poc-m-mechanika", "en", "", "font_small", "That is a floppy disk drive.")
 dialog_addDialog("poc-m-mechanika", "cs", "sound/"..codename.."/cs/poc-m-mechanika.ogg", "font_small", "To je disketová mechanika.")
+dialog_addDialog("poc-m-mechanika", "fr", "", "font_small", "C'est un lecteur de disquette.")
 
 dialog_addDialog("poc-m-zezadu", "en", "", "font_small", "I have to get to the back of it.")
 dialog_addDialog("poc-m-zezadu", "cs", "sound/"..codename.."/cs/poc-m-zezadu.ogg", "font_small", "Musím se k němu dostat zezadu.")
+dialog_addDialog("poc-m-zezadu", "fr", "", "font_small", "Je dois arriver à l'arrière du lecteur.")
 
 dialog_addDialog("poc-m-vyvrtka", "en", "", "font_small", "Put down that corkscrew and come to help me.")
 dialog_addDialog("poc-m-vyvrtka", "cs", "sound/"..codename.."/cs/poc-m-vyvrtka.ogg", "font_small", "Polož tu vývrtku a pojď mi pomoct.")
+dialog_addDialog("poc-m-vyvrtka", "fr", "", "font_small", "Pause cette visse et viens m'aider.")
 
 dialog_addDialog("poc-m-kram", "en", "", "font_small", "Oh my, what a sloppy machine. So big, so heavy and surely so slow.")
 dialog_addDialog("poc-m-kram", "cs", "sound/"..codename.."/cs/poc-m-kram.ogg", "font_small", "To je ale odporný krám. Velký, těžký a určitě strašně pomalý.")
+dialog_addDialog("poc-m-kram", "fr", "", "font_small", "Oh, quelle vieille machine. Si grande, si lourde et surement très lente.")
 
 dialog_addDialog("poc-v-mono", "en", "", "font_big", "And moreover only mono.")
 dialog_addDialog("poc-v-mono", "cs", "sound/"..codename.."/cs/poc-v-mono.ogg", "font_big", "A navíc mono.")
+dialog_addDialog("poc-v-mono", "fr", "", "font_big", "Et de plus juste mono.")
 
--- script/elk/dialogs.lua
+++ script/elk/dialogs.lua
@@ -1,79 +1,105 @@
 dialog_addDialog("deu-m-valka", "en", "", "font_small", "The war goes on even here, on the ocean floor.")
 dialog_addDialog("deu-m-valka", "cs", "sound/"..codename.."/cs/deu-m-valka.ogg", "font_small", "Válka pokračuje i v hlubinách.")
+dialog_addDialog("deu-m-valka", "fr", "", "font_small", "La guerre a eut lieu ici aussi, au fond de l'océan.")
 
 dialog_addDialog("deu-v-nepratelstvi", "en", "", "font_big", "The hostility will never ebb.")
 dialog_addDialog("deu-v-nepratelstvi", "cs", "sound/"..codename.."/cs/deu-v-nepratelstvi.ogg", "font_big", "Nepřátelství nikdy neutichne.")
+dialog_addDialog("deu-v-nepratelstvi", "fr", "", "font_big", "Les hostillités ne cesseront jamais.")
 
 dialog_addDialog("deu-m-bojovat", "en", "", "font_small", "They will fight to the last... parrot.")
 dialog_addDialog("deu-m-bojovat", "cs", "sound/"..codename.."/cs/deu-m-bojovat.ogg", "font_small", "Budou bojovat až do posledního... papouška.")
+dialog_addDialog("deu-m-bojovat", "fr", "", "font_small", "Ils se sont battu pour le dernier... perroquet.")
 
 dialog_addDialog("deu-v-losa", "en", "", "font_big", "And elk.")
 dialog_addDialog("deu-v-losa", "cs", "sound/"..codename.."/cs/deu-v-losa.ogg", "font_big", "A losa.")
+dialog_addDialog("deu-v-losa", "fr", "", "font_big", "et élan.")
 
 dialog_addDialog("deu-m-zvlastni", "en", "", "font_small", "Now, this is strange. I thought that elks only live in Sweden.")
 dialog_addDialog("deu-m-zvlastni", "cs", "sound/"..codename.."/cs/deu-m-zvlastni.ogg", "font_small", "To je zvláštní. Myslela jsem, že losové žijí jen ve Švédsku.")
+dialog_addDialog("deu-m-zvlastni", "fr", "", "font_small", "Plutôt étrange. Je pensais que les élans ne vivaient qu'en Suède.")
 
 dialog_addDialog("deu-v-slysel", "en", "", "font_big", "Yes. I too have heard that they bite tourists there.")
 dialog_addDialog("deu-v-slysel", "cs", "sound/"..codename.."/cs/deu-v-slysel.ogg", "font_big", "Taky jsem slyšel, že tam hryžou turisty.")
+dialog_addDialog("deu-v-slysel", "fr", "", "font_big", "Oui. J'ai même entendu dire qu'ils mordaient les touristes là bas.")
 
 dialog_addDialog("deu-l-los", "en", "", "font_elk", "Ya rusky los.")
 dialog_addDialog("deu-l-los", "cs", "sound/"..codename.."/cs/deu-l-los.ogg", "font_elk", "Ja ruskij los.")
+dialog_addDialog("deu-l-los", "fr", "", "font_elk", "Ya rusky los.")
 
 dialog_addDialog("deu-v-radsi", "en", "", "font_big", "I`d like this elk much more if it wasn`t blocking our way so much.")
 dialog_addDialog("deu-v-radsi", "cs", "sound/"..codename.."/cs/deu-v-radsi.ogg", "font_big", "Toho losa bych měl mnohem radši, kdyby tu tolik nepřekážel.")
+dialog_addDialog("deu-v-radsi", "fr", "", "font_big", "J'apprécierai bien plus cet élan s'il ne bloquait pas tant notre chemin.")
 
 dialog_addDialog("deu-l-pozalsta", "en", "", "font_elk", "Pozhalsta, izvinitye.")
 dialog_addDialog("deu-l-pozalsta", "cs", "sound/"..codename.."/cs/deu-l-pozalsta.ogg", "font_elk", "Požalsta, izvinitě.")
+dialog_addDialog("deu-l-pozalsta", "fr", "", "font_elk", "Pozhalsta, izvinitye.")
 
 dialog_addDialog("deu-p-schnell", "en", "", "font_parrot", "Schnell, schnell.")
 dialog_addDialog("deu-p-schnell", "cs", "sound/"..codename.."/cs/deu-p-schnell.ogg", "font_parrot", "Schnell, schnell.")
+dialog_addDialog("deu-p-schnell", "fr", "", "font_parrot", "Schnell, schnell.")
 
 dialog_addDialog("deu-p-los", "en", "", "font_parrot", "Was is den los?")
 dialog_addDialog("deu-p-los", "cs", "sound/"..codename.."/cs/deu-p-los.ogg", "font_parrot", "Was is den los?")
+dialog_addDialog("deu-p-los", "fr", "", "font_parrot", "Was is den los?")
 
 dialog_addDialog("deu-l-los0", "en", "", "font_elk", "Ja los!")
 dialog_addDialog("deu-l-los0", "cs", "sound/"..codename.."/cs/deu-l-los0.ogg", "font_elk", "Ja los!")
+dialog_addDialog("deu-l-los0", "fr", "", "font_elk", "Ja los!")
 
 dialog_addDialog("deu-l-los1", "en", "", "font_elk", "Chto sluchilos?")
 dialog_addDialog("deu-l-los1", "cs", "sound/"..codename.."/cs/deu-l-los1.ogg", "font_elk", "Čto slučilos?")
+dialog_addDialog("deu-l-los1", "fr", "", "font_elk", "Chto sluchilos?")
 
 dialog_addDialog("deu-p-jawohl", "en", "", "font_parrot", "Jawohl, mein Herr!")
 dialog_addDialog("deu-p-jawohl", "cs", "sound/"..codename.."/cs/deu-p-jawohl.ogg", "font_parrot", "Jawohl, mein Herr!")
+dialog_addDialog("deu-p-jawohl", "fr", "", "font_parrot", "Jawohl, mein Herr!")
 
 dialog_addDialog("deu-p-stimmt", "en", "", "font_parrot", "Das stimmt, mein Herr!")
 dialog_addDialog("deu-p-stimmt", "cs", "sound/"..codename.."/cs/deu-p-stimmt.ogg", "font_parrot", "Das stimmt, mein Herr!")
+dialog_addDialog("deu-p-stimmt", "fr", "", "font_parrot", "Das stimmt, mein Herr!")
 
 dialog_addDialog("deu-p-streng", "en", "", "font_parrot", "Streng geheim!")
 dialog_addDialog("deu-p-streng", "cs", "sound/"..codename.."/cs/deu-p-streng.ogg", "font_parrot", "Streng geheim!")
+dialog_addDialog("deu-p-streng", "fr", "", "font_parrot", "Streng geheim!")
 
 dialog_addDialog("deu-p-ordnung", "en", "", "font_parrot", "Das ist ein Ordnung, Herr Oberleitnant!")
 dialog_addDialog("deu-p-ordnung", "cs", "sound/"..codename.."/cs/deu-p-ordnung.ogg", "font_parrot", "Das ist ein Ordnung, Herr Oberleitnant!")
+dialog_addDialog("deu-p-ordnung", "fr", "", "font_parrot", "Das ist ein Ordnung, Herr Oberleitnant!")
 
 dialog_addDialog("deu-p-skoll", "en", "", "font_parrot", "Skoll!")
 dialog_addDialog("deu-p-skoll", "cs", "sound/"..codename.."/cs/deu-p-skoll.ogg", "font_parrot", "Skoll!")
+dialog_addDialog("deu-p-skoll", "fr", "", "font_parrot", "Skoll!")
 
 dialog_addDialog("deu-p-trinken0", "en", "", "font_parrot", "Guten Morgen, mein Herr! Wollen Sie noch ein Tee haben?")
 dialog_addDialog("deu-p-trinken0", "cs", "sound/"..codename.."/cs/deu-p-trinken0.ogg", "font_parrot", "Guten Morgen, mein Herr! Wollen Sie noch ein Tee haben?")
+dialog_addDialog("deu-p-trinken0", "fr", "", "font_parrot", "Guten Morgen, mein Herr! Wollen Sie noch ein Tee haben?")
 
 dialog_addDialog("deu-p-trinken1", "en", "", "font_parrot", "Guten Tag, mein Herr! Wollen Sie etwas zum trinken?")
 dialog_addDialog("deu-p-trinken1", "cs", "sound/"..codename.."/cs/deu-p-trinken1.ogg", "font_parrot", "Guten Tag, mein Herr! Wollen Sie etwas zum trinken?")
+dialog_addDialog("deu-p-trinken1", "fr", "", "font_parrot", "Guten Tag, mein Herr! Wollen Sie etwas zum trinken?")
 
 dialog_addDialog("deu-l-ja", "en", "", "font_elk", "Ya los...")
 dialog_addDialog("deu-l-ja", "cs", "sound/"..codename.."/cs/deu-l-ja.ogg", "font_elk", "Ja los...")
+dialog_addDialog("deu-l-ja", "fr", "", "font_elk", "Ya los...")
 
 dialog_addDialog("deu-l-neznaju", "en", "", "font_elk", "Ya nichevo nyeznayu.")
 dialog_addDialog("deu-l-neznaju", "cs", "sound/"..codename.."/cs/deu-l-neznaju.ogg", "font_elk", "Ja ničevo neznaju.")
+dialog_addDialog("deu-l-neznaju", "fr", "", "font_elk", "Ya nichevo nyeznayu.")
 
 dialog_addDialog("deu-l-nesmotrel", "en", "", "font_elk", "Ya nichevo nyesmotrel.")
 dialog_addDialog("deu-l-nesmotrel", "cs", "sound/"..codename.."/cs/deu-l-nesmotrel.ogg", "font_elk", "Ja ničevo nesmotrel.")
+dialog_addDialog("deu-l-nesmotrel", "fr", "", "font_elk", "Ya nichevo nyesmotrel.")
 
 dialog_addDialog("deu-l-zivjot", "en", "", "font_elk", "Pustye zhivyot moguchy Sovietsky Soyuz!")
 dialog_addDialog("deu-l-zivjot", "cs", "sound/"..codename.."/cs/deu-l-zivjot.ogg", "font_elk", "Pusť živjot mogučij Sovětskij Sojuz!")
+dialog_addDialog("deu-l-zivjot", "fr", "", "font_elk", "Pustye zhivyot moguchy Sovietsky Soyuz!")
 
 dialog_addDialog("deu-l-necital", "en", "", "font_elk", "Ja nichego nechital.")
 dialog_addDialog("deu-l-necital", "cs", "sound/"..codename.."/cs/deu-l-necital.ogg", "font_elk", "Ja ničevo nečital.")
+dialog_addDialog("deu-l-necital", "fr", "", "font_elk", "Ja nichego nechital.")
 
 dialog_addDialog("deu-l-tovarisci", "en", "", "font_elk", "Tovarishchi, bratya i sestri, dorogiye druzya...")
 dialog_addDialog("deu-l-tovarisci", "cs", "sound/"..codename.."/cs/deu-l-tovarisci.ogg", "font_elk", "Tovarišči, braťja i sestri, dorogije druzja...")
+dialog_addDialog("deu-l-tovarisci", "fr", "", "font_elk", "Tovarishchi, bratya i sestri, dorogiye druzya...")
 
 
