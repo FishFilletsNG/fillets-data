73e1d99f6c5de8e467e2f389f2f4c240dd8c93ed
mrpaja <mrpaja>
2004-07-01 01:13:35 +0000

    converted level elevator1

---
+++ script/elevator1/dialogs.lua
@@ -0,0 +1,55 @@
+dialog_addDialog("zd1-v-civil", "en", "", "font_big", "What a glorious civilization. So many centuries under water and their elevators still work.")
+dialog_addDialog("zd1-v-civil", "cs", "sound/"..codename.."/cs/zd1-v-civil.ogg", "font_big", "Jaká to byla vyspělá civilizace. Staletí pod vodou a jejich výtahy ještě jezdí.")
+
+dialog_addDialog("zd1-m-tlac", "en", "", "font_small", "Especially, when we push them.")
+dialog_addDialog("zd1-m-tlac", "cs", "sound/"..codename.."/cs/zd1-m-tlac.ogg", "font_small", "Zvláště, když zatlačíme.")
+
+dialog_addDialog("zd1-m-dolu", "en", "", "font_small", "But only down.")
+dialog_addDialog("zd1-m-dolu", "cs", "sound/"..codename.."/cs/zd1-m-dolu.ogg", "font_small", "Ale jenom dolů.")
+
+dialog_addDialog("zd1-m-last", "en", "", "font_small", "Do you see that shell?")
+dialog_addDialog("zd1-m-last", "cs", "sound/"..codename.."/cs/zd1-m-last.ogg", "font_small", "Vidíš tu lasturu?")
+
+dialog_addDialog("zd1-v-talis", "en", "", "font_big", "It`s an age-old talisman for elevator builders who always said 'Build well. Build with Shell`.")
+dialog_addDialog("zd1-v-talis", "cs", "sound/"..codename.."/cs/zd1-v-talis.ogg", "font_big", "To je prastarý talisman stavitelů zdviží. Aby jim výtah skvěle Shell.")
+
+dialog_addDialog("zd1-m-poved", "en", "", "font_small", "Isn`t that shell a little bit familiar?")
+dialog_addDialog("zd1-m-poved", "cs", "sound/"..codename.."/cs/zd1-m-poved.ogg", "font_small", "Není ti ta lastura nějak povědomá?")
+
+dialog_addDialog("zd1-v-styd", "en", "", "font_big", "They should be ashamed of themselves. This is subliminal advertising.")
+dialog_addDialog("zd1-v-styd", "cs", "sound/"..codename.."/cs/zd1-v-styd.ogg", "font_big", "Že se nestydí. Taková zjevná skrytá reklama!")
+
+dialog_addDialog("zd1-v-lebka", "en", "", "font_big", "That skull looks familiar.")
+dialog_addDialog("zd1-v-lebka", "cs", "sound/"..codename.."/cs/zd1-v-lebka.ogg", "font_big", "Ta lebka je mi nějak povědomá.")
+
+dialog_addDialog("zd1-m-stejne", "en", "", "font_small", "All human skulls look the same... Unlike fish skeletons.")
+dialog_addDialog("zd1-m-stejne", "cs", "sound/"..codename.."/cs/zd1-m-stejne.ogg", "font_small", "Všechny lidské lebky vypadají stejně... Na rozdíl od rybích kostřiček.")
+
+dialog_addDialog("zd1-m-cesta", "en", "", "font_small", "This is a tough path.")
+dialog_addDialog("zd1-m-cesta", "cs", "sound/"..codename.."/cs/zd1-m-cesta.ogg", "font_small", "To je ale cesta.")
+
+dialog_addDialog("zd1-v-krecek", "en", "", "font_big", "I feel like a mouse on a treadmill.")
+dialog_addDialog("zd1-v-krecek", "cs", "sound/"..codename.."/cs/zd1-v-krecek.ogg", "font_big", "Připadám si jako křeček ve šlapacím kole.")
+
+dialog_addDialog("zd1-m-slap", "en", "", "font_small", "A treadmill for fish? But we only have fins.")
+dialog_addDialog("zd1-m-slap", "cs", "sound/"..codename.."/cs/zd1-m-slap.ogg", "font_small", "Šlapací kolo na ryby? Vždyť máme jen ploutve.")
+
+dialog_addDialog("zd1-x-huhu1", "en", "", "font_lightgrey", "Hoo, hooooo, hoooo... I`m over heeeere...")
+dialog_addDialog("zd1-x-huhu1", "cs", "sound/"..codename.."/cs/zd1-x-huhu1.ogg", "font_lightgrey", "Hu húúú húúúúú... Já jsem táádýýý...")
+
+dialog_addDialog("zd1-x-huhu2", "en", "", "font_lightgrey", "Hoooo, if you want I`ll tell you how to solve all the levels...")
+dialog_addDialog("zd1-x-huhu2", "cs", "sound/"..codename.."/cs/zd1-x-huhu2.ogg", "font_lightgrey", "Húúú, jestli chcete, prozradím vám řešení všech místností...")
+
+dialog_addDialog("zd1-x-huhu3", "en", "", "font_lightgrey", "Hooo, ho, hooo, I know why the city sank!")
+dialog_addDialog("zd1-x-huhu3", "cs", "sound/"..codename.."/cs/zd1-x-huhu3.ogg", "font_lightgrey", "Húúúú hu húúúú, já vím, proč se potopilo město!")
+
+dialog_addDialog("zd1-x-huhu4", "en", "", "font_lightgrey", "Hoooo, why isn`t anyone paying attention to me?")
+dialog_addDialog("zd1-x-huhu4", "cs", "sound/"..codename.."/cs/zd1-x-huhu4.ogg", "font_lightgrey", "Húúúú, proč si mě nikdo nevšímá?")
+
+dialog_addDialog("zd1-x-huhu5", "en", "", "font_lightgrey", "Hoooo ho hooo! Why are you ignoring me?")
+dialog_addDialog("zd1-x-huhu5", "cs", "sound/"..codename.."/cs/zd1-x-huhu5.ogg", "font_lightgrey", "Húúúú hu húúú húúú! Proč mě ignorujete?")
+
+dialog_addDialog("zd1-x-huhu6", "en", "", "font_lightgrey", "Hooooo, hooooo, fish! Pay attention to me!")
+dialog_addDialog("zd1-x-huhu6", "cs", "sound/"..codename.."/cs/zd1-x-huhu6.ogg", "font_lightgrey", "Hahúúú, holahúúúú, ryby! Všimněte si mě!")
+
+
