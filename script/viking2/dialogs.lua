987e300a95189917e34fc5e81f9e21e6f1dd0eb2
fidlej <fidlej>
2004-07-01 16:57:23 +0000

    Added level 'viking2'.

---
+++ script/viking2/dialogs.lua
@@ -0,0 +1,193 @@
+dialog_addDialog("dr1-m-dlouho", "en", "", "font_small", "This ship must sank long time ago.")
+dialog_addDialog("dr1-m-dlouho", "cs", "sound/"..codename.."/cs/dr1-m-dlouho.ogg", "font_small", "Tahle loď tu asi leží už dlouho.")
+
+dialog_addDialog("dr1-v-urcite", "en", "", "font_big", "Definitely. It`s one of the earliest drakkars. Judging by the shape of the head, number of spirals and color of the water I think it belonged to the grandfather of Eric the Red.")
+dialog_addDialog("dr1-v-urcite", "cs", "sound/"..codename.."/cs/dr1-v-urcite.ogg", "font_big", "Určitě. Je to jeden z raných drakarů. Podle tvaru hlavy, počtu závitů a barvy vody soudím, že patřil dědečkovi Erika Rudého.")
+
+dialog_addDialog("dr1-v-olaf", "en", "", "font_big", "Or to the father-in-law of Olaf the Brave.")
+dialog_addDialog("dr1-v-olaf", "cs", "sound/"..codename.."/cs/dr1-v-olaf.ogg", "font_big", "Nebo tchánovi Olafa Udatného.")
+
+dialog_addDialog("dr1-v-leif", "en", "", "font_big", "Or maybe to the great-uncle of Leif the Skillful.")
+dialog_addDialog("dr1-v-leif", "cs", "sound/"..codename.."/cs/dr1-v-leif.ogg", "font_big", "Nebo možná prastrýci Leifa Smělého.")
+
+dialog_addDialog("dr1-v-harold", "en", "", "font_big", "It could also belong to the niece of Harold the Great.")
+dialog_addDialog("dr1-v-harold", "cs", "sound/"..codename.."/cs/dr1-v-harold.ogg", "font_big", "Není vyloučeno, že neteři Harolda Velkého.")
+
+dialog_addDialog("dr1-v-snorr", "en", "", "font_big", "Or possibly to the aunt of Snorr the Courageous.")
+dialog_addDialog("dr1-v-snorr", "cs", "sound/"..codename.."/cs/dr1-v-snorr.ogg", "font_big", "Je možné, že i tetě Snorra Statečného.")
+
+dialog_addDialog("dr1-v-thorson", "en", "", "font_big", "It is even possible it belonged to Thorson the Hard.")
+dialog_addDialog("dr1-v-thorson", "cs", "sound/"..codename.."/cs/dr1-v-thorson.ogg", "font_big", "Nedá se vyloučit ani Thorson Tvrdý.")
+
+dialog_addDialog("dr1-x-erik", "en", "", "font_lightgrey", "I always thought I am the fishing boat of Eric the Unready... But today I learned the truth. Yes, it`s never too late to start a new life!")
+dialog_addDialog("dr1-x-erik", "cs", "sound/"..codename.."/cs/dr1-x-erik.ogg", "font_lightgrey", "Vždycky jsem si myslel, že jsem byl rybářský člun Erika Nepřipraveného... Ale dnes jsem se konečně dozvěděl pravdu. Nu což, nikdy není pozdě začít nový život!")
+
+dialog_addDialog("dr-m-podivej", "en", "", "font_small", "Look at that poor dog.")
+dialog_addDialog("dr-m-podivej", "cs", "sound/"..codename.."/cs/dr-m-podivej.ogg", "font_small", "Podívej, ten chudáček pejsek.")
+
+dialog_addDialog("dr-m-nedycha", "en", "", "font_small", "Doesn't he seem to be breathing kind of strangely?")
+dialog_addDialog("dr-m-nedycha", "cs", "sound/"..codename.."/cs/dr-m-nedycha.ogg", "font_small", "Nedýchá nějak nemocně?")
+
+dialog_addDialog("dr-v-napsa", "en", "", "font_big", "For a dog under water I think he's doing rather well.")
+dialog_addDialog("dr-v-napsa", "cs", "sound/"..codename.."/cs/dr-v-napsa.ogg", "font_big", "Na psa pod vodou dýchá celkem slušně.")
+
+dialog_addDialog("dr-v-nato", "en", "", "font_big", "He doesn't look all that bad considering that he's been here for a few centuries.")
+dialog_addDialog("dr-v-nato", "cs", "sound/"..codename.."/cs/dr-v-nato.ogg", "font_big", "Na to, že tu musí být několik století, je celkem čilý.")
+
+dialog_addDialog("dr-8-ztichni1", "en", "", "font_viking8", "Shut up!")
+dialog_addDialog("dr-8-ztichni1", "cs", "sound/"..codename.."/cs/dr-8-ztichni1.ogg", "font_viking8", "Ztichni!")
+
+dialog_addDialog("dr-8-ztichni2", "en", "", "font_viking8", "Will you shut up already!")
+dialog_addDialog("dr-8-ztichni2", "cs", "sound/"..codename.."/cs/dr-8-ztichni2.ogg", "font_viking8", "Buď už zticha!")
+
+dialog_addDialog("dr-8-nenechas", "en", "", "font_viking8", "So, you`re not going to shut up, are you?")
+dialog_addDialog("dr-8-nenechas", "cs", "sound/"..codename.."/cs/dr-8-nenechas.ogg", "font_viking8", "Tak ty toho nenecháš?!")
+
+dialog_addDialog("dr-8-aaa", "en", "", "font_viking8", "Aaaaargh...")
+dialog_addDialog("dr-8-aaa", "cs", "sound/"..codename.."/cs/dr-8-aaa.ogg", "font_viking8", "Ááááá...")
+
+dialog_addDialog("dr-6-checheche", "en", "", "font_viking6", "He-he-he-he-he... Eh-eh-eh-eh...")
+dialog_addDialog("dr-6-checheche", "cs", "sound/"..codename.."/cs/dr-6-checheche.ogg", "font_viking6", "Chééééchechech... chéééchecheche... chechechechéééchéé...")
+
+dialog_addDialog("dr-5-srab1", "en", "", "font_viking5", "Is it over?")
+dialog_addDialog("dr-5-srab1", "cs", "sound/"..codename.."/cs/dr-5-srab1.ogg", "font_viking5", "Už je po všem?")
+
+dialog_addDialog("dr-5-srab2", "en", "", "font_viking5", "Are we safe now?")
+dialog_addDialog("dr-5-srab2", "cs", "sound/"..codename.."/cs/dr-5-srab2.ogg", "font_viking5", "Je tady bezpečno?")
+
+dialog_addDialog("dr-5-srab3", "en", "", "font_viking5", "Is the fight over?")
+dialog_addDialog("dr-5-srab3", "cs", "sound/"..codename.."/cs/dr-5-srab3.ogg", "font_viking5", "Už skončil boj?")
+
+dialog_addDialog("dr-5-srab4", "en", "", "font_viking5", "Is the enemy finally gone?")
+dialog_addDialog("dr-5-srab4", "cs", "sound/"..codename.."/cs/dr-5-srab4.ogg", "font_viking5", "Už jsou nepřátelé pryč?")
+
+dialog_addDialog("dr-5-srab5", "en", "", "font_viking5", "Are we still fighting?")
+dialog_addDialog("dr-5-srab5", "cs", "sound/"..codename.."/cs/dr-5-srab5.ogg", "font_viking5", "Ještě se bojuje?")
+
+dialog_addDialog("dr-2-uzbudeme1", "en", "", "font_viking2", "Are we in Valhalla yet, chief?")
+dialog_addDialog("dr-2-uzbudeme1", "cs", "sound/"..codename.."/cs/dr-2-uzbudeme1.ogg", "font_viking2", "Už jsme ve Valhale, náčelníku?")
+
+dialog_addDialog("dr-2-uzbudeme2", "en", "", "font_viking2", "When are we going to get to Valhalla?")
+dialog_addDialog("dr-2-uzbudeme2", "cs", "sound/"..codename.."/cs/dr-2-uzbudeme2.ogg", "font_viking2", "Už budeme ve Valhale?")
+
+dialog_addDialog("dr-1-aztambudem", "en", "", "font_viking1", "I`ll let you know once we get there.")
+dialog_addDialog("dr-1-aztambudem", "cs", "sound/"..codename.."/cs/dr-1-aztambudem.ogg", "font_viking1", "Až tam budem, tak ti řeknu.")
+
+dialog_addDialog("dr-2-odskocit", "en", "", "font_viking2", "Can I go relieve myself, sir?")
+dialog_addDialog("dr-2-odskocit", "cs", "sound/"..codename.."/cs/dr-2-odskocit.ogg", "font_viking2", "Náčelníku, můžu si odskočit?")
+
+dialog_addDialog("dr-1-pockej", "en", "", "font_viking1", "Wait till we get to Valhalla.")
+dialog_addDialog("dr-1-pockej", "cs", "sound/"..codename.."/cs/dr-1-pockej.ogg", "font_viking1", "Počkej, až budeme ve Valhale.")
+
+dialog_addDialog("dr-2-netrva", "en", "", "font_viking2", "Isn`t this journey to Valhalla taking a bit too long?")
+dialog_addDialog("dr-2-netrva", "cs", "sound/"..codename.."/cs/dr-2-netrva.ogg", "font_viking2", "Netrvá ta cesta do Valhaly už nějak dlouho?")
+
+dialog_addDialog("dr-1-trpelivost", "en", "", "font_viking1", "Patience is the sign of a true warrior.")
+dialog_addDialog("dr-1-trpelivost", "cs", "sound/"..codename.."/cs/dr-1-trpelivost.ogg", "font_viking1", "Trpělivost, to je odznak bojovníka.")
+
+dialog_addDialog("dr-2-urcite", "en", "", "font_viking2", "Are you sure this is how to get to Valhalla?")
+dialog_addDialog("dr-2-urcite", "cs", "sound/"..codename.."/cs/dr-2-urcite.ogg", "font_viking2", "Určitě se takhle dostaneme do Valhaly?")
+
+dialog_addDialog("dr-1-bojovnik", "en", "", "font_viking1", "A warrior has to trust his commander!")
+dialog_addDialog("dr-1-bojovnik", "cs", "sound/"..codename.."/cs/dr-1-bojovnik.ogg", "font_viking1", "Bojovník musí svému veliteli důvěřovat!")
+
+dialog_addDialog("dr-1-achjo", "en", "", "font_viking1", "Ahhh, I`m in charge of a ship full of cowards.")
+dialog_addDialog("dr-1-achjo", "cs", "sound/"..codename.."/cs/dr-1-achjo.ogg", "font_viking1", "Ach jo, velím lodi plné zbabělců.")
+
+dialog_addDialog("dr-1-procja", "en", "", "font_viking1", "Why did I have to get a ship full of laughing buffoons?")
+dialog_addDialog("dr-1-procja", "cs", "sound/"..codename.."/cs/dr-1-procja.ogg", "font_viking1", "Proč zrovna já musím mít loď plnou chechtajících se kreténů.")
+
+dialog_addDialog("dr-1-chechtajici", "en", "", "font_viking1", "Laughing buffoons, a slobbering dog. I think I`ll go drown myself.")
+dialog_addDialog("dr-1-chechtajici", "cs", "sound/"..codename.."/cs/dr-1-chechtajici.ogg", "font_viking1", "Chechtající se kreténi, slintající pes. Já se snad utopím.")
+
+dialog_addDialog("dr-3-spravny", "en", "", "font_viking3", "Real Vikings have beards.")
+dialog_addDialog("dr-3-spravny", "cs", "sound/"..codename.."/cs/dr-3-spravny.ogg", "font_viking3", "Správný viking má mít vous.")
+
+dialog_addDialog("dr-4-copy", "en", "", "font_viking4", "B-b-braids are in n-nowadays.")
+dialog_addDialog("dr-4-copy", "cs", "sound/"..codename.."/cs/dr-4-copy.ogg", "font_viking4", "C-c-copy jsou teď v m-módě.")
+
+dialog_addDialog("dr-3-cojeto", "en", "", "font_viking3", "What kind of fad is that? Braids? For crying out loud.")
+dialog_addDialog("dr-3-cojeto", "cs", "sound/"..codename.."/cs/dr-3-cojeto.ogg", "font_viking3", "Co je tohle za módu. Copy. To snad ne.")
+
+dialog_addDialog("dr-4-magazin", "en", "", "font_viking4", "V-v-viking Fashion M-m-monthly r-recommends a blonde braid to complement a d-d-dark helmet and b-blue shield.")
+dialog_addDialog("dr-4-magazin", "cs", "sound/"..codename.."/cs/dr-4-magazin.ogg", "font_viking4", "M-m-magazín Vikingská m-m-móda d-doporučuje k tmavé helmě a m-modrému štítu právě světlý c-c-cop.")
+
+dialog_addDialog("dr-3-stakovou", "en", "", "font_viking3", "I simply don`t agree with such a fad.")
+dialog_addDialog("dr-3-stakovou", "cs", "sound/"..codename.."/cs/dr-3-stakovou.ogg", "font_viking3", "S takovou módou zásadně nesouhlasím.")
+
+dialog_addDialog("dr-3-radeji", "en", "", "font_viking3", "You should stick to the Viking traditions instead.")
+dialog_addDialog("dr-3-radeji", "cs", "sound/"..codename.."/cs/dr-3-radeji.ogg", "font_viking3", "Raději kdyby ses držel vikingských tradic.")
+
+dialog_addDialog("dr-4-moderni", "en", "", "font_viking4", "B-b-but I`m c-c-cool!")
+dialog_addDialog("dr-4-moderni", "cs", "sound/"..codename.."/cs/dr-4-moderni.ogg", "font_viking4", "N-n-náhodou, jsem m-moderní!")
+
+dialog_addDialog("dr-4-erik", "en", "", "font_viking4", "Even Erik the G-g-great Eric had a b-b-braid!")
+dialog_addDialog("dr-4-erik", "cs", "sound/"..codename.."/cs/dr-4-erik.ogg", "font_viking4", "Už v-v-velký Erik měl c-c-cop!")
+
+dialog_addDialog("dr-3-nesmysl", "en", "", "font_viking3", "Nonsense. He had a beard.")
+dialog_addDialog("dr-3-nesmysl", "cs", "sound/"..codename.."/cs/dr-3-nesmysl.ogg", "font_viking3", "Nesmysl. Měl vousy.")
+
+dialog_addDialog("dr-4-taky", "en", "", "font_viking4", "But h-he also h-had a b-b-braid.")
+dialog_addDialog("dr-4-taky", "cs", "sound/"..codename.."/cs/dr-4-taky.ogg", "font_viking4", "Ale c-c-cop měl t-t-taky.")
+
+dialog_addDialog("dr-3-nemel", "en", "", "font_viking3", "He did not. And that`s final.")
+dialog_addDialog("dr-3-nemel", "cs", "sound/"..codename.."/cs/dr-3-nemel.ogg", "font_viking3", "Neměl. A dost řečí.")
+
+dialog_addDialog("dr-4-myslis", "en", "", "font_viking4", "D-d-do you think that a b-b-beard would suit me b-b-better?")
+dialog_addDialog("dr-4-myslis", "cs", "sound/"..codename.."/cs/dr-4-myslis.ogg", "font_viking4", "M-m-myslíš, že by mi v-v-vous slušel l-lépe?")
+
+dialog_addDialog("dr-3-samozrejme", "en", "", "font_viking3", "Definitely.")
+dialog_addDialog("dr-3-samozrejme", "cs", "sound/"..codename.."/cs/dr-3-samozrejme.ogg", "font_viking3", "Samozřejmě.")
+
+dialog_addDialog("dr-4-budu", "en", "", "font_viking4", "I`ll th-th-think it o-over.")
+dialog_addDialog("dr-4-budu", "cs", "sound/"..codename.."/cs/dr-4-budu.ogg", "font_viking4", "B-b-budu o tom u-uvažovat.")
+
+dialog_addDialog("dr-4-hmmm", "en", "", "font_viking4", "Hmmm... Ehmmm... Hahmmm...")
+dialog_addDialog("dr-4-hmmm", "cs", "sound/"..codename.."/cs/dr-4-hmmm.ogg", "font_viking4", "Hmmm... Ehmmm... Hahmmm...")
+
+dialog_addDialog("dr-4-ne", "en", "", "font_viking4", "N-no, b-b-braids are d-definitely b-b-better.")
+dialog_addDialog("dr-4-ne", "cs", "sound/"..codename.."/cs/dr-4-ne.ogg", "font_viking4", "N-ne, c-c-copy jsou určitě l-l-lepší.")
+
+dialog_addDialog("dr-3-mladez", "en", "", "font_viking3", "Today`s youth - absolutely unbelievable.")
+dialog_addDialog("dr-3-mladez", "cs", "sound/"..codename.."/cs/dr-3-mladez.ogg", "font_viking3", "Ta dnešní mládež je příšerná.")
+
+dialog_addDialog("dr-3-chlap", "en", "", "font_viking3", "A warrior with braids. It`s unheard of.")
+dialog_addDialog("dr-3-chlap", "cs", "sound/"..codename.."/cs/dr-3-chlap.ogg", "font_viking3", "Chlap a copy. Kdo to kdy slyšel!?")
+
+dialog_addDialog("dr-3-mladi", "en", "", "font_viking3", "You youngsters just think you can get away with anything today.")
+dialog_addDialog("dr-3-mladi", "cs", "sound/"..codename.."/cs/dr-3-mladi.ogg", "font_viking3", "Dnes si ti mladí dovolí snad cokoliv.")
+
+dialog_addDialog("dr-4-stejne", "en", "", "font_viking4", "Well, I still look cool.")
+dialog_addDialog("dr-4-stejne", "cs", "sound/"..codename.."/cs/dr-4-stejne.ogg", "font_viking4", "Stejně mi to sluší...")
+
+dialog_addDialog("dr-7-sm1", "en", "sound/"..codename.."/en/dr-7-sm1.ogg")
+
+dialog_addDialog("dr-7-sm2", "en", "sound/"..codename.."/en/dr-7-sm2.ogg")
+
+dialog_addDialog("dr-7-sm3", "en", "sound/"..codename.."/en/dr-7-sm3.ogg")
+
+dialog_addDialog("dr-7-sm4", "en", "sound/"..codename.."/en/dr-7-sm4.ogg")
+
+dialog_addDialog("dr-7-sm5", "en", "sound/"..codename.."/en/dr-7-sm5.ogg")
+
+dialog_addDialog("dr-7-sm6", "en", "sound/"..codename.."/en/dr-7-sm6.ogg")
+
+dialog_addDialog("dr-7-sm7", "en", "sound/"..codename.."/en/dr-7-sm7.ogg")
+
+dialog_addDialog("dr-7-sm8", "en", "sound/"..codename.."/en/dr-7-sm8.ogg")
+
+dialog_addDialog("dr-7-brble1", "en", "sound/"..codename.."/en/dr-7-brble1.ogg")
+
+dialog_addDialog("dr-7-brble2", "en", "sound/"..codename.."/en/dr-7-brble2.ogg")
+
+dialog_addDialog("dr-7-brble3", "en", "sound/"..codename.."/en/dr-7-brble3.ogg")
+
+dialog_addDialog("dr-7-brble4", "en", "sound/"..codename.."/en/dr-7-brble4.ogg")
+
+dialog_addDialog("dr-7-brble5", "en", "sound/"..codename.."/en/dr-7-brble5.ogg")
+
+dialog_addDialog("dr-x-buch", "en", "sound/"..codename.."/en/dr-x-buch.ogg")
+
+dialog_addDialog("dr-x-pes", "en", "sound/"..codename.."/en/dr-x-pes.ogg")
+
+
