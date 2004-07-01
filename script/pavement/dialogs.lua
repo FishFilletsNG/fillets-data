3c836155ece41ca17cf0d1c56e36033832863250
fidlej <fidlej>
2004-07-01 18:25:24 +0000

    Added level 'pavement'.

---
+++ script/pavement/dialogs.lua
@@ -0,0 +1,83 @@
+dialog_addDialog("dir-m-rada0", "en", "", "font_small", "Stuff yourself with your advice.")
+dialog_addDialog("dir-m-rada0", "cs", "sound/"..codename.."/cs/dir-m-rada0.ogg", "font_small", "Nech si ty rady.")
+
+dialog_addDialog("dir-m-rada1", "en", "", "font_small", "We know that very well.")
+dialog_addDialog("dir-m-rada1", "cs", "sound/"..codename.."/cs/dir-m-rada1.ogg", "font_small", "To víme i bez tebe.")
+
+dialog_addDialog("dir-m-rada2", "en", "", "font_small", "Get lost.")
+dialog_addDialog("dir-m-rada2", "cs", "sound/"..codename.."/cs/dir-m-rada2.ogg", "font_small", "Běž se vycpat.")
+
+dialog_addDialog("dir-m-rada3", "en", "", "font_small", "My patience is running out.")
+dialog_addDialog("dir-m-rada3", "cs", "sound/"..codename.."/cs/dir-m-rada3.ogg", "font_small", "Pomalu mi dochází trpělivost.")
+
+dialog_addDialog("dir-m-rada4", "en", "", "font_small", "Arrgh... I`ll smash it to pieces.")
+dialog_addDialog("dir-m-rada4", "cs", "sound/"..codename.."/cs/dir-m-rada4.ogg", "font_small", "Grrr... asi ji brzy něčím majznu.")
+
+dialog_addDialog("dir-v-rada0", "en", "", "font_big", "Ignore it, let it talk.")
+dialog_addDialog("dir-v-rada0", "cs", "sound/"..codename.."/cs/dir-v-rada0.ogg", "font_big", "Nevšímej si jí, ať si tlachá.")
+
+dialog_addDialog("dir-v-rada1", "en", "", "font_big", "Let us think.")
+dialog_addDialog("dir-v-rada1", "cs", "sound/"..codename.."/cs/dir-v-rada1.ogg", "font_big", "Nech nás v klidu přemýšlet.")
+
+dialog_addDialog("dir-v-rada2", "en", "", "font_big", "This was some advice, again.")
+dialog_addDialog("dir-v-rada2", "cs", "sound/"..codename.."/cs/dir-v-rada2.ogg", "font_big", "To jsi nám zase poradila.")
+
+dialog_addDialog("dir-v-rada3", "en", "", "font_big", "I am sorry I have no ears. I could plug them.")
+dialog_addDialog("dir-v-rada3", "cs", "sound/"..codename.."/cs/dir-v-rada3.ogg", "font_big", "Ach jo, škoda že nemám uši. Zacpal bych si je.")
+
+dialog_addDialog("dir-v-rada4", "en", "", "font_big", "I am fed up to my gills with its nonsenses.")
+dialog_addDialog("dir-v-rada4", "cs", "sound/"..codename.."/cs/dir-v-rada4.ogg", "font_big", "Už těch jejích nesmyslů mám po krk.")
+
+dialog_addDialog("dir-hs-uvod0", "en", "", "font_statue", "It would be easiest if you")
+dialog_addDialog("dir-hs-uvod0", "cs", "sound/"..codename.."/cs/dir-hs-uvod0.ogg", "font_statue", "Nejjednodušší by bylo")
+
+dialog_addDialog("dir-hs-uvod1", "en", "", "font_statue", "You would be better off if you")
+dialog_addDialog("dir-hs-uvod1", "cs", "sound/"..codename.."/cs/dir-hs-uvod1.ogg", "font_statue", "Určitě by pomohlo")
+
+dialog_addDialog("dir-hs-uvod2", "en", "", "font_statue", "It would help you if you")
+dialog_addDialog("dir-hs-uvod2", "cs", "sound/"..codename.."/cs/dir-hs-uvod2.ogg", "font_statue", "Možná by nebylo špatné")
+
+dialog_addDialog("dir-hs-uvod3", "en", "", "font_statue", "I have an idea. What if you")
+dialog_addDialog("dir-hs-uvod3", "cs", "sound/"..codename.."/cs/dir-hs-uvod3.ogg", "font_statue", "Mám takový nápad. Co zkusit")
+
+dialog_addDialog("dir-hs-uvod4", "en", "", "font_statue", "Sorry to interrupt, but what if you")
+dialog_addDialog("dir-hs-uvod4", "cs", "sound/"..codename.."/cs/dir-hs-uvod4.ogg", "font_statue", "Promiňte, že vám do toho mluvím, ale co takhle")
+
+dialog_addDialog("dir-hs-konec0", "en", "", "font_statue", "demolish some wall.")
+dialog_addDialog("dir-hs-konec0", "cs", "sound/"..codename.."/cs/dir-hs-konec0.ogg", "font_statue", "vybourat nějakou tu zeď.")
+
+dialog_addDialog("dir-hs-konec1", "en", "", "font_statue", "break off few stones from the wall and plug the holes with them.")
+dialog_addDialog("dir-hs-konec1", "cs", "sound/"..codename.."/cs/dir-hs-konec1.ogg", "font_statue", "vylomit pár kamenů ze zdi a ucpat ty díry.")
+
+dialog_addDialog("dir-hs-konec2", "en", "", "font_statue", "plug those holes with something so that that hooked steel wouldn`t get stuck there.")
+dialog_addDialog("dir-hs-konec2", "cs", "sound/"..codename.."/cs/dir-hs-konec2.ogg", "font_statue", "něčím ucpat ty díry, aby vám tam nezapadla ta zahnutá ocel.")
+
+dialog_addDialog("dir-hs-konec3", "en", "", "font_statue", "smash that pillar by the exit.")
+dialog_addDialog("dir-hs-konec3", "cs", "sound/"..codename.."/cs/dir-hs-konec3.ogg", "font_statue", "rozbít ten sloupek u východu.")
+
+dialog_addDialog("dir-hs-konec4", "en", "", "font_statue", "saw off some of that hooked steel.")
+dialog_addDialog("dir-hs-konec4", "cs", "sound/"..codename.."/cs/dir-hs-konec4.ogg", "font_statue", "uříznout kus té zahnuté oceli.")
+
+dialog_addDialog("dir-hs-konec5", "en", "", "font_statue", "rearrange the objects so that you can swim out.")
+dialog_addDialog("dir-hs-konec5", "cs", "sound/"..codename.."/cs/dir-hs-konec5.ogg", "font_statue", "nějak ty předměty přeskládat, abyste mohly odjet z místnosti.")
+
+dialog_addDialog("dir-hs-konec6", "en", "", "font_statue", "solve it somehow and get to the next level.")
+dialog_addDialog("dir-hs-konec6", "cs", "sound/"..codename.."/cs/dir-hs-konec6.ogg", "font_statue", "celé to nějak vyřešit a pustit se do dalších místností.")
+
+dialog_addDialog("dir-hs-konec7", "en", "", "font_statue", "start to think about it really hard.")
+dialog_addDialog("dir-hs-konec7", "cs", "sound/"..codename.."/cs/dir-hs-konec7.ogg", "font_statue", "opravdu pořádně se nad tím zamyslet.")
+
+dialog_addDialog("dir-hs-konec8", "en", "", "font_statue", "swim out through that hole on the left.")
+dialog_addDialog("dir-hs-konec8", "cs", "sound/"..codename.."/cs/dir-hs-konec8.ogg", "font_statue", "nějak zkusit odjet tou dírou nalevo.")
+
+dialog_addDialog("k1-chob-1", "en", "sound/"..codename.."/en/k1-chob-1.ogg")
+
+dialog_addDialog("k1-chob-2", "en", "sound/"..codename.."/en/k1-chob-2.ogg")
+
+dialog_addDialog("k1-chob-3", "en", "sound/"..codename.."/en/k1-chob-3.ogg")
+
+dialog_addDialog("k1-chob-p", "en", "sound/"..codename.."/en/k1-chob-p.ogg")
+
+dialog_addDialog("k1-x-vrz", "en", "sound/"..codename.."/en/k1-x-vrz.ogg")
+
+
