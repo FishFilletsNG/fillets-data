2bf3bbc9d1c71f5179a8c1e620c2370730131278
mrpaja <mrpaja>
2004-07-02 01:21:43 +0000

    level music

---
+++ script/music/dialogs.lua
@@ -0,0 +1,32 @@
+dialog_addDialog("ves-v-veci", "en", "", "font_big", "There are most interesting things in this city!")
+dialog_addDialog("ves-v-veci", "cs", "sound/"..codename.."/cs/ves-v-veci.ogg", "font_big", "V tomhle městě se teda najdou věci!")
+
+dialog_addDialog("ves-m-krab", "en", "", "font_small", "The poor crab!")
+dialog_addDialog("ves-m-krab", "cs", "sound/"..codename.."/cs/ves-m-krab.ogg", "font_small", "Chudáček krab!")
+
+dialog_addDialog("ves-v-vyp", "en", "", "font_big", "We should turn it off!")
+dialog_addDialog("ves-v-vyp", "cs", "sound/"..codename.."/cs/ves-v-vyp.ogg", "font_big", "Měli bychom to vypnout!")
+
+dialog_addDialog("ves-m-uz", "en", "", "font_small", "Oh, noo...")
+dialog_addDialog("ves-m-uz", "cs", "sound/"..codename.."/cs/ves-m-uz.ogg", "font_small", "Už né...")
+
+dialog_addDialog("ves-v-pokoj", "en", "", "font_big", "And it`s over.")
+dialog_addDialog("ves-v-pokoj", "cs", "sound/"..codename.."/cs/ves-v-pokoj.ogg", "font_big", "A je pokoj.")
+
+dialog_addDialog("ves-m-dik", "en", "", "font_small", "Thank you player. We couldn`t bear it any longer.")
+dialog_addDialog("ves-m-dik", "cs", "sound/"..codename.."/cs/ves-m-dik.ogg", "font_small", "Děkujeme ti, hráči. Už se to nedalo vydržet.")
+
+dialog_addDialog("ves-v-stejne", "en", "", "font_big", "But we have to push it down anyway.")
+dialog_addDialog("ves-v-stejne", "cs", "sound/"..codename.."/cs/ves-v-stejne.ogg", "font_big", "Stejně je ale budeme muset shodit.")
+
+dialog_addDialog("ves-hs-hrajeme", "en", "", "font_statue", "Now listen to the bit of music!")
+dialog_addDialog("ves-hs-hrajeme", "cs", "sound/"..codename.."/cs/ves-hs-hrajeme.ogg", "font_statue", "Hrajeme vám pro potěšení!")
+
+dialog_addDialog("ves-ampliony", "en", "sound/"..codename.."/en/ves-ampliony.ogg")
+
+dialog_addDialog("ves-hs-papa", "en", "", "font_statue", "Pa pada pa paaapa pa pada pa paaa...")
+dialog_addDialog("ves-hs-papa", "cs", "sound/"..codename.."/cs/ves-hs-papa.ogg", "font_statue", "Pa pada pa pápa pa pada pa pá...")
+
+dialog_addDialog("sp-smrt", "en", "sound/"..codename.."/en/sp-smrt.ogg")
+
+
