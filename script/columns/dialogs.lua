113cc6a151f1dda76e3a3a624badad17996946e0
fidlej <fidlej>
2004-07-04 15:03:14 +0000

    Added level 'columns'.

---
+++ script/columns/dialogs.lua
@@ -0,0 +1,42 @@
+dialog_addDialog("sl-v-stopa", "en", "", "font_big", "I think that we are finally on the trail of that mysterious city.")
+dialog_addDialog("sl-v-stopa", "cs", "sound/"..codename.."/cs/sl-v-stopa.ogg", "font_big", "Myslím, že jsme skutečně na stopě tomu tajemnému městu.")
+
+dialog_addDialog("sl-m-velkolepe", "en", "", "font_small", "It looks magnificent.")
+dialog_addDialog("sl-m-velkolepe", "cs", "sound/"..codename.."/cs/sl-m-velkolepe.ogg", "font_small", "Vypadá to velkolepě.")
+
+dialog_addDialog("sl-v-vkapse", "en", "", "font_big", "Now we only have to find out why it sank and we are done.")
+dialog_addDialog("sl-v-vkapse", "cs", "sound/"..codename.."/cs/sl-v-vkapse.ogg", "font_big", "Teď už musíme jen zjistit, proč se vlastně potopilo a máme to v kapse.")
+
+dialog_addDialog("sl-m-trvat", "en", "", "font_small", "But it may take us a little while.")
+dialog_addDialog("sl-m-trvat", "cs", "sound/"..codename.."/cs/sl-m-trvat.ogg", "font_small", "Ale možná nám to bude ještě chvíli trvat.")
+
+dialog_addDialog("sl-v-nechme", "en", "", "font_big", "Leave me alone. I adore the classic beauty of this temple.")
+dialog_addDialog("sl-v-nechme", "cs", "sound/"..codename.."/cs/sl-v-nechme.ogg", "font_big", "Nech mě. Kochám se antickou dokonalostí tohoto chrámu.")
+
+dialog_addDialog("sl-m-sedi", "en", "", "font_small", "Who is sitting on that chair?")
+dialog_addDialog("sl-m-sedi", "cs", "sound/"..codename.."/cs/sl-m-sedi.ogg", "font_small", "Kdo to tam sedí na židli?")
+
+dialog_addDialog("sl-v-feidios", "en", "", "font_big", "It`s a copy of Feidios` Zeus. One of the seven wonders of the world.")
+dialog_addDialog("sl-v-feidios", "cs", "sound/"..codename.."/cs/sl-v-feidios.ogg", "font_big", "To je kopie Feidiova Dia. Jeden ze sedmi divů světa.")
+
+dialog_addDialog("sl-m-tehdy", "en", "", "font_small", "Hmm. It was another age.")
+dialog_addDialog("sl-m-tehdy", "cs", "sound/"..codename.."/cs/sl-m-tehdy.ogg", "font_small", "No. Tehdy byla jiná doba.")
+
+dialog_addDialog("sl-v-opatrne", "en", "", "font_big", "Be careful! Try not to damage it!")
+dialog_addDialog("sl-v-opatrne", "cs", "sound/"..codename.."/cs/sl-v-opatrne.ogg", "font_big", "Opatrně! Ať to nepoškodíš.")
+
+dialog_addDialog("sl-v-skoda", "en", "", "font_big", "Such a waste!")
+dialog_addDialog("sl-v-skoda", "cs", "sound/"..codename.."/cs/sl-v-skoda.ogg", "font_big", "Taková škoda!")
+
+dialog_addDialog("sl-v-pust", "en", "", "font_big", "And now ease it down, carefully.")
+dialog_addDialog("sl-v-pust", "cs", "sound/"..codename.."/cs/sl-v-pust.ogg", "font_big", "Teď to opatrně pusť.")
+
+dialog_addDialog("sl-m-jekot", "en", "sound/"..codename.."/en/sl-m-jekot.ogg")
+
+dialog_addDialog("sl-v-barbarka", "en", "", "font_big", "You barbarian! Can`t you be a little bit cautious?!")
+dialog_addDialog("sl-v-barbarka", "cs", "sound/"..codename.."/cs/sl-v-barbarka.ogg", "font_big", "Barbarko! Nemůžeš si dát trochu pozor?!")
+
+dialog_addDialog("sl-m-nelibila", "en", "", "font_small", "I didn`t like this sculpture anyway.")
+dialog_addDialog("sl-m-nelibila", "cs", "sound/"..codename.."/cs/sl-m-nelibila.ogg", "font_small", "Stejně se mi ta socha nelíbila.")
+
+
