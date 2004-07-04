1fc82fa172783d6a702b92613e14c1ea90e17a97
fidlej <fidlej>
2004-07-04 21:06:31 +0000

    Added level 'pyramid'.

---
+++ script/pyramid/dialogs.lua
@@ -0,0 +1,37 @@
+dialog_addDialog("pyr-m-kam", "en", "", "font_small", "Where are we now?")
+dialog_addDialog("pyr-m-kam", "cs", "sound/"..codename.."/cs/pyr-m-kam.ogg", "font_small", "Kam jsme se to dostali?")
+
+dialog_addDialog("pyr-v-vsim", "en", "", "font_big", "The Pyramids... Notice how the classical motifs mix in this city.")
+dialog_addDialog("pyr-v-vsim", "cs", "sound/"..codename.."/cs/pyr-v-vsim.ogg", "font_big", "Pyramidy... Povšimni si, jak se v tomto městě mísí antické motivy.")
+
+dialog_addDialog("pyr-m-plaz", "en", "", "font_small", "What is that crawling around over there?")
+dialog_addDialog("pyr-m-plaz", "cs", "sound/"..codename.."/cs/pyr-m-plaz.ogg", "font_small", "Co se to tam plazí?")
+
+dialog_addDialog("pyr-v-druha", "en", "", "font_big", "You can`t see it from here? It`s on the other side of wall.")
+dialog_addDialog("pyr-v-druha", "cs", "sound/"..codename.."/cs/pyr-v-druha.ogg", "font_big", "To přece nevidíš. Vždyť je to na druhé straně zdi.")
+
+dialog_addDialog("pyr-m-nudi", "en", "", "font_small", "Look, the woman is bored!")
+dialog_addDialog("pyr-m-nudi", "cs", "sound/"..codename.."/cs/pyr-m-nudi.ogg", "font_small", "Podívej, ta ženská se snad nudí!")
+
+dialog_addDialog("pyr-v-sark", "en", "", "font_big", "Do you think that this is taking us too long?")
+dialog_addDialog("pyr-v-sark", "cs", "sound/"..codename.."/cs/pyr-v-sark.ogg", "font_big", "Tak tobě se zdá, že to trvá dlouho?")
+
+dialog_addDialog("pyr-m-zkus", "en", "", "font_small", "Try it yourself, if you`re so clever.")
+dialog_addDialog("pyr-m-zkus", "cs", "sound/"..codename.."/cs/pyr-m-zkus.ogg", "font_small", "Zkus si to sama, když jsi tak chytrá.")
+
+dialog_addDialog("pyr-m-comy", "en", "", "font_small", "What should we say?")
+dialog_addDialog("pyr-m-comy", "cs", "sound/"..codename.."/cs/pyr-m-comy.ogg", "font_small", "A co máme říkat my?")
+
+dialog_addDialog("pyr-m-nic", "en", "", "font_small", "You don`t have to carry anything.")
+dialog_addDialog("pyr-m-nic", "cs", "sound/"..codename.."/cs/pyr-m-nic.ogg", "font_small", "Ty tu nic nosit nemusíš.")
+
+dialog_addDialog("pyr-v-sfing", "en", "", "font_big", "Don`t be afraid.")
+dialog_addDialog("pyr-v-sfing", "cs", "sound/"..codename.."/cs/pyr-v-sfing.ogg", "font_big", "Neboj se.")
+
+dialog_addDialog("pyr-m-dest", "en", "", "font_small", "What is it written on these tablets?")
+dialog_addDialog("pyr-m-dest", "cs", "sound/"..codename.."/cs/pyr-m-dest.ogg", "font_small", "Co je asi napsáno na těch destičkách?")
+
+dialog_addDialog("pyr-v-sbohem", "en", "", "font_big", "So long and thanks for all the fish.")
+dialog_addDialog("pyr-v-sbohem", "cs", "sound/"..codename.."/cs/pyr-v-sbohem.ogg", "font_big", "Sbohem a dík za všechny ty ryby.")
+
+
