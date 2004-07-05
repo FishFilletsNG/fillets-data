1e40e8574fbbdffbac325c7dc724d3b6be3053b3
fidlej <fidlej>
2004-07-05 17:58:22 +0000

    Added level 'elevator2'.

---
+++ script/elevator2/dialogs.lua
@@ -0,0 +1,67 @@
+dialog_addDialog("zd2-m-dalsi", "en", "", "font_small", "Another elevator? How many more is there to come?")
+dialog_addDialog("zd2-m-dalsi", "cs", "sound/"..codename.."/cs/zd2-m-dalsi.ogg", "font_small", "Další výtah? Kolik jich ještě bude?")
+
+dialog_addDialog("zd2-v-odlis0", "en", "", "font_big", "I can see some differences here... And somehow I don`t think it`s going to be easier.")
+dialog_addDialog("zd2-v-odlis0", "cs", "sound/"..codename.."/cs/zd2-v-odlis0.ogg", "font_big", "Vidím tu jisté drobné odlišnosti... A něco mi říká, že to teď není jednodušší.")
+
+dialog_addDialog("zd2-v-odlis1", "en", "", "font_big", "The last one had a little bit more advantageous architecture, I think.")
+dialog_addDialog("zd2-v-odlis1", "cs", "sound/"..codename.."/cs/zd2-v-odlis1.ogg", "font_big", "Ten minulý měl, myslím, trochu příznivější architekturu.")
+
+dialog_addDialog("zd2-v-vlevo", "en", "", "font_big", "Can you see that thing in the lower left? This must be some ancient religious symbol.")
+dialog_addDialog("zd2-v-vlevo", "cs", "sound/"..codename.."/cs/zd2-v-vlevo.ogg", "font_big", "Vidíš tu věc vlevo dole? To je jistě nějaký prastarý náboženský symbol.")
+
+dialog_addDialog("zd2-m-nevid0", "en", "", "font_small", "Fortunately, I can`t see much of it.")
+dialog_addDialog("zd2-m-nevid0", "cs", "sound/"..codename.."/cs/zd2-m-nevid0.ogg", "font_small", "Naštěstí na něj moc nevidím.")
+
+dialog_addDialog("zd2-m-nevid1", "en", "", "font_small", "If you are right, I`d rather wouldn`t know what kind of religion it was.")
+dialog_addDialog("zd2-m-nevid1", "cs", "sound/"..codename.."/cs/zd2-m-nevid1.ogg", "font_small", "Jestli máš pravdu, tak radši nechci vědět koho.")
+
+dialog_addDialog("zd2-v-symbol", "en", "", "font_big", "I am afraid that if we use that strange symbol we could unwillingly initiate some dark ritual.")
+dialog_addDialog("zd2-v-symbol", "cs", "sound/"..codename.."/cs/zd2-v-symbol.ogg", "font_big", "Bojím se, že při použítí toho podivného symbolu bezděky spustíme nějaký temný rituál.")
+
+dialog_addDialog("zd2-m-douf", "en", "", "font_small", "I just hope it won`t drop on my head.")
+dialog_addDialog("zd2-m-douf", "cs", "sound/"..codename.."/cs/zd2-m-douf.ogg", "font_small", "Já jen doufám, že mi při tom nespadne na hlavu.")
+
+dialog_addDialog("zd2-m-lebka", "en", "", "font_small", "Another skull. It`s worse than morgue.")
+dialog_addDialog("zd2-m-lebka", "cs", "sound/"..codename.."/cs/zd2-m-lebka.ogg", "font_small", "Zase nějaká lebka. Je to tu horší než v márnici.")
+
+dialog_addDialog("zd2-v-haml", "en", "", "font_big", "It`s worse than Hamlet.")
+dialog_addDialog("zd2-v-haml", "cs", "sound/"..codename.."/cs/zd2-v-haml.ogg", "font_big", "Je to tu horší než v Hamletovi.")
+
+dialog_addDialog("zd2-x-hus0", "en", "", "font_lightgrey", "Hush, hush!")
+dialog_addDialog("zd2-x-hus0", "cs", "sound/"..codename.."/cs/zd2-x-hus0.ogg", "font_lightgrey", "Huš, huš!")
+
+dialog_addDialog("zd2-x-hus1", "en", "", "font_lightgrey", "Don`t come near!")
+dialog_addDialog("zd2-x-hus1", "cs", "sound/"..codename.."/cs/zd2-x-hus1.ogg", "font_lightgrey", "Nepřibližuj se!")
+
+dialog_addDialog("zd2-x-kricet", "en", "", "font_lightgrey", "I`ll scream if you touch me.")
+dialog_addDialog("zd2-x-kricet", "cs", "sound/"..codename.."/cs/zd2-x-kricet.ogg", "font_lightgrey", "Jestli na mě sáhneš, budu křičet.")
+
+dialog_addDialog("zd2-x-krik0", "en", "", "font_lightgrey", "Aaaaa")
+dialog_addDialog("zd2-x-krik0", "cs", "sound/"..codename.."/cs/zd2-x-krik0.ogg", "font_lightgrey", "ááááaa")
+
+dialog_addDialog("zd2-x-krik1", "en", "", "font_lightgrey", "Help!")
+dialog_addDialog("zd2-x-krik1", "cs", "sound/"..codename.."/cs/zd2-x-krik1.ogg", "font_lightgrey", "Pomóc.")
+
+dialog_addDialog("zd2-x-ritual", "en", "", "font_lightgrey", "I`ll initiate dark ritual!")
+dialog_addDialog("zd2-x-ritual", "cs", "sound/"..codename.."/cs/zd2-x-ritual.ogg", "font_lightgrey", "Spustím temný rituál.")
+
+dialog_addDialog("zd2-x-nechteme", "en", "", "font_lightgrey", "Leave me alone!")
+dialog_addDialog("zd2-x-nechteme", "cs", "sound/"..codename.."/cs/zd2-x-nechteme.ogg", "font_lightgrey", "Nechte mě na pokoji!")
+
+dialog_addDialog("zd2-x-nechme", "en", "", "font_lightgrey", "Leave me alone and get lost!")
+dialog_addDialog("zd2-x-nechme", "cs", "sound/"..codename.."/cs/zd2-x-nechme.ogg", "font_lightgrey", "Necháš mě, dej pokoj!")
+
+dialog_addDialog("zd2-x-pokoj", "en", "", "font_lightgrey", "Cut it out!")
+dialog_addDialog("zd2-x-pokoj", "cs", "sound/"..codename.."/cs/zd2-x-pokoj.ogg", "font_lightgrey", "Dej pokoj!")
+
+dialog_addDialog("zd2-x-fuj", "en", "", "font_lightgrey", "Yuck, don`t nibble me!")
+dialog_addDialog("zd2-x-fuj", "cs", "sound/"..codename.."/cs/zd2-x-fuj.ogg", "font_lightgrey", "Fuj, neočuchávej mě!")
+
+dialog_addDialog("zd2-x-necurat", "en", "", "font_lightgrey", "Don`t think you can pee on me.")
+dialog_addDialog("zd2-x-necurat", "cs", "sound/"..codename.."/cs/zd2-x-necurat.ogg", "font_lightgrey", "Ne že se ke mě vyčůráš.")
+
+dialog_addDialog("zd2-x-neklast", "en", "", "font_lightgrey", "Just forget about lying your spawn here.")
+dialog_addDialog("zd2-x-neklast", "cs", "sound/"..codename.."/cs/zd2-x-neklast.ogg", "font_lightgrey", "Jestli tady chceš klást jikry, tak na to zapomeň.")
+
+
