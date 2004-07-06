1d49e405557f81d109a98b762f6a2482ad96b809
fidlej <fidlej>
2004-07-06 12:22:52 +0000

    Added unfinished 'party2' level.

---
+++ script/party2/dialogs.lua
@@ -0,0 +1,40 @@
+dialog_addDialog("pt2-m-parnik", "en", "", "font_small", "That ghostly boat, again.")
+dialog_addDialog("pt2-m-parnik", "cs", "sound/"..codename.."/cs/pt2-m-parnik.ogg", "font_small", "Zase ten přízračný parník.")
+
+dialog_addDialog("pt2-v-zmena", "en", "", "font_big", "Can you see it? Something has changed while we were away. I tell you, somebody is watching us.")
+dialog_addDialog("pt2-v-zmena", "cs", "sound/"..codename.."/cs/pt2-v-zmena.ogg", "font_big", "Vidíš? Něco se změnilo, zatímco jsme byli pryč. Říkám ti, že nás někdo pozoruje.")
+
+dialog_addDialog("pt2-m-hrac", "en", "", "font_small", "Why do you keep looking at us? Can`t you see it makes us nervous?")
+dialog_addDialog("pt2-m-hrac", "cs", "sound/"..codename.."/cs/pt2-m-hrac.ogg", "font_small", "Co se na nás pořád díváš? Nevidíš, že nás to znervózňuje?")
+
+dialog_addDialog("pt2-m-piknik0", "en", "", "font_small", "It looks like their picnic is coming to an end.")
+dialog_addDialog("pt2-m-piknik0", "cs", "sound/"..codename.."/cs/pt2-m-piknik0.ogg", "font_small", "Koukám, že ten jejich piknik už pomalu končí.")
+
+dialog_addDialog("pt2-m-piknik1", "en", "", "font_small", "The picnic is over.")
+dialog_addDialog("pt2-m-piknik1", "cs", "sound/"..codename.."/cs/pt2-m-piknik1.ogg", "font_small", "Už mají po pikniku.")
+
+dialog_addDialog("pt2-m-piknik2", "en", "", "font_small", "That`s all that is left from the picnic.")
+dialog_addDialog("pt2-m-piknik2", "cs", "sound/"..codename.."/cs/pt2-m-piknik2.ogg", "font_small", "Tohle tu zbylo po pikniku.")
+
+dialog_addDialog("pt2-m-piknik3", "en", "", "font_small", "They have had a kind of picnic by the bottom.")
+dialog_addDialog("pt2-m-piknik3", "cs", "sound/"..codename.."/cs/pt2-m-piknik3.ogg", "font_small", "Měli tu takový piknik u dna.")
+
+dialog_addDialog("pt2-v-unaveni0", "en", "", "font_big", "I think that those party-goers are little bit tired.")
+dialog_addDialog("pt2-v-unaveni0", "cs", "sound/"..codename.."/cs/pt2-v-unaveni0.ogg", "font_big", "Zdá se, že ti výletníci jsou poněkud společensky unaveni.")
+
+dialog_addDialog("pt2-v-unaveni1", "en", "", "font_big", "Yes, they have had enough.")
+dialog_addDialog("pt2-v-unaveni1", "cs", "sound/"..codename.."/cs/pt2-v-unaveni1.ogg", "font_big", "Ti už mají opravdu dost.")
+
+dialog_addDialog("pt2-v-nemohou0", "en", "", "font_big", "I think they cannot think about harming us now.")
+dialog_addDialog("pt2-v-nemohou0", "cs", "sound/"..codename.."/cs/pt2-v-nemohou0.ogg", "font_big", "Myslím, že ti už nemají náladu nějak nám ublížit.")
+
+dialog_addDialog("pt2-v-nemohou1", "en", "", "font_big", "I think they are quite harmless now.")
+dialog_addDialog("pt2-v-nemohou1", "cs", "sound/"..codename.."/cs/pt2-v-nemohou1.ogg", "font_big", "Myslím, že ti už nám nic udělat nemohou.")
+
+dialog_addDialog("pt2-v-minule0", "en", "", "font_big", "It won`t be that easy like the last time.")
+dialog_addDialog("pt2-v-minule0", "cs", "sound/"..codename.."/cs/pt2-v-minule0.ogg", "font_big", "To nebude tak jednoduché jako minule.")
+
+dialog_addDialog("pt2-v-minule1", "en", "", "font_big", "It seems it`s going to be more difficult this time.")
+dialog_addDialog("pt2-v-minule1", "cs", "sound/"..codename.."/cs/pt2-v-minule1.ogg", "font_big", "Zdá se, že to bude trochu složitější.")
+
+
