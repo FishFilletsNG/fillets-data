4ba09248a2bc77ceffb43cbe232d66a42eb300a3
fidlej <fidlej>
2004-07-10 20:10:23 +0000

    Added french dialogs for black jokes.

--- script/share/blackdialogs.lua
+++ script/share/blackdialogs.lua
@@ -1,82 +1,109 @@
 dialog_addDialog("smrt-v-0", "en", "", "font_big", "Oh, shoot!")
 dialog_addDialog("smrt-v-0", "cs", "sound/blackjokes/cs/smrt-v-0.ogg", "font_big", "No a je to v ...")
+dialog_addDialog("smrt-v-0", "fr", "", "font_big", "Oh, mer.. !")
 
 dialog_addDialog("smrt-m-psst", "en", "", "font_small", "Shhh, children can be playing this game!")
 dialog_addDialog("smrt-m-psst", "cs", "sound/blackjokes/cs/smrt-m-psst.ogg", "font_small", "Pššt, mohou to hrát i děti!")
+dialog_addDialog("smrt-m-psst", "fr", "", "font_small", "Chut, des enfants peuvent jouer à ce jeu !")
 
 dialog_addDialog("smrt-v-1", "en", "", "font_big", "Darn.")
 dialog_addDialog("smrt-v-1", "cs", "sound/blackjokes/cs/smrt-v-1.ogg", "font_big", "Zatraceně.")
+dialog_addDialog("smrt-v-1", "fr", "", "font_big", "Malheur.")
 
 dialog_addDialog("smrt-v-2", "en", "", "font_big", "Dang!")
 dialog_addDialog("smrt-v-2", "cs", "sound/blackjokes/cs/smrt-v-2.ogg", "font_big", "Shit!")
+dialog_addDialog("smrt-v-2", "fr", "", "font_big", "Shit !")
 
 dialog_addDialog("smrt-v-3", "en", "", "font_big", "Scheisse!")
 dialog_addDialog("smrt-v-3", "cs", "sound/blackjokes/cs/smrt-v-3.ogg", "font_big", "Scheisse!")
+dialog_addDialog("smrt-v-3", "fr", "", "font_big", "Scheisse!")
 
 dialog_addDialog("smrt-v-4", "en", "", "font_big", "Hmmm... and now I am alone.")
 dialog_addDialog("smrt-v-4", "cs", "sound/blackjokes/cs/smrt-v-4.ogg", "font_big", "Hmmm... a jsem sám.")
+dialog_addDialog("smrt-v-4", "fr", "", "font_big", "Hmmm... Et maintenant je suis seul.")
 
 dialog_addDialog("smrt-v-5", "en", "", "font_big", "Did she know she has such a sexy skeleton?")
 dialog_addDialog("smrt-v-5", "cs", "sound/blackjokes/cs/smrt-v-5.ogg", "font_big", "Jestlipak věděla, že má tak sexy kostru?")
+dialog_addDialog("smrt-v-5", "fr", "", "font_big", "Sait-elle qu'elle a un squelette vraiment sexy ?")
 
 dialog_addDialog("smrt-v-6", "en", "", "font_big", "Terrible end of the little fish...")
 dialog_addDialog("smrt-v-6", "cs", "sound/blackjokes/cs/smrt-v-6.ogg", "font_big", "Strašlivý konec pro malou rybku...")
+dialog_addDialog("smrt-v-6", "fr", "", "font_big", "Terrible fin d'un petit poisson...")
 
 dialog_addDialog("smrt-v-7", "en", "", "font_big", "What can be the constant of decay of the fish skeleton?")
 dialog_addDialog("smrt-v-7", "cs", "sound/blackjokes/cs/smrt-v-7.ogg", "font_big", "Jaký je asi poločas rozpadu rybí kostřičky?")
+dialog_addDialog("smrt-v-7", "fr", "", "font_big", "Quelle constance dans la décrépitude du squelette de poisson.")
 
 dialog_addDialog("smrt-m-0", "en", "", "font_small", "So much for him.")
 dialog_addDialog("smrt-m-0", "cs", "sound/blackjokes/cs/smrt-m-0.ogg", "font_small", "No, a je to.")
+dialog_addDialog("smrt-m-0", "fr", "", "font_small", "Trop pour lui.")
 
 dialog_addDialog("smrt-m-1", "en", "", "font_small", "Oh, nooo.")
 dialog_addDialog("smrt-m-1", "cs", "sound/blackjokes/cs/smrt-m-1.ogg", "font_small", "Ach nééé.")
+dialog_addDialog("smrt-m-1", "fr", "", "font_small", "Oh, nonnn.")
 
 dialog_addDialog("smrt-m-2", "en", "", "font_small", "And now I am alone.")
 dialog_addDialog("smrt-m-2", "cs", "sound/blackjokes/cs/smrt-m-2.ogg", "font_small", "A jsem tady sama.")
+dialog_addDialog("smrt-m-2", "fr", "", "font_small", "Et maintenant je suis seule.")
 
 dialog_addDialog("smrt-m-3", "en", "", "font_small", "That must have hurt.")
 dialog_addDialog("smrt-m-3", "cs", "sound/blackjokes/cs/smrt-m-3.ogg", "font_small", "To muselo ale bolet.")
+dialog_addDialog("smrt-m-3", "fr", "", "font_small", "Ca devait être mortel.")
 
 dialog_addDialog("smrt-m-4", "en", "", "font_small", "He didn`t deserve such a terrible end...")
 dialog_addDialog("smrt-m-4", "cs", "sound/blackjokes/cs/smrt-m-4.ogg", "font_small", "Chudák, takový konec si nezasloužil...")
+dialog_addDialog("smrt-m-4", "fr", "", "font_small", "Il ne méritait pas une fin si terrible...")
 
 dialog_addDialog("smrt-m-5", "en", "", "font_small", "So, let`s start again...")
 dialog_addDialog("smrt-m-5", "cs", "sound/blackjokes/cs/smrt-m-5.ogg", "font_small", "Tak znovu...")
+dialog_addDialog("smrt-m-5", "fr", "", "font_small", "Bien, recommençons...")
 
 dialog_addDialog("smrt-x-obe", "en", "", "font_parrot", "Aye, caramba!")
 dialog_addDialog("smrt-x-obe", "cs", "sound/blackjokes/cs/smrt-x-obe.ogg", "font_parrot", "Oj, karamba!")
+dialog_addDialog("smrt-x-obe", "fr", "", "font_parrot", "Hé, caramba!")
 
 dialog_addDialog("smrt-v-obe", "en", "", "font_big", "But there is no parrot here!")
 dialog_addDialog("smrt-v-obe", "cs", "sound/blackjokes/cs/smrt-v-obe.ogg", "font_big", "Tady přeci papoušek není!")
+dialog_addDialog("smrt-v-obe", "fr", "", "font_big", "Mais il n'y a pas de perroquet ici !")
 
 dialog_addDialog("smrt-m-obe", "en", "", "font_small", "Shut up, you are dead.")
 dialog_addDialog("smrt-m-obe", "cs", "sound/blackjokes/cs/smrt-m-obe.ogg", "font_small", "Mlč, jsi mrtvý.")
+dialog_addDialog("smrt-m-obe", "fr", "", "font_small", "La ferme, tu es mort.")
 
 dialog_addDialog("smrt-m-6", "en", "", "font_small", "And I never told him I loved him.")
 dialog_addDialog("smrt-m-6", "cs", "sound/blackjokes/cs/smrt-m-6.ogg", "font_small", "Ani jsem mu nestačila říci, že ho miluji.")
+dialog_addDialog("smrt-m-6", "fr", "", "font_small", "Et je ne lui ai jamais dit que je l'aimais.")
 
 dialog_addDialog("smrt-m-restart", "en", "", "font_small", "Luckily, it`s possible to restart the level.")
 dialog_addDialog("smrt-m-restart", "cs", "sound/blackjokes/cs/smrt-m-restart.ogg", "font_small", "Naštěstí je možno místnost restartovat.")
+dialog_addDialog("smrt-m-restart", "fr", "", "font_small", "Nous avons la chance de pouvoir redémarrer le niveau.")
 
 dialog_addDialog("smrt-v-restart", "en", "", "font_big", "Fortunately, we can restart the level.")
 dialog_addDialog("smrt-v-restart", "cs", "sound/blackjokes/cs/smrt-v-restart.ogg", "font_big", "Ještě že je možno místnost restartovat.")
+dialog_addDialog("smrt-v-restart", "fr", "", "font_big", "Heureusement, nous pouvons redémarrer le niveau.")
 
 dialog_addDialog("smrt-v-zahrobi", "en", "", "font_big", "Restart the level! The afterlife is too tedious.")
 dialog_addDialog("smrt-v-zahrobi", "cs", "sound/blackjokes/cs/smrt-v-zahrobi.ogg", "font_big", "Restartuj místnost! Posmrtný život není moc zábavný.")
+dialog_addDialog("smrt-v-zahrobi", "fr", "", "font_big", "Relance le niveau ! L'après-vie est très ennuyeux.")
 
 dialog_addDialog("smrt-m-zahrobi", "en", "", "font_small", "Restart the level! This afterlife is too scary.")
 dialog_addDialog("smrt-m-zahrobi", "cs", "sound/blackjokes/cs/smrt-m-zahrobi.ogg", "font_small", "Restartuj místnost. Tady v záhrobí je to hrozně strašidelné.")
+dialog_addDialog("smrt-m-zahrobi", "fr", "", "font_small", "Relance le niveau ! L'après-vie est très effrayant.")
 
 dialog_addDialog("smrt-v-posmrtny", "en", "", "font_big", "Hellooo? Can you hear me hereafter? Is there life after death?")
 dialog_addDialog("smrt-v-posmrtny", "cs", "sound/blackjokes/cs/smrt-v-posmrtny.ogg", "font_big", "Halóóó. Slyšíš mě tam někde v záhrobí? Existuje posmrtný život?")
+dialog_addDialog("smrt-v-posmrtny", "fr", "", "font_big", "Saluuuut ? Pouvez-vous m'entendre de l'au-delà ? Y a-t-il une vie après la mort ?")
 
 dialog_addDialog("smrt-m-posmrtny", "en", "", "font_small", "I don`t know. I don`t think so.")
 dialog_addDialog("smrt-m-posmrtny", "cs", "sound/blackjokes/cs/smrt-m-posmrtny.ogg", "font_small", "Nevím. Myslím, že ne.")
+dialog_addDialog("smrt-m-posmrtny", "fr", "", "font_small", "Je ne sais pas. Je ne crois pas.")
 
 dialog_addDialog("smrt-m-autorest", "en", "", "font_small", "I am not going to solve it alone. Don`t trouble yourself with restart, I`ll do it myself.")
 dialog_addDialog("smrt-m-autorest", "cs", "sound/blackjokes/cs/smrt-m-autorest.ogg", "font_small", "Sama to řešit nebudu. Ani se nemusíš obtěžovat s restartem, udělám to sama.")
+dialog_addDialog("smrt-m-autorest", "fr", "", "font_small", "Je ne pourrais pas finir toute seule. T'occupe pas du redémarrage, je le fais moi-même.")
 
 dialog_addDialog("smrt-v-autorest", "en", "", "font_big", "And she is gone. I`m sure you liked her as much as I did. Certainly, you have nothing against restarting the level.")
 dialog_addDialog("smrt-v-autorest", "cs", "sound/blackjokes/cs/smrt-v-autorest.ogg", "font_big", "Je pryč. Určitě sis ji za tu dobu taky tak oblíbil jako já. Takže nemáš nic proti restartu.")
+dialog_addDialog("smrt-v-autorest", "fr", "", "font_big", "Et elle est partie. Je suis sûr que tu l'appréciais plus que moi. Certainement tu n'as rien contre relancer le niveau.")
 
 
