47ccf28d1508dba5d8c6be7684eb24268be98f50
fidlej <fidlej>
2004-07-13 17:20:16 +0000

    Michal Ambroz convert 'bathroom' level.

---
+++ script/bathroom/dialogs.lua
@@ -0,0 +1,72 @@
+dialog_addDialog("br-v-komfort", "en", "", "font_big", "It`s quite comfortable for a wasteyard.")
+dialog_addDialog("br-v-komfort", "cs", "sound/"..codename.."/cs/br-v-komfort.ogg", "font_big", "Na to, že je to vlastně smetiště, je to docela komfort.")
+
+dialog_addDialog("br-m-bydli", "en", "", "font_small", "What do you think, does anybody live here?")
+dialog_addDialog("br-m-bydli", "cs", "sound/"..codename.."/cs/br-m-bydli.ogg", "font_small", "Myslíš, že tu někdo bydlí?")
+
+dialog_addDialog("br-v-santusak", "en", "", "font_big", "Some underwater homeless.")
+dialog_addDialog("br-v-santusak", "cs", "sound/"..codename.."/cs/br-v-santusak.ogg", "font_big", "Nějaký podvodní santusák.")
+
+dialog_addDialog("br-m-podvodnik", "en", "", "font_small", "We call them shell-less.")
+dialog_addDialog("br-m-podvodnik", "cs", "sound/"..codename.."/cs/br-m-podvodnik.ogg", "font_small", "Nebo santusácký podvodník.")
+
+dialog_addDialog("br-m-vsim0", "en", "", "font_small", "Did you notice that...")
+dialog_addDialog("br-m-vsim0", "cs", "sound/"..codename.."/cs/br-m-vsim0.ogg", "font_small", "Všiml sis, že...")
+
+dialog_addDialog("br-m-vsim1", "en", "", "font_small", "Don`t you think that...")
+dialog_addDialog("br-m-vsim1", "cs", "sound/"..codename.."/cs/br-m-vsim1.ogg", "font_small", "Není ti divné, že...")
+
+dialog_addDialog("br-m-vsim2", "en", "", "font_small", "Isn`t it strange that...")
+dialog_addDialog("br-m-vsim2", "cs", "sound/"..codename.."/cs/br-m-vsim2.ogg", "font_small", "To je zvláštní, že...")
+
+dialog_addDialog("br-v-nerozvadet0", "en", "", "font_big", "Don`t speak about it.")
+dialog_addDialog("br-v-nerozvadet0", "cs", "sound/"..codename.."/cs/br-v-nerozvadet0.ogg", "font_big", "Já bych to nerozváděl.")
+
+dialog_addDialog("br-v-nerozvadet1", "en", "", "font_big", "Please, no jokes about defecation.")
+dialog_addDialog("br-v-nerozvadet1", "cs", "sound/"..codename.."/cs/br-v-nerozvadet1.ogg", "font_big", "Prosím tě, žádné fekální vtipy.")
+
+dialog_addDialog("br-v-nerozvadet2", "en", "", "font_big", "Yes, I know about that. We won`t comment on it.")
+dialog_addDialog("br-v-nerozvadet2", "cs", "sound/"..codename.."/cs/br-v-nerozvadet2.ogg", "font_big", "Ano, vím o tom. Nebudeme to komentovat.")
+
+dialog_addDialog("br-m-dva", "en", "", "font_small", "Yeah, but why TWO?")
+dialog_addDialog("br-m-dva", "cs", "sound/"..codename.."/cs/br-m-dva.ogg", "font_small", "Ale proč DVA?")
+
+dialog_addDialog("br-v-dost", "en", "", "font_big", "Stop it, will you?")
+dialog_addDialog("br-v-dost", "cs", "sound/"..codename.."/cs/br-v-dost.ogg", "font_big", "Prosím tě, nech toho!")
+
+dialog_addDialog("br-m-sprcha", "en", "", "font_small", "I`ve seen many useless things, but an underwater shower...")
+dialog_addDialog("br-m-sprcha", "cs", "sound/"..codename.."/cs/br-m-sprcha.ogg", "font_small", "Viděla jsem tu hodně zbytečností, ale podvodní sprcha...")
+
+dialog_addDialog("br-v-lazen", "en", "", "font_big", "Maybe it sprays air, like in the bubble bath.")
+dialog_addDialog("br-v-lazen", "cs", "sound/"..codename.."/cs/br-v-lazen.ogg", "font_big", "Třeba z ní tryská vzduch, jako ve vířivé lázni.")
+
+dialog_addDialog("br-m-zapnout", "en", "", "font_small", "Do you think we could turn it on?")
+dialog_addDialog("br-m-zapnout", "cs", "sound/"..codename.."/cs/br-m-zapnout.ogg", "font_small", "Myslíš, že bychom ji mohli zapnout?")
+
+dialog_addDialog("br-v-shodit", "en", "", "font_big", "Hardly. But we could push it down.")
+dialog_addDialog("br-v-shodit", "cs", "sound/"..codename.."/cs/br-v-shodit.ogg", "font_big", "Těžko. Ale mohli bychom ji shodit.")
+
+dialog_addDialog("br-m-poklady", "en", "", "font_small", "Now this is real treasure.")
+dialog_addDialog("br-m-poklady", "cs", "sound/"..codename.."/cs/br-m-poklady.ogg", "font_small", "No to jsou poklady.")
+
+dialog_addDialog("br-v-nechat", "en", "", "font_big", "We should have let it alone.")
+dialog_addDialog("br-v-nechat", "cs", "sound/"..codename.."/cs/br-v-nechat.ogg", "font_big", "Měli jsme ji nechat tam, kde byla.")
+
+dialog_addDialog("br-m-nefunguje", "en", "", "font_small", "And it doesn`t work anyway.")
+dialog_addDialog("br-m-nefunguje", "cs", "sound/"..codename.."/cs/br-m-nefunguje.ogg", "font_small", "A navíc stejně nefunguje.")
+
+dialog_addDialog("br-m-ahoj", "en", "", "font_small", "Hello, inside there.")
+dialog_addDialog("br-m-ahoj", "cs", "sound/"..codename.."/cs/br-m-ahoj.ogg", "font_small", "Ahoj tam uvnitř.")
+
+dialog_addDialog("br-v-draha", "en", "", "font_big", "This must be better than rollercoaster.")
+dialog_addDialog("br-v-draha", "cs", "sound/"..codename.."/cs/br-v-draha.ogg", "font_big", "To je určitě lepší než horská dráha.")
+
+dialog_addDialog("br-m-zkusit", "en", "", "font_small", "I would love to try it myself.")
+dialog_addDialog("br-m-zkusit", "cs", "sound/"..codename.."/cs/br-m-zkusit.ogg", "font_small", "Hned bych si to taky zkusila.")
+
+dialog_addDialog("br-m-bavi", "en", "", "font_small", "He seems to be enjoying himself.")
+dialog_addDialog("br-m-bavi", "cs", "sound/"..codename.."/cs/br-m-bavi.ogg", "font_small", "Zdá se, že se dobře baví.")
+
+dialog_addDialog("br-x-pracka", "en", "sound/"..codename.."/en/br-x-pracka.ogg")
+
+
