49ee882a6e13e177f4336e70ec6d44d7861efab1
fidlej <fidlej>
2004-07-13 22:33:48 +0000

    Michal convert 'dump' level.

---
+++ script/dump/dialogs.lua
@@ -0,0 +1,69 @@
+dialog_addDialog("sm-m-prolezame", "en", "", "font_small", "We are crawling through wasteyard for six levels now.")
+dialog_addDialog("sm-m-prolezame", "cs", "sound/"..codename.."/cs/sm-m-prolezame.ogg", "font_small", "Už šestou místnost prolézáme smetiště.")
+
+dialog_addDialog("sm-v-jine0", "en", "", "font_big", "This one looks really very waste-like.")
+dialog_addDialog("sm-v-jine0", "cs", "sound/"..codename.."/cs/sm-v-jine0.ogg", "font_big", "Tohle je obzvlášť vydařené.")
+
+dialog_addDialog("sm-v-jine1", "en", "", "font_big", "But no, I think that there is something strange about it.")
+dialog_addDialog("sm-v-jine1", "cs", "sound/"..codename.."/cs/sm-v-jine1.ogg", "font_big", "Ba ne, mám pocit, že tohle je něčím zvláštní.")
+
+dialog_addDialog("sm-v-jine2", "en", "", "font_big", "No, I think there is something here, what cannot be found anywhere else.")
+dialog_addDialog("sm-v-jine2", "cs", "sound/"..codename.."/cs/sm-v-jine2.ogg", "font_big", "Ne, myslím, že tu je něco, co není nikde jinde.")
+
+dialog_addDialog("sm-v-budik", "en", "", "font_big", "Look at that clock. Doesn`t it resemble a child-comforter a little?")
+dialog_addDialog("sm-v-budik", "cs", "sound/"..codename.."/cs/sm-v-budik.ogg", "font_big", "Podívej se na ten budík. Nepřipomíná ti něčím dudlík?")
+
+dialog_addDialog("sm-m-normalni", "en", "", "font_small", "Do you feel all right?")
+dialog_addDialog("sm-m-normalni", "cs", "sound/"..codename.."/cs/sm-m-normalni.ogg", "font_small", "Jsi normální?")
+
+dialog_addDialog("sm-m-kramy0", "en", "", "font_small", "This is a pile of garbage.")
+dialog_addDialog("sm-m-kramy0", "cs", "sound/"..codename.."/cs/sm-m-kramy0.ogg", "font_small", "Tady je ale krámů.")
+
+dialog_addDialog("sm-m-kramy1", "en", "", "font_small", "This must be a wreck of a flea shop.")
+dialog_addDialog("sm-m-kramy1", "cs", "sound/"..codename.."/cs/sm-m-kramy1.ogg", "font_small", "Tady muselo ztroskotat vetešnictví.")
+
+dialog_addDialog("sm-v-kramy2", "en", "", "font_big", "It`s worse here than in an oil spill.")
+dialog_addDialog("sm-v-kramy2", "cs", "sound/"..codename.."/cs/sm-v-kramy2.ogg", "font_big", "Tady je to horší než v ropné skvrně.")
+
+dialog_addDialog("sm-v-kramy3", "en", "", "font_big", "There is so much filth here.")
+dialog_addDialog("sm-v-kramy3", "cs", "sound/"..codename.."/cs/sm-v-kramy3.ogg", "font_big", "Tady je ale svinstva.")
+
+dialog_addDialog("sm-v-lod", "en", "", "font_big", "Can you see that boat?")
+dialog_addDialog("sm-v-lod", "cs", "sound/"..codename.."/cs/sm-v-lod.ogg", "font_big", "Vidíš tu loďku?")
+
+dialog_addDialog("sm-m-dedek", "en", "", "font_small", "The one with that old man?")
+dialog_addDialog("sm-m-dedek", "cs", "sound/"..codename.."/cs/sm-m-dedek.ogg", "font_small", "Tu s tím dědkem?")
+
+dialog_addDialog("sm-v-charon", "en", "", "font_big", "It`s old Charon, ferryman of the dead.")
+dialog_addDialog("sm-v-charon", "cs", "sound/"..codename.."/cs/sm-v-charon.ogg", "font_big", "To je starý Cháron, převozník mrtvých.")
+
+dialog_addDialog("sm-m-codela", "en", "", "font_small", "And what does he do on the wasteyard?")
+dialog_addDialog("sm-m-codela", "cs", "sound/"..codename.."/cs/sm-m-codela.ogg", "font_small", "A co dělá na smetišti?")
+
+dialog_addDialog("sm-v-duchodce0", "en", "", "font_big", "He`s probably retired.")
+dialog_addDialog("sm-v-duchodce0", "cs", "sound/"..codename.."/cs/sm-v-duchodce0.ogg", "font_big", "Asi je z něj dneska důchodce.")
+
+dialog_addDialog("sm-v-duchodce1", "en", "", "font_big", "The corpses were no longer profitable and so they started a recycling business.")
+dialog_addDialog("sm-v-duchodce1", "cs", "sound/"..codename.."/cs/sm-v-duchodce1.ogg", "font_big", "Mrtvoly nevynášely dost, tak se s mladým dali na odpadky.")
+
+dialog_addDialog("sm-v-sbirka", "en", "", "font_big", "Can you see that collection of useless stuff, weird objects and refute... All of them were useful things, carefully and skillfully manufactured.")
+dialog_addDialog("sm-v-sbirka", "cs", "sound/"..codename.."/cs/sm-v-sbirka.ogg", "font_big", "Vidíš tu sbírku neužitečností, podivností a odpadků... Všechno to kdysi byly užitečné věci, které někdo pracně vyrobil.")
+
+dialog_addDialog("sm-m-namaloval", "en", "", "font_small", "Or painted.")
+dialog_addDialog("sm-m-namaloval", "cs", "sound/"..codename.."/cs/sm-m-namaloval.ogg", "font_small", "Nebo namaloval.")
+
+dialog_addDialog("sm-v-marnost", "en", "", "font_big", "Everything is in vain, like that medusa over there.")
+dialog_addDialog("sm-v-marnost", "cs", "sound/"..codename.."/cs/sm-v-marnost.ogg", "font_big", "Je to všechno marnost, jako ta medúza.")
+
+dialog_addDialog("sm-m-proc", "en", "", "font_small", "Why?")
+dialog_addDialog("sm-m-proc", "cs", "sound/"..codename.."/cs/sm-m-proc.ogg", "font_small", "Proč?")
+
+dialog_addDialog("sm-v-podivej", "en", "", "font_big", "Just look at it. It keeps running forward but stays in one place. Isn`t it the most fitting example of what I was speaking about?")
+dialog_addDialog("sm-v-podivej", "cs", "sound/"..codename.."/cs/sm-v-podivej.ogg", "font_big", "Podívej se na ni. Běží stále vpřed a zůstává na místě. Není to snad nejkrásnější příklad toho, o čem jsem mluvil?")
+
+dialog_addDialog("sm-x-meduza", "en", "", "font_magenta", "I am jogging to lose weight, you fools!")
+dialog_addDialog("sm-x-meduza", "cs", "sound/"..codename.."/cs/sm-x-meduza.ogg", "font_magenta", "Běhám, abych zhubla, vy troubové!")
+
+dialog_addDialog("sm-x-tiktak", "en", "sound/"..codename.."/en/sm-x-tiktak.ogg")
+
+
