a7e5d3233c369e3c5f2582e6fd5d51e45e624f32
fidlej <fidlej>
2004-07-15 22:02:18 +0000

    Added level cabin2.

---
+++ script/cabin2/dialogs.lua
@@ -0,0 +1,89 @@
+dialog_addDialog("k1-pap-3xkruty", "en", "", "font_parrot", "Crrruel, crrruel, crrruel Captain Silverrrr...")
+dialog_addDialog("k1-pap-3xkruty", "cs", "sound/"..codename.."/cs/k1-pap-3xkruty.ogg", "font_parrot", "Krrrutý, krrrutý, krrrutý kapitán Silverrrr...")
+
+dialog_addDialog("k1-pap-kruty", "en", "", "font_parrot", "Crrruel Captain Silverrrr...")
+dialog_addDialog("k1-pap-kruty", "cs", "sound/"..codename.."/cs/k1-pap-kruty.ogg", "font_parrot", "Krrrutý kapitán Silverrrr...")
+
+dialog_addDialog("k1-pap-kruci", "en", "", "font_parrot", "Good grrrief!")
+dialog_addDialog("k1-pap-kruci", "cs", "sound/"..codename.."/cs/k1-pap-kruci.ogg", "font_parrot", "Krrruci!")
+
+dialog_addDialog("k1-pap-problem", "en", "", "font_parrot", "Trrricky prrroblem, isn`t it?")
+dialog_addDialog("k1-pap-problem", "cs", "sound/"..codename.."/cs/k1-pap-problem.ogg", "font_parrot", "Zatrrraceny prrroblém, není-liž prrravda?")
+
+dialog_addDialog("k1-pap-sakris", "en", "", "font_parrot", "Darrrn!")
+dialog_addDialog("k1-pap-sakris", "cs", "sound/"..codename.."/cs/k1-pap-sakris.ogg", "font_parrot", "Sákrrriš")
+
+dialog_addDialog("k1-pap-sucharek", "en", "", "font_parrot", "Polly wants a crrracker!")
+dialog_addDialog("k1-pap-sucharek", "cs", "sound/"..codename.."/cs/k1-pap-sucharek.ogg", "font_parrot", "Lórrra chce suchárrrek!")
+
+dialog_addDialog("k1-pap-trhnisi", "en", "", "font_parrot", "Beat it, sirrr...")
+dialog_addDialog("k1-pap-trhnisi", "cs", "sound/"..codename.."/cs/k1-pap-trhnisi.ogg", "font_parrot", "Trrrhni si...")
+
+dialog_addDialog("ka2-v-nekde", "en", "", "font_big", "Didn`t we see this somewhere already?")
+dialog_addDialog("ka2-v-nekde", "cs", "sound/"..codename.."/cs/ka2-v-nekde.ogg", "font_big", "To jsme už někde viděli, ne?")
+
+dialog_addDialog("ka2-m-kdepak", "en", "", "font_small", "No, you`re wrong. This cabin is one hundred and forty four squares larger...")
+dialog_addDialog("ka2-m-kdepak", "cs", "sound/"..codename.."/cs/ka2-m-kdepak.ogg", "font_small", "Kdepak, tahle kajuta je o sto čtyřicet čtyři políček větší...")
+
+dialog_addDialog("ka2-v-papousek", "en", "", "font_big", "Look at that parrot!!!")
+dialog_addDialog("ka2-v-papousek", "cs", "sound/"..codename.."/cs/ka2-v-papousek.ogg", "font_big", "Podívej, ten papoušek!!!")
+
+dialog_addDialog("ka2-m-kostra", "en", "", "font_small", "So what, a normal skeleton.")
+dialog_addDialog("ka2-m-kostra", "cs", "sound/"..codename.."/cs/ka2-m-kostra.ogg", "font_small", "No co, obyčejná kostřička.")
+
+dialog_addDialog("ka2-m-posledni", "en", "", "font_small", "Did you see it? This room is almost the last one. Could this really be that treasure?")
+dialog_addDialog("ka2-m-posledni", "cs", "sound/"..codename.."/cs/ka2-m-posledni.ogg", "font_small", "Hele, tahle místnost už je skoro poslední v řadě. Že by v té truhle byl přece jenom ten poklad?")
+
+dialog_addDialog("ka2-v-mapa0", "en", "", "font_big", "And what would be in the other two?")
+dialog_addDialog("ka2-v-mapa0", "cs", "sound/"..codename.."/cs/ka2-v-mapa0.ogg", "font_big", "Co by bylo v těch dalších dvou?")
+
+dialog_addDialog("ka2-v-mapa1", "en", "", "font_big", "They told us clearly to find the map. Nobody mentioned a treasure.")
+dialog_addDialog("ka2-v-mapa1", "cs", "sound/"..codename.."/cs/ka2-v-mapa1.ogg", "font_big", "Říkali nám jasně: najděte mapu. O pokladu řeč nebyla.")
+
+dialog_addDialog("ka2-v-mapa2", "en", "", "font_big", "Don`t be so annoying. The authors of the game would surely let the player know.")
+dialog_addDialog("ka2-v-mapa2", "cs", "sound/"..codename.."/cs/ka2-v-mapa2.ogg", "font_big", "Neotravuj pořád. Autoři hry jistě nenechají hráče v nejistotě!")
+
+dialog_addDialog("ka2-m-chapadlo", "en", "", "font_small", "This octopus has unpleasantly knotted tentacles. I won`t fit between them.")
+dialog_addDialog("ka2-m-chapadlo", "cs", "sound/"..codename.."/cs/ka2-m-chapadlo.ogg", "font_small", "Ta chobotnice má nepříjemně zasukovaná chapadla. Nevejdu se mezi ně.")
+
+dialog_addDialog("ka2-v-fik", "en", "", "font_big", "... so what if we cut one off?!")
+dialog_addDialog("ka2-v-fik", "cs", "sound/"..codename.."/cs/ka2-v-fik.ogg", "font_big", "... a co kdybychom jí nějaké ufikli?!")
+
+dialog_addDialog("ka2-v-napad", "en", "", "font_big", "I am sorry, it was just an idea.")
+dialog_addDialog("ka2-v-napad", "cs", "sound/"..codename.."/cs/ka2-v-napad.ogg", "font_big", "Promiň, to byl jen takový nápad.")
+
+dialog_addDialog("ka2-m-hej", "en", "", "font_small", "Little octopus, couldn`t you move away a little bit, please?")
+dialog_addDialog("ka2-m-hej", "cs", "sound/"..codename.."/cs/ka2-m-hej.ogg", "font_small", "Hej, chobotničko, nemohla bys kousek uhnout?")
+
+dialog_addDialog("ka2-m-diky", "en", "", "font_small", "Hmm... thanks a lot.")
+dialog_addDialog("ka2-m-diky", "cs", "sound/"..codename.."/cs/ka2-m-diky.ogg", "font_small", "Hmmm... tak díky.")
+
+dialog_addDialog("ka2-v-kostry", "en", "", "font_big", "Skeletons seem to be a favorite motif of this game authors.")
+dialog_addDialog("ka2-v-kostry", "cs", "sound/"..codename.."/cs/ka2-v-kostry.ogg", "font_big", "Kostry jsou asi oblíbeným motivem autorů této hry.")
+
+dialog_addDialog("ka2-m-patrne", "en", "", "font_small", "Who are probably living on land. These skeletons are visible proof of their effort to stuff the ocean with all the things that don`t belong there.")
+dialog_addDialog("ka2-m-patrne", "cs", "sound/"..codename.."/cs/ka2-m-patrne.ogg", "font_small", "Patrně jsou to suchozemci. Ty kostry jsou zřejmým důsledkem snahy nacpat pod vodu co nejvíce tvorů, kteří tam nemají co dělat.")
+
+dialog_addDialog("ka2-v-myslet", "en", "", "font_big", "Let`s try to think. Can`t we take our solution of that previous cabin and modify it a little?")
+dialog_addDialog("ka2-v-myslet", "cs", "sound/"..codename.."/cs/ka2-v-myslet.ogg", "font_big", "Zkusme se zamyslet. Nedalo by se nějak vyjít z toho, jak jsme projeli tu předchozí kajutu?")
+
+dialog_addDialog("ka2-m-tezko", "en", "", "font_small", "Hmm... I don`t think so.")
+dialog_addDialog("ka2-m-tezko", "cs", "sound/"..codename.."/cs/ka2-m-tezko.ogg", "font_small", "Hmmm... těžko.")
+
+dialog_addDialog("ka2-m-svitit", "en", "", "font_small", "So, let`s shed some light on it.")
+dialog_addDialog("ka2-m-svitit", "cs", "sound/"..codename.."/cs/ka2-m-svitit.ogg", "font_small", "Tak si na to posvítíme.")
+
+dialog_addDialog("ka2-v-hrbet", "en", "", "font_big", "My back aches.")
+dialog_addDialog("ka2-v-hrbet", "cs", "sound/"..codename.."/cs/ka2-v-hrbet.ogg", "font_big", "Už mě bolí hřbet.")
+
+dialog_addDialog("k1-chob-1", "en", "sound/"..codename.."/en/k1-chob-1.ogg")
+
+dialog_addDialog("k1-chob-2", "en", "sound/"..codename.."/en/k1-chob-2.ogg")
+
+dialog_addDialog("k1-chob-3", "en", "sound/"..codename.."/en/k1-chob-3.ogg")
+
+dialog_addDialog("k1-chob-p", "en", "sound/"..codename.."/en/k1-chob-p.ogg")
+
+dialog_addDialog("k1-x-vrz", "en", "sound/"..codename.."/en/k1-x-vrz.ogg")
+
+
