620d742ccb7eeb43282a9b481d4b325baaba31bf
ronnystandtke <ronnystandtke>
2004-07-15 17:15:25 +0000

    added German translation

--- script/start/dialogs.lua
+++ script/start/dialogs.lua
@@ -1,137 +1,173 @@
 dialog_addDialog("1st-m-cotobylo", "en", "", "font_small", "What was that?")
 dialog_addDialog("1st-m-cotobylo", "cs", "sound/"..codename.."/cs/1st-m-cotobylo.ogg", "font_small", "Co to bylo?")
 dialog_addDialog("1st-m-cotobylo", "fr", "", "font_small", "Qu'est ce que c'est ?")
+dialog_addDialog("1st-m-cotobylo", "de", "", "font_small", "Was war das?")
 
 dialog_addDialog("1st-v-netusim", "en", "", "font_big", "I have no idea.")
 dialog_addDialog("1st-v-netusim", "cs", "sound/"..codename.."/cs/1st-v-netusim.ogg", "font_big", "Nemám tušení.")
 dialog_addDialog("1st-v-netusim", "fr", "", "font_big", "Aucune idée.")
+dialog_addDialog("1st-v-netusim", "de", "", "font_big", "Keine Ahnung.")
 
 dialog_addDialog("1st-v-ven", "en", "", "font_big", "We should go and have a look outside.")
 dialog_addDialog("1st-v-ven", "cs", "sound/"..codename.."/cs/1st-v-ven.ogg", "font_big", "Měli bychom se podívat ven.")
 dialog_addDialog("1st-v-ven", "fr", "", "font_big", "Nous devrions aller voir dehors.")
+dialog_addDialog("1st-v-ven", "de", "", "font_big", "Wir sollten einen Blick nach draußen werfen.")
+dialog_addDialog("1st-v-ven", "de_CH", "", "font_big", "Wir sollten einen Blick nach draussen werfen.")
 
 dialog_addDialog("1st-m-pockej", "en", "", "font_small", "Wait! I`m going with you.")
 dialog_addDialog("1st-m-pockej", "cs", "sound/"..codename.."/cs/1st-m-pockej.ogg", "font_small", "Počkej na mě, jdu s tebou!")
 dialog_addDialog("1st-m-pockej", "fr", "", "font_small", "Attends ! Je viens avec toi")
+dialog_addDialog("1st-m-pockej", "de", "", "font_small", "Warte! Ich komme mit.")
 
 dialog_addDialog("1st-m-proc", "en", "", "font_small", "Why isn`t anything happening?")
 dialog_addDialog("1st-m-proc", "cs", "sound/"..codename.."/cs/1st-m-proc.ogg", "font_small", "Proč se nic neděje?")
 dialog_addDialog("1st-m-proc", "fr", "", "font_small", "Pourquoi rien ne se passe ?")
+dialog_addDialog("1st-m-proc", "de", "", "font_small", "Warum passiert nichts?")
 
 dialog_addDialog("1st-m-hej", "en", "", "font_small", "Hey, player!")
 dialog_addDialog("1st-m-hej", "cs", "sound/"..codename.."/cs/1st-m-hej.ogg", "font_small", "Hej, hráči!")
 dialog_addDialog("1st-m-hej", "fr", "", "font_small", "Hé, Joueur !")
+dialog_addDialog("1st-m-hej", "de", "", "font_small", "He, Spieler!")
 
 dialog_addDialog("1st-v-navod1", "en", "", "font_big", "You can control us with arrow keys and use the space bar to switch fish.")
 dialog_addDialog("1st-v-navod1", "cs", "sound/"..codename.."/cs/1st-v-navod1.ogg", "font_big", "Můžeš nás ovládat kurzorovými šipkami a mezerníkem mezi námi přepínat.")
 dialog_addDialog("1st-v-navod1", "fr", "", "font_big", "Tu peux nous contrôler avec les flèches, et utiliser la barre espace pour changer de poisson.")
+dialog_addDialog("1st-v-navod1", "de", "", "font_big", "Du kannst uns mit den Pfeiltasten steuern und mit der Leertaste zwischen uns umschalten.")
 
 dialog_addDialog("1st-m-navod2", "en", "", "font_small", "Or you can use the keyboard -- you can see the keys on the control panel.")
 dialog_addDialog("1st-m-navod2", "cs", "sound/"..codename.."/cs/1st-m-navod2.ogg", "font_small", "Také můžeš použít přímé ovládání - klávesy jsou napsány na ovládacím panelu.")
 dialog_addDialog("1st-m-navod2", "fr", "", "font_small", "Ou tu peux utiliser le clavier -- Les touches sont indiquées dans le panneau de contrôle.")
+dialog_addDialog("1st-m-navod2", "de", "", "font_small", "Oder Du kannst die Tastatur benutzen -- Du kannst die Tasten auf dem Schaltpult sehen.")
 
 dialog_addDialog("1st-v-navod3", "en", "", "font_big", "There you can also see which one of us is presently active.")
 dialog_addDialog("1st-v-navod3", "cs", "sound/"..codename.."/cs/1st-v-navod3.ogg", "font_big", "Tam také vidíš, kdo z nás je zrovna aktivní.")
 dialog_addDialog("1st-v-navod3", "fr", "", "font_big", "Tu peux aussi voir quel poisson de nous deux est actif.")
+dialog_addDialog("1st-v-navod3", "de", "", "font_big", "Dort kannst Du auch sehen, wer von uns gerade ausgewählt ist.")
 
 dialog_addDialog("1st-m-navod4", "en", "", "font_small", "Or you can control us with the mouse -- left-click somewhere and...")
 dialog_addDialog("1st-m-navod4", "cs", "sound/"..codename.."/cs/1st-m-navod4.ogg", "font_small", "A nebo myškou - někam ukaž levým tlačítkem a když tam můžeme dojet...")
 dialog_addDialog("1st-m-navod4", "fr", "", "font_small", "Ou tu peux nous contrôler à la souris -- Clique quelque part et ...")
+dialog_addDialog("1st-m-navod4", "de", "", "font_small", "Oder Du kannst uns mit der Maus steuern -- Klicke mit der linken Taste irgendwohin und ...")
 
 dialog_addDialog("1st-v-navod5", "en", "", "font_big", "...if we can get there without moving anything...")
 dialog_addDialog("1st-v-navod5", "cs", "sound/"..codename.."/cs/1st-v-navod5.ogg", "font_big", "...aniž bychom něčím hýbali...")
 dialog_addDialog("1st-v-navod5", "fr", "", "font_big", "... si nous pouvons y aller sans déplacer d'objet...")
+dialog_addDialog("1st-v-navod5", "de", "", "font_big", "...wenn wir dorthin kommen, ohne etwas zu bewegen...")
 
 dialog_addDialog("1st-m-navod6", "en", "", "font_small", "...we`ll follow your clicks.")
 dialog_addDialog("1st-m-navod6", "cs", "sound/"..codename.."/cs/1st-m-navod6.ogg", "font_small", "...tak tam jedeme.")
 dialog_addDialog("1st-m-navod6", "fr", "", "font_small", "... nous suivrons tes cliques.")
+dialog_addDialog("1st-m-navod6", "de", "", "font_small", "...werden wir Deinen Klicks folgen.")
 
 dialog_addDialog("1st-v-navod7", "en", "", "font_big", "And when you right-click somewhere, we`ll try to get there, pushing aside all obstacles in our way.")
 dialog_addDialog("1st-v-navod7", "cs", "sound/"..codename.."/cs/1st-v-navod7.ogg", "font_big", "A když někam klikneš pravým, tak tím směrem zkrátka zkusíme jet, překážky nepřekážky...")
 dialog_addDialog("1st-v-navod7", "fr", "", "font_big", "Et si tu cliques sur le bouton droit quelque part, nous essaierons d'y parvenir en poussant tous les objets devant nous.")
+dialog_addDialog("1st-v-navod7", "de", "", "font_big", "Und wenn Du irgendwo mit der rechten Maustaste hinklickst, versuchen wir dorthin zu kommen und alle Hinernisse aus dem Weg zu schieben.")
 
 dialog_addDialog("1st-m-navod8", "en", "", "font_small", "Your goal should be to get both of us outside.")
 dialog_addDialog("1st-m-navod8", "cs", "sound/"..codename.."/cs/1st-m-navod8.ogg", "font_small", "No a tvým cílem by mělo být dostat nás oba ven.")
 dialog_addDialog("1st-m-navod8", "fr", "", "font_small", "Ton but est de nous faire sortir tous les deux.")
+dialog_addDialog("1st-m-navod8", "de", "", "font_small", "Dein Ziel sollte sein, uns beide nach draußen zu bekommen.")
+dialog_addDialog("1st-m-navod8", "de_CH", "", "font_small", "Dein Ziel sollte sein, uns beide nach draussen zu bekommen.")
 
 dialog_addDialog("1st-v-davej", "en", "", "font_big", "And be careful not to hurt us. If you drop something on us or...")
 dialog_addDialog("1st-v-davej", "cs", "sound/"..codename.."/cs/1st-v-davej.ogg", "font_big", "A dávej pozor, ať nám neublížíš. Když nám něco hodíš na hlavu nebo...")
 dialog_addDialog("1st-v-davej", "fr", "", "font_big", "Attention à ne pas nous blesser. Si tu nous fait tomber quelque chose dessus ou ...")
+dialog_addDialog("1st-v-davej", "de", "", "font_big", "Und sei vorsichtig, uns dabei nicht wehzutun. Wenn Du etwas auf uns fallenlässt...")
 
 dialog_addDialog("1st-m-nechtoho", "en", "", "font_small", "Oh, stop it. We`ll tell him in the next level.")
 dialog_addDialog("1st-m-nechtoho", "cs", "sound/"..codename.."/cs/1st-m-nechtoho.ogg", "font_small", "Už toho nech, to mu řekneme v příští místnosti.")
 dialog_addDialog("1st-m-nechtoho", "fr", "", "font_small", "Oh, arrête. Nous lui dirons dans le prochain niveau.")
+dialog_addDialog("1st-m-nechtoho", "de", "", "font_small", "Ach hör auf. Das werden wir ihm in der nächsten Ebene sagen.")
 
 dialog_addDialog("1st-v-takdobre", "en", "", "font_big", "Okay.")
 dialog_addDialog("1st-v-takdobre", "cs", "sound/"..codename.."/cs/1st-v-takdobre.ogg", "font_big", "Tak dobře.")
 dialog_addDialog("1st-v-takdobre", "fr", "", "font_big", "D'accord.")
+dialog_addDialog("1st-v-takdobre", "de", "", "font_big", "Gut.")
 
 dialog_addDialog("1st-m-neprojedu", "en", "", "font_small", "I can`t get through here. That steel cylinder is in the way.")
 dialog_addDialog("1st-m-neprojedu", "cs", "sound/"..codename.."/cs/1st-m-neprojedu.ogg", "font_small", "Tudy neprojedu. Zavazí mi ten ocelový válec.")
 dialog_addDialog("1st-m-neprojedu", "fr", "", "font_small", "Je ne peux pas sortir d'ici. Ce cylindre en acier bloque le chemin.")
+dialog_addDialog("1st-m-neprojedu", "de", "", "font_small", "Ich kann hier nicht durch. Dieser Stahlzylinder ist im Weg.")
 
 dialog_addDialog("1st-m-nepohnu", "en", "", "font_small", "I can`t move that cylinder. Can you help me?")
 dialog_addDialog("1st-m-nepohnu", "cs", "sound/"..codename.."/cs/1st-m-nepohnu.ogg", "font_small", "S tou ocelí nepohnu. Nechceš mi pomoct?")
 dialog_addDialog("1st-m-nepohnu", "fr", "", "font_small", "Je ne pas bouger ce cylindre. Peux-tu m'aider ?")
+dialog_addDialog("1st-m-nepohnu", "de", "", "font_small", "Ich kann diesen Zylinder nicht bewegen. Kannst Du mir helfen?")
 
 dialog_addDialog("1st-v-takukaz", "en", "", "font_big", "No problem...")
 dialog_addDialog("1st-v-takukaz", "cs", "sound/"..codename.."/cs/1st-v-takukaz.ogg", "font_big", "Tak ukaž...")
 dialog_addDialog("1st-v-takukaz", "fr", "", "font_big", "Pas de problème...")
+dialog_addDialog("1st-v-takukaz", "de", "", "font_big", "Kein Problem...")
 
 dialog_addDialog("1st-m-hmmm", "en", "", "font_small", "Wow, you moved it! I could never do that!")
 dialog_addDialog("1st-m-hmmm", "cs", "sound/"..codename.."/cs/1st-m-hmmm.ogg", "font_small", "Hmmm... Pohneš s tím. To já bych nedokázala!")
 dialog_addDialog("1st-m-hmmm", "fr", "", "font_small", "Wow, Tu le déplaces ! Je ne pourrais jamais faire ça !")
+dialog_addDialog("1st-m-hmmm", "de", "", "font_small", "Toll, Du hast ihn bewegt! Das könnte ich nie!")
 
 dialog_addDialog("1st-m-diky", "en", "", "font_small", "Thanks, now I can go with you.")
 dialog_addDialog("1st-m-diky", "cs", "sound/"..codename.."/cs/1st-m-diky.ogg", "font_small", "Díky, už můžu jít s tebou.")
 dialog_addDialog("1st-m-diky", "fr", "", "font_small", "Merci, maintenant je peux partir avec toi.")
+dialog_addDialog("1st-m-diky", "de", "", "font_small", "Danke, jetzt kann ich mit Dir gehen.")
 
 dialog_addDialog("1st-v-nemuzu", "en", "", "font_big", "Darn it, I can`t get through.")
 dialog_addDialog("1st-v-nemuzu", "cs", "sound/"..codename.."/cs/1st-v-nemuzu.ogg", "font_big", "Zatraceně, nemůžu tudy prolézt.")
 dialog_addDialog("1st-v-nemuzu", "fr", "", "font_big", "Malheur, je ne peux pas passer.")
+dialog_addDialog("1st-v-nemuzu", "de", "", "font_big", "Verflixt, ich komme nicht durch.")
 
 dialog_addDialog("1st-v-pribral", "en", "", "font_big", "I must have gained some weight.")
 dialog_addDialog("1st-v-pribral", "cs", "sound/"..codename.."/cs/1st-v-pribral.ogg", "font_big", "Nějak jsem asi přibral.")
 dialog_addDialog("1st-v-pribral", "fr", "", "font_big", "J'ai du prendre du poids.")
+dialog_addDialog("1st-v-pribral", "de", "", "font_big", "Ich muss zugenommen haben.")
 
 dialog_addDialog("1st-v-posunout", "en", "", "font_big", "Could you push the chair a little bit to the left?")
 dialog_addDialog("1st-v-posunout", "cs", "sound/"..codename.."/cs/1st-v-posunout.ogg", "font_big", "Nemůžeš posunout tu židli kousek vlevo?")
 dialog_addDialog("1st-v-posunout", "fr", "", "font_big", "Peux-tu pousser un petit peu la chaise sur la gauche ?")
+dialog_addDialog("1st-v-posunout", "de", "", "font_big", "Könntest Du den Stuhl ein wenig nach links schieben?")
 
 dialog_addDialog("1st-v-chyba", "en", "", "font_big", "Ooops! That was a mistake. Both of us have to be able to leave in order to continue.")
 dialog_addDialog("1st-v-chyba", "cs", "sound/"..codename.."/cs/1st-v-chyba.ogg", "font_big", "Tak to byla chyba. Správné řešení musí být takové, abychom odjeli oba.")
 dialog_addDialog("1st-v-chyba", "fr", "", "font_big", "Ooops ! Quelle erreur. Nous devons sortir tous les deux pour pouvoir continuer.")
+dialog_addDialog("1st-v-chyba", "de", "", "font_big", "Ups! Das war ein Fehler. Wir müssen beide rauskommen, um weiterzumachen.")
 
 dialog_addDialog("1st-v-nedostanu", "en", "", "font_big", "There`s no way I can get out now. You`ll have to restart the level.")
 dialog_addDialog("1st-v-nedostanu", "cs", "sound/"..codename.."/cs/1st-v-nedostanu.ogg", "font_big", "Já už se teď ven nedostanu. Musíš celou místnost hrát znovu.")
 dialog_addDialog("1st-v-nedostanu", "fr", "", "font_big", "Aucun moyen de sortir maintenant. Tu dois refaire le niveau.")
+dialog_addDialog("1st-v-nedostanu", "de", "", "font_big", "Ich komme jetzt nicht mehr raus. Du musst die Ebene neu starten.")
 
 dialog_addDialog("1st-v-stiskni", "en", "", "font_big", "You have to press the long arrow on your keyboard.")
 dialog_addDialog("1st-v-stiskni", "cs", "sound/"..codename.."/cs/1st-v-stiskni.ogg", "font_big", "Stiskni tlačítko zpětného výmazu.")
 dialog_addDialog("1st-v-stiskni", "fr", "", "font_big", "Tu dois appuyer sur la longue flèche du clavier.")
+dialog_addDialog("1st-v-stiskni", "de", "", "font_big", "Du musst den langen Pfeil auf Deiner Tastatur drücken.")
 
 dialog_addDialog("1st-m-backspace", "en", "", "font_small", "Humans call it a backspace key.")
 dialog_addDialog("1st-m-backspace", "cs", "sound/"..codename.."/cs/1st-m-backspace.ogg", "font_small", "On myslí backspace.")
 dialog_addDialog("1st-m-backspace", "fr", "", "font_small", "Les Hommes l'appelle touche retour.")
+dialog_addDialog("1st-m-backspace", "de", "", "font_small", "Menschen nennen es die Rücktaste.")
 
 dialog_addDialog("1st-v-jedno", "en", "", "font_big", "Whatever. It was funnier in Czech.")
 dialog_addDialog("1st-v-jedno", "cs", "sound/"..codename.."/cs/1st-v-jedno.ogg", "font_big", "To je jedno.")
 dialog_addDialog("1st-v-jedno", "fr", "", "font_big", "Oui, c'est bien plus marrant en Tchèque.")
+dialog_addDialog("1st-v-jedno", "de", "", "font_big", "Egal. Auf Tschechisch war es lustiger.")
 
 dialog_addDialog("1st-v-najit", "en", "", "font_big", "The restart option is also located on the control panel.")
 dialog_addDialog("1st-v-najit", "cs", "sound/"..codename.."/cs/1st-v-najit.ogg", "font_big", "Můžeš si to najít i na ovládacím panelu.")
 dialog_addDialog("1st-v-najit", "fr", "", "font_big", "L'option redémarrage se trouve aussi dans le panneau de contrôle.")
+dialog_addDialog("1st-v-najit", "de", "", "font_big", "Die Möglichkeit zum Neustart gibt es auch auf dem Schaltpult.")
 
 dialog_addDialog("1st-m-pokud", "en", "", "font_small", "If you think you would rather restart the level, no problem.")
 dialog_addDialog("1st-m-pokud", "cs", "sound/"..codename.."/cs/1st-m-pokud.ogg", "font_small", "Pokud máš pocit, že jsi už řešení zkazil, nevadí.")
 dialog_addDialog("1st-m-pokud", "fr", "", "font_small", "Si tu penses qu'il est préférable de refaire le niveau, pas de soucis.")
+dialog_addDialog("1st-m-pokud", "de", "", "font_small", "Wenn Du die Ebene lieber neustarten möchtest, kein Problem.")
 
 dialog_addDialog("1st-v-znovu", "en", "", "font_big", "You just have to press the long arrow on your keyboard.")
 dialog_addDialog("1st-v-znovu", "cs", "sound/"..codename.."/cs/1st-v-znovu.ogg", "font_big", "Chceš-li to zkustit znovu, stiskni tlačítko zpětného výmazu.")
 dialog_addDialog("1st-v-znovu", "fr", "", "font_big", "Tu n'as qu'à appuyer sur la longue flèche du clavier.")
+dialog_addDialog("1st-v-znovu", "de", "", "font_big", "Du musst nur den langen Pfeil auf Deiner Tastatur drücken.")
 
 dialog_addDialog("1st-x-ocel", "en", "", "font_darkgrey", "I`m a heavy steel cylinder. It`s not easy to move me around. Such a tiny fish, like the orange one over there, simply has no chance. I could smash her without any problem.")
 dialog_addDialog("1st-x-ocel", "cs", "sound/"..codename.."/cs/1st-x-ocel.ogg", "font_darkgrey", "Jsem těžký ocelový válec. Pohnout se mnou není nic jednoduchého. Taková malá rybka, jako je támhleta oranžová, vůbec nemá šanci. Rozmáčknul bych ji jedna dvě.")
 dialog_addDialog("1st-x-ocel", "fr", "", "font_darkgrey", "Je suis un lourd cylindre en acier. Il n'est pas facile de me déplacer. Un petit poisson comme l'orange, n'a aucune chance. Je peux l'écraser sans peine.")
+dialog_addDialog("1st-x-ocel", "de", "", "font_darkgrey", "Ich bin ein schwerer Stahlzylinder. Es ist nicht leicht, mich zu bewegen. So ein winziger Fisch, wie der orange da drüben, hat einfach keine Chance. Ich würde sie problemlos zerdrücken.")
 
 
