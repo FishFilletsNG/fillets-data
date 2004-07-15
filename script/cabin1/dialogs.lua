5f0727262c9b5feaabbe0498ab0a32cf6cbd1e68
fidlej <fidlej>
2004-07-15 21:36:22 +0000

    Added level cabin1.

---
+++ script/cabin1/dialogs.lua
@@ -0,0 +1,122 @@
+dialog_addDialog("k1-pap-3xkruty", "en", "", "font_parrot", "Crrruel, crrruel, crrruel Captain Silverrrr...")
+dialog_addDialog("k1-pap-3xkruty", "cs", "sound/"..codename.."/cs/k1-pap-3xkruty.ogg", "font_parrot", "Krrrutý, krrrutý, krrrutý kapitán Silverrrr...")
+
+dialog_addDialog("k1-pap-karamba", "en", "", "font_parrot", "Ay... caramba!!!")
+dialog_addDialog("k1-pap-karamba", "cs", "sound/"..codename.."/cs/k1-pap-karamba.ogg", "font_parrot", "Oj... karamba!!!")
+
+dialog_addDialog("k1-pap-kruty", "en", "", "font_parrot", "Crrruel Captain Silverrrr...")
+dialog_addDialog("k1-pap-kruty", "cs", "sound/"..codename.."/cs/k1-pap-kruty.ogg", "font_parrot", "Krrrutý kapitán Silverrrr...")
+
+dialog_addDialog("k1-pap-drahokamy", "en", "", "font_parrot", "Trrreasurre, you foools...")
+dialog_addDialog("k1-pap-drahokamy", "cs", "sound/"..codename.."/cs/k1-pap-drahokamy.ogg", "font_parrot", "Drrrahokamy, trrroubové...")
+
+dialog_addDialog("k1-pap-kruci", "en", "", "font_parrot", "Good grrrief!")
+dialog_addDialog("k1-pap-kruci", "cs", "sound/"..codename.."/cs/k1-pap-kruci.ogg", "font_parrot", "Krrruci!")
+
+dialog_addDialog("k1-pap-drahousek", "en", "", "font_parrot", "That`s rrrrrridiculous! Polly is his masterrr`s darrrling.")
+dialog_addDialog("k1-pap-drahousek", "cs", "sound/"..codename.."/cs/k1-pap-drahousek.ogg", "font_parrot", "To si vyprrrošuju, Lóra je pánův drrrahoušek.")
+
+dialog_addDialog("k1-pap-nestrkej", "en", "", "font_parrot", "Don`t ppppush me!")
+dialog_addDialog("k1-pap-nestrkej", "cs", "sound/"..codename.."/cs/k1-pap-nestrkej.ogg", "font_parrot", "Nestrrrkej do mě!")
+
+dialog_addDialog("k1-pap-noproto", "en", "", "font_parrot", "That`s betterrr!")
+dialog_addDialog("k1-pap-noproto", "cs", "sound/"..codename.."/cs/k1-pap-noproto.ogg", "font_parrot", "No prroto!")
+
+dialog_addDialog("k1-pap-prcice", "en", "", "font_parrot", "And it`s currrtains forrr you!")
+dialog_addDialog("k1-pap-prcice", "cs", "sound/"..codename.."/cs/k1-pap-prcice.ogg", "font_parrot", "A jste v prrrčicích!")
+
+dialog_addDialog("k1-pap-prekazet", "en", "", "font_parrot", "I`m going to be in yourrr way herrre.")
+dialog_addDialog("k1-pap-prekazet", "cs", "sound/"..codename.."/cs/k1-pap-prekazet.ogg", "font_parrot", "Tady vám budu strrrašně překážet.")
+
+dialog_addDialog("k1-pap-problem", "en", "", "font_parrot", "Trrricky prrroblem, isn`t it?")
+dialog_addDialog("k1-pap-problem", "cs", "sound/"..codename.."/cs/k1-pap-problem.ogg", "font_parrot", "Zatrrraceny prrroblém, není-liž prrravda?")
+
+dialog_addDialog("k1-pap-sakris", "en", "", "font_parrot", "Dang it!")
+dialog_addDialog("k1-pap-sakris", "cs", "sound/"..codename.."/cs/k1-pap-sakris.ogg", "font_parrot", "Sákrrriš")
+
+dialog_addDialog("k1-pap-sestlustej", "en", "", "font_parrot", "You`rrre too fat, you can`t make it thrrrrough.")
+dialog_addDialog("k1-pap-sestlustej", "cs", "sound/"..codename.."/cs/k1-pap-sestlustej.ogg", "font_parrot", "Jseš tlustej, neprrrojedeš...")
+
+dialog_addDialog("k1-pap-sucharek", "en", "", "font_parrot", "Polly wants a crrracker!")
+dialog_addDialog("k1-pap-sucharek", "cs", "sound/"..codename.."/cs/k1-pap-sucharek.ogg", "font_parrot", "Lórrra chce suchárrrek!")
+
+dialog_addDialog("k1-pap-trhnisi", "en", "", "font_parrot", "Beat it, sirrr...")
+dialog_addDialog("k1-pap-trhnisi", "cs", "sound/"..codename.."/cs/k1-pap-trhnisi.ogg", "font_parrot", "Trrrhni si...")
+
+dialog_addDialog("k1-pap-vodprejskni", "en", "", "font_parrot", "Leave me alone and get lost!")
+dialog_addDialog("k1-pap-vodprejskni", "cs", "sound/"..codename.."/cs/k1-pap-vodprejskni.ogg", "font_parrot", "Nevotrrravuj a vodprrrejskni!")
+
+dialog_addDialog("k1-m-copak", "en", "", "font_small", "What could be inside?")
+dialog_addDialog("k1-m-copak", "cs", "sound/"..codename.."/cs/k1-m-copak.ogg", "font_small", "Copak je asi uvnitř?")
+
+dialog_addDialog("k1-v-kdovi", "en", "", "font_big", "Who knows...")
+dialog_addDialog("k1-v-kdovi", "cs", "sound/"..codename.."/cs/k1-v-kdovi.ogg", "font_big", "Kdo ví...")
+
+dialog_addDialog("k1-m-myslis", "en", "", "font_small", "Could this be the legendary Silver`s treasure?")
+dialog_addDialog("k1-m-myslis", "cs", "sound/"..codename.."/cs/k1-m-myslis.ogg", "font_small", "Myslíš, že by to mohl být legendarní Silverův poklad?")
+
+dialog_addDialog("k1-v-bedna", "en", "", "font_big", "In such a small chest? The boss surely wouldn`t contact us for such a little case.")
+dialog_addDialog("k1-v-bedna", "cs", "sound/"..codename.."/cs/k1-v-bedna.ogg", "font_big", "V tak malé truhle? Kvůli jedné takové bedničce by nás šéf určitě nekontaktoval.")
+
+dialog_addDialog("k1-m-fuj", "en", "", "font_small", "Yuck!")
+dialog_addDialog("k1-m-fuj", "cs", "sound/"..codename.."/cs/k1-m-fuj.ogg", "font_small", "Fuj!")
+
+dialog_addDialog("k1-m-diky", "en", "", "font_small", "Thanks.")
+dialog_addDialog("k1-m-diky", "cs", "sound/"..codename.."/cs/k1-m-diky.ogg", "font_small", "Díky.")
+
+dialog_addDialog("k1-v-radose", "en", "", "font_big", "No problem.")
+dialog_addDialog("k1-v-radose", "cs", "sound/"..codename.."/cs/k1-v-radose.ogg", "font_big", "Rádo se stalo.")
+
+dialog_addDialog("k1-m-codelas", "en", "", "font_small", "What are you doing?!")
+dialog_addDialog("k1-m-codelas", "cs", "sound/"..codename.."/cs/k1-m-codelas.ogg", "font_small", "Co to děláš?!")
+
+dialog_addDialog("k1-v-promin", "en", "", "font_big", "I`m sorry. I`ll be more careful next time.")
+dialog_addDialog("k1-v-promin", "cs", "sound/"..codename.."/cs/k1-v-promin.ogg", "font_big", "Promiň, už si dám pozor...")
+
+dialog_addDialog("k1-v-opatrne", "en", "", "font_big", "Be careful! Don`t hurt it!")
+dialog_addDialog("k1-v-opatrne", "cs", "sound/"..codename.."/cs/k1-v-opatrne.ogg", "font_big", "Opatrně, ať jí neublížíš.")
+
+dialog_addDialog("k1-m-tospisona", "en", "", "font_small", "It`s more the other way around!")
+dialog_addDialog("k1-m-tospisona", "cs", "sound/"..codename.."/cs/k1-m-tospisona.ogg", "font_small", "To spíš ona mně!")
+
+dialog_addDialog("k1-m-chobotnice", "en", "", "font_small", "That`s some octopus! I wonder who`s her owner?")
+dialog_addDialog("k1-m-chobotnice", "cs", "sound/"..codename.."/cs/k1-m-chobotnice.ogg", "font_small", "To je ale chobotnice! Komupak asi patří?")
+
+dialog_addDialog("k1-v-patrila", "en", "", "font_big", "Maybe it belonged to the person who lived in this cabin.")
+dialog_addDialog("k1-v-patrila", "cs", "sound/"..codename.."/cs/k1-v-patrila.ogg", "font_big", "Možná patřila obyvateli této kajuty.")
+
+dialog_addDialog("k1-m-podivin", "en", "", "font_small", "Whoever lived here sure was weird.")
+dialog_addDialog("k1-m-podivin", "cs", "sound/"..codename.."/cs/k1-m-podivin.ogg", "font_small", "Ten, kdo tu bydlel, byl asi podivín.")
+
+dialog_addDialog("k1-v-proc", "en", "", "font_big", "Why?")
+dialog_addDialog("k1-v-proc", "cs", "sound/"..codename.."/cs/k1-v-proc.ogg", "font_big", "Proč?")
+
+dialog_addDialog("k1-m-lebku", "en", "", "font_small", "Look, he had a human skull here!")
+dialog_addDialog("k1-m-lebku", "cs", "sound/"..codename.."/cs/k1-m-lebku.ogg", "font_small", "Podívej, měl tu lidskou lebku!")
+
+dialog_addDialog("k1-v-jejeho", "en", "", "font_big", "Well, maybe it`s his own skull.")
+dialog_addDialog("k1-v-jejeho", "cs", "sound/"..codename.."/cs/k1-v-jejeho.ogg", "font_big", "Víš, možná je to ta jeho.")
+
+dialog_addDialog("k1-m-mysli", "en", "", "font_small", "You think so?")
+dialog_addDialog("k1-m-mysli", "cs", "sound/"..codename.."/cs/k1-m-mysli.ogg", "font_small", "Myslíš?")
+
+dialog_addDialog("k1-v-citis", "en", "", "font_big", "Can you sense the atmosphere of rot and decay? The sunken wreck, the parrot`s skeleton, the human skull, and the broken cradle...")
+dialog_addDialog("k1-v-citis", "cs", "sound/"..codename.."/cs/k1-v-citis.ogg", "font_big", "Cítíš tu tíživou atmosféru? Potopený vrak, kostra papouška, lidská lebka, převrácená kolébka...")
+
+dialog_addDialog("k1-m-kolebku", "en", "", "font_small", "Where do you see the cradle?")
+dialog_addDialog("k1-m-kolebku", "cs", "sound/"..codename.."/cs/k1-m-kolebku.ogg", "font_small", "Kde vidíš tu kolébku?")
+
+dialog_addDialog("k1-v-cit", "en", "", "font_big", "Oh, you don`t have any feeling for poetry at all...")
+dialog_addDialog("k1-v-cit", "cs", "sound/"..codename.."/cs/k1-v-cit.ogg", "font_big", "Ach jo, ty nemáš vůbec cit pro poezii...")
+
+dialog_addDialog("k1-chob-1", "en", "sound/"..codename.."/en/k1-chob-1.ogg")
+
+dialog_addDialog("k1-chob-2", "en", "sound/"..codename.."/en/k1-chob-2.ogg")
+
+dialog_addDialog("k1-chob-3", "en", "sound/"..codename.."/en/k1-chob-3.ogg")
+
+dialog_addDialog("k1-chob-p", "en", "sound/"..codename.."/en/k1-chob-p.ogg")
+
+dialog_addDialog("k1-x-vrz", "en", "sound/"..codename.."/en/k1-x-vrz.ogg")
+
+
