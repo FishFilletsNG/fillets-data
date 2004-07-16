2d10fd99966964d9def520ff243df96905831c93
mrpaja <mrpaja>
2004-07-16 14:43:15 +0000

    french translation update

--- script/cabin2/dialogs.lua
+++ script/cabin2/dialogs.lua
@@ -1,80 +1,106 @@
 dialog_addDialog("k1-pap-3xkruty", "en", "", "font_parrot", "Crrruel, crrruel, crrruel Captain Silverrrr...")
 dialog_addDialog("k1-pap-3xkruty", "cs", "sound/"..codename.."/cs/k1-pap-3xkruty.ogg", "font_parrot", "Krrrutý, krrrutý, krrrutý kapitán Silverrrr...")
+dialog_addDialog("k1-pap-3xkruty", "fr", "", "font_parrot", "Crrruel, crrruel, crrruel Captaine Silverrrr...")
 
 dialog_addDialog("k1-pap-kruty", "en", "", "font_parrot", "Crrruel Captain Silverrrr...")
 dialog_addDialog("k1-pap-kruty", "cs", "sound/"..codename.."/cs/k1-pap-kruty.ogg", "font_parrot", "Krrrutý kapitán Silverrrr...")
+dialog_addDialog("k1-pap-kruty", "fr", "", "font_parrot", "Crrruel Captaine Silverrrr...")
 
 dialog_addDialog("k1-pap-kruci", "en", "", "font_parrot", "Good grrrief!")
 dialog_addDialog("k1-pap-kruci", "cs", "sound/"..codename.."/cs/k1-pap-kruci.ogg", "font_parrot", "Krrruci!")
+dialog_addDialog("k1-pap-kruci", "fr", "", "font_parrot", "Bel ennnnnui !")
 
 dialog_addDialog("k1-pap-problem", "en", "", "font_parrot", "Trrricky prrroblem, isn`t it?")
 dialog_addDialog("k1-pap-problem", "cs", "sound/"..codename.."/cs/k1-pap-problem.ogg", "font_parrot", "Zatrrraceny prrroblém, není-liž prrravda?")
+dialog_addDialog("k1-pap-problem", "fr", "", "font_parrot", "Dangerrrreuse ssssituation, n'est-ce pas ?")
 
 dialog_addDialog("k1-pap-sakris", "en", "", "font_parrot", "Darrrn!")
 dialog_addDialog("k1-pap-sakris", "cs", "sound/"..codename.."/cs/k1-pap-sakris.ogg", "font_parrot", "Sákrrriš")
+dialog_addDialog("k1-pap-sakris", "fr", "", "font_parrot", "Malheurrrr!")
 
 dialog_addDialog("k1-pap-sucharek", "en", "", "font_parrot", "Polly wants a crrracker!")
 dialog_addDialog("k1-pap-sucharek", "cs", "sound/"..codename.."/cs/k1-pap-sucharek.ogg", "font_parrot", "Lórrra chce suchárrrek!")
+dialog_addDialog("k1-pap-sucharek", "fr", "", "font_parrot", "Polly veut un pétard !")
 
 dialog_addDialog("k1-pap-trhnisi", "en", "", "font_parrot", "Beat it, sirrr...")
 dialog_addDialog("k1-pap-trhnisi", "cs", "sound/"..codename.."/cs/k1-pap-trhnisi.ogg", "font_parrot", "Trrrhni si...")
+dialog_addDialog("k1-pap-trhnisi", "fr", "", "font_parrot", "Frapper le, sirrr...")
 
 dialog_addDialog("ka2-v-nekde", "en", "", "font_big", "Didn`t we see this somewhere already?")
 dialog_addDialog("ka2-v-nekde", "cs", "sound/"..codename.."/cs/ka2-v-nekde.ogg", "font_big", "To jsme už někde viděli, ne?")
+dialog_addDialog("ka2-v-nekde", "fr", "", "font_big", "Nous n'avons pas déjà vu ça quelque part ?")
 
 dialog_addDialog("ka2-m-kdepak", "en", "", "font_small", "No, you`re wrong. This cabin is one hundred and forty four squares larger...")
 dialog_addDialog("ka2-m-kdepak", "cs", "sound/"..codename.."/cs/ka2-m-kdepak.ogg", "font_small", "Kdepak, tahle kajuta je o sto čtyřicet čtyři políček větší...")
+dialog_addDialog("ka2-m-kdepak", "fr", "", "font_small", "Non, tu te trompes. Cette cabine est cent quarante quatre carrés plus large...")
 
 dialog_addDialog("ka2-v-papousek", "en", "", "font_big", "Look at that parrot!!!")
 dialog_addDialog("ka2-v-papousek", "cs", "sound/"..codename.."/cs/ka2-v-papousek.ogg", "font_big", "Podívej, ten papoušek!!!")
+dialog_addDialog("ka2-v-papousek", "fr", "", "font_big", "Regarde ce perroquet !!!")
 
 dialog_addDialog("ka2-m-kostra", "en", "", "font_small", "So what, a normal skeleton.")
 dialog_addDialog("ka2-m-kostra", "cs", "sound/"..codename.."/cs/ka2-m-kostra.ogg", "font_small", "No co, obyčejná kostřička.")
+dialog_addDialog("ka2-m-kostra", "fr", "", "font_small", "Et alors, un squelette normal.")
 
 dialog_addDialog("ka2-m-posledni", "en", "", "font_small", "Did you see it? This room is almost the last one. Could this really be that treasure?")
 dialog_addDialog("ka2-m-posledni", "cs", "sound/"..codename.."/cs/ka2-m-posledni.ogg", "font_small", "Hele, tahle místnost už je skoro poslední v řadě. Že by v té truhle byl přece jenom ten poklad?")
+dialog_addDialog("ka2-m-posledni", "fr", "", "font_small", "Tu as vu ? Cette pièce et quasiment identique à la précédente. Se pourrait-il que se soit vraiment le trésor ?")
 
 dialog_addDialog("ka2-v-mapa0", "en", "", "font_big", "And what would be in the other two?")
 dialog_addDialog("ka2-v-mapa0", "cs", "sound/"..codename.."/cs/ka2-v-mapa0.ogg", "font_big", "Co by bylo v těch dalších dvou?")
+dialog_addDialog("ka2-v-mapa0", "fr", "", "font_big", "Et qu'est qu'il y a dans les deux autres ?")
 
 dialog_addDialog("ka2-v-mapa1", "en", "", "font_big", "They told us clearly to find the map. Nobody mentioned a treasure.")
 dialog_addDialog("ka2-v-mapa1", "cs", "sound/"..codename.."/cs/ka2-v-mapa1.ogg", "font_big", "Říkali nám jasně: najděte mapu. O pokladu řeč nebyla.")
+dialog_addDialog("ka2-v-mapa1", "fr", "", "font_big", "Ils nous ont clairement dit de trouver la carte. Personne n'a parlé de trésor.")
 
 dialog_addDialog("ka2-v-mapa2", "en", "", "font_big", "Don`t be so annoying. The authors of the game would surely let the player know.")
 dialog_addDialog("ka2-v-mapa2", "cs", "sound/"..codename.."/cs/ka2-v-mapa2.ogg", "font_big", "Neotravuj pořád. Autoři hry jistě nenechají hráče v nejistotě!")
+dialog_addDialog("ka2-v-mapa2", "fr", "", "font_big", "Soit pas si contrariant. Les auteurs du jeu voulait surement que le joueur sache.")
 
 dialog_addDialog("ka2-m-chapadlo", "en", "", "font_small", "This octopus has unpleasantly knotted tentacles. I won`t fit between them.")
 dialog_addDialog("ka2-m-chapadlo", "cs", "sound/"..codename.."/cs/ka2-m-chapadlo.ogg", "font_small", "Ta chobotnice má nepříjemně zasukovaná chapadla. Nevejdu se mezi ně.")
+dialog_addDialog("ka2-m-chapadlo", "fr", "", "font_small", "Cette pieuvre a emmélé ces tentacules de manière disgracieuse. Je ne peux pas me glisser entre elles.")
 
 dialog_addDialog("ka2-v-fik", "en", "", "font_big", "... so what if we cut one off?!")
 dialog_addDialog("ka2-v-fik", "cs", "sound/"..codename.."/cs/ka2-v-fik.ogg", "font_big", "... a co kdybychom jí nějaké ufikli?!")
+dialog_addDialog("ka2-v-fik", "fr", "", "font_big", "... et si nous en coupons une ?!")
 
 dialog_addDialog("ka2-v-napad", "en", "", "font_big", "I am sorry, it was just an idea.")
 dialog_addDialog("ka2-v-napad", "cs", "sound/"..codename.."/cs/ka2-v-napad.ogg", "font_big", "Promiň, to byl jen takový nápad.")
+dialog_addDialog("ka2-v-napad", "fr", "", "font_big", "Désolé, c'était juste une idée.")
 
 dialog_addDialog("ka2-m-hej", "en", "", "font_small", "Little octopus, couldn`t you move away a little bit, please?")
 dialog_addDialog("ka2-m-hej", "cs", "sound/"..codename.."/cs/ka2-m-hej.ogg", "font_small", "Hej, chobotničko, nemohla bys kousek uhnout?")
+dialog_addDialog("ka2-m-hej", "fr", "", "font_small", "Petite pieuvre, pourrais-tu te déplacer un petit peu, s'il te plaît ?")
 
 dialog_addDialog("ka2-m-diky", "en", "", "font_small", "Hmm... thanks a lot.")
 dialog_addDialog("ka2-m-diky", "cs", "sound/"..codename.."/cs/ka2-m-diky.ogg", "font_small", "Hmmm... tak díky.")
+dialog_addDialog("ka2-m-diky", "fr", "", "font_small", "Hmm... Merci beaucoup.")
 
 dialog_addDialog("ka2-v-kostry", "en", "", "font_big", "Skeletons seem to be a favorite motif of this game authors.")
 dialog_addDialog("ka2-v-kostry", "cs", "sound/"..codename.."/cs/ka2-v-kostry.ogg", "font_big", "Kostry jsou asi oblíbeným motivem autorů této hry.")
+dialog_addDialog("ka2-v-kostry", "fr", "", "font_big", "Les squelettes semblent être un motif favori pour les auteurs de ce jeu.")
 
 dialog_addDialog("ka2-m-patrne", "en", "", "font_small", "Who are probably living on land. These skeletons are visible proof of their effort to stuff the ocean with all the things that don`t belong there.")
 dialog_addDialog("ka2-m-patrne", "cs", "sound/"..codename.."/cs/ka2-m-patrne.ogg", "font_small", "Patrně jsou to suchozemci. Ty kostry jsou zřejmým důsledkem snahy nacpat pod vodu co nejvíce tvorů, kteří tam nemají co dělat.")
+dialog_addDialog("ka2-m-patrne", "fr", "", "font_small", "Qui vivent probablement sur terre. Ces squelettes sont la preuve visible de leur effort de bourrer l'océan avec tout ce qu'ils ne veulent plus là haut.")
 
 dialog_addDialog("ka2-v-myslet", "en", "", "font_big", "Let`s try to think. Can`t we take our solution of that previous cabin and modify it a little?")
 dialog_addDialog("ka2-v-myslet", "cs", "sound/"..codename.."/cs/ka2-v-myslet.ogg", "font_big", "Zkusme se zamyslet. Nedalo by se nějak vyjít z toho, jak jsme projeli tu předchozí kajutu?")
+dialog_addDialog("ka2-v-myslet", "fr", "", "font_big", "Réfléchissons. Est-ce que nous ne pouvons pas appliquer la solution de la cabine précédente en la modifiant un peu.")
 
 dialog_addDialog("ka2-m-tezko", "en", "", "font_small", "Hmm... I don`t think so.")
 dialog_addDialog("ka2-m-tezko", "cs", "sound/"..codename.."/cs/ka2-m-tezko.ogg", "font_small", "Hmmm... těžko.")
+dialog_addDialog("ka2-m-tezko", "fr", "", "font_small", "Hmm... Je ne crois pas.")
 
 dialog_addDialog("ka2-m-svitit", "en", "", "font_small", "So, let`s shed some light on it.")
 dialog_addDialog("ka2-m-svitit", "cs", "sound/"..codename.."/cs/ka2-m-svitit.ogg", "font_small", "Tak si na to posvítíme.")
+dialog_addDialog("ka2-m-svitit", "fr", "", "font_small", "Ainsi, faisons un peu de lumière là dessus.")
 
 dialog_addDialog("ka2-v-hrbet", "en", "", "font_big", "My back aches.")
 dialog_addDialog("ka2-v-hrbet", "cs", "sound/"..codename.."/cs/ka2-v-hrbet.ogg", "font_big", "Už mě bolí hřbet.")
+dialog_addDialog("ka2-v-hrbet", "fr", "", "font_big", "Mon dos me fait mal.")
 
 dialog_addDialog("k1-chob-1", "en", "sound/"..codename.."/en/k1-chob-1.ogg")
 
--- script/society/dialogs.lua
+++ script/society/dialogs.lua
@@ -1,47 +1,62 @@
 dialog_addDialog("mik-v-sakra", "en", "", "font_big", "Dang, that snail is blocking the way.")
 dialog_addDialog("mik-v-sakra", "cs", "sound/"..codename.."/cs/mik-v-sakra.ogg", "font_big", "Sakryš, ten šnek tam hrozně zavazí.")
+dialog_addDialog("mik-v-sakra", "fr", "", "font_big", "Bon, cet escargot bloque le chemin.")
 
 dialog_addDialog("mik-v-projet", "en", "", "font_big", "I can`t swim through.")
 dialog_addDialog("mik-v-projet", "cs", "sound/"..codename.."/cs/mik-v-projet.ogg", "font_big", "Teď neprojedu.")
+dialog_addDialog("mik-v-projet", "fr", "", "font_big", "Je ne peux pas nager au travers.")
 
 dialog_addDialog("mik-m-krab", "en", "", "font_small", "Those crabs are too talkative.")
 dialog_addDialog("mik-m-krab", "cs", "sound/"..codename.."/cs/mik-m-krab.ogg", "font_small", "Ti krabi jsou nějak ukecaní.")
+dialog_addDialog("mik-m-krab", "fr", "", "font_small", "Ces crabes sont trop bavards.")
 
 dialog_addDialog("mik-m-poust", "en", "", "font_small", "They don`t look like hermits.")
 dialog_addDialog("mik-m-poust", "cs", "sound/"..codename.."/cs/mik-m-poust.ogg", "font_small", "Vůbec nevypadají na poustevníčky.")
+dialog_addDialog("mik-m-poust", "fr", "", "font_small", "Ils ne ressemblent pas à des ermites.")
 
 dialog_addDialog("mik-m-tusit", "en", "", "font_small", "I had no idea that hermit crabs could be so talkative.")
 dialog_addDialog("mik-m-tusit", "cs", "sound/"..codename.."/cs/mik-m-tusit.ogg", "font_small", "Netušila jsem, že krabi poustevníčci mohou být takhle ukecaní.")
+dialog_addDialog("mik-m-tusit", "fr", "", "font_small", "Je n'avais pas idée que des crabes reclus puissent être si bavards.")
 
 dialog_addDialog("mik-v-ticho0", "en", "", "font_big", "Shut up!")
 dialog_addDialog("mik-v-ticho0", "cs", "sound/"..codename.."/cs/mik-v-ticho0.ogg", "font_big", "Ticho tady!")
+dialog_addDialog("mik-v-ticho0", "fr", "", "font_big", "La ferme !")
 
 dialog_addDialog("mik-v-ticho1", "en", "", "font_big", "Silence!")
 dialog_addDialog("mik-v-ticho1", "cs", "sound/"..codename.."/cs/mik-v-ticho1.ogg", "font_big", "Ticho!")
+dialog_addDialog("mik-v-ticho1", "fr", "", "font_big", "Silence !")
 
 dialog_addDialog("mik-v-ticho2", "en", "", "font_big", "What is it now?!")
 dialog_addDialog("mik-v-ticho2", "cs", "sound/"..codename.."/cs/mik-v-ticho2.ogg", "font_big", "Tak co je to zase?!")
+dialog_addDialog("mik-v-ticho2", "fr", "", "font_big", "Qu'est-ce qu'il y a encore ?")
 
 dialog_addDialog("mik-v-proto", "en", "", "font_big", "That`s better.")
 dialog_addDialog("mik-v-proto", "cs", "sound/"..codename.."/cs/mik-v-proto.ogg", "font_big", "No proto.")
+dialog_addDialog("mik-v-proto", "fr", "", "font_big", "C'est mieux.")
 
 dialog_addDialog("mik-v-tak", "en", "", "font_big", "Yes.")
 dialog_addDialog("mik-v-tak", "cs", "sound/"..codename.."/cs/mik-v-tak.ogg", "font_big", "Tak.")
+dialog_addDialog("mik-v-tak", "fr", "", "font_big", "Oui.")
 
 dialog_addDialog("mik-m-nezlob", "en", "", "font_small", "Don`t be mad at them.")
 dialog_addDialog("mik-m-nezlob", "cs", "sound/"..codename.."/cs/mik-m-nezlob.ogg", "font_small", "Už se na ně nezlob.")
+dialog_addDialog("mik-m-nezlob", "fr", "", "font_small", "T'énerve pas sur eux.")
 
 dialog_addDialog("mik-m-myslit", "en", "", "font_small", "Now we can calmly ponder how to get out of here.")
 dialog_addDialog("mik-m-myslit", "cs", "sound/"..codename.."/cs/mik-m-myslit.ogg", "font_small", "Teď se v klidu zamyslíme, jak ven.")
+dialog_addDialog("mik-m-myslit", "fr", "", "font_small", "Maintenant nous pouvons calmement méditer à comment sortir d'ici.")
 
 dialog_addDialog("mik-m-proc", "en", "", "font_small", "White fish, why do you have such big eyes?")
 dialog_addDialog("mik-m-proc", "cs", "sound/"..codename.."/cs/mik-m-proc.ogg", "font_small", "Bílá rybo, proč máš tak veliké oči?")
+dialog_addDialog("mik-m-proc", "fr", "", "font_small", "Poisson blanc, pourquoi as-tu des yeux si grand ?")
 
 dialog_addDialog("mik-v-videt", "en", "", "font_big", "The better to see you with.")
 dialog_addDialog("mik-v-videt", "cs", "sound/"..codename.."/cs/mik-v-videt.ogg", "font_big", "To aby tě lépe viděla.")
+dialog_addDialog("mik-v-videt", "fr", "", "font_big", "C'est pour mieux te voir.")
 
 dialog_addDialog("mik-m-konik", "en", "", "font_small", "I`ve seen this seahorse somewhere before.")
 dialog_addDialog("mik-m-konik", "cs", "sound/"..codename.."/cs/mik-m-konik.ogg", "font_small", "Tohohle koníka jsem už někde viděla.")
+dialog_addDialog("mik-m-konik", "fr", "", "font_small", "J'ai déjà vu cet hypocampe quelque part.")
 
 dialog_addDialog("mik-x-stebet0", "en", "sound/"..codename.."/en/mik-x-stebet0.ogg")
 
