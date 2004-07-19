25e76f6424542a9cb92bc21caa2291305303bd20
fidlej <fidlej>
2004-07-19 20:46:56 +0000

    Added level tetris.

---
+++ script/tetris/dialogs.lua
@@ -0,0 +1,46 @@
+dialog_addDialog("tet-m-vypadala", "en", "", "font_small", "So this is how the most successful game of all times looked like.")
+dialog_addDialog("tet-m-vypadala", "cs", "sound/"..codename.."/cs/tet-m-vypadala.ogg", "font_small", "Tak takhle vypadala nejúspěšnější hra všech dob.")
+
+dialog_addDialog("tet-v-ucta", "en", "", "font_big", "Only the deep respect for its memory prevents me from saying how it looks like...")
+dialog_addDialog("tet-v-ucta", "cs", "sound/"..codename.."/cs/tet-v-ucta.ogg", "font_big", "Jenom úcta k její památce mi brání v tom, abych řekl, jak to vypadá...")
+
+dialog_addDialog("tet-m-usudek", "en", "", "font_small", "But everybody can see it for himself.")
+dialog_addDialog("tet-m-usudek", "cs", "sound/"..codename.."/cs/tet-m-usudek.ogg", "font_small", "Ale každý si může udělat úsudek sám.")
+
+dialog_addDialog("tet-v-myslim", "en", "", "font_big", "Do you know what I have in mind?")
+dialog_addDialog("tet-v-myslim", "cs", "sound/"..codename.."/cs/tet-v-myslim.ogg", "font_big", "Víš, co si myslím?")
+
+dialog_addDialog("tet-m-ano", "en", "", "font_small", "Well?")
+dialog_addDialog("tet-m-ano", "cs", "sound/"..codename.."/cs/tet-m-ano.ogg", "font_small", "Ano?")
+
+dialog_addDialog("tet-v-lepsi", "en", "", "font_big", "I think we could do a better tetris than this room.")
+dialog_addDialog("tet-v-lepsi", "cs", "sound/"..codename.."/cs/tet-v-lepsi.ogg", "font_big", "Dokázali bychom udělat lepší tetris, než je tahle místnost.")
+
+dialog_addDialog("tet-m-jaklepsi", "en", "", "font_small", "What do you mean, better?")
+dialog_addDialog("tet-m-jaklepsi", "cs", "sound/"..codename.."/cs/tet-m-jaklepsi.ogg", "font_small", "Jak lepší?")
+
+dialog_addDialog("tet-v-hybat", "en", "", "font_big", "At least, you won`t have to move all the pieces to the right.")
+dialog_addDialog("tet-v-hybat", "cs", "sound/"..codename.."/cs/tet-v-hybat.ogg", "font_big", "No, přinejmenším tak, aby se tam dalo hýbat kostkou nejen doprava.")
+
+dialog_addDialog("tet-m-predmety", "en", "", "font_small", "The truth is we have an amply supply of suitable objects here.")
+dialog_addDialog("tet-m-predmety", "cs", "sound/"..codename.."/cs/tet-m-predmety.ogg", "font_small", "Pravda je, že tu máme spoustu vhodných předmětů.")
+
+dialog_addDialog("tet-v-uprava", "en", "", "font_big", "I think that little patch of the code can give the player the possibility to enjoy some original tetris.")
+dialog_addDialog("tet-v-uprava", "cs", "sound/"..codename.."/cs/tet-v-uprava.ogg", "font_big", "Myslím, že by stačila jen malá úprava kódu, aby si hráč mohl vychutnat trochu originálního tetrisu.")
+
+dialog_addDialog("tet-m-program", "en", "", "font_small", "Okay, try to program it.")
+dialog_addDialog("tet-m-program", "cs", "sound/"..codename.."/cs/tet-m-program.ogg", "font_small", "Tak to zkus naprogramovat.")
+
+dialog_addDialog("tet-m-pozor", "en", "", "font_small", "Be careful, not to hurt us.")
+dialog_addDialog("tet-m-pozor", "cs", "sound/"..codename.."/cs/tet-m-pozor.ogg", "font_small", "Dej si pozor, ať nám něco neuděláš.")
+
+dialog_addDialog("tet-v-hotovo", "en", "", "font_big", "Okay, I`m done. Input XTETRIS and you can get to it. Control the cubes by arrow keys or numpad.")
+dialog_addDialog("tet-v-hotovo", "cs", "sound/"..codename.."/cs/tet-v-hotovo.ogg", "font_big", "A je to hotovo. Napiš XTETRIS a můžeš se dát do toho. Ovládej to šipkama nebo numerickou klávesnicí.")
+
+dialog_addDialog("tet-v-kostky", "en", "", "font_big", "We will have to stack these pieces more efficiently, perhaps.")
+dialog_addDialog("tet-v-kostky", "cs", "sound/"..codename.."/cs/tet-v-kostky.ogg", "font_big", "Asi tam budeme muset ty kostky naskládat úsporněji.")
+
+dialog_addDialog("tet-m-lepe", "en", "", "font_small", "Hmm... I should have arranged it better.")
+dialog_addDialog("tet-m-lepe", "cs", "sound/"..codename.."/cs/tet-m-lepe.ogg", "font_small", "Hmmm... měla jsem to tam naskládat nějak lépe.")
+
+
