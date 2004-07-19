fbf207abe79b2c0e6e1e645a057a67768dc19c52
fidlej <fidlej>
2004-07-19 20:48:22 +0000

    Added level warcraft.

---
+++ script/warcraft/dialogs.lua
@@ -0,0 +1,63 @@
+dialog_addDialog("war-m-hrad", "en", "", "font_small", "I am imprisoned under the castle.")
+dialog_addDialog("war-m-hrad", "cs", "sound/"..codename.."/cs/war-m-hrad.ogg", "font_small", "Jsem uvězněná pod hradem.")
+
+dialog_addDialog("war-m-ocel", "en", "", "font_small", "How did the steel got into this castle?")
+dialog_addDialog("war-m-ocel", "cs", "sound/"..codename.."/cs/war-m-ocel.ogg", "font_small", "Jak se do toho hradu dostala ocel?")
+
+dialog_addDialog("war-v-vesnicane", "en", "", "font_big", "Hold on, I`ll bring some peasants to help you.")
+dialog_addDialog("war-v-vesnicane", "cs", "sound/"..codename.."/cs/war-v-vesnicane.ogg", "font_big", "Vydrž tam, přivedu na pomoc pár vesničanů.")
+
+dialog_addDialog("war-m-peoni", "en", "", "font_small", "These are not peasants but peons.")
+dialog_addDialog("war-m-peoni", "cs", "sound/"..codename.."/cs/war-m-peoni.ogg", "font_small", "To nejsou vesničani, ale peóni.")
+
+dialog_addDialog("war-v-doly", "en", "", "font_big", "Do you think that these mines contain captain Silver`s treasure?")
+dialog_addDialog("war-v-doly", "cs", "sound/"..codename.."/cs/war-v-doly.ogg", "font_big", "Myslíš, že v těch dolech jsou poklady kapitána Silvera?")
+
+dialog_addDialog("war-m-povazuji", "en", "", "font_small", "I consider this highly unlikely.")
+dialog_addDialog("war-m-povazuji", "cs", "sound/"..codename.."/cs/war-m-povazuji.ogg", "font_small", "Považuji to za krajně nepravděpodobné.")
+
+dialog_addDialog("war-m-kam", "en", "", "font_small", "Where did we get us, again?")
+dialog_addDialog("war-m-kam", "cs", "sound/"..codename.."/cs/war-m-kam.ogg", "font_small", "Kam jsme se to zase dostali?")
+
+dialog_addDialog("war-v-povedome", "en", "", "font_big", "I find it oddly familiar here.")
+dialog_addDialog("war-v-povedome", "cs", "sound/"..codename.."/cs/war-v-povedome.ogg", "font_big", "Je mi to tu nějaké povědomé.")
+
+dialog_addDialog("war-v-pohadka", "en", "", "font_big", "When the MS Word or another boring application is running on this machine, we, the computer games characters, get together in the C:\\WINDOWS\\CONFIG directory and talk.")
+dialog_addDialog("war-v-pohadka", "cs", "sound/"..codename.."/cs/war-v-pohadka.ogg", "font_big", "Když na tomhle počítači běží Word nebo jiná zbytečnost, my, postavičky z počítačových her, se scházíme v adresáři C:\\WINDOWS\\CONFIG a povídáme si.")
+
+dialog_addDialog("war-m-pichat", "en", "", "font_small", "When we don`t know what to do, we play hide-and-seek in registry. That`s why your Windows sometimes crashes if you don`t play any game for a long period of time.")
+dialog_addDialog("war-m-pichat", "cs", "sound/"..codename.."/cs/war-m-pichat.ogg", "font_small", "Když nemáme do čeho píchnout, hrajeme si na schovávanou v registrační databázi. Proto ti občas přestanou fungovat Windows, když dlouho nehraješ žádnou hru.")
+
+dialog_addDialog("war-v-prozradit", "en", "", "font_big", "You shouldn`t give that away. I only wanted to say that it looks very similar to this level. So many computer games characters.")
+dialog_addDialog("war-v-prozradit", "cs", "sound/"..codename.."/cs/war-v-prozradit.ogg", "font_big", "To jsi neměla prozrazovat. Já jsem chtěl jenom říct, že to tam vypadá skoro stejně jako tady. Spousta postaviček z her.")
+
+dialog_addDialog("war-m-aznato", "en", "", "font_small", "But it`s not so difficult to get out of there.")
+dialog_addDialog("war-m-aznato", "cs", "sound/"..codename.."/cs/war-m-aznato.ogg", "font_small", "Až na to, že odtamtud není tak těžké se dostat.")
+
+dialog_addDialog("war-v-blizzard", "en", "", "font_big", "The makers of this game would like to express their admiration for the Blizzard Entertainment company and would wish it many successful games.")
+dialog_addDialog("war-v-blizzard", "cs", "sound/"..codename.."/cs/war-v-blizzard.ogg", "font_big", "Tvůrci této hry by rádi vyjádřili svůj obdiv firmě Blizzard Entertainment a popřáli jí mnoho dalších úspěchů.")
+
+dialog_addDialog("war-m-hodiny", "en", "", "font_small", "And they would also like to thank for so many hours of neatly killed time.")
+dialog_addDialog("war-m-hodiny", "cs", "sound/"..codename.."/cs/war-m-hodiny.ogg", "font_small", "A také jí poděkovali za nespočet hodin příjemně zabitého času.")
+
+dialog_addDialog("war-k-ready0", "en", "sound/"..codename.."/en/war-k-ready0.ogg")
+
+dialog_addDialog("war-k-ready1", "en", "sound/"..codename.."/en/war-k-ready1.ogg")
+
+dialog_addDialog("war-k-ready2", "en", "sound/"..codename.."/en/war-k-ready2.ogg")
+
+dialog_addDialog("war-k-move0", "en", "sound/"..codename.."/en/war-k-move0.ogg")
+
+dialog_addDialog("war-k-move1", "en", "sound/"..codename.."/en/war-k-move1.ogg")
+
+dialog_addDialog("war-k-move2", "en", "sound/"..codename.."/en/war-k-move2.ogg")
+
+dialog_addDialog("war-a-ready0", "en", "sound/"..codename.."/en/war-a-ready0.ogg")
+
+dialog_addDialog("war-a-ready1", "en", "sound/"..codename.."/en/war-a-ready1.ogg")
+
+dialog_addDialog("war-a-move0", "en", "sound/"..codename.."/en/war-a-move0.ogg")
+
+dialog_addDialog("war-a-move1", "en", "sound/"..codename.."/en/war-a-move1.ogg")
+
+
