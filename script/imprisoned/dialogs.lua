f72477bbd652f40fcb93e13375ca892252d1e36d
fidlej <fidlej>
2004-07-19 20:41:42 +0000

    Added level imprisoned.

---
+++ script/imprisoned/dialogs.lua
@@ -0,0 +1,49 @@
+dialog_addDialog("ncp-m-tesno0", "en", "", "font_small", "Look, how I am supposed to get out of here?")
+dialog_addDialog("ncp-m-tesno0", "cs", "sound/"..codename.."/cs/ncp-m-tesno0.ogg", "font_small", "Hele, jak mám z toho vylézt?")
+
+dialog_addDialog("ncp-m-tesno1", "en", "", "font_small", "It`s quite tight in here.")
+dialog_addDialog("ncp-m-tesno1", "cs", "sound/"..codename.."/cs/ncp-m-tesno1.ogg", "font_small", "Je tu celkem těsno.")
+
+dialog_addDialog("ncp-v-dostala", "en", "", "font_big", "How did you manage to get there?")
+dialog_addDialog("ncp-v-dostala", "cs", "sound/"..codename.."/cs/ncp-v-dostala.ogg", "font_big", "Jak ses tam dostala?")
+
+dialog_addDialog("ncp-v-sasanka", "en", "", "font_big", "Stop playing with anemone and help me to get us out of here.")
+dialog_addDialog("ncp-v-sasanka", "cs", "sound/"..codename.."/cs/ncp-v-sasanka.ogg", "font_big", "Nehrej si se sasankou a pojďme se radši nějak dostat ven.")
+
+dialog_addDialog("ncp-m-nekoukej", "en", "", "font_small", "Don`t be surprised.")
+dialog_addDialog("ncp-m-nekoukej", "cs", "sound/"..codename.."/cs/ncp-m-nekoukej.ogg", "font_small", "Moc nekoukej.")
+
+dialog_addDialog("ncp-m-komari", "en", "", "font_small", "There is so many carols here...")
+dialog_addDialog("ncp-m-komari", "cs", "sound/"..codename.."/cs/ncp-m-komari.ogg", "font_small", "Tady je ale všude komárů...")
+
+dialog_addDialog("ncp-v-ceho", "en", "", "font_big", "What?")
+dialog_addDialog("ncp-v-ceho", "cs", "sound/"..codename.."/cs/ncp-v-ceho.ogg", "font_big", "Čeho?")
+
+dialog_addDialog("ncp-m-koraly", "en", "", "font_small", "So many corals. Are you deaf or what?")
+dialog_addDialog("ncp-m-koraly", "cs", "sound/"..codename.."/cs/ncp-m-koraly.ogg", "font_small", "No korálů, jsi hluchý?")
+
+dialog_addDialog("ncp-m-tvrdy", "en", "", "font_small", "Oh my, this is a hard coral.")
+dialog_addDialog("ncp-m-tvrdy", "cs", "sound/"..codename.."/cs/ncp-m-tvrdy.ogg", "font_small", "To je ale tvrdý korál.")
+
+dialog_addDialog("ncp-v-mekky", "en", "", "font_big", "Oh my, this is a soft coral.")
+dialog_addDialog("ncp-v-mekky", "cs", "sound/"..codename.."/cs/ncp-v-mekky.ogg", "font_big", "To je ale měkký korál.")
+
+dialog_addDialog("ncp-v-tak", "en", "", "font_big", "So!")
+dialog_addDialog("ncp-v-tak", "cs", "sound/"..codename.."/cs/ncp-v-tak.ogg", "font_big", "Tak!")
+
+dialog_addDialog("ncp-m-muzes", "en", "", "font_small", "Now you can get out, can`t you?")
+dialog_addDialog("ncp-m-muzes", "cs", "sound/"..codename.."/cs/ncp-m-muzes.ogg", "font_small", "Teď už můžeš ven, ne?")
+
+dialog_addDialog("ncp-v-neprojedu", "en", "", "font_big", "Hmm, I can`t get through here...")
+dialog_addDialog("ncp-v-neprojedu", "cs", "sound/"..codename.."/cs/ncp-v-neprojedu.ogg", "font_big", "Hmm, tak tudy neprojedu...")
+
+dialog_addDialog("ncp-m-barvy", "en", "", "font_small", "I love colors...")
+dialog_addDialog("ncp-m-barvy", "cs", "sound/"..codename.."/cs/ncp-m-barvy.ogg", "font_small", "Miluju barvy...")
+
+dialog_addDialog("ncp-x-tik", "en", "sound/"..codename.."/en/ncp-x-tik.ogg")
+
+dialog_addDialog("ncp-x-tup", "en", "sound/"..codename.."/en/ncp-x-tup.ogg")
+
+dialog_addDialog("ncp-x-ihaha", "en", "sound/"..codename.."/en/ncp-x-ihaha.ogg")
+
+
