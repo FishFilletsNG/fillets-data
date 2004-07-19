e4f530df3a5bb09175ced585eb80aef3ed1784bc
fidlej <fidlej>
2004-07-19 20:35:21 +0000

    Added level captain.

---
+++ script/captain/dialogs.lua
@@ -0,0 +1,40 @@
+dialog_addDialog("vl-m-hara", "en", "", "font_small", "There is a lot of garbage here!")
+dialog_addDialog("vl-m-hara", "cs", "sound/"..codename.."/cs/vl-m-hara.ogg", "font_small", "To je ale haraburdí!")
+
+dialog_addDialog("vl-v-kaj1", "en", "", "font_big", "This was surely a captain`s cabin.")
+dialog_addDialog("vl-v-kaj1", "cs", "sound/"..codename.."/cs/vl-v-kaj1.ogg", "font_big", "To byla určitě kapitánova kajuta.")
+
+dialog_addDialog("vl-v-kaj2", "en", "", "font_big", "What would you expect after so many years.")
+dialog_addDialog("vl-v-kaj2", "cs", "sound/"..codename.."/cs/vl-v-kaj2.ogg", "font_big", "Co bys chtěla po tolika letech.")
+
+dialog_addDialog("vl-m-hak", "en", "", "font_small", "Do you think that Silver had this hook in place of his hand?")
+dialog_addDialog("vl-m-hak", "cs", "sound/"..codename.."/cs/vl-m-hak.ogg", "font_small", "Myslíš, že ten hák měl Silver místo ruky? Jak by se do té kajuty vešel?")
+
+dialog_addDialog("vl-v-lodni", "en", "", "font_big", "This is a ship hook. It`s used to pull up boats...")
+dialog_addDialog("vl-v-lodni", "cs", "sound/"..codename.."/cs/vl-v-lodni.ogg", "font_big", "To je lodní hák. S ním se vytahují čluny...")
+
+dialog_addDialog("vl-x-site", "en", "", "font_brown", "... and nets!")
+dialog_addDialog("vl-x-site", "cs", "sound/"..codename.."/cs/vl-x-site.ogg", "font_brown", "... a sítě!")
+
+dialog_addDialog("vl-m-oko", "en", "", "font_small", "It`s a strange looking eye.")
+dialog_addDialog("vl-m-oko", "cs", "sound/"..codename.."/cs/vl-m-oko.ogg", "font_small", "S tím okem to nevidím dobře.")
+
+dialog_addDialog("vl-v-silha", "en", "", "font_big", "This eye squints kind of sneaky.")
+dialog_addDialog("vl-v-silha", "cs", "sound/"..codename.."/cs/vl-v-silha.ogg", "font_big", "To oko potměšile šilhá.")
+
+dialog_addDialog("vl-leb-kecy0", "en", "", "font_lightgrey", "Haven`t you seen my eye somewhere?")
+dialog_addDialog("vl-leb-kecy0", "cs", "sound/"..codename.."/cs/vl-leb-kecy0.ogg", "font_lightgrey", "Neviděli jste někde moje oko?")
+
+dialog_addDialog("vl-leb-kecy1", "en", "", "font_lightgrey", "This scarf is very important. The human skull with an empty eye socket looks really disgusting, you know.")
+dialog_addDialog("vl-leb-kecy1", "cs", "sound/"..codename.."/cs/vl-leb-kecy1.ogg", "font_lightgrey", "Tenhle šátek je velice důležitý. Víte, ona lidská lebka s prázdným očním důlkem vypadá opravdu odpudivě.")
+
+dialog_addDialog("vl-leb-kecy2", "en", "", "font_lightgrey", "After that unfortunate accident with a teaspoon I have a completely different viewpoint of the world.")
+dialog_addDialog("vl-leb-kecy2", "cs", "sound/"..codename.."/cs/vl-leb-kecy2.ogg", "font_lightgrey", "Po té nešťastné nehodě s čajovou lžičkou se na svět dívám úplně jinak.")
+
+dialog_addDialog("vl-leb-kecy3", "en", "", "font_lightgrey", "Why am I here, after all? As if they can`t put here some chest... or chamber pot.")
+dialog_addDialog("vl-leb-kecy3", "cs", "sound/"..codename.."/cs/vl-leb-kecy3.ogg", "font_lightgrey", "Proč já tady vlastně musím být. Jako by sem místo mě nemohli vrznout třeba truhlu... nebo kastról.")
+
+dialog_addDialog("vl-leb-kecy4", "en", "", "font_lightgrey", "Do you appreciate my facial expressions? Not bad for a skeleton, is it?")
+dialog_addDialog("vl-leb-kecy4", "cs", "sound/"..codename.."/cs/vl-leb-kecy4.ogg", "font_lightgrey", "Všímáte si práce mých mimických svalů? Dost dobrý, na kostru, ne?")
+
+
