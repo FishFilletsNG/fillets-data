75d5aa1be3f367bff6af26d7b317877964330368
ronnystandtke <ronnystandtke>
2004-07-19 20:41:13 +0000

    added German translation

--- script/library/dialogs.lua
+++ script/library/dialogs.lua
@@ -1,144 +1,182 @@
 dialog_addDialog("vrak-v-vraky0", "en", "", "font_big", "These shipwrecs grow more and more arrogant.")
 dialog_addDialog("vrak-v-vraky0", "cs", "sound/"..codename.."/cs/vrak-v-vraky0.ogg", "font_big", "Ty vraky lodí jsou čím dál drzejší.")
 dialog_addDialog("vrak-v-vraky0", "fr", "", "font_big", "Ces épaves de bateaux sont de plus en plus énervants.")
+dialog_addDialog("vrak-v-vraky0", "de", "", "font_big", "Diese Schiffwracks werden immer arroganter.")
 
 dialog_addDialog("vrak-v-vraky1", "en", "", "font_big", "These sunken ships really irritate me.")
 dialog_addDialog("vrak-v-vraky1", "cs", "sound/"..codename.."/cs/vrak-v-vraky1.ogg", "font_big", "Ty potopené lodě už mi jdou na nervy.")
 dialog_addDialog("vrak-v-vraky1", "fr", "", "font_big", "Ces navires coulés m'énervent vraiment.")
+dialog_addDialog("vrak-v-vraky1", "de", "", "font_big", "Diese versunkenen Schiffe ärgern mich wirklich.")
 
 dialog_addDialog("vrak-v-vraky2", "en", "", "font_big", "Oh my, another ship sank. It really makes me itchy.")
 dialog_addDialog("vrak-v-vraky2", "cs", "sound/"..codename.."/cs/vrak-v-vraky2.ogg", "font_big", "Ach jo, zase se někde potopila loď. Už mám těch vraků dost!")
 dialog_addDialog("vrak-v-vraky2", "fr", "", "font_big", "Oh, encore un navire échoué. Je vais devenir hargneux.")
+dialog_addDialog("vrak-v-vraky2", "de", "", "font_big", "Oh Mann, noch ein versunkenes Schiff. Das macht mich echt wütend.")
 
 dialog_addDialog("vrak-m-vrak0", "en", "", "font_small", "Look, it broke our bookcase!")
 dialog_addDialog("vrak-m-vrak0", "cs", "sound/"..codename.."/cs/vrak-m-vrak0.ogg", "font_small", "Podívej, jak nám poničily knihovničku!")
 dialog_addDialog("vrak-m-vrak0", "fr", "", "font_small", "Regarde, il a cassé notre mallette !")
+dialog_addDialog("vrak-m-vrak0", "de", "", "font_small", "Schau, es hat unser Bücherregal zerhauen!")
 
 dialog_addDialog("vrak-m-vrak1", "en", "", "font_small", "Look, it smashed our shelves.")
 dialog_addDialog("vrak-m-vrak1", "cs", "sound/"..codename.."/cs/vrak-m-vrak1.ogg", "font_small", "Podívej, rozbily nám poličky.")
 dialog_addDialog("vrak-m-vrak1", "fr", "", "font_small", "Et ici, il a écrasé nos étagères.")
+dialog_addDialog("vrak-m-vrak1", "de", "", "font_small", "Schau, es hat unsere Regale kaputt gemacht.")
 
 dialog_addDialog("vrak-m-vrak2", "en", "", "font_small", "Look, it invaded our library.")
 dialog_addDialog("vrak-m-vrak2", "cs", "sound/"..codename.."/cs/vrak-m-vrak2.ogg", "font_small", "Podívej, vlezly nám až do knihovny.")
 dialog_addDialog("vrak-m-vrak2", "fr", "", "font_small", "Regarde, il envahit la bibliothèque.")
+dialog_addDialog("vrak-m-vrak2", "de", "", "font_small", "Schau, es ist in unsere Bibliothek eingedrungen.")
 
 dialog_addDialog("vrak-m-kupovat0", "en", "", "font_small", "See? You shouldn`t have bought so many books.")
 dialog_addDialog("vrak-m-kupovat0", "cs", "sound/"..codename.."/cs/vrak-m-kupovat0.ogg", "font_small", "To máš z toho, že kupuješ tolik knížek.")
 dialog_addDialog("vrak-m-kupovat0", "fr", "", "font_small", "Tu vois ? Tu n'aurais pas dû acheter tant de livres.")
+dialog_addDialog("vrak-m-kupovat0", "de", "", "font_small", "Siehst Du? Du hättest nicht so viele Bücher kaufen sollen.")
 
 dialog_addDialog("vrak-m-kupovat1", "en", "", "font_small", "You buy every book you can lay your hand on and now you have problems.")
 dialog_addDialog("vrak-m-kupovat1", "cs", "sound/"..codename.."/cs/vrak-m-kupovat1.ogg", "font_small", "To máš z toho, že koupíš každou knížku, kterou vidíš.")
 dialog_addDialog("vrak-m-kupovat1", "fr", "", "font_small", "Tu achètes tout les livres qui te passent sous les mains et maintenant tu as des problèmes.")
+dialog_addDialog("vrak-m-kupovat1", "de", "", "font_small", "Du kaufst jedes Buch, dass Du in die Finger bekommst und jetzt hast Du Probleme.")
 
 dialog_addDialog("vrak-m-naco", "en", "", "font_small", "Why do you have to have so many of them?")
 dialog_addDialog("vrak-m-naco", "cs", "sound/"..codename.."/cs/vrak-m-naco.ogg", "font_small", "Na co ti všechny jsou?")
 dialog_addDialog("vrak-m-naco", "fr", "", "font_small", "Pourquoi dois-tu en posséder autant ?")
+dialog_addDialog("vrak-m-naco", "de", "", "font_small", "Warum hast Du so viele?")
 
 dialog_addDialog("vrak-m-knihy0", "en", "", "font_small", "ESP")
 dialog_addDialog("vrak-m-knihy0", "cs", "sound/"..codename.."/cs/vrak-m-knihy0.ogg", "font_small", "Paranormální jevy.")
 dialog_addDialog("vrak-m-knihy0", "fr", "", "font_small", "Perception extra-sensorielle.")
+dialog_addDialog("vrak-m-knihy0", "de", "", "font_small", "Außersinnliche Wahrnehmung.")
+dialog_addDialog("vrak-m-knihy0", "de_CH", "", "font_small", "Aussersinnliche Wahrnehmung.")
 
 dialog_addDialog("vrak-m-knihy1", "en", "", "font_small", "Life after life.")
 dialog_addDialog("vrak-m-knihy1", "cs", "sound/"..codename.."/cs/vrak-m-knihy1.ogg", "font_small", "Život po životě.")
 dialog_addDialog("vrak-m-knihy1", "fr", "", "font_small", "La vie après la vie.")
+dialog_addDialog("vrak-m-knihy1", "de", "", "font_small", "Leben nach dem Leben.")
 
 dialog_addDialog("vrak-m-knihy2", "en", "", "font_small", "Memories of the future.")
 dialog_addDialog("vrak-m-knihy2", "cs", "sound/"..codename.."/cs/vrak-m-knihy2.ogg", "font_small", "Vzpomínky na budoucnost.")
 dialog_addDialog("vrak-m-knihy2", "fr", "", "font_small", "Mémoires du futur.")
+dialog_addDialog("vrak-m-knihy2", "de", "", "font_small", "Erinnerungen an die Zunkunft.")
 
 dialog_addDialog("vrak-m-knihy3", "en", "", "font_small", "Holistic methods.")
 dialog_addDialog("vrak-m-knihy3", "cs", "sound/"..codename.."/cs/vrak-m-knihy3.ogg", "font_small", "Holistické metody.")
 dialog_addDialog("vrak-m-knihy3", "fr", "", "font_small", "Méthodes holistiques.")
+dialog_addDialog("vrak-m-knihy3", "de", "", "font_small", "Ganzheitliche Methoden.")
 
 dialog_addDialog("vrak-m-knihy4", "en", "", "font_small", "Teach yourself telepathy.")
 dialog_addDialog("vrak-m-knihy4", "cs", "sound/"..codename.."/cs/vrak-m-knihy4.ogg", "font_small", "Telepatem snadno a rychle.")
 dialog_addDialog("vrak-m-knihy4", "fr", "", "font_small", "La télépathie en dix leçons.")
+dialog_addDialog("vrak-m-knihy4", "de", "", "font_small", "Telepatie im Selbststudium.")
 
 dialog_addDialog("vrak-m-knihy5", "en", "", "font_small", "Unwilling clairvoyant.")
 dialog_addDialog("vrak-m-knihy5", "cs", "sound/"..codename.."/cs/vrak-m-knihy5.ogg", "font_small", "Jasnovidcem proti své vůli.")
 dialog_addDialog("vrak-m-knihy5", "fr", "", "font_small", "Clairvoyant fou.")
+dialog_addDialog("vrak-m-knihy5", "de", "", "font_small", "Widerwilliger Hellseher.")
 
 dialog_addDialog("vrak-m-knihy6", "en", "", "font_small", "Outreaching Daniken.")
 dialog_addDialog("vrak-m-knihy6", "cs", "sound/"..codename.."/cs/vrak-m-knihy6.ogg", "font_small", "Co Daniken netušil.")
 dialog_addDialog("vrak-m-knihy6", "fr", "", "font_small", "Inaccessible Daniken.")
+dialog_addDialog("vrak-m-knihy6", "de", "", "font_small", "Däniken übertrumpfen.")
 
 dialog_addDialog("vrak-v-vyhodit", "en", "", "font_big", "I`d throw out these, for example.")
 dialog_addDialog("vrak-v-vyhodit", "cs", "sound/"..codename.."/cs/vrak-v-vyhodit.ogg", "font_big", "Já bych vyhodil třeba támhlety.")
 dialog_addDialog("vrak-v-vyhodit", "fr", "", "font_big", "Je jette ça, par exemple.")
+dialog_addDialog("vrak-v-vyhodit", "de", "", "font_big", "Die würde ich zum Beispiel wegschmeißen.")
+dialog_addDialog("vrak-v-vyhodit", "de_CH", "", "font_big", "Die würde ich zum Beispiel wegschmeissen.")
 
 dialog_addDialog("vrak-v-knihy0", "en", "", "font_big", "The Three Little Shrimp")
 dialog_addDialog("vrak-v-knihy0", "cs", "sound/"..codename.."/cs/vrak-v-knihy0.ogg", "font_big", "Jak Frk a Brk putovali za sumýškem.")
 dialog_addDialog("vrak-v-knihy0", "fr", "", "font_big", "Les trois petites crevettes.")
+dialog_addDialog("vrak-v-knihy0", "de", "", "font_big", "Der Hai und die sieben Fischlein.")
 
 dialog_addDialog("vrak-v-knihy1", "en", "", "font_big", "The Gingerbread Submarine")
 dialog_addDialog("vrak-v-knihy1", "cs", "sound/"..codename.."/cs/vrak-v-knihy1.ogg", "font_big", "O perníkové ponorce.")
 dialog_addDialog("vrak-v-knihy1", "fr", "", "font_big", "Le sous-marin en pain d'épice.")
+dialog_addDialog("vrak-v-knihy1", "de", "", "font_big", "Der Seeigel mit den drei goldenen Stacheln.")
 
 dialog_addDialog("vrak-v-knihy2", "en", "", "font_big", "Goldilocks and the Three Lobsters")
 dialog_addDialog("vrak-v-knihy2", "cs", "sound/"..codename.."/cs/vrak-v-knihy2.ogg", "font_big", "Dlouhý, Široký a Hluboký.")
 dialog_addDialog("vrak-v-knihy2", "fr", "", "font_big", "Les 101 bigorneaux.")
+dialog_addDialog("vrak-v-knihy2", "de", "", "font_big", "Das tapfere Schwertfischlein.")
 
 dialog_addDialog("vrak-v-knihy3", "en", "", "font_big", "Sharkerella")
 dialog_addDialog("vrak-v-knihy3", "cs", "sound/"..codename.."/cs/vrak-v-knihy3.ogg", "font_big", "O rybce se zlatou mušlí na čele.")
 dialog_addDialog("vrak-v-knihy3", "fr", "", "font_big", "Les contes des milles et un fonds marins.")
+dialog_addDialog("vrak-v-knihy3", "de", "", "font_big", "Krabbenputtel.")
 
 dialog_addDialog("vrak-v-knihy4", "en", "", "font_big", "Slimy Squid and the Seven Dwarfs")
 dialog_addDialog("vrak-v-knihy4", "cs", "sound/"..codename.."/cs/vrak-v-knihy4.ogg", "font_big", "Sépijka a sedm trpaslíků.")
 dialog_addDialog("vrak-v-knihy4", "fr", "", "font_big", "Le Calmar gluant et les sept Nains")
+dialog_addDialog("vrak-v-knihy4", "de", "", "font_big", "Tintenfischchen und die sieben Zwerge.")
 
 dialog_addDialog("vrak-m-pohadky", "en", "", "font_small", "Never! My lovely fairy-tales.")
 dialog_addDialog("vrak-m-pohadky", "cs", "sound/"..codename.."/cs/vrak-m-pohadky.ogg", "font_small", "Nikdy! Moje oblíbené pohádky!")
 dialog_addDialog("vrak-m-pohadky", "fr", "", "font_small", "Jamais ! Mes merveilleux contes de fées.")
+dialog_addDialog("vrak-m-pohadky", "de", "", "font_small", "Niemals! Meine Lieblingsmärchen!")
 
 dialog_addDialog("vrak-v-nevejdu0", "en", "", "font_big", "I can`t hide myself here.")
 dialog_addDialog("vrak-v-nevejdu0", "cs", "sound/"..codename.."/cs/vrak-v-nevejdu0.ogg", "font_big", "Sem se neschovám.")
 dialog_addDialog("vrak-v-nevejdu0", "fr", "", "font_big", "Je ne peux pas me cacher ici.")
+dialog_addDialog("vrak-v-nevejdu0", "de", "", "font_big", "Ich kann mich hier nicht verstecken.")
 
 dialog_addDialog("vrak-v-nevejdu1", "en", "", "font_big", "I can`t fit in here.")
 dialog_addDialog("vrak-v-nevejdu1", "cs", "sound/"..codename.."/cs/vrak-v-nevejdu1.ogg", "font_big", "Sem se nevejdu.")
 dialog_addDialog("vrak-v-nevejdu1", "fr", "", "font_big", "Je ne rentre pas là dedans.")
+dialog_addDialog("vrak-v-nevejdu1", "de", "", "font_big", "Ich passe hier nicht rein.")
 
 dialog_addDialog("vrak-m-cteni0", "en", "", "font_small", "Why do we have so many books if we cannot pull them out anyway?")
 dialog_addDialog("vrak-m-cteni0", "cs", "sound/"..codename.."/cs/vrak-m-cteni0.ogg", "font_small", "K čemu je nám tolik knížek, když stejně nejdou vytáhnout z poliček?")
 dialog_addDialog("vrak-m-cteni0", "fr", "", "font_small", "Pourquoi avons-nous tant de livres si nous ne pouvons pas les pousser de toute façon ?")
+dialog_addDialog("vrak-m-cteni0", "de", "", "font_small", "Wieso haben wir eigentlich so viele Bücher, wenn wir sie nicht rausziehen können?")
 
 dialog_addDialog("vrak-m-cteni1", "en", "", "font_small", "If you did some work-outs instead of reading about silly affairs you could fit yourself in.")
 dialog_addDialog("vrak-m-cteni1", "cs", "sound/"..codename.."/cs/vrak-m-cteni1.ogg", "font_small", "Kdybys místo čtení o nesmyslech trochu sportoval, vešel by ses tam.")
 dialog_addDialog("vrak-m-cteni1", "fr", "", "font_small", "Si tu faisais des travaux d'extérieur au lieu de lire ces idioties, tu pourrais passer.")
+dialog_addDialog("vrak-m-cteni1", "de", "", "font_small", "Wenn Du ein wenig Sport machen würdest, anstatt blödes Zeug zu lesen, würdest Du reinpassen.")
 
 dialog_addDialog("vrak-m-cteni2", "en", "", "font_small", "I told you not to buy that Compleate Dictionary of Abnormal.")
 dialog_addDialog("vrak-m-cteni2", "cs", "sound/"..codename.."/cs/vrak-m-cteni2.ogg", "font_small", "Říkala jsem ti, ať nekupuješ ten Velký slovník abnormálních výrazů.")
 dialog_addDialog("vrak-m-cteni2", "fr", "", "font_small", "Je t'avais dis de ne pas acheter ce dictionnaire complet de l'anormal.")
+dialog_addDialog("vrak-m-cteni2", "de", "", "font_small", "Ich hab Dir doch gesagt, Du sollst nicht dieses Wörterbuch des Abnormalen kaufen.")
 
 dialog_addDialog("vrak-v-policky", "en", "", "font_big", "Let`s think. We can`t get these books off the shelf.")
 dialog_addDialog("vrak-v-policky", "cs", "sound/"..codename.."/cs/vrak-v-policky.ogg", "font_big", "Zamysleme se. Ty knihy nedostaneme z poliček.")
 dialog_addDialog("vrak-v-policky", "fr", "", "font_big", "Pense un peu. Nous ne pouvons pas sortir les livres des étagères.")
+dialog_addDialog("vrak-v-policky", "de", "", "font_big", "Lass uns nachdenken. Wir können diese Bücher nicht aus dem Regal ziehen.")
 
 dialog_addDialog("vrak-m-predmety", "en", "", "font_small", "There are only two objects here which we can move with any results.")
 dialog_addDialog("vrak-m-predmety", "cs", "sound/"..codename.."/cs/vrak-m-predmety.ogg", "font_small", "Jsou tu jen dva předměty, se kterými má smysl hýbat.")
 dialog_addDialog("vrak-m-predmety", "fr", "", "font_small", "Il y a seulement deux objets que nous pouvons bouger de manière significative.")
+dialog_addDialog("vrak-m-predmety", "de", "", "font_small", "Es gibt hier nur zwei Gegenstände, die zu einem Ergebnis führen, wenn wir sie bewegen.")
 
 dialog_addDialog("vrak-m-ostnatec", "en", "", "font_small", "How did that prickler get here?")
 dialog_addDialog("vrak-m-ostnatec", "cs", "sound/"..codename.."/cs/vrak-m-ostnatec.ogg", "font_small", "Kde se tady vzal ten ostnatec?")
 dialog_addDialog("vrak-m-ostnatec", "fr", "", "font_small", "Comment ce plein d'épines est arrivé ici ?")
+dialog_addDialog("vrak-m-ostnatec", "de", "", "font_small", "Wie ist der Stachler denn hierher gekommen?")
 
 dialog_addDialog("vrak-m-zivocich", "en", "", "font_small", "You are in the way, strange life form!")
 dialog_addDialog("vrak-m-zivocich", "cs", "sound/"..codename.."/cs/vrak-m-zivocich.ogg", "font_small", "Živočichu, zavazíš!")
 dialog_addDialog("vrak-m-zivocich", "fr", "", "font_small", "Tu es sur le chemin, forme de vie étrange !")
+dialog_addDialog("vrak-m-zivocich", "de", "", "font_small", "Du bist im Weg, Du komische Lebensform!")
 
 dialog_addDialog("vrak-v-potvurka", "en", "", "font_big", "You are an obstacle, strange creature!")
 dialog_addDialog("vrak-v-potvurka", "cs", "sound/"..codename.."/cs/vrak-v-potvurka.ogg", "font_big", "Zavazíš, potvůrko!")
 dialog_addDialog("vrak-v-potvurka", "fr", "", "font_big", "Tu es un obstacle, créature étrange !")
+dialog_addDialog("vrak-v-potvurka", "de", "", "font_big", "Du bist ein Hindernis, komische Kreatur!")
 
 dialog_addDialog("vrak-v-snek", "en", "", "font_big", "Can`t you push that snail to me?")
 dialog_addDialog("vrak-v-snek", "cs", "sound/"..codename.."/cs/vrak-v-snek.ogg", "font_big", "Nemůžeš toho šneka posunout sem ke mně?")
 dialog_addDialog("vrak-v-snek", "fr", "", "font_big", "Peux-tu pousser cet escargot vers moi ?")
+dialog_addDialog("vrak-v-snek", "de", "", "font_big", "Kannst Du diese Schnecke zu mir schieben?")
 
 dialog_addDialog("vrak-m-ocel", "en", "", "font_small", "How am I to get there through that steel?!")
 dialog_addDialog("vrak-m-ocel", "cs", "sound/"..codename.."/cs/vrak-m-ocel.ogg", "font_small", "Jak se tam mám asi dostat, přes tu ocel?!")
 dialog_addDialog("vrak-m-ocel", "fr", "", "font_small", "Et comment suis-je censé passer à travers l'acier ?!")
+dialog_addDialog("vrak-m-ocel", "de", "", "font_small", "Wie soll ich denn dort durch den Stahl kommen?!")
 
 dialog_addDialog("vrak-m-restart", "en", "", "font_small", "What about restarting and trying it again from the scratch?")
 dialog_addDialog("vrak-m-restart", "cs", "sound/"..codename.."/cs/vrak-m-restart.ogg", "font_small", "Co takhle restartovat a zkusit to znovu?")
 dialog_addDialog("vrak-m-restart", "fr", "", "font_small", "Que penses-tu de recommencer à zéro ce niveau.")
+dialog_addDialog("vrak-m-restart", "de", "", "font_small", "Wie wäre es mit neustarten und von vorne anfangen?")
 
