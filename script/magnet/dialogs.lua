c7edac18c35bd14b7c5dccbf749851ff1d13df03
fidlej <fidlej>
2004-07-19 20:42:35 +0000

    Added level magnet.

---
+++ script/magnet/dialogs.lua
@@ -0,0 +1,73 @@
+dialog_addDialog("pap-v-ha", "en", "", "font_big", "Look. Another energy source.")
+dialog_addDialog("pap-v-ha", "cs", "sound/"..codename.."/cs/pap-v-ha.ogg", "font_big", "Ha. Další zdroj energie.")
+
+dialog_addDialog("pap-m-magnet", "en", "", "font_small", "Don`t worry. This magnet is here only to hinder us.")
+dialog_addDialog("pap-m-magnet", "cs", "sound/"..codename.."/cs/pap-m-magnet.ogg", "font_small", "Dej pokoj, ten magnet je tady jen proto, aby nám překážel.")
+
+dialog_addDialog("pap-v-potrebovat", "en", "", "font_big", "I think we are going to need it.")
+dialog_addDialog("pap-v-potrebovat", "cs", "sound/"..codename.."/cs/pap-v-potrebovat.ogg", "font_big", "Já myslím, že ho ještě budeme potřebovat.")
+
+dialog_addDialog("pap-m-zvlastni", "en", "", "font_small", "This is a very strange object.")
+dialog_addDialog("pap-m-zvlastni", "cs", "sound/"..codename.."/cs/pap-m-zvlastni.ogg", "font_small", "To je ale zvláštní předmět.")
+
+dialog_addDialog("pap-v-prekvapeni", "en", "", "font_big", "I think that UFO is going to be full of such surprises.")
+dialog_addDialog("pap-v-prekvapeni", "cs", "sound/"..codename.."/cs/pap-v-prekvapeni.ogg", "font_big", "Myslím, že v Ufu bude spousta takových překvapení.")
+
+dialog_addDialog("pap-m-teorie", "en", "", "font_small", "Stop developing your theories and start solving.")
+dialog_addDialog("pap-m-teorie", "cs", "sound/"..codename.."/cs/pap-m-teorie.ogg", "font_small", "Nerozvíjej svoje teorie a raději to nějak vyřešme.")
+
+dialog_addDialog("pap-m-radio", "en", "", "font_small", "This looks like a radio.")
+dialog_addDialog("pap-m-radio", "cs", "sound/"..codename.."/cs/pap-m-radio.ogg", "font_small", "Tohle vypadá jako rádio.")
+
+dialog_addDialog("pap-v-radio", "en", "", "font_big", "How could a radio get here?")
+dialog_addDialog("pap-v-radio", "cs", "sound/"..codename.."/cs/pap-v-radio.ogg", "font_big", "Kde by se tady vzalo rádio?")
+
+dialog_addDialog("pap-m-nechme", "en", "", "font_small", "Let it be.")
+dialog_addDialog("pap-m-nechme", "cs", "sound/"..codename.."/cs/pap-m-nechme.ogg", "font_small", "Nechme toho.")
+
+dialog_addDialog("pap-v-divny", "en", "", "font_big", "This is a strange radio.")
+dialog_addDialog("pap-v-divny", "cs", "sound/"..codename.."/cs/pap-v-divny.ogg", "font_big", "To je ale divný rádio.")
+
+dialog_addDialog("pap-m-coje", "en", "", "font_small", "What have we here?")
+dialog_addDialog("pap-m-coje", "cs", "sound/"..codename.."/cs/pap-m-coje.ogg", "font_small", "Co je to tady?")
+
+dialog_addDialog("pap-m-pistole", "en", "", "font_small", "It looks like a pistol to me.")
+dialog_addDialog("pap-m-pistole", "cs", "sound/"..codename.."/cs/pap-m-pistole.ogg", "font_small", "Připomíná to pistoli.")
+
+dialog_addDialog("pap-v-laserova", "en", "", "font_big", "Maybe it`s a laser weapon.")
+dialog_addDialog("pap-v-laserova", "cs", "sound/"..codename.."/cs/pap-v-laserova.ogg", "font_big", "Třeba je to laserová zbraň.")
+
+dialog_addDialog("pap-m-jejedno", "en", "", "font_small", "It doesn`t matter. We don`t need it.")
+dialog_addDialog("pap-m-jejedno", "cs", "sound/"..codename.."/cs/pap-m-jejedno.ogg", "font_small", "To je jedno, tu snad nepotřebujeme.")
+
+dialog_addDialog("pap-v-nemir", "en", "", "font_big", "Stop pointing at me with it!")
+dialog_addDialog("pap-v-nemir", "cs", "sound/"..codename.."/cs/pap-v-nemir.ogg", "font_big", "Hlavně s ní nemiř na mě!")
+
+dialog_addDialog("pap-m-nejde", "en", "", "font_small", "As if you don`t know that you can`t turn objects in this game.")
+dialog_addDialog("pap-m-nejde", "cs", "sound/"..codename.."/cs/pap-m-nejde.ogg", "font_small", "Jako bys nevěděl, že v této hře nejde předměty otáčet!")
+
+dialog_addDialog("pap-m-ocel", "en", "", "font_small", "There are too many steel constructions here.")
+dialog_addDialog("pap-m-ocel", "cs", "sound/"..codename.."/cs/pap-m-ocel.ogg", "font_small", "Je tu nějak moc těch ocelových konstrukcí.")
+
+dialog_addDialog("pap-v-vufu", "en", "", "font_big", "Don`t forget that we are inside of the flying saucer!")
+dialog_addDialog("pap-v-vufu", "cs", "sound/"..codename.."/cs/pap-v-vufu.ogg", "font_big", "Nezapomínej, že jsme uvnitř létajícího talíře!")
+
+dialog_addDialog("pap-m-naucit", "en", "", "font_small", "Maybe I could learn how to manipulate these steel tubes.")
+dialog_addDialog("pap-m-naucit", "cs", "sound/"..codename.."/cs/pap-m-naucit.ogg", "font_small", "Možná bych se mohla naučit manipulovat s těmi ocelovými válci.")
+
+dialog_addDialog("pap-m-nepohnu", "en", "", "font_small", "No, I can`t move it.")
+dialog_addDialog("pap-m-nepohnu", "cs", "sound/"..codename.."/cs/pap-m-nepohnu.ogg", "font_small", "Ne, s tím nepohnu.")
+
+dialog_addDialog("pap-v-tesno", "en", "", "font_big", "It`s quite tight here.")
+dialog_addDialog("pap-v-tesno", "cs", "sound/"..codename.."/cs/pap-v-tesno.ogg", "font_big", "Je  tu nějak těsno.")
+
+dialog_addDialog("pap-m-mraz", "en", "", "font_small", "These magnets are giving me the creeps.")
+dialog_addDialog("pap-m-mraz", "cs", "sound/"..codename.."/cs/pap-m-mraz.ogg", "font_small", "Běhá mi z těch magnetů mráz po zádech.")
+
+dialog_addDialog("pap-v-pole", "en", "", "font_big", "This magnetic field probably has some invigorating effects.")
+dialog_addDialog("pap-v-pole", "cs", "sound/"..codename.."/cs/pap-v-pole.ogg", "font_big", "To magnetické pole má asi povzbuzující účinky.")
+
+dialog_addDialog("pap-m-nedobre", "en", "", "font_small", "It`s doing me no good.")
+dialog_addDialog("pap-m-nedobre", "cs", "sound/"..codename.."/cs/pap-m-nedobre.ogg", "font_small", "Mně to teda moc dobře nedělá.")
+
+
