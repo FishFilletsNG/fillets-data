a91677562e8fbea416d929a2a4d9b6dc44705fc9
ronnystandtke <ronnystandtke>
2004-07-20 07:28:11 +0000

    added German Translation

--- script/wc/dialogs.lua
+++ script/wc/dialogs.lua
@@ -1,44 +1,56 @@
 dialog_addDialog("wc-v-oblibene", "en", "", "font_big", "Toilet is David`s favorite place.")
 dialog_addDialog("wc-v-oblibene", "cs", "sound/"..codename.."/cs/wc-v-oblibene.ogg", "font_big", "WC je oblíbeným Davidovým místem.")
 dialog_addDialog("wc-v-oblibene", "fr", "", "font_big", "Les toilettes est le lieu préféré de David.")
+dialog_addDialog("wc-v-oblibene", "de", "", "font_big", "Die Toilette ist Davids Lieblingsplatz.")
 
 dialog_addDialog("wc-m-coze", "en", "", "font_small", "What?")
 dialog_addDialog("wc-m-coze", "cs", "sound/"..codename.."/cs/wc-m-coze.ogg", "font_small", "Cože?")
 dialog_addDialog("wc-m-coze", "fr", "", "font_small", "Pardon ?")
+dialog_addDialog("wc-m-coze", "de", "", "font_small", "Wie bitte?")
 
 dialog_addDialog("wc-v-neznas", "en", "", "font_big", "You don`t know David? It`s one of the artists who worked on this game.")
 dialog_addDialog("wc-v-neznas", "cs", "sound/"..codename.."/cs/wc-v-neznas.ogg", "font_big", "Ty neznáš Davida? To je grafik, který na téhle hře pracoval!")
 dialog_addDialog("wc-v-neznas", "fr", "", "font_big", "Tu ne connais pas David ? Il est l'un des artistes qui a travaillé sur ce jeu.")
+dialog_addDialog("wc-v-neznas", "de", "", "font_big", "Du kennst David nicht? Er ist einer der Künstler, der an diesem Spiel gearbeitet hat.")
 
 dialog_addDialog("wc-m-sochar", "en", "", "font_small", "David was no graphic artist. He was a sculpturer. His sculptures are one of the best-known Renaissance artifacts.")
 dialog_addDialog("wc-m-sochar", "cs", "sound/"..codename.."/cs/wc-m-sochar.ogg", "font_small", "David nebyl žádný malíř. David byl sochař. Davidova socha je jednou z nejznámějších renesančních památek.")
 dialog_addDialog("wc-m-sochar", "fr", "", "font_small", "David n'est pas dessinateur. Il est sculpteur. Ces sculptures sont parmi les plus connues de la Renaissance.")
+dialog_addDialog("wc-m-sochar", "de", "", "font_small", "David war kein Grafiker. Er war Bildhauer. Seine Plastiken gehören zu den bekanntesten Gegenständen der Renaissance.")
 
 dialog_addDialog("wc-v-zmatek", "en", "", "font_big", "You`ve got it all wrong.")
 dialog_addDialog("wc-v-zmatek", "cs", "sound/"..codename.."/cs/wc-v-zmatek.ogg", "font_big", "Máš v tom nějaký zmatek.")
 dialog_addDialog("wc-v-zmatek", "fr", "", "font_big", "Tu as tout faux.")
+dialog_addDialog("wc-v-zmatek", "de", "", "font_big", "Du hast alles falsch verstanden.")
 
 dialog_addDialog("wc-m-prasecinky", "en", "", "font_small", "Didn`t I tell you, that WE don`t need a flushing toilet? And decorated with your smut, too!")
 dialog_addDialog("wc-m-prasecinky", "cs", "sound/"..codename.."/cs/wc-m-prasecinky.ogg", "font_small", "Já jsem ti říkala, že zrovna my splachovací záchod nepotřebujeme. A ještě tu visí ty tvoje prasečinky.")
 dialog_addDialog("wc-m-prasecinky", "fr", "", "font_small", "Ne t'avais-je pas dis que NOUS n'avons pas besoin de chasse d'eau ? Et décoré avec tes mochetés, en plus !")
+dialog_addDialog("wc-m-prasecinky", "de", "", "font_small", "Habe ich Dir nicht gesagt, dass WIR keine Toilette mit Wasserspülung brauchen?")
 
 dialog_addDialog("wc-v-hygiena", "en", "", "font_big", "The flushing toilet is more hygienical and when under water even more ecological.")
 dialog_addDialog("wc-v-hygiena", "cs", "sound/"..codename.."/cs/wc-v-hygiena.ogg", "font_big", "Splachovací záchod je hygieničtější a pod vodou i ekologičtější.")
 dialog_addDialog("wc-v-hygiena", "fr", "", "font_big", "La chasse d'eau est plus hygiénique et sous l'eau même plus écologique.")
+dialog_addDialog("wc-v-hygiena", "de", "", "font_big", "Eine Wasserspülung ist hygienischer und unter Wasser auch umweltfreundlicher.")
 
 dialog_addDialog("wc-m-vlezt", "en", "", "font_small", "Luckily, I needn`t climb inside.")
 dialog_addDialog("wc-m-vlezt", "cs", "sound/"..codename.."/cs/wc-m-vlezt.ogg", "font_small", "Ještě, že nemusím vlézt přímo do mísy.")
 dialog_addDialog("wc-m-vlezt", "fr", "", "font_small", "Heureusement, je ne dois pas grimper dedans.")
+dialog_addDialog("wc-m-vlezt", "de", "", "font_small", "Zum Glück muss ich nicht reinklettern.")
 
 dialog_addDialog("wc-m-hrbitov", "en", "", "font_small", "I don`t feel very well here. I feel like in the cemetery.")
 dialog_addDialog("wc-m-hrbitov", "cs", "sound/"..codename.."/cs/wc-m-hrbitov.ogg", "font_small", "Necítím se tady dobře. Připadám si jako na hřbitově.")
 dialog_addDialog("wc-m-hrbitov", "fr", "", "font_small", "Je ne me sens pas très bien ici. Je me sens comme au cimetière.")
+dialog_addDialog("wc-m-hrbitov", "de", "", "font_small", "Ich fühle mich hier nicht gut. Ist wie auf dem Friedhof.")
 
 dialog_addDialog("wc-v-coze", "en", "", "font_big", "What do you mean?")
 dialog_addDialog("wc-v-coze", "cs", "sound/"..codename.."/cs/wc-v-coze.ogg", "font_big", "Cože?")
 dialog_addDialog("wc-v-coze", "fr", "", "font_big", "Que veux-tu dire ?")
+dialog_addDialog("wc-v-coze", "de", "", "font_big", "Was meinst Du damit?")
 
 dialog_addDialog("wc-m-nevis", "en", "", "font_small", "Don`t you know where so many aquarium fish end their life?")
 dialog_addDialog("wc-m-nevis", "cs", "sound/"..codename.."/cs/wc-m-nevis.ogg", "font_small", "Copak nevíš, kde končí svůj život spousta akvarijních rybiček?")
 dialog_addDialog("wc-m-nevis", "fr", "", "font_small", "Ne sais-tu pas ou tant de poissons d'aquarium finissent leur vie ?")
+dialog_addDialog("wc-m-nevis", "de", "", "font_small", "Weißt Du nicht, wo so viele Aquariumfische ihr Leben lassen?")
+dialog_addDialog("wc-m-nevis", "de_CH", "", "font_small", "Weisst Du nicht, wo so viele Aquariumfische ihr Leben lassen?")
 
