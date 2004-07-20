2a223592f96d46348a0b1474c34507c876de0254
mrpaja <mrpaja>
2004-07-20 16:02:44 +0000

    french translation update

--- script/imprisoned/dialogs.lua
+++ script/imprisoned/dialogs.lua
@@ -1,44 +1,58 @@
 dialog_addDialog("ncp-m-tesno0", "en", "", "font_small", "Look, how I am supposed to get out of here?")
 dialog_addDialog("ncp-m-tesno0", "cs", "sound/"..codename.."/cs/ncp-m-tesno0.ogg", "font_small", "Hele, jak mám z toho vylézt?")
+dialog_addDialog("ncp-m-tesno0", "fr", "", "font_small", "Regarde, Comment vais-je me sortir d'ici ?")
 
 dialog_addDialog("ncp-m-tesno1", "en", "", "font_small", "It`s quite tight in here.")
 dialog_addDialog("ncp-m-tesno1", "cs", "sound/"..codename.."/cs/ncp-m-tesno1.ogg", "font_small", "Je tu celkem těsno.")
+dialog_addDialog("ncp-m-tesno1", "fr", "", "font_small", "Je suis plutôt à l'étroit ici.")
 
 dialog_addDialog("ncp-v-dostala", "en", "", "font_big", "How did you manage to get there?")
 dialog_addDialog("ncp-v-dostala", "cs", "sound/"..codename.."/cs/ncp-v-dostala.ogg", "font_big", "Jak ses tam dostala?")
+dialog_addDialog("ncp-v-dostala", "fr", "", "font_big", "Comment tu t'es débrouillé pour arriver là-dedans ?")
 
 dialog_addDialog("ncp-v-sasanka", "en", "", "font_big", "Stop playing with anemone and help me to get us out of here.")
 dialog_addDialog("ncp-v-sasanka", "cs", "sound/"..codename.."/cs/ncp-v-sasanka.ogg", "font_big", "Nehrej si se sasankou a pojďme se radši nějak dostat ven.")
+dialog_addDialog("ncp-v-sasanka", "fr", "", "font_big", "Arrête de jouer avec l'anémone et aide-moi à nous sortir d'ici.")
 
 dialog_addDialog("ncp-m-nekoukej", "en", "", "font_small", "Don`t be surprised.")
 dialog_addDialog("ncp-m-nekoukej", "cs", "sound/"..codename.."/cs/ncp-m-nekoukej.ogg", "font_small", "Moc nekoukej.")
+dialog_addDialog("ncp-m-nekoukej", "fr", "", "font_small", "Ne soit pas étonné.")
 
 dialog_addDialog("ncp-m-komari", "en", "", "font_small", "There is so many carols here...")
 dialog_addDialog("ncp-m-komari", "cs", "sound/"..codename.."/cs/ncp-m-komari.ogg", "font_small", "Tady je ale všude komárů...")
+dialog_addDialog("ncp-m-komari", "fr", "", "font_small", "Il y a tellement de corales ici...")
 
 dialog_addDialog("ncp-v-ceho", "en", "", "font_big", "What?")
 dialog_addDialog("ncp-v-ceho", "cs", "sound/"..codename.."/cs/ncp-v-ceho.ogg", "font_big", "Čeho?")
+dialog_addDialog("ncp-v-ceho", "fr", "", "font_big", "Quoi ?")
 
 dialog_addDialog("ncp-m-koraly", "en", "", "font_small", "So many corals. Are you deaf or what?")
 dialog_addDialog("ncp-m-koraly", "cs", "sound/"..codename.."/cs/ncp-m-koraly.ogg", "font_small", "No korálů, jsi hluchý?")
+dialog_addDialog("ncp-m-koraly", "fr", "", "font_small", "Tellement de corail. tu es sourd ou quoi ?")
 
 dialog_addDialog("ncp-m-tvrdy", "en", "", "font_small", "Oh my, this is a hard coral.")
 dialog_addDialog("ncp-m-tvrdy", "cs", "sound/"..codename.."/cs/ncp-m-tvrdy.ogg", "font_small", "To je ale tvrdý korál.")
+dialog_addDialog("ncp-m-tvrdy", "fr", "", "font_small", "Hé bien, c'est un corail dur.")
 
 dialog_addDialog("ncp-v-mekky", "en", "", "font_big", "Oh my, this is a soft coral.")
 dialog_addDialog("ncp-v-mekky", "cs", "sound/"..codename.."/cs/ncp-v-mekky.ogg", "font_big", "To je ale měkký korál.")
+dialog_addDialog("ncp-v-mekky", "fr", "", "font_big", "Hé bien, c'est un corail doux.")
 
 dialog_addDialog("ncp-v-tak", "en", "", "font_big", "So!")
 dialog_addDialog("ncp-v-tak", "cs", "sound/"..codename.."/cs/ncp-v-tak.ogg", "font_big", "Tak!")
+dialog_addDialog("ncp-v-tak", "fr", "", "font_big", "Et !")
 
 dialog_addDialog("ncp-m-muzes", "en", "", "font_small", "Now you can get out, can`t you?")
 dialog_addDialog("ncp-m-muzes", "cs", "sound/"..codename.."/cs/ncp-m-muzes.ogg", "font_small", "Teď už můžeš ven, ne?")
+dialog_addDialog("ncp-m-muzes", "fr", "", "font_small", "Maintenant tu peux sortir, non ?")
 
 dialog_addDialog("ncp-v-neprojedu", "en", "", "font_big", "Hmm, I can`t get through here...")
 dialog_addDialog("ncp-v-neprojedu", "cs", "sound/"..codename.."/cs/ncp-v-neprojedu.ogg", "font_big", "Hmm, tak tudy neprojedu...")
+dialog_addDialog("ncp-v-neprojedu", "fr", "", "font_big", "Humm, je peux traverser ici...")
 
 dialog_addDialog("ncp-m-barvy", "en", "", "font_small", "I love colors...")
 dialog_addDialog("ncp-m-barvy", "cs", "sound/"..codename.."/cs/ncp-m-barvy.ogg", "font_small", "Miluju barvy...")
+dialog_addDialog("ncp-m-barvy", "fr", "", "font_small", "J'adore les couleurs...")
 
 dialog_addDialog("ncp-x-tik", "en", "sound/"..codename.."/en/ncp-x-tik.ogg")
 
--- script/kitchen/dialogs.lua
+++ script/kitchen/dialogs.lua
@@ -1,109 +1,145 @@
 dialog_addDialog("kuch-m-objev0", "en", "", "font_small", "Oh my, this is a nice kitchen.")
 dialog_addDialog("kuch-m-objev0", "cs", "sound/"..codename.."/cs/kuch-m-objev0.ogg", "font_small", "To je ale hezká kuchyně.")
+dialog_addDialog("kuch-m-objev0", "fr", "", "font_small", "Hé bien, c'est une chouette cuisine.")
 
 dialog_addDialog("kuch-m-objev1", "en", "", "font_small", "Yeah, we discovered the ship kitchen.")
 dialog_addDialog("kuch-m-objev1", "cs", "sound/"..codename.."/cs/kuch-m-objev1.ogg", "font_small", "Vida, objevili jsme lodní kuchyni.")
+dialog_addDialog("kuch-m-objev1", "fr", "", "font_small", "Ouais, nous avons découvert la cuisine du vaisseau.")
 
 dialog_addDialog("kuch-m-objev2", "en", "", "font_small", "Darned kitchen.")
 dialog_addDialog("kuch-m-objev2", "cs", "sound/"..codename.."/cs/kuch-m-objev2.ogg", "font_small", "Zatracená kuchyně.")
+dialog_addDialog("kuch-m-objev2", "fr", "", "font_small", "Sacrée cuisine.")
 
 dialog_addDialog("kuch-m-objev3", "en", "", "font_small", "And now we are in the captain Silver`s kitchen.")
 dialog_addDialog("kuch-m-objev3", "cs", "sound/"..codename.."/cs/kuch-m-objev3.ogg", "font_small", "Tak, a jsme v kuchyni kapitána Silvera.")
+dialog_addDialog("kuch-m-objev3", "fr", "", "font_small", "Et maintenant nous sommes dans la cuisine du capitaine Silver.")
 
 dialog_addDialog("kuch-v-varil", "en", "", "font_big", "I don`t think he was a cook himself.")
 dialog_addDialog("kuch-v-varil", "cs", "sound/"..codename.."/cs/kuch-v-varil.ogg", "font_big", "Nemyslím, že si vařil sám.")
+dialog_addDialog("kuch-v-varil", "fr", "", "font_big", "Je ne pense pas qu'il cuisinait lui-même.")
 
 dialog_addDialog("kuch-v-problem", "en", "", "font_big", "But this is not our problem.")
 dialog_addDialog("kuch-v-problem", "cs", "sound/"..codename.."/cs/kuch-v-problem.ogg", "font_big", "Ale to není náš problém.")
+dialog_addDialog("kuch-v-problem", "fr", "", "font_big", "Mais ce n'est pas notre problème.")
 
 dialog_addDialog("kuch-m-noproblem", "en", "", "font_small", "There are not many problems here. I can swim out at once.")
 dialog_addDialog("kuch-m-noproblem", "cs", "sound/"..codename.."/cs/kuch-m-noproblem.ogg", "font_small", "Tady vůbec moc problémů není. Já můžu odplout hned.")
+dialog_addDialog("kuch-m-noproblem", "fr", "", "font_small", "Il n'y a pas de problème ici. Je peux nager vers la sortie.")
 
 dialog_addDialog("kuch-v-podivej", "en", "", "font_big", "But just have a look at me.")
 dialog_addDialog("kuch-v-podivej", "cs", "sound/"..codename.."/cs/kuch-v-podivej.ogg", "font_big", "No tak se podívej, kde jsem já.")
+dialog_addDialog("kuch-v-podivej", "fr", "", "font_big", "Mais regarde moi un peu.")
 
 dialog_addDialog("kuch-v-stolky0", "en", "", "font_big", "I can`t understand how these serving tables got there.")
 dialog_addDialog("kuch-v-stolky0", "cs", "sound/"..codename.."/cs/kuch-v-stolky0.ogg", "font_big", "Nechápu, jak sem dostali ty servírovací stolky.")
+dialog_addDialog("kuch-v-stolky0", "fr", "", "font_big", "Je ne comprends pas comment ces tables sont arrivées là.")
 
 dialog_addDialog("kuch-v-stolky1", "en", "", "font_big", "These tables are in the way.")
 dialog_addDialog("kuch-v-stolky1", "cs", "sound/"..codename.."/cs/kuch-v-stolky1.ogg", "font_big", "Ty stolky mně tady hrozně zavazí.")
+dialog_addDialog("kuch-v-stolky1", "fr", "", "font_big", "Ces tables bloquent le chemin.")
 
 dialog_addDialog("kuch-v-stolky2", "en", "", "font_big", "This little chamber gives me the creeps.")
 dialog_addDialog("kuch-v-stolky2", "cs", "sound/"..codename.."/cs/kuch-v-stolky2.ogg", "font_big", "Mám už z téhle komůrky silně klaustrofobní pocit.")
+dialog_addDialog("kuch-v-stolky2", "fr", "", "font_big", "Cette petit pièce me donne la frousse.")
 
 dialog_addDialog("kuch-v-odsud0", "en", "", "font_big", "I can`t get out of here!")
 dialog_addDialog("kuch-v-odsud0", "cs", "sound/"..codename.."/cs/kuch-v-odsud0.ogg", "font_big", "Nemůžu se odsud dostat!")
+dialog_addDialog("kuch-v-odsud0", "fr", "", "font_big", "Je ne peux pas sortir d'ici !")
 
 dialog_addDialog("kuch-m-premyslim0", "en", "", "font_small", "I`m just thinking how to help you.")
 dialog_addDialog("kuch-m-premyslim0", "cs", "sound/"..codename.."/cs/kuch-m-premyslim0.ogg", "font_small", "Zrovna přemýšlím, jak ti pomoci.")
+dialog_addDialog("kuch-m-premyslim0", "fr", "", "font_small", "Je réfléchis simplement à comment t'aider.")
 
 dialog_addDialog("kuch-v-odsud1", "en", "", "font_big", "Help me somehow!")
 dialog_addDialog("kuch-v-odsud1", "cs", "sound/"..codename.."/cs/kuch-v-odsud1.ogg", "font_big", "Pomoz mi nějak!")
+dialog_addDialog("kuch-v-odsud1", "fr", "", "font_big", "Aide moi à tout prix !")
 
 dialog_addDialog("kuch-m-premyslim1", "en", "", "font_small", "I am thinking...")
 dialog_addDialog("kuch-m-premyslim1", "cs", "sound/"..codename.."/cs/kuch-m-premyslim1.ogg", "font_small", "Přemýšlím...")
+dialog_addDialog("kuch-m-premyslim1", "fr", "", "font_small", "Je réfléchis...")
 
 dialog_addDialog("kuch-m-premyslim2", "en", "", "font_small", "I am on my way.")
 dialog_addDialog("kuch-m-premyslim2", "cs", "sound/"..codename.."/cs/kuch-m-premyslim2.ogg", "font_small", "Už se na tom pracuje.")
+dialog_addDialog("kuch-m-premyslim2", "fr", "", "font_small", "J'arrive.")
 
 dialog_addDialog("kuch-m-hrnec0", "en", "", "font_small", "Yuck, there is something left in this pot!")
 dialog_addDialog("kuch-m-hrnec0", "cs", "sound/"..codename.."/cs/kuch-m-hrnec0.ogg", "font_small", "Fuj, v tom hrnci je něco připečené!")
+dialog_addDialog("kuch-m-hrnec0", "fr", "", "font_small", "Beurk, il reste quelque chose dans ce pot !")
 
 dialog_addDialog("kuch-m-hrnec1", "en", "", "font_small", "This pot wasn`t even washed up.")
 dialog_addDialog("kuch-m-hrnec1", "cs", "sound/"..codename.."/cs/kuch-m-hrnec1.ogg", "font_small", "Ten hrnec není ani umytý.")
+dialog_addDialog("kuch-m-hrnec1", "fr", "", "font_small", "Ce pot n'a jamais été lavé.")
 
 dialog_addDialog("kuch-m-hrnec2", "en", "", "font_small", "Nobody washes the dishes here.")
 dialog_addDialog("kuch-m-hrnec2", "cs", "sound/"..codename.."/cs/kuch-m-hrnec2.ogg", "font_small", "Tady asi nikdo neumývá nádobí.")
+dialog_addDialog("kuch-m-hrnec2", "fr", "", "font_small", "Personne ne fait la vaisselle ici.")
 
 dialog_addDialog("kuch-v-kreslo0", "en", "", "font_big", "If there was no chair, it would save us a lot of work.")
 dialog_addDialog("kuch-v-kreslo0", "cs", "sound/"..codename.."/cs/kuch-v-kreslo0.ogg", "font_big", "Kdyby tu nebylo to křeslo, ušetřili by jsme si spoustu práce.")
+dialog_addDialog("kuch-v-kreslo0", "fr", "", "font_big", "Si il n'y avait pas de fauteuil, nous aurions beaucoup moins de travail.")
 
 dialog_addDialog("kuch-v-ja", "en", "", "font_big", "Especially myself.")
 dialog_addDialog("kuch-v-ja", "cs", "sound/"..codename.."/cs/kuch-v-ja.ogg", "font_big", "Hlavně já.")
+dialog_addDialog("kuch-v-ja", "fr", "", "font_big", "Surtout moi.")
 
 dialog_addDialog("kuch-m-kreslo0", "en", "", "font_small", "What is a chair doing in a kitchen?")
 dialog_addDialog("kuch-m-kreslo0", "cs", "sound/"..codename.."/cs/kuch-m-kreslo0.ogg", "font_small", "Co vlastně dělá křeslo v kuchyni?")
+dialog_addDialog("kuch-m-kreslo0", "fr", "", "font_small", "Que fait un fauteuil dans une cuisine ?")
 
 dialog_addDialog("kuch-v-kreslo1", "en", "", "font_big", "There should be no chairs in the kitchen.")
 dialog_addDialog("kuch-v-kreslo1", "cs", "sound/"..codename.."/cs/kuch-v-kreslo1.ogg", "font_big", "Křeslo nemá co dělat v kuchyni.")
+dialog_addDialog("kuch-v-kreslo1", "fr", "", "font_big", "Il ne devrait pas y avoir de fauteuil dans une cuisine.")
 
 dialog_addDialog("kuch-m-kreslo2", "en", "", "font_small", "Everybody want to rest sometimes.")
 dialog_addDialog("kuch-m-kreslo2", "cs", "sound/"..codename.."/cs/kuch-m-kreslo2.ogg", "font_small", "Každý si chce někdy odpočinout.")
+dialog_addDialog("kuch-m-kreslo2", "fr", "", "font_small", "Tout le monde veut se reposer par moment.")
 
 dialog_addDialog("kuch-m-stolky", "en", "", "font_small", "I have seen these tables somewhere.")
 dialog_addDialog("kuch-m-stolky", "cs", "sound/"..codename.."/cs/kuch-m-stolky.ogg", "font_small", "Ty stolky jsem už někde viděla.")
+dialog_addDialog("kuch-m-stolky", "fr", "", "font_small", "J'ai vu ces tables quelque part.")
 
 dialog_addDialog("kuch-v-serie", "en", "", "font_big", "They are mass-produced, you know.")
 dialog_addDialog("kuch-v-serie", "cs", "sound/"..codename.."/cs/kuch-v-serie.ogg", "font_big", "To víš, sériová dřevařská práce.")
+dialog_addDialog("kuch-v-serie", "fr", "", "font_big", "Elles sont produites en masse, tu sais.")
 
 dialog_addDialog("kuch-m-pekne", "en", "", "font_small", "But they are nice. We`ll order us a few, too.")
 dialog_addDialog("kuch-m-pekne", "cs", "sound/"..codename.."/cs/kuch-m-pekne.ogg", "font_small", "Ale jsou pěkné. Taky si takové pořídíme.")
+dialog_addDialog("kuch-m-pekne", "fr", "", "font_small", "Mais elles sont pas mal. Commandons en quelques unes, nous aussi.")
 
 dialog_addDialog("kuch-v-obavam", "en", "", "font_big", "I am afraid that only I can get out of this space.")
 dialog_addDialog("kuch-v-obavam", "cs", "sound/"..codename.."/cs/kuch-v-obavam.ogg", "font_big", "Obávám se, že do této části se dostanu jenom já.")
+dialog_addDialog("kuch-v-obavam", "fr", "", "font_big", "J'ai peur de simplement sortir de là.")
 
 dialog_addDialog("kuch-m-zapeklite", "en", "", "font_small", "It seems that even this ship contains some demanding problems.")
 dialog_addDialog("kuch-m-zapeklite", "cs", "sound/"..codename.."/cs/kuch-m-zapeklite.ogg", "font_small", "Vypadá to, že jsou i v této lodi skutečně zapeklité problémy.")
+dialog_addDialog("kuch-m-zapeklite", "fr", "", "font_small", "Il semblerait que même ce navire contienne quelques problèmes captivants.")
 
 dialog_addDialog("kuch-v-mec", "en", "", "font_big", "Why is that sword here?")
 dialog_addDialog("kuch-v-mec", "cs", "sound/"..codename.."/cs/kuch-v-mec.ogg", "font_big", "Proč je tu ten meč?")
+dialog_addDialog("kuch-v-mec", "fr", "", "font_big", "Pourquoi est-ce qu'il y a une épée ici ?")
 
 dialog_addDialog("kuch-m-porcovani", "en", "", "font_small", "Maybe they used it to cut the meat.")
 dialog_addDialog("kuch-m-porcovani", "cs", "sound/"..codename.."/cs/kuch-m-porcovani.ogg", "font_small", "Asi ho používali k porcování masa.")
+dialog_addDialog("kuch-m-porcovani", "fr", "", "font_small", "Peut-être l'utilisaient-ils pour couper la viande.")
 
 dialog_addDialog("kuch-v-nedela", "en", "", "font_big", "Certainly. But you don`t cut meat with a sword in the kitchen, usually.")
 dialog_addDialog("kuch-v-nedela", "cs", "sound/"..codename.."/cs/kuch-v-nedela.ogg", "font_big", "To jistě. Ale s mečem se to obvykle nedělá v kuchyni.")
+dialog_addDialog("kuch-v-nedela", "fr", "", "font_big", "Certainement. Mais on ne coupe pas la viande avec une épée habituellement.")
 
 dialog_addDialog("kuch-m-kuchari", "en", "", "font_small", "We are lucky there are no cooks here. They could try to cook us.")
 dialog_addDialog("kuch-m-kuchari", "cs", "sound/"..codename.."/cs/kuch-m-kuchari.ogg", "font_small", "Ještě, že už tu nejsou kuchaři. Ještě by nás mohli chtít uvařit.")
+dialog_addDialog("kuch-m-kuchari", "fr", "", "font_small", "Nous avons de la chance, il n'y a pas de cuisinier ici. Ils pourrait essayer de nous cuire.")
 
 dialog_addDialog("kuch-v-svitek0", "en", "", "font_big", "There must be some recipe on that parchment.")
 dialog_addDialog("kuch-v-svitek0", "cs", "sound/"..codename.."/cs/kuch-v-svitek0.ogg", "font_big", "Na tom svitku je nějaký recept.")
+dialog_addDialog("kuch-v-svitek0", "fr", "", "font_big", "Il doit y avoir une recette sur ce parchemin.")
 
 dialog_addDialog("kuch-v-svitek1", "en", "", "font_big", "There must be some culinary mystery on that parchment.")
 dialog_addDialog("kuch-v-svitek1", "cs", "sound/"..codename.."/cs/kuch-v-svitek1.ogg", "font_big", "Na tom svitku je asi nějaké kulinářské tajemství.")
+dialog_addDialog("kuch-v-svitek1", "fr", "", "font_big", "Il doit y avoir quelque mystère culinaire sur ce parchemin.")
 
 dialog_addDialog("kuch-m-recept", "en", "", "font_small", "Possibly, how to cook the fish.")
 dialog_addDialog("kuch-m-recept", "cs", "sound/"..codename.."/cs/kuch-m-recept.ogg", "font_small", "Třeba recept na úpravu ryb.")
+dialog_addDialog("kuch-m-recept", "fr", "", "font_small", "Probablement, comment cuire les poissons.")
 
 
