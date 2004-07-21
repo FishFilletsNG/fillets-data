bfbb5dc44104cb593038675713b60188f80029e8
ronnystandtke <ronnystandtke>
2004-07-21 17:06:46 +0000

    added German translation

--- script/airplane/dialogs.lua
+++ script/airplane/dialogs.lua
@@ -1,33 +1,42 @@
 dialog_addDialog("let-m-divna", "en", "", "font_small", "What kind of strange ship is that?")
 dialog_addDialog("let-m-divna", "cs", "sound/"..codename.."/cs/let-m-divna.ogg", "font_small", "Co je to za divnou loď?")
 dialog_addDialog("let-m-divna", "fr", "", "font_small", "Quel est ce drôle de navire ?")
+dialog_addDialog("let-m-divna", "de", "", "font_small", "Was für ein seltsames Schiff ist das denn?")
 
 dialog_addDialog("let-v-vrak0", "en", "", "font_big", "This is a wreck of civilian airplane LC-10 Lemura.")
 dialog_addDialog("let-v-vrak0", "cs", "sound/"..codename.."/cs/let-v-vrak0.ogg", "font_big", "To je vrak dopravního letadla LC-10 Lemura.")
 dialog_addDialog("let-v-vrak0", "fr", "", "font_big", "C'est l'épave d'un avion civile LC-10 Lemura.")
+dialog_addDialog("let-v-vrak0", "de", "", "font_big", "Das ist das Wrack des Passagierfluzeuges LC-10 Lemura.")
 
 dialog_addDialog("let-v-vrak1", "en", "", "font_big", "This is a wreck of civilian airplane Atlantobus.")
 dialog_addDialog("let-v-vrak1", "cs", "sound/"..codename.."/cs/let-v-vrak1.ogg", "font_big", "To je vrak dopravního letadla Atlantobus.")
 dialog_addDialog("let-v-vrak1", "fr", "", "font_big", "Ca, c'est l'épave d'un avion civile Atlantobus.")
+dialog_addDialog("let-v-vrak1", "de", "", "font_big", "Das ist das Wrack des Passagierfluzeuges Atlantobus.")
 
 dialog_addDialog("let-v-vrak2", "en", "", "font_big", "This is a wreck of civilian airplane Poseidon 737.")
 dialog_addDialog("let-v-vrak2", "cs", "sound/"..codename.."/cs/let-v-vrak2.ogg", "font_big", "To je vrak dopravního letadla Poseidon 737.")
 dialog_addDialog("let-v-vrak2", "fr", "", "font_big", "Et ici, d'un avion civile Poseidon 737.")
+dialog_addDialog("let-v-vrak2", "de", "", "font_big", "Das ist das Wrack des Passagierfluzeuges Poseidon 737.")
 
 dialog_addDialog("let-v-oko", "en", "", "font_big", "Can you see that eye? The silent witness to the tragedy... Somebody trusted this airplane - and the glass eye is everything he left behind.")
 dialog_addDialog("let-v-oko", "cs", "sound/"..codename.."/cs/let-v-oko.ogg", "font_big", "Vidíš to oko? Němý svědek tragédie... Někdo v důvěře usedl do letadla - a zůstalo z něho jen skleněné oko.")
 dialog_addDialog("let-v-oko", "fr", "", "font_big", "Peux-tu voir cet oeil ? Témoin silencieux de la tragédie...Une personne avait foi dans cet avion - et cet oeil de verre est la seule chose qu'il a laissée.")
+dialog_addDialog("let-v-oko", "de", "", "font_big", "Siehst Du das Auge? Ein stiller Zeuge der Tragödie... Jemand traute diesem Flugzeug - und das Glasauge ist alles, was er hinterließ.")
+dialog_addDialog("let-v-oko", "de_CH", "", "font_big", "Siehst Du das Auge? Ein stiller Zeuge der Tragödie... Jemand traute diesem Flugzeug - und das Glasauge ist alles, was er hinterliess.")
 
 dialog_addDialog("let-m-oko", "en", "", "font_small", "This is not a glass eye but gyroscope. At least in this level.")
 dialog_addDialog("let-m-oko", "cs", "sound/"..codename.."/cs/let-m-oko.ogg", "font_small", "To není skleněné oko, ale gyroskop. Aspoň v této místnosti.")
 dialog_addDialog("let-m-oko", "fr", "", "font_small", "Ce n'est pas un oeil de verre mais un gyroscope. Au moins dans ce niveau.")
+dialog_addDialog("let-m-oko", "de", "", "font_small", "Das ist kein Glasauge sondern ein Kreisel. Zumindest in dieser Ebene.")
 
 dialog_addDialog("let-m-sedadlo", "en", "", "font_small", "Seats. Why is there so many seats here?")
 dialog_addDialog("let-m-sedadlo", "cs", "sound/"..codename.."/cs/let-m-sedadlo.ogg", "font_small", "Sedadla. Proč jsou tu všude sedadla?")
 dialog_addDialog("let-m-sedadlo", "fr", "", "font_small", "Des sièges. Pourquoi y en a-t-il autant ici ?")
+dialog_addDialog("let-m-sedadlo", "de", "", "font_small", "Sitze. Warum sind hier so viele Sitze?")
 
 dialog_addDialog("let-v-budrada", "en", "", "font_big", "Be thankful. Could you get out without them?")
 dialog_addDialog("let-v-budrada", "cs", "sound/"..codename.."/cs/let-v-budrada.ogg", "font_big", "Buď ráda. Jak by ses jinak dostala ven?")
 dialog_addDialog("let-v-budrada", "fr", "", "font_big", "Soit reconnaissant. pourrions nous sortir sans eux ?")
+dialog_addDialog("let-v-budrada", "de", "", "font_big", "Sei dankbar. Könntest Du ohne sie rauskommen?")
 
 
