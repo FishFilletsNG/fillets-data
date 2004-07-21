d4ce1276cc98b269d52acf3e6ac0e098e92d31c2
ronnystandtke <ronnystandtke>
2004-07-21 07:43:20 +0000

    added German translation

--- script/viking1/dialogs.lua
+++ script/viking1/dialogs.lua
@@ -17,82 +17,102 @@ dialog_addDialog("d1-z-v2", "en", "sound/"..codename.."/en/d1-z-v2.ogg")
 dialog_addDialog("dr-m-tojesnad", "en", "", "font_small", "This must be a Viking ship!")
 dialog_addDialog("dr-m-tojesnad", "cs", "sound/"..codename.."/cs/dr-m-tojesnad.ogg", "font_small", "To je snad vikingská loď!")
 dialog_addDialog("dr-m-tojesnad", "fr", "", "font_small", "Ce doit être un bateau viking.")
+dialog_addDialog("dr-m-tojesnad", "de", "", "font_small", "Das muss ein Wikingerschiff sein!")
 
 dialog_addDialog("dr-v-jiste", "en", "", "font_big", "Sure, it`s a drakkar.")
 dialog_addDialog("dr-v-jiste", "cs", "sound/"..codename.."/cs/dr-v-jiste.ogg", "font_big", "Jistě, je to drakkar.")
 dialog_addDialog("dr-v-jiste", "fr", "", "font_big", "Sûrement, c'est un drakkar.")
+dialog_addDialog("dr-v-jiste", "de", "", "font_big", "Sicher, es ist eine Drakkar.")
 
 dialog_addDialog("dr-m-musela", "en", "", "font_small", "She must have sunk fast. The oarsmen are still in their places.")
 dialog_addDialog("dr-m-musela", "cs", "sound/"..codename.."/cs/dr-m-musela.ogg", "font_small", "Musela se potopit hrozně rychle. Veslaři ještě sedí na svých místech.")
 dialog_addDialog("dr-m-musela", "fr", "", "font_small", "Il a dû couler très vite. Les rameurs sont toujours à leur place.")
+dialog_addDialog("dr-m-musela", "de", "", "font_small", "Sie muss schnell gesunken sein. Die Ruderer sind noch auf ihren Plätzen.")
 
 dialog_addDialog("dr-v-mozna", "en", "", "font_big", "Maybe they sank her on purpose. Warriors would always accompany their leader on his way to Valhalla.")
 dialog_addDialog("dr-v-mozna", "cs", "sound/"..codename.."/cs/dr-v-mozna.ogg", "font_big", "Možná, že ji potopili schválně. Bojovníci doprovázejí svého velitele do Valhaly...")
 dialog_addDialog("dr-v-mozna", "fr", "", "font_big", "Ils l'ont peut-être voulut. Les guerriers accompagnent toujours leur chef sur le chemin du Walhalla.")
+dialog_addDialog("dr-v-mozna", "de", "", "font_big", "Vielleicht wurde sie auch mit Absicht versenkt. Kämpfer würden ihrem Führer immer auf dem Weg nach Walhalla folgen.")
 
 dialog_addDialog("dr-m-hruza", "en", "", "font_small", "That's awful. They look like they're still alive.")
 dialog_addDialog("dr-m-hruza", "cs", "sound/"..codename.."/cs/dr-m-hruza.ogg", "font_small", "Hrůza... sedí tu jako živí...")
 dialog_addDialog("dr-m-hruza", "fr", "", "font_small", "C'est fou. On dirait qu'ils sont vivants.")
+dialog_addDialog("dr-m-hruza", "de", "", "font_small", "Das ist schrecklich. Die sehen aus, als wären sie immer noch lebendig.")
 
 dialog_addDialog("d1-1-hudba0", "en", "", "font_viking1", "Music is gone!")
 dialog_addDialog("d1-1-hudba0", "cs", "sound/"..codename.."/cs/d1-1-hudba0.ogg", "font_viking1", "Hudba je v tahu!")
 dialog_addDialog("d1-1-hudba0", "fr", "", "font_viking1", "La musique est partie !")
+dialog_addDialog("d1-1-hudba0", "de", "", "font_viking1", "Die Musik ist aus!")
 
 dialog_addDialog("d1-1-hudba1", "en", "", "font_viking1", "They stopped playing!")
 dialog_addDialog("d1-1-hudba1", "cs", "sound/"..codename.."/cs/d1-1-hudba1.ogg", "font_viking1", "Přestali hrát!")
 dialog_addDialog("d1-1-hudba1", "fr", "", "font_viking1", "Ils ont arrêté de jouer.")
+dialog_addDialog("d1-1-hudba1", "de", "", "font_viking1", "Die haben aufgehört zu spielen!")
 
 dialog_addDialog("d1-1-hudba2", "en", "", "font_viking1", "The music stopped again!")
 dialog_addDialog("d1-1-hudba2", "cs", "sound/"..codename.."/cs/d1-1-hudba2.ogg", "font_viking1", "Zase vypadla hudba!")
 dialog_addDialog("d1-1-hudba2", "fr", "", "font_viking1", "La musique s'arrête encore !")
+dialog_addDialog("d1-1-hudba2", "de", "", "font_viking1", "Die Musik hat wieder aufgehört!")
 
 dialog_addDialog("d1-2-brb0", "en", "", "font_viking2", "Oh, no!")
 dialog_addDialog("d1-2-brb0", "cs", "sound/"..codename.."/cs/d1-2-brb0.ogg", "font_viking2", "To snad ne!")
 dialog_addDialog("d1-2-brb0", "fr", "", "font_viking2", "Oh, non !")
+dialog_addDialog("d1-2-brb0", "de", "", "font_viking2", "Oh, nein!")
 
 dialog_addDialog("d1-2-brb1", "en", "", "font_viking2", "Again?")
 dialog_addDialog("d1-2-brb1", "cs", "sound/"..codename.."/cs/d1-2-brb1.ogg", "font_viking2", "Už zase?")
 dialog_addDialog("d1-2-brb1", "fr", "", "font_viking2", "Encore ?")
+dialog_addDialog("d1-2-brb1", "de", "", "font_viking2", "Schon wieder?")
 
 dialog_addDialog("d1-2-brb2", "en", "", "font_viking2", "Oh, not again!")
 dialog_addDialog("d1-2-brb2", "cs", "sound/"..codename.."/cs/d1-2-brb2.ogg", "font_viking2", "Už se to stalo zase?")
 dialog_addDialog("d1-2-brb2", "fr", "", "font_viking2", "Oh, pas encore !")
+dialog_addDialog("d1-2-brb2", "de", "", "font_viking2", "Oh, nicht schon wieder!")
 
 dialog_addDialog("d1-3-brb0", "en", "", "font_viking3", "What happened?")
 dialog_addDialog("d1-3-brb0", "cs", "sound/"..codename.."/cs/d1-3-brb0.ogg", "font_viking3", "Co se děje?")
 dialog_addDialog("d1-3-brb0", "fr", "", "font_viking3", "Qu'est ce qui se passe ?")
+dialog_addDialog("d1-3-brb0", "de", "", "font_viking3", "Was ist passiert?")
 
 dialog_addDialog("d1-3-brb1", "en", "", "font_viking3", "Again?")
 dialog_addDialog("d1-3-brb1", "cs", "sound/"..codename.."/cs/d1-3-brb1.ogg", "font_viking3", "Už zase?")
 dialog_addDialog("d1-3-brb1", "fr", "", "font_viking3", "Encore ?")
+dialog_addDialog("d1-3-brb1", "de", "", "font_viking3", "Schon wieder?")
 
 dialog_addDialog("d1-3-brb2", "en", "", "font_viking3", "Oh, no!")
 dialog_addDialog("d1-3-brb2", "cs", "sound/"..codename.."/cs/d1-3-brb2.ogg", "font_viking3", "To snad ne!")
 dialog_addDialog("d1-3-brb2", "fr", "", "font_viking3", "Oh, non !")
+dialog_addDialog("d1-3-brb2", "de", "", "font_viking3", "Oh, nein!")
 
 dialog_addDialog("d1-4-brb0", "en", "", "font_viking4", "The bloody music stopped!")
 dialog_addDialog("d1-4-brb0", "cs", "sound/"..codename.."/cs/d1-4-brb0.ogg", "font_viking4", "To snad není možný!")
 dialog_addDialog("d1-4-brb0", "fr", "", "font_viking4", "La musique sanglante s'est arrêtée.")
+dialog_addDialog("d1-4-brb0", "de", "", "font_viking4", "Die verdammte Musik hat wieder aufgehört!")
 
 dialog_addDialog("d1-4-brb1", "en", "", "font_viking4", "What are they doing up there?")
 dialog_addDialog("d1-4-brb1", "cs", "sound/"..codename.."/cs/d1-4-brb1.ogg", "font_viking4", "Co tam nahoře dělají?")
 dialog_addDialog("d1-4-brb1", "fr", "", "font_viking4", "Qu'est-ce qu'ils font là haut ?")
+dialog_addDialog("d1-4-brb1", "de", "", "font_viking4", "Was machen die da oben?")
 
 dialog_addDialog("d1-4-brb2", "en", "", "font_viking4", "How could it be?")
 dialog_addDialog("d1-4-brb2", "cs", "sound/"..codename.."/cs/d1-4-brb2.ogg", "font_viking4", "Jak je tohle možný?")
 dialog_addDialog("d1-4-brb2", "fr", "", "font_viking4", "Comment il se peut ?")
+dialog_addDialog("d1-4-brb2", "de", "", "font_viking4", "Wie kann das sein?")
 
 dialog_addDialog("d1-5-nevadi0", "en", "", "font_viking5", "Take it easy, boys. Solo for Olaf!")
 dialog_addDialog("d1-5-nevadi0", "cs", "sound/"..codename.."/cs/d1-5-nevadi0.ogg", "font_viking5", "Nevadí, chapci. Olaf sólo!")
 dialog_addDialog("d1-5-nevadi0", "fr", "", "font_viking5", "C'est facile, les garçons. Un solo pour Olaf !")
+dialog_addDialog("d1-5-nevadi0", "de", "", "font_viking5", "Immer mit der Ruhe, Jungs. Ein Solo für Olaf!")
 
 dialog_addDialog("d1-5-nevadi1", "en", "", "font_viking5", "Never mind that, boys. We can sing ourself!")
 dialog_addDialog("d1-5-nevadi1", "cs", "sound/"..codename.."/cs/d1-5-nevadi1.ogg", "font_viking5", "Nevadí, chapci. Zazpíváme si sami!")
 dialog_addDialog("d1-5-nevadi1", "fr", "", "font_viking5", "Pas de problème, les garçons. nous pouvons chanter nous mêmes.")
+dialog_addDialog("d1-5-nevadi1", "de", "", "font_viking5", "Das macht doch nichts, Jungs. Wir können selbst singen!")
 
 dialog_addDialog("d1-5-nevadi2", "en", "", "font_viking5", "Never mind, boys. We can sing ourself, like in the old times!")
 dialog_addDialog("d1-5-nevadi2", "cs", "sound/"..codename.."/cs/d1-5-nevadi2.ogg", "font_viking5", "To nevadí. Zazpíváme si sami, jako za mlada!")
 dialog_addDialog("d1-5-nevadi2", "fr", "", "font_viking5", "Pas de problème, les garçons. nous pouvons chanter nous mêmes, comme au bon vieux temps.")
+dialog_addDialog("d1-5-nevadi2", "de", "", "font_viking5", "Macht nichts, Jungs. Wir können selbst singen, wie in alten Zeiten!")
 
 
 
