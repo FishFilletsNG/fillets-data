b279a8d3a74b208dd2f0cbd5ab50659ac0c95cbe
ronnystandtke <ronnystandtke>
2004-07-21 05:41:35 +0000

    added German translation

--- script/submarine/dialogs.lua
+++ script/submarine/dialogs.lua
@@ -1,65 +1,83 @@
 dialog_addDialog("zr-x-nabito", "en", "", "font_white", "FIRST TORPEDO BAY LOADED...")
 dialog_addDialog("zr-x-nabito", "cs", "sound/"..codename.."/cs/zr-x-nabito.ogg", "font_white", "PRVNÍ NÁBOJOVÁ KOMORA NABITA...")
 dialog_addDialog("zr-x-nabito", "fr", "", "font_white", "PREMIERE CHAMBRE TORPEDO CHARGEE...")
+dialog_addDialog("zr-x-nabito", "de", "", "font_white", "ERSTES TROPEDOROHR GELADEN...")
 
 dialog_addDialog("zr-m-obliceje", "en", "", "font_small", "Stop making faces and come help me.")
 dialog_addDialog("zr-m-obliceje", "cs", "sound/"..codename.."/cs/zr-m-obliceje.ogg", "font_small", "Nedělej na sebe obličeje a pomoz mi.")
 dialog_addDialog("zr-m-obliceje", "fr", "", "font_small", "Arrête tes grimaces et viens m'aider.")
+dialog_addDialog("zr-m-obliceje", "de", "", "font_small", "Hör auf Grimassen zu ziehen und hilf mir.")
 
 dialog_addDialog("zr-m-prestan", "en", "", "font_small", "Stop looking at yourself in the mirror and make yourself useful.")
 dialog_addDialog("zr-m-prestan", "cs", "sound/"..codename.."/cs/zr-m-prestan.ogg", "font_small", "Přestaň se prohlížet v zrcadle a radši něco dělej.")
 dialog_addDialog("zr-m-prestan", "fr", "", "font_small", "Arrête de t'admirer dans le miroir, et rends toi utile.")
+dialog_addDialog("zr-m-prestan", "de", "", "font_small", "Hör auf, Dich im Spiegel zu bewundern und mach Dich mal nützlich.")
 
 dialog_addDialog("zr-m-takfajn", "en", "", "font_small", "That`s great! He just goes on his merry little way, but what about me?")
 dialog_addDialog("zr-m-takfajn", "cs", "sound/"..codename.."/cs/zr-m-takfajn.ogg", "font_small", "Tak fajn, on si odjede - a co já?")
 dialog_addDialog("zr-m-takfajn", "fr", "", "font_small", "Magnifique ! Il s'en va joyeusement, et moi alors ?")
+dialog_addDialog("zr-m-takfajn", "de", "", "font_small", "Das ist großartig! Er macht sich einfach aus dem Staub. Und ich?")
+dialog_addDialog("zr-m-takfajn", "de_CH", "", "font_small", "Das ist grossartig! Er macht sich einfach aus dem Staub. Und ich?")
 
 dialog_addDialog("zr-m-tojeon", "en", "", "font_small", "As usual. He just leaves me behind.")
 dialog_addDialog("zr-m-tojeon", "cs", "sound/"..codename.."/cs/zr-m-tojeon.ogg", "font_small", "To je celej on. Nechat mě v bryndě...")
 dialog_addDialog("zr-m-tojeon", "fr", "", "font_small", "Comme d'habitude. Il me laisse simplement derrière.")
+dialog_addDialog("zr-m-tojeon", "de", "", "font_small", "Wie immer. Er lässt mich links liegen.")
 
 dialog_addDialog("zr-m-pockej", "en", "", "font_small", "Wait! What about me? I want to get out of here, too!")
 dialog_addDialog("zr-m-pockej", "cs", "sound/"..codename.."/cs/zr-m-pockej.ogg", "font_small", "Počkej, a co já? Já chci taky ven!")
 dialog_addDialog("zr-m-pockej", "fr", "", "font_small", "Attends ! Et moi ! Je veux sortir d'ici moi aussi.")
+dialog_addDialog("zr-m-pockej", "de", "", "font_small", "Warte! Und ich? Ich will hier auch raus!")
 
 dialog_addDialog("zr-v-hej", "en", "", "font_big", "Hey, is anybody down there?")
 dialog_addDialog("zr-v-hej", "cs", "sound/"..codename.."/cs/zr-v-hej.ogg", "font_big", "Hej, jsi tam dole?")
 dialog_addDialog("zr-v-hej", "fr", "", "font_big", "Hé, Il y a quelqu'un en bas ?")
+dialog_addDialog("zr-v-hej", "de", "", "font_big", "Hallo, ist da unten jemand?")
 
 dialog_addDialog("zr-v-halo", "en", "", "font_big", "Hello, can you hear me in there?")
 dialog_addDialog("zr-v-halo", "cs", "sound/"..codename.."/cs/zr-v-halo.ogg", "font_big", "Haló, slyšíš mě tam uvnitř?")
 dialog_addDialog("zr-v-halo", "fr", "", "font_big", "Salut, peux-tu m'entendre là dedans ?")
+dialog_addDialog("zr-v-halo", "de", "", "font_big", "Hallo, kannst Du mich da drin hören?")
 
 dialog_addDialog("zr-v-jetunekdo", "en", "", "font_big", "Is anybody here?")
 dialog_addDialog("zr-v-jetunekdo", "cs", "sound/"..codename.."/cs/zr-v-jetunekdo.ogg", "font_big", "Je tu někdóóó?")
 dialog_addDialog("zr-v-jetunekdo", "fr", "", "font_big", "Il y a quelqu'un ?")
+dialog_addDialog("zr-v-jetunekdo", "de", "", "font_big", "Ist hier jemand?")
 
 dialog_addDialog("zr-m-nervi", "en", "", "font_small", "Stop shouting and just get me out of here.")
 dialog_addDialog("zr-m-nervi", "cs", "sound/"..codename.."/cs/zr-m-nervi.ogg", "font_small", "Neřvi a radši mi nějak pomoz ven.")
 dialog_addDialog("zr-m-nervi", "fr", "", "font_small", "Arrête de crier et sort moi d'ici.")
+dialog_addDialog("zr-m-nervi", "de", "", "font_small", "Hör auf zu brüllen und hole mich hier raus.")
 
 dialog_addDialog("zr-m-nepovykuj", "en", "", "font_small", "Stop your screaming and help me get out of here.")
 dialog_addDialog("zr-m-nepovykuj", "cs", "sound/"..codename.."/cs/zr-m-nepovykuj.ogg", "font_small", "Nech toho povykování a dostaň mě odsud.")
 dialog_addDialog("zr-m-nepovykuj", "fr", "", "font_small", "Cesse de hurler et aide moi à sortir de là.")
+dialog_addDialog("zr-m-nepovykuj", "de", "", "font_small", "Hör auf zu schreien und hilf mir hier rauszukommen.")
 
 dialog_addDialog("zr-m-tadyjsem", "en", "", "font_small", "Here I am. I can`t get out.")
 dialog_addDialog("zr-m-tadyjsem", "cs", "sound/"..codename.."/cs/zr-m-tadyjsem.ogg", "font_small", "Tady jsem. Nemůžu se dostat ven.")
 dialog_addDialog("zr-m-tadyjsem", "fr", "", "font_small", "Voilà. Je peux sortir.")
+dialog_addDialog("zr-m-tadyjsem", "de", "", "font_small", "Ich bin hier. Ich komme nicht raus.")
 
 dialog_addDialog("zr-m-komu", "en", "", "font_small", "Whose eyes are those?")
 dialog_addDialog("zr-m-komu", "cs", "sound/"..codename.."/cs/zr-m-komu.ogg", "font_small", "Komu můžou patřit ty oči?")
 dialog_addDialog("zr-m-komu", "fr", "", "font_small", "A qui sont ces yeux .")
+dialog_addDialog("zr-m-komu", "de", "", "font_small", "Wessen Augen sind das?")
 
 dialog_addDialog("zr-v-nevim", "en", "", "font_big", "I don`t know. Maybe the periscope is connected to some other dimension.")
 dialog_addDialog("zr-v-nevim", "cs", "sound/"..codename.."/cs/zr-v-nevim.ogg", "font_big", "Nevím, snad ten periskop vede do jiné dimenze...")
 dialog_addDialog("zr-v-nevim", "fr", "", "font_big", "Je ne sais pas. Le périscope est peut-être connecté à une autre dimension.")
+dialog_addDialog("zr-v-nevim", "de", "", "font_big", "Ich weiß nicht. Vielleicht ist das Periskop mit einer anderen Dimension verbunden.")
+dialog_addDialog("zr-v-nevim", "de_CH", "", "font_big", "Ich weiss nicht. Vielleicht ist das Periskop mit einer anderen Dimension verbunden.")
 
 dialog_addDialog("zr-v-opatrne", "en", "", "font_big", "Careful...")
 dialog_addDialog("zr-v-opatrne", "cs", "sound/"..codename.."/cs/zr-v-opatrne.ogg", "font_big", "Opatrně...")
 dialog_addDialog("zr-v-opatrne", "fr", "", "font_big", "Attenti...")
+dialog_addDialog("zr-v-opatrne", "de", "", "font_big", "Vorsichtig...")
 
 dialog_addDialog("zr-v-vzdyt", "en", "", "font_big", "Don`t say I didn`t warn you!")
 dialog_addDialog("zr-v-vzdyt", "cs", "sound/"..codename.."/cs/zr-v-vzdyt.ogg", "font_big", "Vždyť jsem ti to říkal!")
 dialog_addDialog("zr-v-vzdyt", "fr", "", "font_big", "Tu ne pourras pas dire que je ne t'ai pas prévenu !")
+dialog_addDialog("zr-v-vzdyt", "de", "", "font_big", "Sage nicht, ich hätte Dich nicht gewarnt!")
 
 
