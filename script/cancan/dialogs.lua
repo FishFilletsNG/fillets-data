ca6795c2eddcd10a86bbbf082477376a78bd276b
fidlej <fidlej>
2004-07-28 12:16:17 +0000

    Added level cancan.

---
+++ script/cancan/dialogs.lua
@@ -0,0 +1,5 @@
+dialog_addDialog("kan-v-proc", "en", "", "font_big", "Why doesn`t he play?")
+dialog_addDialog("kan-v-proc", "cs", "sound/"..codename.."/cs/kan-v-proc.ogg", "font_big", "Proč nehraje?")
+
+dialog_addDialog("kan-klavir-music", "en", "sound/"..codename.."/en/kan-klavir-music.ogg")
+
