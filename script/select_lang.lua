88cf58e3f9b59618bbbef8c6db06324e1d7e73a6
fidlej <fidlej>
2004-07-29 11:09:09 +0000

    Added lang selection menu.

---
+++ script/select_lang.lua
@@ -0,0 +1,6 @@
+
+select_addFlag("en", "images/menu/flags/en.png")
+select_addFlag("cs", "images/menu/flags/cs.png")
+select_addFlag("fr", "images/menu/flags/fr.png")
+select_addFlag("de", "images/menu/flags/de.png")
+
