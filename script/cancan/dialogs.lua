c3f4c3ebc4acb38fe64b74eb31c75cce163a250d
mrpaja <mrpaja>
2004-08-06 15:22:54 +0000

    French translation update

--- script/cancan/dialogs.lua
+++ script/cancan/dialogs.lua
@@ -1,5 +1,6 @@
 dialog_addDialog("kan-v-proc", "en", "", "font_big", "Why doesn`t he play?")
 dialog_addDialog("kan-v-proc", "cs", "sound/"..codename.."/cs/kan-v-proc.ogg", "font_big", "Proč nehraje?")
+dialog_addDialog("kan-v-proc", "fr", "", "font_big", "Pourquoi il ne joue pas ?")
 
 dialog_addDialog("kan-klavir-music", "en", "sound/"..codename.."/en/kan-klavir-music.ogg")
 
--- script/share/boredialogs.lua
+++ script/share/boredialogs.lua
@@ -1,166 +1,221 @@
 dialog_addDialog("ob-m-neverim", "en", "", "font_small", "I don`t believe we can make it...")
 dialog_addDialog("ob-m-neverim", "cs", "sound/share/borejokes/cs/ob-m-neverim.ogg", "font_small", "Nevěřím, že to půjde...")
+dialog_addDialog("ob-m-neverim", "fr", "", "font_small", "Je ne crois pas que nous le ferons...")
 
 dialog_addDialog("ob-m-nedosataneme", "en", "", "font_small", "We aren`t going to get out of here.")
 dialog_addDialog("ob-m-nedosataneme", "cs", "sound/share/borejokes/cs/ob-m-nedosataneme.ogg", "font_small", "Už se odsud nikdy nedostaneme.")
+dialog_addDialog("ob-m-nedosataneme", "fr", "", "font_small", "Nous ne sortirons pas d'ici.")
 
 dialog_addDialog("ob-m-naveky", "en", "", "font_small", "We are shut here forever.")
 dialog_addDialog("ob-m-naveky", "cs", "sound/share/borejokes/cs/ob-m-naveky.ogg", "font_small", "Jsme tu na věky zavření.")
+dialog_addDialog("ob-m-naveky", "fr", "", "font_small", "Nous sommes enfermés ici pour l'éternité.")
 
 dialog_addDialog("ob-v-jit0", "en", "", "font_big", "There must be a way out.")
 dialog_addDialog("ob-v-jit0", "cs", "sound/share/borejokes/cs/ob-v-jit0.ogg", "font_big", "Musí to nějak jít.")
+dialog_addDialog("ob-v-jit0", "fr", "", "font_big", "Il doit y avoir un moyen de sortir.")
 
 dialog_addDialog("ob-v-jit1", "en", "", "font_big", "There is some solution, only we cannot see it.")
 dialog_addDialog("ob-v-jit1", "cs", "sound/share/borejokes/cs/ob-v-jit1.ogg", "font_big", "Řešení tu je, jen ho nevidíme.")
+dialog_addDialog("ob-v-jit1", "fr", "", "font_big", "Il y a une solution, nous ne pouvons simplement pas la voir.")
 
 dialog_addDialog("ob-v-musi", "en", "", "font_big", "Lift up your head, there must be a way.")
 dialog_addDialog("ob-v-musi", "cs", "sound/share/borejokes/cs/ob-v-musi.ogg", "font_big", "Hlavu vzhůru, nějak to jít musí.")
+dialog_addDialog("ob-v-musi", "fr", "", "font_big", "Resaisis-toi, il doit y avoir une solution.")
 
 dialog_addDialog("ob-v-klid", "en", "", "font_big", "Take it easy.")
 dialog_addDialog("ob-v-klid", "cs", "sound/share/borejokes/cs/ob-v-klid.ogg", "font_big", "Jen klid.")
+dialog_addDialog("ob-v-klid", "fr", "", "font_big", "Détends toi.")
 
 dialog_addDialog("ob-v-neobvykle", "en", "", "font_big", "There is something strange about it...")
 dialog_addDialog("ob-v-neobvykle", "cs", "sound/share/borejokes/cs/ob-v-neobvykle.ogg", "font_big", "V tom bude něco neobvyklého...")
+dialog_addDialog("ob-v-neobvykle", "fr", "", "font_big", "Il y a quelque chose d'étrange...")
 
 dialog_addDialog("ob-m-teorie", "en", "", "font_small", "Just spare me your theories, please.")
 dialog_addDialog("ob-m-teorie", "cs", "sound/share/borejokes/cs/ob-m-teorie.ogg", "font_small", "Nepokoušej se zase rozvíjet ty svoje teorie.")
+dialog_addDialog("ob-m-teorie", "fr", "", "font_small", "Epargne moi tes théories, s'il te plaît.")
 
 dialog_addDialog("ob-v-mamto", "en", "", "font_big", "I`ve got it. If we push that one...")
 dialog_addDialog("ob-v-mamto", "cs", "sound/share/borejokes/cs/ob-v-mamto.ogg", "font_big", "Mám to! Když posuneme támhle tu ...")
+dialog_addDialog("ob-v-mamto", "fr", "", "font_big", "Euréka. Si nous poussons celui là...")
 
 dialog_addDialog("ob-m-pokracuj", "en", "", "font_small", "Go on!")
 dialog_addDialog("ob-m-pokracuj", "cs", "sound/share/borejokes/cs/ob-m-pokracuj.ogg", "font_small", "Pokračuj!")
+dialog_addDialog("ob-m-pokracuj", "fr", "", "font_small", "Allons y!")
 
 dialog_addDialog("ob-v-napad", "en", "", "font_big", "Oh no, it was just an idea...")
 dialog_addDialog("ob-v-napad", "cs", "sound/share/borejokes/cs/ob-v-napad.ogg", "font_big", "Ale ne, byl to jen takový nápad...")
+dialog_addDialog("ob-v-napad", "fr", "", "font_big", "Non, c'était juste une idée...")
 
 dialog_addDialog("ob-v-vyzkousej", "en", "", "font_big", "What if we try that....")
 dialog_addDialog("ob-v-vyzkousej", "cs", "sound/share/borejokes/cs/ob-v-vyzkousej.ogg", "font_big", "Co kdybychom vyzkoušeli támhle...")
+dialog_addDialog("ob-v-vyzkousej", "fr", "", "font_big", "Et si on essayait ça...")
 
 dialog_addDialog("ob-m-co", "en", "", "font_small", "What?")
 dialog_addDialog("ob-m-co", "cs", "sound/share/borejokes/cs/ob-m-co.ogg", "font_small", "Co?")
+dialog_addDialog("ob-m-co", "fr", "", "font_small", "Quoi ?")
 
 dialog_addDialog("ob-v-alenic", "en", "", "font_big", "Nothing. Just an idea.")
 dialog_addDialog("ob-v-alenic", "cs", "sound/share/borejokes/cs/ob-v-alenic.ogg", "font_big", "Ale nic, to byl jen takový nápad.")
+dialog_addDialog("ob-v-alenic", "fr", "", "font_big", "Rien. Juste une idée.")
 
 dialog_addDialog("ob-m-zase", "en", "", "font_small", "Not again.")
 dialog_addDialog("ob-m-zase", "cs", "sound/share/borejokes/cs/ob-m-zase.ogg", "font_small", "Už zase...")
+dialog_addDialog("ob-m-zase", "fr", "", "font_small", "Arrête.")
 
 dialog_addDialog("ob-v-nebavi", "en", "", "font_big", "I`m tired of it.")
 dialog_addDialog("ob-v-nebavi", "cs", "sound/share/borejokes/cs/ob-v-nebavi.ogg", "font_big", "Už mě to nebaví.")
+dialog_addDialog("ob-v-nebavi", "fr", "", "font_big", "Il me fatigue.")
 
 dialog_addDialog("ob-v-hrej", "en", "", "font_big", "Just play, we want to get out.")
 dialog_addDialog("ob-v-hrej", "cs", "sound/share/borejokes/cs/ob-v-hrej.ogg", "font_big", "No tak, hrej, chceme se dostat ven.")
+dialog_addDialog("ob-v-hrej", "fr", "", "font_big", "Allez joue, nous voulons sortir.")
 
 dialog_addDialog("ob-v-sami", "en", "", "font_big", "If the player keeps doing nothing, we`ll solve it ourselves.")
 dialog_addDialog("ob-v-sami", "cs", "sound/share/borejokes/cs/ob-v-sami.ogg", "font_big", "Jestli nezačne ten hráč něco dělat, vyřešíme si to sami.")
+dialog_addDialog("ob-v-sami", "fr", "", "font_big", "Si le joueur ne fait rien, nous allons trouver la solution nous-même.")
 
 dialog_addDialog("ob-m-pst", "en", "", "font_small", "Shhh, don`t disturb the player. He thinks again...")
 dialog_addDialog("ob-m-pst", "cs", "sound/share/borejokes/cs/ob-m-pst.ogg", "font_small", "Pssst, neruš hráče, přemýšlí...")
+dialog_addDialog("ob-m-pst", "fr", "", "font_small", "Shhh, ne dérange pas le joueur. Il réfléchit encore...")
 
 dialog_addDialog("ob-m-hlavu", "en", "", "font_small", "Keep calm.")
 dialog_addDialog("ob-m-hlavu", "cs", "sound/share/borejokes/cs/ob-m-hlavu.ogg", "font_small", "Zachovej chladnou hlavu.")
+dialog_addDialog("ob-m-hlavu", "fr", "", "font_small", "Reste calme.")
 
 dialog_addDialog("ob-m-klid", "en", "", "font_small", "Take it easy.")
 dialog_addDialog("ob-m-klid", "cs", "sound/share/borejokes/cs/ob-m-klid.ogg", "font_small", "Jen klid.")
+dialog_addDialog("ob-m-klid", "fr", "", "font_small", "Détends toi.")
 
 dialog_addDialog("ob-m-jesteneco", "en", "", "font_small", "Will there be some action?")
 dialog_addDialog("ob-m-jesteneco", "cs", "sound/share/borejokes/cs/ob-m-jesteneco.ogg", "font_small", "Tak bude se ještě něco dít?")
+dialog_addDialog("ob-m-jesteneco", "fr", "", "font_small", "N'y aurait-il pas quelque chose à faire ?")
 
 dialog_addDialog("ob-m-nedeje", "en", "", "font_small", "Why aren`t  we doing anything?")
 dialog_addDialog("ob-m-nedeje", "cs", "sound/share/borejokes/cs/ob-m-nedeje.ogg", "font_small", "Proč se nic neděje?")
+dialog_addDialog("ob-m-nedeje", "fr", "", "font_small", "Pourquoi ne faisons-nous rien ?")
 
 dialog_addDialog("ob-m-proc", "en", "", "font_small", "Why are we idle?")
 dialog_addDialog("ob-m-proc", "cs", "sound/share/borejokes/cs/ob-m-proc.ogg", "font_small", "Proč se nic neděje?")
+dialog_addDialog("ob-m-proc", "fr", "", "font_small", "Pourquoi attendons-nous ?")
 
 dialog_addDialog("ob-m-ceka", "en", "", "font_small", "Does he expect us to solve ourselves?")
 dialog_addDialog("ob-m-ceka", "cs", "sound/share/borejokes/cs/ob-m-ceka.ogg", "font_small", "On snad čeká, že to vyřešíme za něj?!")
+dialog_addDialog("ob-m-ceka", "fr", "", "font_small", "Espère-t-il que nous trouverons nous-même la solution ?")
 
 dialog_addDialog("ob-v-nerus", "en", "", "font_big", "Don`t disturb. The player thinks again...")
 dialog_addDialog("ob-v-nerus", "cs", "sound/share/borejokes/cs/ob-v-nerus.ogg", "font_big", "Neruš, hráč přemýšlí...")
+dialog_addDialog("ob-v-nerus", "fr", "", "font_big", "Dérange pas. Le joueur réfléchit toujours...")
 
 dialog_addDialog("ob-m-jetam", "en", "", "font_small", "Is there the player at all?")
 dialog_addDialog("ob-m-jetam", "cs", "sound/share/borejokes/cs/ob-m-jetam.ogg", "font_small", "Je tam ten hráč vůbec?")
+dialog_addDialog("ob-m-jetam", "fr", "", "font_small", "Y a-til un joueur tout court ?")
 
 dialog_addDialog("ob-v-leskne", "en", "", "font_big", "I don`t know. The screen glares too much from inside.")
 dialog_addDialog("ob-v-leskne", "cs", "sound/share/borejokes/cs/ob-v-leskne.ogg", "font_big", "Nevím, obrazovka se zevnitř děsně leskne.")
+dialog_addDialog("ob-v-leskne", "fr", "", "font_big", "Je ne sais pas. l'écran brille trop de l'intérieur.")
 
 dialog_addDialog("ob-v-prestavka", "en", "", "font_big", "The little break will help.")
 dialog_addDialog("ob-v-prestavka", "cs", "sound/share/borejokes/cs/ob-v-prestavka.ogg", "font_big", "Malá přestávka nezaškodí.")
+dialog_addDialog("ob-v-prestavka", "fr", "", "font_big", "Une petite pause peut aider.")
 
 dialog_addDialog("ob-o-nebavi", "en", "", "font_both", "We are tired of waiting!")
 dialog_addDialog("ob-o-nebavi", "cs", "sound/share/borejokes/cs/ob-o-nebavi.ogg", "font_both", "Už nás to čekání nebaví!")
+dialog_addDialog("ob-o-nebavi", "fr", "", "font_both", "Nous sommes fatigués d'attendre !")
 
 dialog_addDialog("ob-o-halo", "en", "", "font_both", "Hello!")
 dialog_addDialog("ob-o-halo", "cs", "sound/share/borejokes/cs/ob-o-halo.ogg", "font_both", "Halóóó!")
+dialog_addDialog("ob-o-halo", "fr", "", "font_both", "Hello!")
 
 dialog_addDialog("ob-v-nelekl", "en", "", "font_big", "Did you flinch?")
 dialog_addDialog("ob-v-nelekl", "cs", "sound/share/borejokes/cs/ob-v-nelekl.ogg", "font_big", "Nelekl ses?")
+dialog_addDialog("ob-v-nelekl", "fr", "", "font_big", "Tu es timide ?")
 
 dialog_addDialog("ob-m-mysleli", "en", "", "font_small", "We were just afraid that you might have forgotten about us.")
 dialog_addDialog("ob-m-mysleli", "cs", "sound/share/borejokes/cs/ob-m-mysleli.ogg", "font_small", "My jsme jen mysleli, jestli jsi na nás nezapomněl.")
+dialog_addDialog("ob-m-mysleli", "fr", "", "font_small", "Nous avons simplement peur que tu nous ai oubliés.")
 
 dialog_addDialog("ob-v-nehybes", "en", "", "font_big", "Look, why don`t you move us a little bit?")
 dialog_addDialog("ob-v-nehybes", "cs", "sound/share/borejokes/cs/ob-v-nehybes.ogg", "font_big", "Hele, proč s námi nehýbeš?")
+dialog_addDialog("ob-v-nehybes", "fr", "", "font_big", "Regarde, pourquoi est-ce que tu nous bouges pas un petit peu ?")
 
 dialog_addDialog("ob-m-resit", "en", "", "font_small", "We are the game, you are supposed to solve us.")
 dialog_addDialog("ob-m-resit", "cs", "sound/share/borejokes/cs/ob-m-resit.ogg", "font_small", "My jsme hra, nás máš řešit.")
+dialog_addDialog("ob-m-resit", "fr", "", "font_small", "Nous sommes le jeu, tu es sensé le résoudre.")
 
 dialog_addDialog("ob-v-akvarium", "en", "", "font_big", "Buy yourself a fish tank for observations.")
 dialog_addDialog("ob-v-akvarium", "cs", "sound/share/borejokes/cs/ob-v-akvarium.ogg", "font_big", "Na koukání si pořiď akvárium.")
+dialog_addDialog("ob-v-akvarium", "fr", "", "font_big", "Achète un bocal pour faire des observations.")
 
-dialog_addDialog("ob-v-halo", "en", "", "font_big", "Hellooo! We are here? Did you forget about us?")
+dialog_addDialog("ob-v-halo", "en", "", "font_big", "Hellooo! We are here! Did you forget about us?")
 dialog_addDialog("ob-v-halo", "cs", "sound/share/borejokes/cs/ob-v-halo.ogg", "font_big", "Halóóó! My jsme tady! Nezapomněl jsi na nás?")
+dialog_addDialog("ob-v-halo", "fr", "", "font_big", "Hellooo ! Nous sommes ici ! Nous as-tu oubliés ?")
 
 dialog_addDialog("ob-m-tezky", "en", "", "font_small", "Difficult, isn`t it?")
 dialog_addDialog("ob-m-tezky", "cs", "sound/share/borejokes/cs/ob-m-tezky.ogg", "font_small", "Těžký, co?")
+dialog_addDialog("ob-m-tezky", "fr", "", "font_small", "Difficile, n'est-ce pas ?")
 
 dialog_addDialog("ob-v-jidlo", "en", "", "font_big", "Look player, couldn`t you bring us something to eat? Put it in the TEMP directory, we`ll eat it momentarily.")
 dialog_addDialog("ob-v-jidlo", "cs", "sound/share/borejokes/cs/ob-v-jidlo.ogg", "font_big", "Hele, hráči, neskočil bys nám pro něco k jídlu? Dej nám to do adresáře TMP, hned to sníme.")
+dialog_addDialog("ob-v-jidlo", "fr", "", "font_big", "Attends joueur, tu ne peus pas nous apporter quelque chose à manger ? Mets le dans le répertoire temporaire, nous le mangerons à l'occasion.")
 
 dialog_addDialog("ob-m-strach", "en", "", "font_small", "When you stop playing, don`t turn off the computer, please. It`s so dark and scary here if you do.")
 dialog_addDialog("ob-m-strach", "cs", "sound/share/borejokes/cs/ob-m-strach.ogg", "font_small", "Prosím tě, až přestaneš hrát, nevypínej počítač... je tady pak hrozná tma a mám strach.")
+dialog_addDialog("ob-m-strach", "fr", "", "font_small", "Quand tu arrête de jouer, n'éteins pas l'ordinateur, s'il te plaît. C'est si noir et effrayant quand tu éteins.")
 
 dialog_addDialog("ob-m-jakdlouho", "en", "", "font_small", "How long are we going to solve it?")
 dialog_addDialog("ob-m-jakdlouho", "cs", "sound/share/borejokes/cs/ob-m-jakdlouho.ogg", "font_small", "Jak dlouho to ještě budeme řešit?")
+dialog_addDialog("ob-m-jakdlouho", "fr", "", "font_small", "Combien de temps il nous faudrat pour trouver la solution ?")
 
 dialog_addDialog("ob-v-zvykacka", "en", "", "font_big", "As long as the taste of this chewing gum lasts.")
 dialog_addDialog("ob-v-zvykacka", "cs", "sound/share/borejokes/cs/ob-v-zvykacka.ogg", "font_big", "Tak dlouho, jak dlouho vydrží chuť téhle žvýkačky...")
+dialog_addDialog("ob-v-zvykacka", "fr", "", "font_big", "Aussi longtemps que le goût de ce chewing-gum.")
 
 dialog_addDialog("ob-m-zvykacka", "en", "", "font_small", "What, you are finished? But this chewing gum still tastes extraordinary.")
 dialog_addDialog("ob-m-zvykacka", "cs", "sound/share/borejokes/cs/ob-m-zvykacka.ogg", "font_small", "Cože, hotovo? Vždyť ta žvýkačka chutná ještě pořád skvěle.")
+dialog_addDialog("ob-m-zvykacka", "fr", "", "font_small", "Quoi, tu as fini ? Mais ce chewing-gum a encore un goût extraordinaire.")
 
 dialog_addDialog("ob-v-ostani", "en", "", "font_big", "What do you think, how are the other faring?")
 dialog_addDialog("ob-v-ostani", "cs", "sound/share/borejokes/cs/ob-v-ostani.ogg", "font_big", "Co myslíš, jak jsou na tom ostatní?")
+dialog_addDialog("ob-v-ostani", "fr", "", "font_big", "Dis donc, comment sont les autres fish-filleteurs ?")
 
 dialog_addDialog("ob-m-kdo", "en", "", "font_small", "The other who?")
 dialog_addDialog("ob-m-kdo", "cs", "sound/share/borejokes/cs/ob-m-kdo.ogg", "font_small", "To jako kdo?")
+dialog_addDialog("ob-m-kdo", "fr", "", "font_small", "Les autres quoi ?")
 
 dialog_addDialog("ob-v-kdoresi", "en", "", "font_big", "The other player who are solving this game. Maybe they managed to solve more levels.")
 dialog_addDialog("ob-v-kdoresi", "cs", "sound/share/borejokes/cs/ob-v-kdoresi.ogg", "font_big", "No ostatní, kdo to řeší. Třeba už mají víc místností.")
+dialog_addDialog("ob-v-kdoresi", "fr", "", "font_big", "Les autres joueurs qui cogitent sur ce jeux. Peut-être qu'ils ont résolu plus de niveaux.")
 
 dialog_addDialog("ob-m-pravdepodobne", "en", "", "font_small", "It`s very likely.")
 dialog_addDialog("ob-m-pravdepodobne", "cs", "sound/share/borejokes/cs/ob-m-pravdepodobne.ogg", "font_small", "To je dost pravděpodobné.")
+dialog_addDialog("ob-m-pravdepodobne", "fr", "", "font_small", "C'est très probable.")
 
 dialog_addDialog("ob-m-ach", "en", "", "font_small", "Oh, yeah.")
 dialog_addDialog("ob-m-ach", "cs", "sound/share/borejokes/cs/ob-m-ach.ogg", "font_small", "Ach jo.")
+dialog_addDialog("ob-m-ach", "fr", "", "font_small", "Oh, yeah.")
 
 dialog_addDialog("ob-v-copak", "en", "", "font_big", "What happened?")
 dialog_addDialog("ob-v-copak", "cs", "sound/share/borejokes/cs/ob-v-copak.ogg", "font_big", "Copak?")
+dialog_addDialog("ob-v-copak", "fr", "", "font_big", "Que se passe-t-il ?")
 
 dialog_addDialog("ob-m-lito", "en", "", "font_small", "I just feel so sorry...")
 dialog_addDialog("ob-m-lito", "cs", "sound/share/borejokes/cs/ob-m-lito.ogg", "font_small", "Přišlo mi to tak nějak líto.")
+dialog_addDialog("ob-m-lito", "fr", "", "font_small", "Je me sens si navrée...")
 
 dialog_addDialog("ob-v-colito", "en", "", "font_big", "Sorry for what?")
 dialog_addDialog("ob-v-colito", "cs", "sound/share/borejokes/cs/ob-v-colito.ogg", "font_big", "Co ti přišlo líto?")
+dialog_addDialog("ob-v-colito", "fr", "", "font_big", "Navrée de quoi ?")
 
 dialog_addDialog("ob-m-vsechno", "en", "", "font_small", "Sorry for everything.")
 dialog_addDialog("ob-m-vsechno", "cs", "sound/share/borejokes/cs/ob-m-vsechno.ogg", "font_small", "No... všechno.")
+dialog_addDialog("ob-m-vsechno", "fr", "", "font_small", "Navrée de tout.")
 
 dialog_addDialog("ob-v-covsechno", "en", "", "font_big", "What everything?")
 dialog_addDialog("ob-v-covsechno", "cs", "sound/share/borejokes/cs/ob-v-covsechno.ogg", "font_big", "Co všechno?")
+dialog_addDialog("ob-v-covsechno", "fr", "", "font_big", "Quoi tout ?")
 
 dialog_addDialog("ob-m-uplnevse", "en", "", "font_small", "Completely everything! And leave me alone, I beg you!")
 dialog_addDialog("ob-m-uplnevse", "cs", "sound/share/borejokes/cs/ob-m-uplnevse.ogg", "font_small", "Úplně všechno! A vůbec, starej se o sebe!")
+dialog_addDialog("ob-m-uplnevse", "fr", "", "font_small", "Vraiment tout ! Et laisse moi seule, je t'en supplie !")
 
 
