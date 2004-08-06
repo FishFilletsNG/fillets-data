c0bf1bf31c22b39c617ad67617f28c6c0b9abb24
fidlej <fidlej>
2004-08-06 22:24:38 +0000

    Added a final level 'atlantis'.

---
+++ script/atlantis/dialogs.lua
@@ -0,0 +1,94 @@
+dialog_addDialog("sp-v-no0", "en", "", "font_big", "Lo and behold!")
+dialog_addDialog("sp-v-no0", "cs", "sound/"..codename.."/cs/sp-v-no0.ogg", "font_big", "No tohle!")
+
+dialog_addDialog("sp-m-no1", "en", "", "font_small", "Just imagine!")
+dialog_addDialog("sp-m-no1", "cs", "sound/"..codename.."/cs/sp-m-no1.ogg", "font_small", "No teda!")
+
+dialog_addDialog("sp-v-kdoby", "en", "", "font_big", "Who could have guessed?!")
+dialog_addDialog("sp-v-kdoby", "cs", "sound/"..codename.."/cs/sp-v-kdoby.ogg", "font_big", "Kdo by to řekl?!")
+
+dialog_addDialog("sp-m-neopatrnost", "en", "", "font_small", "Such a carelessness.")
+dialog_addDialog("sp-m-neopatrnost", "cs", "sound/"..codename.."/cs/sp-m-neopatrnost.ogg", "font_small", "Taková neopatrnost.")
+
+dialog_addDialog("sp-v-zahynuli", "en", "", "font_big", "Thousands perished, the whole city disappeared in the waves because of such ineptness.")
+dialog_addDialog("sp-v-zahynuli", "cs", "sound/"..codename.."/cs/sp-v-zahynuli.ogg", "font_big", "Tisíce lidí zahynuly, celé město zmizelo v rozbouřených vlnách kvůli takové hlouposti.")
+
+dialog_addDialog("sp-m-vytazeny", "en", "", "font_small", "Unplugged plug.")
+dialog_addDialog("sp-m-vytazeny", "cs", "sound/"..codename.."/cs/sp-m-vytazeny.ogg", "font_small", "Vytažený špunt.")
+
+dialog_addDialog("sp-v-trapne", "en", "", "font_big", "Embarrassing blunder.")
+dialog_addDialog("sp-v-trapne", "cs", "sound/"..codename.."/cs/sp-v-trapne.ogg", "font_big", "Trapné přehlédnutí.")
+
+dialog_addDialog("sp-m-costim", "en", "", "font_small", "What are we to do with it?")
+dialog_addDialog("sp-m-costim", "cs", "sound/"..codename.."/cs/sp-m-costim.ogg", "font_small", "Co s ním teď uděláme?")
+
+dialog_addDialog("sp-v-vratit0", "en", "", "font_big", "We can try to put it back in place.")
+dialog_addDialog("sp-v-vratit0", "cs", "sound/"..codename.."/cs/sp-v-vratit0.ogg", "font_big", "Můžem ho zkusit vrátit na místo.")
+
+dialog_addDialog("sp-m-vratit0", "en", "", "font_small", "And then? Shall we drink the water that poured in or what?")
+dialog_addDialog("sp-m-vratit0", "cs", "sound/"..codename.."/cs/sp-m-vratit0.ogg", "font_small", "A co potom? Myslíš, že tu vodu, co sem natekla, vypijem?")
+
+dialog_addDialog("sp-v-vratit1", "en", "", "font_big", "We can put it back in place as a token of our esteem of the citizens` heroic effort to keep it afloat. As a memorial to their industrious, adroit and... persistent nature.")
+dialog_addDialog("sp-v-vratit1", "cs", "sound/"..codename.."/cs/sp-v-vratit1.ogg", "font_big", "Můžem ho vrátit na místo jako vyjádření našeho obdivu k hrdinskému úsilí obyvatel města udržet ho nad hladinou. Jako pomník jejich důmyslu, zručnosti a ... vytrvalosti.")
+
+dialog_addDialog("sp-m-vratit1", "en", "", "font_small", "Of what? 'Where are you running? I need to order eight swords.` The Providence itself unplugged that hole. Just imagine you`d hear such things at home. Day by day.")
+dialog_addDialog("sp-m-vratit1", "cs", "sound/"..codename.."/cs/sp-m-vratit1.ogg", "font_small", "Čeho že? 'Kam běžíš? Pro sedm mečů!` Ten špunt podle mě vytáhla sama prozřetelnost. Představ si, že bys takové věci slýchal doma. Den co den.")
+
+dialog_addDialog("sp-m-kalet", "en", "", "font_small", "And moreover: is it likely that anybody would ever come here? Only sepias will nibble it occasionally.")
+dialog_addDialog("sp-m-kalet", "cs", "sound/"..codename.."/cs/sp-m-kalet.ogg", "font_small", "A navíc: copak sem ještě někdo někdy přijde? Tady budou na ten špunt akorát kálet sépie.")
+
+dialog_addDialog("sp-v-pocit", "en", "", "font_big", "We are going to feel well about it.")
+dialog_addDialog("sp-v-pocit", "cs", "sound/"..codename.."/cs/sp-v-pocit.ogg", "font_big", "Budem z toho mít dobrý pocit.")
+
+dialog_addDialog("sp-m-potize", "en", "", "font_small", "We are going to feel down about it. Do you think the boss is going to believe it? The giant unplugged plug. And what have you done with it? We plugged it again. He, he, he.")
+dialog_addDialog("sp-m-potize", "cs", "sound/"..codename.."/cs/sp-m-potize.ogg", "font_small", "Budem z toho mít potíže. Myslíš, že nám to šéf uvěří? Obří vytažený špunt. A co jste s ním udělali? Ucpali jsme s ním díru. Ha, ha, ha.")
+
+dialog_addDialog("sp-v-vzit", "en", "", "font_big", "You might be right. Maybe it would be better to take it along.")
+dialog_addDialog("sp-v-vzit", "cs", "sound/"..codename.."/cs/sp-v-vzit.ogg", "font_big", "Na tom něco je. Možná by bylo lepší ho vzít s sebou.")
+
+dialog_addDialog("sp-m-taky", "en", "", "font_small", "I think so, too.")
+dialog_addDialog("sp-m-taky", "cs", "sound/"..codename.."/cs/sp-m-taky.ogg", "font_small", "To si taky myslím.")
+
+dialog_addDialog("sp-v-dotoho", "en", "", "font_big", "Let `s get to work.")
+dialog_addDialog("sp-v-dotoho", "cs", "sound/"..codename.."/cs/sp-v-dotoho.ogg", "font_big", "Dáme se do toho.")
+
+dialog_addDialog("sp-m-nechat", "en", "", "font_small", "What if we just leave that plug here?")
+dialog_addDialog("sp-m-nechat", "cs", "sound/"..codename.."/cs/sp-m-nechat.ogg", "font_small", "Co kdybychom tady ten špunt prostě nechali?")
+
+dialog_addDialog("sp-v-centrala", "en", "", "font_big", "What would the Agency say?")
+dialog_addDialog("sp-v-centrala", "cs", "sound/"..codename.."/cs/sp-v-centrala.ogg", "font_big", "Co by tomu řekla centrála?")
+
+dialog_addDialog("sp-v-jedno", "en", "", "font_big", "I think I don`t mind what the boss is going to think about me.")
+dialog_addDialog("sp-v-jedno", "cs", "sound/"..codename.."/cs/sp-v-jedno.ogg", "font_big", "Už mi začíná být jedno, co si o mně šéf pomyslí.")
+
+dialog_addDialog("sp-m-vydrz", "en", "", "font_small", "Hold on. We are sure to solve it.")
+dialog_addDialog("sp-m-vydrz", "cs", "sound/"..codename.."/cs/sp-m-vydrz.ogg", "font_small", "Vydrž. Určitě na to přijdem.")
+
+dialog_addDialog("sp-m-spunt", "en", "", "font_small", "The darned plug. What if we just make up something.")
+dialog_addDialog("sp-m-spunt", "cs", "sound/"..codename.."/cs/sp-m-spunt.ogg", "font_small", "Zatracený špunt. Co kdybychom si něco vymysleli?")
+
+dialog_addDialog("sp-v-co", "en", "", "font_big", "Like what, for example?")
+dialog_addDialog("sp-v-co", "cs", "sound/"..codename.."/cs/sp-v-co.ogg", "font_big", "Jako třeba co?")
+
+dialog_addDialog("sp-m-vymluva0", "en", "", "font_small", "Well, we could say that the city in fact never existed.")
+dialog_addDialog("sp-m-vymluva0", "cs", "sound/"..codename.."/cs/sp-m-vymluva0.ogg", "font_small", "No třeba, že to město nikdy neexistovalo.")
+
+dialog_addDialog("sp-m-vymluva1", "en", "", "font_small", "Well, we could say that the city sank because the Arctic ice had melted.")
+dialog_addDialog("sp-m-vymluva1", "cs", "sound/"..codename.."/cs/sp-m-vymluva1.ogg", "font_small", "Třeba, že se to město potopilo, protože roztály ledovce.")
+
+dialog_addDialog("sp-m-vymluva2", "en", "", "font_small", "An earthquake could sink the city, for example.")
+dialog_addDialog("sp-m-vymluva2", "cs", "sound/"..codename.."/cs/sp-m-vymluva2.ogg", "font_small", "Například ho mohlo potopit zemětřesení.")
+
+dialog_addDialog("sp-m-vymluva3", "en", "", "font_small", "A tsunami might sink the city, for example.")
+dialog_addDialog("sp-m-vymluva3", "cs", "sound/"..codename.."/cs/sp-m-vymluva3.ogg", "font_small", "Třeba, že ho smetla tsunami.")
+
+dialog_addDialog("sp-m-vymluva4", "en", "", "font_small", "We could try to say that a volcano erupted in the center of the city.")
+dialog_addDialog("sp-m-vymluva4", "cs", "sound/"..codename.."/cs/sp-m-vymluva4.ogg", "font_small", "Můžem říkat, že na ostrově vybuchla sopka.")
+
+dialog_addDialog("sp-v-nesmysl", "en", "", "font_big", "This is a silly nonsense.")
+dialog_addDialog("sp-v-nesmysl", "cs", "sound/"..codename.."/cs/sp-v-nesmysl.ogg", "font_big", "To je pěkný nesmysl.")
+
+dialog_addDialog("sp-v-ven", "en", "", "font_big", "This time our goal is to get out that plug.")
+dialog_addDialog("sp-v-ven", "cs", "sound/"..codename.."/cs/sp-v-ven.ogg", "font_big", "Naším úkolem tentokrát bude dostat ven z místnosti ten špunt!")
+
+
