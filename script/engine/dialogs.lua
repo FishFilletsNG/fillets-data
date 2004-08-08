1c42cbc4f98e140aae38a7f31c6b214a3d63da18
fidlej <fidlej>
2004-08-08 17:33:54 +0000

    Added level 'engine'.

---
+++ script/engine/dialogs.lua
@@ -0,0 +1,81 @@
+dialog_addDialog("mot-m-info", "en", "", "font_small", "I think we can gain some information about the interstellar propulsion here.")
+dialog_addDialog("mot-m-info", "cs", "sound/"..codename.."/cs/mot-m-info.ogg", "font_small", "Myslím, že bychom tu mohli získat nějaké informace o mezihvězdném pohonu.")
+
+dialog_addDialog("mot-v-konvencni", "en", "", "font_big", "This looks more like a conventional propulsion for the landing craft.")
+dialog_addDialog("mot-v-konvencni", "cs", "sound/"..codename.."/cs/mot-v-konvencni.ogg", "font_big", "Tohle vypadá spíš na konvenční pohon pro výsadková vozidla.")
+
+dialog_addDialog("mot-m-tak", "en", "", "font_small", "So. We found the drive. We have achieved one of the objectives of our mission.")
+dialog_addDialog("mot-m-tak", "cs", "sound/"..codename.."/cs/mot-m-tak.ogg", "font_small", "Tak. Našli jsme motor. Mohli bychom prohlásit jeden z našich úkolů za splněný.")
+
+dialog_addDialog("mot-v-zavery", "en", "", "font_big", "Don`t be hasty. We didn`t search the whole wreck yet.")
+dialog_addDialog("mot-v-zavery", "cs", "sound/"..codename.."/cs/mot-v-zavery.ogg", "font_big", "Nedělej ukvapené závěry. Ještě jsme neprohlédli celý vrak.")
+
+dialog_addDialog("mot-m-akce0", "en", "", "font_small", "I am sorry that none of these technical marvels around us work.")
+dialog_addDialog("mot-m-akce0", "cs", "sound/"..codename.."/cs/mot-m-akce0.ogg", "font_small", "To je škoda, že všechna ta technika, kterou tu vidíme, už nefunguje.")
+
+dialog_addDialog("mot-m-akce1", "en", "", "font_small", "I`d like to see some of these extraterrestrial gizmos in action.")
+dialog_addDialog("mot-m-akce1", "cs", "sound/"..codename.."/cs/mot-m-akce1.ogg", "font_small", "Já bych tak chtěla vidět trochu té mimozemské techniky v akci.")
+
+dialog_addDialog("mot-m-akce2", "en", "", "font_small", "I wonder if this motor could work under water.")
+dialog_addDialog("mot-m-akce2", "cs", "sound/"..codename.."/cs/mot-m-akce2.ogg", "font_small", "To by mě zajímalo, jestli by tenhle motor fungoval i pod vodou.")
+
+dialog_addDialog("mot-v-funkce0", "en", "", "font_big", "Maybe it`s better for us if it doesn`t work.")
+dialog_addDialog("mot-v-funkce0", "cs", "sound/"..codename.."/cs/mot-v-funkce0.ogg", "font_big", "Možná je pro nás lepší, že už tu nic nepracuje.")
+
+dialog_addDialog("mot-v-funkce1", "en", "", "font_big", "I am rather glad we cannot turn anything on here.")
+dialog_addDialog("mot-v-funkce1", "cs", "sound/"..codename.."/cs/mot-v-funkce1.ogg", "font_big", "Já jsem spíš rád, že tu nemůžeme nic zapnout.")
+
+dialog_addDialog("mot-v-funkce2", "en", "", "font_big", "We should be happy that we didn`t switch anything on yet.")
+dialog_addDialog("mot-v-funkce2", "cs", "sound/"..codename.."/cs/mot-v-funkce2.ogg", "font_big", "Naše štěstí je, že tu nemůžeme nic zapnout.")
+
+dialog_addDialog("mot-v-klic", "en", "", "font_big", "Careful with that wrench.")
+dialog_addDialog("mot-v-klic", "cs", "sound/"..codename.."/cs/mot-v-klic.ogg", "font_big", "Opatrně s tím klíčem.")
+
+dialog_addDialog("mot-m-ublizit", "en", "", "font_small", "I can`t harm anything here.")
+dialog_addDialog("mot-m-ublizit", "cs", "sound/"..codename.."/cs/mot-m-ublizit.ogg", "font_small", "Tady přece nemůžu ničemu ublížit.")
+
+dialog_addDialog("mot-m-zvuky0", "en", "", "font_small", "What have you done? Turn off that roar!")
+dialog_addDialog("mot-m-zvuky0", "cs", "sound/"..codename.."/cs/mot-m-zvuky0.ogg", "font_small", "Co jsi to udělal? Vypni ten randál!")
+
+dialog_addDialog("mot-m-zvuky1", "en", "", "font_small", "This is terrible! Turn it off before it`s too late!")
+dialog_addDialog("mot-m-zvuky1", "cs", "sound/"..codename.."/cs/mot-m-zvuky1.ogg", "font_small", "To jsou strašné zvuky! Vypni to dřív, než bude pozdě.")
+
+dialog_addDialog("mot-v-nemuzu0", "en", "", "font_big", "I can`t! I can`t take it out!")
+dialog_addDialog("mot-v-nemuzu0", "cs", "sound/"..codename.."/cs/mot-v-nemuzu0.ogg", "font_big", "Nemůžu! Jak to mám vytáhnout?!")
+
+dialog_addDialog("mot-v-nemuzu1", "en", "", "font_big", "I don`t know how! I can`t take it out!")
+dialog_addDialog("mot-v-nemuzu1", "cs", "sound/"..codename.."/cs/mot-v-nemuzu1.ogg", "font_big", "Nevím jak! Nedá se to vyndat!")
+
+dialog_addDialog("mot-m-mayday", "en", "", "font_small", "Mayday! Mayday!")
+dialog_addDialog("mot-m-mayday", "cs", "sound/"..codename.."/cs/mot-m-mayday.ogg", "font_small", "Mayday! Mayday!")
+
+dialog_addDialog("mot-v-konecne0", "en", "", "font_big", "Finally.")
+dialog_addDialog("mot-v-konecne0", "cs", "sound/"..codename.."/cs/mot-v-konecne0.ogg", "font_big", "Konečně.")
+
+dialog_addDialog("mot-v-konecne1", "en", "", "font_big", "What a relief.")
+dialog_addDialog("mot-v-konecne1", "cs", "sound/"..codename.."/cs/mot-v-konecne1.ogg", "font_big", "To se mi ulevilo")
+
+dialog_addDialog("mot-v-zvuky0", "en", "", "font_big", "What are you doing? Where are we going?")
+dialog_addDialog("mot-v-zvuky0", "cs", "sound/"..codename.."/cs/mot-v-zvuky0.ogg", "font_big", "Co to děláš? Kam to jedeme?")
+
+dialog_addDialog("mot-v-zvuky1", "en", "", "font_big", "What have you activated? Where is it taking us?")
+dialog_addDialog("mot-v-zvuky1", "cs", "sound/"..codename.."/cs/mot-v-zvuky1.ogg", "font_big", "Co jsi to aktivovala? Kam nás to veze?")
+
+dialog_addDialog("mot-m-nemuzu0", "en", "", "font_small", "How can I turn it off?!")
+dialog_addDialog("mot-m-nemuzu0", "cs", "sound/"..codename.."/cs/mot-m-nemuzu0.ogg", "font_small", "Kde se to vypíná?!")
+
+dialog_addDialog("mot-m-nemuzu1", "en", "", "font_small", "I can`t turn it off!")
+dialog_addDialog("mot-m-nemuzu1", "cs", "sound/"..codename.."/cs/mot-m-nemuzu1.ogg", "font_small", "Nedá se to vypnout!")
+
+dialog_addDialog("mot-m-konecne0", "en", "", "font_small", "Thanks.")
+dialog_addDialog("mot-m-konecne0", "cs", "sound/"..codename.."/cs/mot-m-konecne0.ogg", "font_small", "Díky.")
+
+dialog_addDialog("mot-m-konecne1", "en", "", "font_small", "Finally.")
+dialog_addDialog("mot-m-konecne1", "cs", "sound/"..codename.."/cs/mot-m-konecne1.ogg", "font_small", "Konečně.")
+
+dialog_addDialog("mot-v-znovu0", "en", "", "font_big", "I am only afraid we`ll have to turn it on again.")
+dialog_addDialog("mot-v-znovu0", "cs", "sound/"..codename.."/cs/mot-v-znovu0.ogg", "font_big", "Bojím se, že to budeme muset zapnout znovu.")
+
+dialog_addDialog("mot-x-motor", "en", "sound/"..codename.."/en/mot-x-motor.ogg")
+
+
