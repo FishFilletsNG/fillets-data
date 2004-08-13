f7025c1d65ead0f8e532c8828b75d7f558fa8312
mrpaja <mrpaja>
2004-08-13 19:52:11 +0000

    added level hardware

---
+++ script/hardware/dialogs.lua
@@ -0,0 +1,49 @@
+dialog_addDialog("pz-v-zaskladanej", "en", "", "font_big", "I`m completely blocked in here.")
+dialog_addDialog("pz-v-zaskladanej", "cs", "sound/"..codename.."/cs/pz-v-zaskladanej.ogg", "font_big", "Jsem tady úplně zaskládanej.")
+
+dialog_addDialog("pz-m-co", "en", "", "font_small", "What are you doing up there?")
+dialog_addDialog("pz-m-co", "cs", "sound/"..codename.."/cs/pz-m-co.ogg", "font_small", "Co to tam nahoře máš?")
+
+dialog_addDialog("pz-v-klice1", "en", "", "font_big", "These must me some hardware keys.")
+dialog_addDialog("pz-v-klice1", "cs", "sound/"..codename.."/cs/pz-v-klice1.ogg", "font_big", "To jsou asi nějaké hardwarové klíče.")
+
+dialog_addDialog("pz-v-klice2", "en", "", "font_big", "There are many hardware keys here.")
+dialog_addDialog("pz-v-klice2", "cs", "sound/"..codename.."/cs/pz-v-klice2.ogg", "font_big", "Je tu spousta hardwarových klíčů.")
+
+dialog_addDialog("pz-v-co1", "en", "", "font_big", "What do you have down there?")
+dialog_addDialog("pz-v-co1", "cs", "sound/"..codename.."/cs/pz-v-co1.ogg", "font_big", "Co to tam dole máš, nevidím tam...")
+
+dialog_addDialog("pz-v-co2", "en", "", "font_big", "What`s down there?")
+dialog_addDialog("pz-v-co2", "cs", "sound/"..codename.."/cs/pz-v-co2.ogg", "font_big", "Co je tam dole?")
+
+dialog_addDialog("pz-m-spoje1", "en", "", "font_small", "There are some printed circuits here.")
+dialog_addDialog("pz-m-spoje1", "cs", "sound/"..codename.."/cs/pz-m-spoje1.ogg", "font_small", "Je tu několik plošných spojů.")
+
+dialog_addDialog("pz-m-spoje2", "en", "", "font_small", "There are some integrated circuits here.")
+dialog_addDialog("pz-m-spoje2", "cs", "sound/"..codename.."/cs/pz-m-spoje2.ogg", "font_small", "Jsou tu nějaké integrované obvody.")
+
+dialog_addDialog("pz-m-spoje3", "en", "", "font_small", "I am surrounded by electronics here.")
+dialog_addDialog("pz-m-spoje3", "cs", "sound/"..codename.."/cs/pz-m-spoje3.ogg", "font_small", "Jsem tu obklopena elektronikou...")
+
+dialog_addDialog("pz-m-pocitace", "en", "", "font_small", "Computers. They`ve always fascinated me. But now I`m sick of them.")
+dialog_addDialog("pz-m-pocitace", "cs", "sound/"..codename.."/cs/pz-m-pocitace.ogg", "font_small", "Počítače. Vždycky jsem jimi byla fascinována. Teď jich už ale mám plný zuby.")
+
+dialog_addDialog("pz-x-pocitac", "en", "", "font_white", "Fish. I`ve always hated them. And now my circuits are overloaded with them.")
+dialog_addDialog("pz-x-pocitac", "cs", "sound/"..codename.."/cs/pz-x-pocitac.ogg", "font_white", "Ryby. Vždycky jsem je nenáviděl. A teď jich tady mám plné obvody.")
+
+dialog_addDialog("pz-v-hej", "en", "", "font_big", "Hey, how are you? It`s quite boring here.")
+dialog_addDialog("pz-v-hej", "cs", "sound/"..codename.."/cs/pz-v-hej.ogg", "font_big", "Hej, jak je tam dole? Tady je celkem nuda.")
+
+dialog_addDialog("pz-m-nech", "en", "", "font_small", "Can it, will you?")
+dialog_addDialog("pz-m-nech", "cs", "sound/"..codename.."/cs/pz-m-nech.ogg", "font_small", "Nech si to.")
+
+dialog_addDialog("pz-m-vylez", "en", "", "font_small", "Get out!")
+dialog_addDialog("pz-m-vylez", "cs", "sound/"..codename.."/cs/pz-m-vylez.ogg", "font_small", "Vylez!")
+
+dialog_addDialog("pz-v-dat", "en", "", "font_big", "We have to put these keys somewhere.")
+dialog_addDialog("pz-v-dat", "cs", "sound/"..codename.."/cs/pz-v-dat.ogg", "font_big", "Musíme někam dát ty klíče.")
+
+dialog_addDialog("pz-m-nepasuje", "en", "", "font_small", "It doesn`t seem to fit.")
+dialog_addDialog("pz-m-nepasuje", "cs", "sound/"..codename.."/cs/pz-m-nepasuje.ogg", "font_small", "Nějak to tam nepasuje.")
+
+
