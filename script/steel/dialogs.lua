4f8452be8e6652d31a498a3bc28c32b4207b90b3
fidlej <fidlej>
2004-08-13 22:46:53 +0000

    Added level steel.

---
+++ script/steel/dialogs.lua
@@ -0,0 +1,9 @@
+dialog_addDialog("steel-m-0", "en", "", "font_small", "It`s so quiet here...")
+dialog_addDialog("steel-m-0", "cs", "sound/"..codename.."/cs/steel-m-0.ogg", "font_small", "Tady je ale ticho...")
+
+dialog_addDialog("steel-m-1", "en", "", "font_small", "Boy is it quiet...")
+dialog_addDialog("steel-m-1", "cs", "sound/"..codename.."/cs/steel-m-1.ogg", "font_small", "Tady je takový ticho...")
+
+dialog_addDialog("steel-x-redalert", "en", "sound/"..codename.."/en/steel-x-redalert.ogg")
+
+
