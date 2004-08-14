4e07a5b1d108c4ca784eabd84cb831de799c3046
fidlej <fidlej>
2004-08-14 15:42:06 +0000

    Added level experiments. Pldlici are on TODO.

---
+++ script/experiments/dialogs.lua
@@ -0,0 +1,100 @@
+dialog_addDialog("bank-m-labolator1", "en", "", "font_small", "This must be some kind of laboratory.")
+dialog_addDialog("bank-m-labolator1", "cs", "sound/"..codename.."/cs/bank-m-labolator1.ogg", "font_small", "Jsme asi v nějaké vědecké laboratoři.")
+
+dialog_addDialog("bank-m-labolator2", "en", "", "font_small", "It seems that even UFOs can be equipped with an on-board lab.")
+dialog_addDialog("bank-m-labolator2", "cs", "sound/"..codename.."/cs/bank-m-labolator2.ogg", "font_small", "Vypadá to, že i UFO může mít vědeckou laboratoř na palubě.")
+
+dialog_addDialog("bank-m-labolator3", "en", "", "font_small", "This looks like a laboratory.")
+dialog_addDialog("bank-m-labolator3", "cs", "sound/"..codename.."/cs/bank-m-labolator3.ogg", "font_small", "Tohle vypadá na vědeckou laboratoř!")
+
+dialog_addDialog("bank-v-pokusy1", "en", "", "font_big", "These experiments seem quite suspicious to me.")
+dialog_addDialog("bank-v-pokusy1", "cs", "sound/"..codename.."/cs/bank-v-pokusy1.ogg", "font_big", "Koukám, že se tady provádějí dost podezřelé pokusy.")
+
+dialog_addDialog("bank-v-pokusy2", "en", "", "font_big", "I think that someone`s carrying out some rather suspicious experiments here.")
+dialog_addDialog("bank-v-pokusy2", "cs", "sound/"..codename.."/cs/bank-v-pokusy2.ogg", "font_big", "Myslím, že se zde provádějí velmi podezřelé pokusy.")
+
+dialog_addDialog("bank-m-jejda", "en", "", "font_small", "My gosh, what`s this?!?")
+dialog_addDialog("bank-m-jejda", "cs", "sound/"..codename.."/cs/bank-m-jejda.ogg", "font_small", "Jejda, co je tohle?!?")
+
+dialog_addDialog("bank-v-zije", "en", "", "font_big", "Everything`s alive here.")
+dialog_addDialog("bank-v-zije", "cs", "sound/"..codename.."/cs/bank-v-zije.ogg", "font_big", "Všude to tady žije.")
+
+dialog_addDialog("bank-m-nerus", "en", "", "font_small", "Shhhhh, I need to examine this laboratory.")
+dialog_addDialog("bank-m-nerus", "cs", "sound/"..codename.."/cs/bank-m-nerus.ogg", "font_small", "Neruš, vědecky zkoumám.")
+
+dialog_addDialog("bank-m-bojim", "en", "", "font_small", "I`m little bit afraid of that thing behind the door...")
+dialog_addDialog("bank-m-bojim", "cs", "sound/"..codename.."/cs/bank-m-bojim.ogg", "font_small", "Trochu se bojím té věci za dveřmi...")
+
+dialog_addDialog("bank-m-ocicka", "en", "", "font_small", "Why does it have three eyes?")
+dialog_addDialog("bank-m-ocicka", "cs", "sound/"..codename.."/cs/bank-m-ocicka.ogg", "font_small", "Proč to má tři oči?")
+
+dialog_addDialog("bank-v-pomoc", "en", "", "font_big", "Stop what you`re doing down there and come help me!")
+dialog_addDialog("bank-v-pomoc", "cs", "sound/"..codename.."/cs/bank-v-pomoc.ogg", "font_big", "Nech toho tam dole a pojď mi radši pomoct!")
+
+dialog_addDialog("bank-m-organismy", "en", "", "font_small", "I would never have thought that we`d find so many living things inside a UFO...")
+dialog_addDialog("bank-m-organismy", "cs", "sound/"..codename.."/cs/bank-m-organismy.ogg", "font_small", "Netušila jsem, že uvnitř UFA najdeme tolik živých organismů...")
+
+dialog_addDialog("bank-v-potvory", "en", "", "font_big", "There are an incredible number of different creatures here!")
+dialog_addDialog("bank-v-potvory", "cs", "sound/"..codename.."/cs/bank-v-potvory.ogg", "font_big", "Je tady neuvěřitelné množství různých potvor!")
+
+dialog_addDialog("bank-v-mnozeni", "en", "", "font_big", "My goodness, look at those things reproduce!")
+dialog_addDialog("bank-v-mnozeni", "cs", "sound/"..codename.."/cs/bank-v-mnozeni.ogg", "font_big", "Proboha, tady se něco strašně množí!")
+
+dialog_addDialog("bank-m-nesetkala", "en", "", "font_small", "I`ve never seen this kind of organism before.")
+dialog_addDialog("bank-m-nesetkala", "cs", "sound/"..codename.."/cs/bank-m-nesetkala.ogg", "font_small", "S tímto typem organismu jsem se ještě nesetkala.")
+
+dialog_addDialog("bank-m-kouka", "en", "", "font_small", "Help! It`s looking at me!")
+dialog_addDialog("bank-m-kouka", "cs", "sound/"..codename.."/cs/bank-m-kouka.ogg", "font_small", "Pomoc, ono to na mne kouká!")
+
+dialog_addDialog("bank-m-fuj", "en", "", "font_small", "Yuck.")
+dialog_addDialog("bank-m-fuj", "cs", "sound/"..codename.."/cs/bank-m-fuj.ogg", "font_small", "Fuj.")
+
+dialog_addDialog("bank-m-nervozni", "en", "", "font_small", "That hand gives me the creeps...")
+dialog_addDialog("bank-m-nervozni", "cs", "sound/"..codename.."/cs/bank-m-nervozni.ogg", "font_small", "Jsem z té ruky trochu nervózní...")
+
+dialog_addDialog("bank-m-zkumavka", "en", "", "font_small", "There`s a strange odor coming from this test-tube...")
+dialog_addDialog("bank-m-zkumavka", "cs", "sound/"..codename.."/cs/bank-m-zkumavka.ogg", "font_small", "Z té zkumavky je něco podezřelého cítit...")
+
+dialog_addDialog("bank-m-hlavakolem", "en", "", "font_small", "All of this is making my head spin...")
+dialog_addDialog("bank-m-hlavakolem", "cs", "sound/"..codename.."/cs/bank-m-hlavakolem.ogg", "font_small", "Z toho všeho tady už mi jde hlava kolem...")
+
+dialog_addDialog("bank-v-neproplavu1", "en", "", "font_big", "I can`t swim through here. We`re going to have to push this aside.")
+dialog_addDialog("bank-v-neproplavu1", "cs", "sound/"..codename.."/cs/bank-v-neproplavu1.ogg", "font_big", "Tudy neproplavu, budeme to muset odsunout.")
+
+dialog_addDialog("bank-v-neproplavu2", "en", "", "font_big", "I can`t swim through here. We`re going to have to push this down.")
+dialog_addDialog("bank-v-neproplavu2", "cs", "sound/"..codename.."/cs/bank-v-neproplavu2.ogg", "font_big", "Tudy neproplavu, budeme to muset shodit.")
+
+dialog_addDialog("bank-v-nahazet", "en", "", "font_big", "We`re going to need to throw on a lot more of these things.")
+dialog_addDialog("bank-v-nahazet", "cs", "sound/"..codename.."/cs/bank-v-nahazet.ogg", "font_big", "Budeme tam těch věcí muset naházet asi víc.")
+
+dialog_addDialog("bank-v-jeste", "en", "", "font_big", "More.")
+dialog_addDialog("bank-v-jeste", "cs", "sound/"..codename.."/cs/bank-v-jeste.ogg", "font_big", "Ještě.")
+
+dialog_addDialog("bank-m-prohlednout", "en", "", "font_small", "We have to look everything here over very carefully.")
+dialog_addDialog("bank-m-prohlednout", "cs", "sound/"..codename.."/cs/bank-m-prohlednout.ogg", "font_small", "Musíme si tu všechno důkladně prohlédnout.")
+
+dialog_addDialog("bank-v-vypad1", "en", "", "font_big", "I`d rather get out of here.")
+dialog_addDialog("bank-v-vypad1", "cs", "sound/"..codename.."/cs/bank-v-vypad1.ogg", "font_big", "Raději bych rychle vypad`.")
+
+dialog_addDialog("bank-v-vypad2", "en", "", "font_big", "You know, I`d really rather get out of here.")
+dialog_addDialog("bank-v-vypad2", "cs", "sound/"..codename.."/cs/bank-v-vypad2.ogg", "font_big", "No, já bych nejraději už vypadl.")
+
+dialog_addDialog("bank-v-flaska", "en", "", "font_big", "The odor from this bottle is making me sick.")
+dialog_addDialog("bank-v-flaska", "cs", "sound/"..codename.."/cs/bank-v-flaska.ogg", "font_big", "Tahle flaška mi nedělá dobře.")
+
+dialog_addDialog("bank-m-rozbila", "en", "", "font_small", "Ooops, it broke...")
+dialog_addDialog("bank-m-rozbila", "cs", "sound/"..codename.."/cs/bank-m-rozbila.ogg", "font_small", "Jé, rozbila se...")
+
+dialog_addDialog("bank-m-tvorove", "en", "", "font_small", "I think that some of these creatures here are a result of genetic engineering.")
+dialog_addDialog("bank-m-tvorove", "cs", "sound/"..codename.."/cs/bank-m-tvorove.ogg", "font_small", "Myslím, že někteří ti tvorové tady jsou výsledkem genetických manipulací.")
+
+dialog_addDialog("bank-m-prehnal1", "en", "", "font_small", "This is just too much!")
+dialog_addDialog("bank-m-prehnal1", "cs", "sound/"..codename.."/cs/bank-m-prehnal1.ogg", "font_small", "Tohle někdo přehnal!")
+
+dialog_addDialog("bank-m-prehnal2", "en", "", "font_small", "How morbid!")
+dialog_addDialog("bank-m-prehnal2", "cs", "sound/"..codename.."/cs/bank-m-prehnal2.ogg", "font_small", "Tohle je tedy extrémně morbidní!")
+
+dialog_addDialog("bank-m-mrtvolka", "en", "", "font_small", "This corpse looks familiar to me.")
+dialog_addDialog("bank-m-mrtvolka", "cs", "sound/"..codename.."/cs/bank-m-mrtvolka.ogg", "font_small", "Tahle mrtvolka mi připadá povědomá.")
+
+
