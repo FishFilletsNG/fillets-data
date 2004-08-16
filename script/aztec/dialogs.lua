a2e73ac97c61b80478dec78c7713fbb207c8cde0
fidlej <fidlej>
2004-08-16 21:08:31 +0000

    Added level aztec.

---
+++ script/aztec/dialogs.lua
@@ -0,0 +1,52 @@
+dialog_addDialog("bot-m-vidis", "en", "", "font_small", "Can you see that seahorse?")
+dialog_addDialog("bot-m-vidis", "cs", "sound/"..codename.."/cs/bot-m-vidis.ogg", "font_small", "Vidíš toho koníka?")
+
+dialog_addDialog("bot-v-uveznen0", "en", "", "font_big", "It is blocked by amphoras.")
+dialog_addDialog("bot-v-uveznen0", "cs", "sound/"..codename.."/cs/bot-v-uveznen0.ogg", "font_big", "Je uvězněný mezi amforami.")
+
+dialog_addDialog("bot-v-uveznen1", "en", "", "font_big", "It got itself drunk among amphoras.")
+dialog_addDialog("bot-v-uveznen1", "cs", "sound/"..codename.."/cs/bot-v-uveznen1.ogg", "font_big", "Opíjel se mezi amforami.")
+
+dialog_addDialog("bot-m-zajem", "en", "", "font_small", "I wonder if there is anything left in them.")
+dialog_addDialog("bot-m-zajem", "cs", "sound/"..codename.."/cs/bot-m-zajem.ogg", "font_small", "Zajímalo by mě, jestli v nich ještě něco je.")
+
+dialog_addDialog("bot-v-podivat", "en", "", "font_big", "You probably have to go check for yourself.")
+dialog_addDialog("bot-v-podivat", "cs", "sound/"..codename.."/cs/bot-v-podivat.ogg", "font_big", "Asi se stejně budeš muset jít podívat.")
+
+dialog_addDialog("bot-m-vidim", "en", "", "font_small", "Finally, I can see some new kind of skull.")
+dialog_addDialog("bot-m-vidim", "cs", "sound/"..codename.."/cs/bot-m-vidim.ogg", "font_small", "Konečně vidím nějakou novou lebku.")
+
+dialog_addDialog("bot-v-vsim", "en", "", "font_big", "Did you notice that totem? This is Mexican god Shelloxuatl.")
+dialog_addDialog("bot-v-vsim", "cs", "sound/"..codename.."/cs/bot-v-vsim.ogg", "font_big", "Všimla sis toho totemu? To je mexický bůh Škleboxuatl.")
+
+dialog_addDialog("bot-m-vypada", "en", "", "font_small", "It looks like that.")
+dialog_addDialog("bot-m-vypada", "cs", "sound/"..codename.."/cs/bot-m-vypada.ogg", "font_small", "Vypadá na to.")
+
+dialog_addDialog("bot-m-padaji", "en", "", "font_small", "These amphores fall unbearably slow.")
+dialog_addDialog("bot-m-padaji", "cs", "sound/"..codename.."/cs/bot-m-padaji.ogg", "font_small", "Ty amfory padají nelidsky pomalu.")
+
+dialog_addDialog("bot-v-vsak0", "en", "", "font_big", "Well, you are not a bear, after all.")
+dialog_addDialog("bot-v-vsak0", "cs", "sound/"..codename.."/cs/bot-v-vsak0.ogg", "font_big", "Však také nejsi člověk.")
+
+dialog_addDialog("bot-v-vsak1", "en", "", "font_big", "Don`t forget we are under water.")
+dialog_addDialog("bot-v-vsak1", "cs", "sound/"..codename.."/cs/bot-v-vsak1.ogg", "font_big", "Však jsme také pod vodou.")
+
+dialog_addDialog("bot-m-ble", "en", "", "font_small", "Yuck. The authors could spare us that animation.")
+dialog_addDialog("bot-m-ble", "cs", "sound/"..codename.."/cs/bot-m-ble.ogg", "font_small", "Blé... Tuhle animaci si autoři mohli odpustit.")
+
+dialog_addDialog("bot-v-totem", "en", "", "font_big", "This totem looks good to me.")
+dialog_addDialog("bot-v-totem", "cs", "sound/"..codename.."/cs/bot-v-totem.ogg", "font_big", "Mně se ten totem líbí.")
+
+dialog_addDialog("bot-v-lebka", "en", "", "font_big", "That skull seems to radiate something strange.")
+dialog_addDialog("bot-v-lebka", "cs", "sound/"..codename.."/cs/bot-v-lebka.ogg", "font_big", "Z té lebky vyzařuje něco tajemného.")
+
+dialog_addDialog("bot-m-zivy", "en", "", "font_small", "Is it alive or is it some kind of spell?")
+dialog_addDialog("bot-m-zivy", "cs", "sound/"..codename.."/cs/bot-m-zivy.ogg", "font_small", "Je to živý, nebo je to kouzlo?")
+
+dialog_addDialog("bot-x-smich", "en", "sound/"..codename.."/en/bot-x-smich.ogg")
+
+dialog_addDialog("bot-x-gr0", "en", "sound/"..codename.."/en/bot-x-gr0.ogg")
+
+dialog_addDialog("bot-x-gr1", "en", "sound/"..codename.."/en/bot-x-gr1.ogg")
+
+
