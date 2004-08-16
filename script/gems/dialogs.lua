e19c2950a754e3d76ab76ab793dadf80bf125076
fidlej <fidlej>
2004-08-16 21:39:59 +0000

    Added level gems.

---
+++ script/gems/dialogs.lua
@@ -0,0 +1,40 @@
+dialog_addDialog("zav-v-sto", "en", "", "font_big", "Please note, that this level contains one hundred and nine objects.")
+dialog_addDialog("zav-v-sto", "cs", "sound/"..codename.."/cs/zav-v-sto.ogg", "font_big", "Povšimni si, že tato místnost obsahuje sto devět předmětů!")
+
+dialog_addDialog("zav-v-trpyt", "en", "", "font_big", "And all of them gleam beautifully.")
+dialog_addDialog("zav-v-trpyt", "cs", "sound/"..codename.."/cs/zav-v-trpyt.ogg", "font_big", "A všechny se krásně třpytí.")
+
+dialog_addDialog("zav-m-pohnout", "en", "", "font_small", "Please note, that I can`t move a fin here.")
+dialog_addDialog("zav-m-pohnout", "cs", "sound/"..codename.."/cs/zav-m-pohnout.ogg", "font_small", "Povšimni si toho, že se nemůžu pohnout.")
+
+dialog_addDialog("zav-m-krasa", "en", "", "font_small", "It`s beautiful! Everything gleams so much...")
+dialog_addDialog("zav-m-krasa", "cs", "sound/"..codename.."/cs/zav-m-krasa.ogg", "font_small", "To je ale krása! Všechno se tu třpytí...")
+
+dialog_addDialog("zav-v-venku", "en", "", "font_big", "It`ll be much nicer when we get out.")
+dialog_addDialog("zav-v-venku", "cs", "sound/"..codename.."/cs/zav-v-venku.ogg", "font_big", "Ještě krásnější to bude, až budeme venku.")
+
+dialog_addDialog("zav-v-zaval", "en", "", "font_big", "This is very dangerous cave-in. We are lucky we stayed alive.")
+dialog_addDialog("zav-v-zaval", "cs", "sound/"..codename.."/cs/zav-v-zaval.ogg", "font_big", "To je ale nebezpečný zával. Máme štěstí, že jsme zůstaly naživu.")
+
+dialog_addDialog("zav-m-hopskok", "en", "", "font_small", "Don`t speak so soon. We`re still in here.")
+dialog_addDialog("zav-m-hopskok", "cs", "sound/"..codename.."/cs/zav-m-hopskok.ogg", "font_small", "Neříkej hop, dokud nepřeskočíš.")
+
+dialog_addDialog("zav-m-kameny", "en", "", "font_small", "Don`t you think that these stones are arranged in a different way?")
+dialog_addDialog("zav-m-kameny", "cs", "sound/"..codename.."/cs/zav-m-kameny.ogg", "font_small", "Nezdá se ti, že jsou ty kameny jinak poskládané?")
+
+dialog_addDialog("zav-v-zeleny", "en", "", "font_big", "You are right. That green one was surely over there!")
+dialog_addDialog("zav-v-zeleny", "cs", "sound/"..codename.."/cs/zav-v-zeleny.ogg", "font_big", "Máš pravdu. Ten zelený byl určitě támhle!")
+
+dialog_addDialog("zav-v-restart", "en", "", "font_big", "I think that these jewels arrange themselves in a different way after each restart.")
+dialog_addDialog("zav-v-restart", "cs", "sound/"..codename.."/cs/zav-v-restart.ogg", "font_big", "Mám pocit, že po každém restartu jsou tu jinak poskládané drahokamy.")
+
+dialog_addDialog("zav-m-pravda", "en", "", "font_small", "Yeah, you`re right. Maybe they form some sensible pattern sometimes.")
+dialog_addDialog("zav-m-pravda", "cs", "sound/"..codename.."/cs/zav-m-pravda.ogg", "font_small", "Jé... Asi máš pravdu. Třeba někdy vytvoří nějaký smysluplný obrazec.")
+
+dialog_addDialog("zav-m-hrac", "en", "", "font_small", "Look, our player obviously satiated his need for colors.")
+dialog_addDialog("zav-m-hrac", "cs", "sound/"..codename.."/cs/zav-m-hrac.ogg", "font_small", "Hele, náš hráč už se nabažil barviček.")
+
+dialog_addDialog("zav-v-zachranit", "en", "", "font_big", "Maybe he could try to save us instead now.")
+dialog_addDialog("zav-v-zachranit", "cs", "sound/"..codename.."/cs/zav-v-zachranit.ogg", "font_big", "Teď by nás pro změnu mohl zkusit zachránit.")
+
+
