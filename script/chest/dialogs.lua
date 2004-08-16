8df16239a2efc15e7ab2939fcdc337dfdafa7bc0
fidlej <fidlej>
2004-08-16 21:47:49 +0000

    Added level chest.

---
+++ script/chest/dialogs.lua
@@ -0,0 +1,70 @@
+dialog_addDialog("tru-v-nasly", "en", "", "font_big", "So we have found it finally.")
+dialog_addDialog("tru-v-nasly", "cs", "sound/"..codename.."/cs/tru-v-nasly.ogg", "font_big", "Tak jsme to konečně našly.")
+
+dialog_addDialog("tru-m-co", "en", "", "font_small", "And what?")
+dialog_addDialog("tru-m-co", "cs", "sound/"..codename.."/cs/tru-m-co.ogg", "font_small", "Co?")
+
+dialog_addDialog("tru-v-poklad", "en", "", "font_big", "Captain Silver`s treasure, of course.")
+dialog_addDialog("tru-v-poklad", "cs", "sound/"..codename.."/cs/tru-v-poklad.ogg", "font_big", "No přeci poklady kapitána Silvera.")
+
+dialog_addDialog("tru-v-gral", "en", "", "font_big", "The Holy Grail, of course.")
+dialog_addDialog("tru-v-gral", "cs", "sound/"..codename.."/cs/tru-v-gral.ogg", "font_big", "No přeci svatý grál.")
+
+dialog_addDialog("tru-v-vkupe", "en", "", "font_big", "It`s neatly in one place: the treasure and the Grail.")
+dialog_addDialog("tru-v-vkupe", "cs", "sound/"..codename.."/cs/tru-v-vkupe.ogg", "font_big", "Tak to tu máme pěkně v kupě: poklady i Grál.")
+
+dialog_addDialog("tru-m-zrada", "en", "", "font_small", "I think it`s much too easy to be true.")
+dialog_addDialog("tru-m-zrada", "cs", "sound/"..codename.."/cs/tru-m-zrada.ogg", "font_small", "Jen aby v tom nebyla nějaká zrada.")
+
+dialog_addDialog("tru-m-oznamit", "en", "", "font_small", "I think that if it really was our goal, the game would let the player know.")
+dialog_addDialog("tru-m-oznamit", "cs", "sound/"..codename.."/cs/tru-m-oznamit.ogg", "font_small", "Já myslím, že kdyby to bylo skutečně ono, hra by to hráči nějak výrazně oznámila.")
+
+dialog_addDialog("tru-v-stacit", "en", "", "font_big", "But I have let him know. This could be enough.")
+dialog_addDialog("tru-v-stacit", "cs", "sound/"..codename.."/cs/tru-v-stacit.ogg", "font_big", "Však jsem mu to řekl. To by mu mohlo stačit.")
+
+dialog_addDialog("tru-m-zpochybnit", "en", "", "font_small", "But I have casted doubts on it. This could confuse him.")
+dialog_addDialog("tru-m-zpochybnit", "cs", "sound/"..codename.."/cs/tru-m-zpochybnit.ogg", "font_small", "Jenže já jsem to zpochybnila. To by jej mohlo zmást.")
+
+dialog_addDialog("tru-v-nejspis", "en", "", "font_big", "Well, all right. PROBABLY this is not what we are looking for.")
+dialog_addDialog("tru-v-nejspis", "cs", "sound/"..codename.."/cs/tru-v-nejspis.ogg", "font_big", "No tak dobře. Tak to NEJSPÍŠ ještě není ono.")
+
+dialog_addDialog("tru-m-nejistota", "en", "", "font_small", "This is better. There remains a bit of uncertainty.")
+dialog_addDialog("tru-m-nejistota", "cs", "sound/"..codename.."/cs/tru-m-nejistota.ogg", "font_small", "Tak je to lepší. Zůstává v tom střípek nejistoty.")
+
+dialog_addDialog("tru-m-truhla0", "en", "", "font_small", "Couldn`t we close that chest?")
+dialog_addDialog("tru-m-truhla0", "cs", "sound/"..codename.."/cs/tru-m-truhla0.ogg", "font_small", "Nemůže se ta truhla najednou zavřít?")
+
+dialog_addDialog("tru-m-truhla1", "en", "", "font_small", "What if we try to close that chest?")
+dialog_addDialog("tru-m-truhla1", "cs", "sound/"..codename.."/cs/tru-m-truhla1.ogg", "font_small", "Co kdybychom zkusili tu truhlu zavřít?")
+
+dialog_addDialog("tru-v-truhla0", "en", "", "font_big", "It would be much easier with the closed chest.")
+dialog_addDialog("tru-v-truhla0", "cs", "sound/"..codename.."/cs/tru-v-truhla0.ogg", "font_big", "Se zavřenou truhlou by bylo všechno jednodušší.")
+
+dialog_addDialog("tru-v-truhla1", "en", "", "font_big", "Try to push its lid.")
+dialog_addDialog("tru-v-truhla1", "cs", "sound/"..codename.."/cs/tru-v-truhla1.ogg", "font_big", "Zkus jí strčit do víka.")
+
+dialog_addDialog("tru-m-vzit0", "en", "", "font_small", "Won`t we take something with us?")
+dialog_addDialog("tru-m-vzit0", "cs", "sound/"..codename.."/cs/tru-m-vzit0.ogg", "font_small", "Nevezmeme si s sebou něco?")
+
+dialog_addDialog("tru-m-vzit1", "en", "", "font_small", "What about taking some treasure with us?")
+dialog_addDialog("tru-m-vzit1", "cs", "sound/"..codename.."/cs/tru-m-vzit1.ogg", "font_small", "Co si takhle vzít nějaký poklad s sebou?")
+
+dialog_addDialog("tru-m-vzit2", "en", "", "font_small", "Ain`t you sorry to leave all these treasures behind?")
+dialog_addDialog("tru-m-vzit2", "cs", "sound/"..codename.."/cs/tru-m-vzit2.ogg", "font_small", "Není ti líto, že tu necháme všechny ty poklady?")
+
+dialog_addDialog("tru-v-vzit0", "en", "", "font_big", "We will be glad if we manage to get out ourselves.")
+dialog_addDialog("tru-v-vzit0", "cs", "sound/"..codename.."/cs/tru-v-vzit0.ogg", "font_big", "Budeme rádi, když odjedem aspoň my samy.")
+
+dialog_addDialog("tru-v-vzit1", "en", "", "font_big", "Don`t forget our mission.")
+dialog_addDialog("tru-v-vzit1", "cs", "sound/"..codename.."/cs/tru-v-vzit1.ogg", "font_big", "Nezapomeň, že máme poslání.")
+
+dialog_addDialog("tru-v-vzit2", "en", "", "font_big", "A treasure would only hinder us.")
+dialog_addDialog("tru-v-vzit2", "cs", "sound/"..codename.."/cs/tru-v-vzit2.ogg", "font_big", "Poklady by nás jenom zdržovaly.")
+
+dialog_addDialog("tru-v-zrak", "en", "", "font_big", "All this glint is a sight for the sore eyes.")
+dialog_addDialog("tru-v-zrak", "cs", "sound/"..codename.."/cs/tru-v-zrak.ogg", "font_big", "Už mě z toho lesku přechází zrak.")
+
+dialog_addDialog("tru-m-trpyt", "en", "", "font_small", "Everything here gleams so. It makes me dizzy.")
+dialog_addDialog("tru-m-trpyt", "cs", "sound/"..codename.."/cs/tru-m-trpyt.ogg", "font_small", "Všechno se tu tak třpytí. Až z toho oči přecházejí.")
+
+
