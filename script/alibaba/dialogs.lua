633e5242d3e290d2921830a4521fb7e0dbc4a644
fidlej <fidlej>
2004-08-18 12:02:03 +0000

    Added level 'alibaba'.

---
+++ script/alibaba/dialogs.lua
@@ -0,0 +1,43 @@
+dialog_addDialog("kni-m-svicny", "en", "", "font_small", "From treasury to the candlestick warehouse. Now, this is what I call a progress.")
+dialog_addDialog("kni-m-svicny", "cs", "sound/"..codename.."/cs/kni-m-svicny.ogg", "font_small", "Z pokladnice do skladu svícnů. Tomu říkám pokrok.")
+
+dialog_addDialog("kni-v-ber", "en", "", "font_big", "You have to look at the bright side. There is nothing that looks like grail here.")
+dialog_addDialog("kni-v-ber", "cs", "sound/"..codename.."/cs/kni-v-ber.ogg", "font_big", "Ber to z té lepší stránky. Tady například není nic, co by se podobalo grálu.")
+
+dialog_addDialog("kni-v-prolezt", "en", "", "font_big", "It seems that I`ll have to go through that horrible maze.")
+dialog_addDialog("kni-v-prolezt", "cs", "sound/"..codename.."/cs/kni-v-prolezt.ogg", "font_big", "Zdá se, že budu muset prolézt tím strašným bludištěm.")
+
+dialog_addDialog("kni-m-tloustka", "en", "", "font_small", "Your fat body. It always hinders us.")
+dialog_addDialog("kni-m-tloustka", "cs", "sound/"..codename.."/cs/kni-m-tloustka.ogg", "font_small", "Ta tvoje tloušťka. Ta nás vždycky zdržuje.")
+
+dialog_addDialog("kni-v-padavko", "en", "", "font_big", "Just leave me alone, you emaciated pollywog.")
+dialog_addDialog("kni-v-padavko", "cs", "sound/"..codename.."/cs/kni-v-padavko.ogg", "font_big", "Nezačínej s tím zase, ty padavko.")
+
+dialog_addDialog("kni-m-hromado", "en", "", "font_small", "Cut this crap, you misshaped heap of muscles and scales, will you?")
+dialog_addDialog("kni-m-hromado", "cs", "sound/"..codename.."/cs/kni-m-hromado.ogg", "font_small", "Na ty tvoje řeči jsem tak akorát zvědavá, ty hromado svalů a ploutví.")
+
+dialog_addDialog("kni-v-vypni", "en", "", "font_big", "Could you restart the level, please? We don`t seem to be getting along very well now.")
+dialog_addDialog("kni-v-vypni", "cs", "sound/"..codename.."/cs/kni-v-vypni.ogg", "font_big", "Mohl bys, prosím tě, restartovat level? Zdá se, že nám to teď spolu moc nejde.")
+
+dialog_addDialog("kni-m-hrncirstvi", "en", "", "font_small", "I feel like I am in the pottery warehouse.")
+dialog_addDialog("kni-m-hrncirstvi", "cs", "sound/"..codename.."/cs/kni-m-hrncirstvi.ogg", "font_small", "Připadám si tu jako ve skladu hrnčířství.")
+
+dialog_addDialog("kni-v-amforstvi", "en", "", "font_big", "You mean, amphory warehouse.")
+dialog_addDialog("kni-v-amforstvi", "cs", "sound/"..codename.."/cs/kni-v-amforstvi.ogg", "font_big", "Chceš říci: amfórství.")
+
+dialog_addDialog("kni-m-amfornictvi", "en", "", "font_small", "No, it should be amphoraware.")
+dialog_addDialog("kni-m-amfornictvi", "cs", "sound/"..codename.."/cs/kni-m-amfornictvi.ogg", "font_small", "Když už, tak: amfórnictví.")
+
+dialog_addDialog("kni-m-mise", "en", "", "font_small", "You know what? I think that this mission really helped me.")
+dialog_addDialog("kni-m-mise", "cs", "sound/"..codename.."/cs/kni-m-mise.ogg", "font_small", "Víš, myslím, že tahle celá jeskynní mise mi hodně pomohla.")
+
+dialog_addDialog("kni-v-proc", "en", "", "font_big", "And why?")
+dialog_addDialog("kni-v-proc", "cs", "sound/"..codename.."/cs/kni-v-proc.ogg", "font_big", "A proč?")
+
+dialog_addDialog("kni-m-cetky", "en", "", "font_small", "I grew to understand that jewels and gold are but cheap trinkets.")
+dialog_addDialog("kni-m-cetky", "cs", "sound/"..codename.."/cs/kni-m-cetky.ogg", "font_small", "Pochopila jsem, že šperky a zlato jsou jenom laciné cetky.")
+
+dialog_addDialog("kni-m-kramy", "en", "", "font_small", "I can`t bear the darned stuff any longer!")
+dialog_addDialog("kni-m-kramy", "cs", "sound/"..codename.."/cs/kni-m-kramy.ogg", "font_small", "Už ty krámy nemůžu ani vidět!")
+
+
