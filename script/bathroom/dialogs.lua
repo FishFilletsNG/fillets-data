c28ad782802a069e543e58fa5c7a9ecf254d3a02
ronnystandtke <ronnystandtke>
2004-08-18 17:16:24 +0000

    added German translation

--- script/bathroom/dialogs.lua
+++ script/bathroom/dialogs.lua
@@ -1,94 +1,119 @@
 dialog_addDialog("br-v-komfort", "en", "", "font_big", "It`s quite comfortable for a wasteyard.")
 dialog_addDialog("br-v-komfort", "cs", "sound/"..codename.."/cs/br-v-komfort.ogg", "font_big", "Na to, že je to vlastně smetiště, je to docela komfort.")
 dialog_addDialog("br-v-komfort", "fr", "", "font_big", "Plutôt confortable pour un taudis.")
+dialog_addDialog("br-v-komfort", "de", "", "font_big", "Ziemlich gemütlich für einen Schrottplatz.")
 
 dialog_addDialog("br-m-bydli", "en", "", "font_small", "What do you think, does anybody live here?")
 dialog_addDialog("br-m-bydli", "cs", "sound/"..codename.."/cs/br-m-bydli.ogg", "font_small", "Myslíš, že tu někdo bydlí?")
 dialog_addDialog("br-m-bydli", "fr", "", "font_small", "Que crois-tu, que personne ne vit ici ?")
+dialog_addDialog("br-m-bydli", "de", "", "font_small", "Meinst Du, hier wohnt jemand?")
 
 dialog_addDialog("br-v-santusak", "en", "", "font_big", "Some underwater homeless.")
 dialog_addDialog("br-v-santusak", "cs", "sound/"..codename.."/cs/br-v-santusak.ogg", "font_big", "Nějaký podvodní santusák.")
 dialog_addDialog("br-v-santusak", "fr", "", "font_big", "une sorte sans-abris des mers.")
+dialog_addDialog("br-v-santusak", "de", "", "font_big", "Irgendein Unterwasser-Obdachloser.")
 
 dialog_addDialog("br-m-podvodnik", "en", "", "font_small", "We call them shell-less.")
 dialog_addDialog("br-m-podvodnik", "cs", "sound/"..codename.."/cs/br-m-podvodnik.ogg", "font_small", "Nebo santusácký podvodník.")
 dialog_addDialog("br-m-podvodnik", "fr", "", "font_small", "Nous les appellons les sans-coquilles.")
+dialog_addDialog("br-m-podvodnik", "de", "", "font_small", "Wir sagen dazu \"Schalenloser\".")
 
 dialog_addDialog("br-m-vsim0", "en", "", "font_small", "Did you notice that...")
 dialog_addDialog("br-m-vsim0", "cs", "sound/"..codename.."/cs/br-m-vsim0.ogg", "font_small", "Všiml sis, že...")
 dialog_addDialog("br-m-vsim0", "fr", "", "font_small", "As-tu noté que...")
+dialog_addDialog("br-m-vsim0", "de", "", "font_small", "Hast Du bemerkt, dass...")
 
 dialog_addDialog("br-m-vsim1", "en", "", "font_small", "Don`t you think that...")
 dialog_addDialog("br-m-vsim1", "cs", "sound/"..codename.."/cs/br-m-vsim1.ogg", "font_small", "Není ti divné, že...")
 dialog_addDialog("br-m-vsim1", "fr", "", "font_small", "Ne penses-tu pas que...")
+dialog_addDialog("br-m-vsim1", "de", "", "font_small", "Meinst Du, dass...")
 
 dialog_addDialog("br-m-vsim2", "en", "", "font_small", "Isn`t it strange that...")
 dialog_addDialog("br-m-vsim2", "cs", "sound/"..codename.."/cs/br-m-vsim2.ogg", "font_small", "To je zvláštní, že...")
 dialog_addDialog("br-m-vsim2", "fr", "", "font_small", "N'est-il pas étrange que...")
+dialog_addDialog("br-m-vsim2", "de", "", "font_small", "Ist es nicht seltsam, dass...")
 
 dialog_addDialog("br-v-nerozvadet0", "en", "", "font_big", "Don`t speak about it.")
 dialog_addDialog("br-v-nerozvadet0", "cs", "sound/"..codename.."/cs/br-v-nerozvadet0.ogg", "font_big", "Já bych to nerozváděl.")
 dialog_addDialog("br-v-nerozvadet0", "fr", "", "font_big", "N'en parlons pas.")
+dialog_addDialog("br-v-nerozvadet0", "de", "", "font_big", "Schweig still!")
 
 dialog_addDialog("br-v-nerozvadet1", "en", "", "font_big", "Please, no jokes about defecation.")
 dialog_addDialog("br-v-nerozvadet1", "cs", "sound/"..codename.."/cs/br-v-nerozvadet1.ogg", "font_big", "Prosím tě, žádné fekální vtipy.")
 dialog_addDialog("br-v-nerozvadet1", "fr", "", "font_big", "S'il te plaît, pas de plaisanterie sur la défécation.")
+dialog_addDialog("br-v-nerozvadet1", "de", "", "font_big", "Bitte, keine Witze über Stuhlgang!")
 
 dialog_addDialog("br-v-nerozvadet2", "en", "", "font_big", "Yes, I know about that. We won`t comment on it.")
 dialog_addDialog("br-v-nerozvadet2", "cs", "sound/"..codename.."/cs/br-v-nerozvadet2.ogg", "font_big", "Ano, vím o tom. Nebudeme to komentovat.")
 dialog_addDialog("br-v-nerozvadet2", "fr", "", "font_big", "Oui, je suis au courant. Pas de commentaire.")
+dialog_addDialog("br-v-nerozvadet2", "de", "", "font_big", "Ja, ich weiß. Dazu sagen wir nichts.")
+dialog_addDialog("br-v-nerozvadet2", "de_CH", "", "font_big", "Ja, ich weiss. Dazu sagen wir nichts.")
 
 dialog_addDialog("br-m-dva", "en", "", "font_small", "Yeah, but why TWO?")
 dialog_addDialog("br-m-dva", "cs", "sound/"..codename.."/cs/br-m-dva.ogg", "font_small", "Ale proč DVA?")
 dialog_addDialog("br-m-dva", "fr", "", "font_small", "Ouais, mais pourquoi DEUX ?")
+dialog_addDialog("br-m-dva", "de", "", "font_small", "Ja, aber warum ZWEI?")
 
 dialog_addDialog("br-v-dost", "en", "", "font_big", "Stop it, will you?")
 dialog_addDialog("br-v-dost", "cs", "sound/"..codename.."/cs/br-v-dost.ogg", "font_big", "Prosím tě, nech toho!")
 dialog_addDialog("br-v-dost", "fr", "", "font_big", "Arrête, veux-tu ?")
+dialog_addDialog("br-v-dost", "de", "", "font_big", "Wirst Du wohl aufhören!?")
 
 dialog_addDialog("br-m-sprcha", "en", "", "font_small", "I`ve seen many useless things, but an underwater shower...")
 dialog_addDialog("br-m-sprcha", "cs", "sound/"..codename.."/cs/br-m-sprcha.ogg", "font_small", "Viděla jsem tu hodně zbytečností, ale podvodní sprcha...")
 dialog_addDialog("br-m-sprcha", "fr", "", "font_small", "J'ai vu beaucoup de choses inutiles, mais une douche sous-marine...")
+dialog_addDialog("br-m-sprcha", "de", "", "font_small", "Ich habe schon viele nutzlose Dinge gesehen, aber eine Unterwasserdusche...")
 
 dialog_addDialog("br-v-lazen", "en", "", "font_big", "Maybe it sprays air, like in the bubble bath.")
 dialog_addDialog("br-v-lazen", "cs", "sound/"..codename.."/cs/br-v-lazen.ogg", "font_big", "Třeba z ní tryská vzduch, jako ve vířivé lázni.")
 dialog_addDialog("br-v-lazen", "fr", "", "font_big", "Peut-être qu'elle lance de l'air, comme pour le bain à bulles.")
+dialog_addDialog("br-v-lazen", "de", "", "font_big", "Vielleicht versprüht sie Luft, wie im Blubberbad.")
 
 dialog_addDialog("br-m-zapnout", "en", "", "font_small", "Do you think we could turn it on?")
 dialog_addDialog("br-m-zapnout", "cs", "sound/"..codename.."/cs/br-m-zapnout.ogg", "font_small", "Myslíš, že bychom ji mohli zapnout?")
 dialog_addDialog("br-m-zapnout", "fr", "", "font_small", "Crois-tu que nous pourrons ouvrir le robinet ?")
+dialog_addDialog("br-m-zapnout", "de", "", "font_small", "Glaubst Du, wir können sie aufdrehen?")
 
 dialog_addDialog("br-v-shodit", "en", "", "font_big", "Hardly. But we could push it down.")
 dialog_addDialog("br-v-shodit", "cs", "sound/"..codename.."/cs/br-v-shodit.ogg", "font_big", "Těžko. Ale mohli bychom ji shodit.")
 dialog_addDialog("br-v-shodit", "fr", "", "font_big", "Difficilement. Mais nous pouvons la faire tomber.")
+dialog_addDialog("br-v-shodit", "de", "", "font_big", "Kaum. Aber wir können sie runterschieben.")
 
 dialog_addDialog("br-m-poklady", "en", "", "font_small", "Now this is real treasure.")
 dialog_addDialog("br-m-poklady", "cs", "sound/"..codename.."/cs/br-m-poklady.ogg", "font_small", "No to jsou poklady.")
 dialog_addDialog("br-m-poklady", "fr", "", "font_small", "Maintenant c'est un vrai trésor.")
+dialog_addDialog("br-m-poklady", "de", "", "font_small", "Das ist mal ein echter Schatz!")
 
 dialog_addDialog("br-v-nechat", "en", "", "font_big", "We should have let it alone.")
 dialog_addDialog("br-v-nechat", "cs", "sound/"..codename.."/cs/br-v-nechat.ogg", "font_big", "Měli jsme ji nechat tam, kde byla.")
 dialog_addDialog("br-v-nechat", "fr", "", "font_big", "Nous devrions le laissez seul.")
+dialog_addDialog("br-v-nechat", "de", "", "font_big", "Wir hätten es in Ruhe lassen sollen.")
 
 dialog_addDialog("br-m-nefunguje", "en", "", "font_small", "And it doesn`t work anyway.")
 dialog_addDialog("br-m-nefunguje", "cs", "sound/"..codename.."/cs/br-m-nefunguje.ogg", "font_small", "A navíc stejně nefunguje.")
 dialog_addDialog("br-m-nefunguje", "fr", "", "font_small", "Et il ne marchera pas de toute façon.")
+dialog_addDialog("br-m-nefunguje", "de", "", "font_small", "Es funktioniert doch sowieso nicht.")
 
 dialog_addDialog("br-m-ahoj", "en", "", "font_small", "Hello, inside there.")
 dialog_addDialog("br-m-ahoj", "cs", "sound/"..codename.."/cs/br-m-ahoj.ogg", "font_small", "Ahoj tam uvnitř.")
 dialog_addDialog("br-m-ahoj", "fr", "", "font_small", "Hého, à l'intérieur.")
+dialog_addDialog("br-m-ahoj", "de", "", "font_small", "Hallo da drin!.")
 
 dialog_addDialog("br-v-draha", "en", "", "font_big", "This must be better than rollercoaster.")
 dialog_addDialog("br-v-draha", "cs", "sound/"..codename.."/cs/br-v-draha.ogg", "font_big", "To je určitě lepší než horská dráha.")
 dialog_addDialog("br-v-draha", "fr", "", "font_big", "Ce doit être mieux que le rouleau des vagues.")
+dialog_addDialog("br-v-draha", "de", "", "font_big", "Das muss besser als Achterbahn sein.")
 
 dialog_addDialog("br-m-zkusit", "en", "", "font_small", "I would love to try it myself.")
 dialog_addDialog("br-m-zkusit", "cs", "sound/"..codename.."/cs/br-m-zkusit.ogg", "font_small", "Hned bych si to taky zkusila.")
 dialog_addDialog("br-m-zkusit", "fr", "", "font_small", "J'adorerai l'essayer moi-même.")
+dialog_addDialog("br-m-zkusit", "de", "", "font_small", "Ich würde es gerne mal selbst versuchen.")
 
 dialog_addDialog("br-m-bavi", "en", "", "font_small", "He seems to be enjoying himself.")
 dialog_addDialog("br-m-bavi", "cs", "sound/"..codename.."/cs/br-m-bavi.ogg", "font_small", "Zdá se, že se dobře baví.")
 dialog_addDialog("br-m-bavi", "fr", "", "font_small", "Il semble beaucoup apprécier lui-même.")
+dialog_addDialog("br-m-bavi", "de", "", "font_small", "Scheint ihm Spaß zu machen.")
+dialog_addDialog("br-m-bavi", "de_CH", "", "font_small", "Scheint ihm Spass zu machen.")
 
 dialog_addDialog("br-x-pracka", "en", "sound/"..codename.."/en/br-x-pracka.ogg")
 
