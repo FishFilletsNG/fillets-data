dad73b6710e275e2026b9eea71222dab8b43cb48
ronnystandtke <ronnystandtke>
2004-08-25 05:47:25 +0000

    added German translation

--- script/cancan/dialogs.lua
+++ script/cancan/dialogs.lua
@@ -1,6 +1,7 @@
 dialog_addDialog("kan-v-proc", "en", "", "font_big", "Why doesn`t he play?")
 dialog_addDialog("kan-v-proc", "cs", "sound/"..codename.."/cs/kan-v-proc.ogg", "font_big", "Proč nehraje?")
 dialog_addDialog("kan-v-proc", "fr", "", "font_big", "Pourquoi il ne joue pas ?")
+dialog_addDialog("kan-v-proc", "de", "", "font_big", "Warum spielt er nicht?")
 
 dialog_addDialog("kan-klavir-music", "en", "sound/"..codename.."/en/kan-klavir-music.ogg")
 
