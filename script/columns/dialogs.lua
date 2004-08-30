8cf18a38ad2d2d1021c2fcec3c194395e0fb630b
ronnystandtke <ronnystandtke>
2004-08-30 06:31:43 +0000

    added German translation

--- script/columns/dialogs.lua
+++ script/columns/dialogs.lua
@@ -1,55 +1,68 @@
 dialog_addDialog("sl-v-stopa", "en", "", "font_big", "I think that we are finally on the trail of that mysterious city.")
 dialog_addDialog("sl-v-stopa", "cs", "sound/"..codename.."/cs/sl-v-stopa.ogg", "font_big", "Myslím, že jsme skutečně na stopě tomu tajemnému městu.")
 dialog_addDialog("sl-v-stopa", "fr", "", "font_big", "Je pense que nous sommes sur le point de trouver cette mystérieuse cité.")
+dialog_addDialog("sl-v-stopa", "de", "", "font_big", "Ich denke, dass wir dieser geheimnisvollen Stadt endlich auf der Spur sind.")
 
 dialog_addDialog("sl-m-velkolepe", "en", "", "font_small", "It looks magnificent.")
 dialog_addDialog("sl-m-velkolepe", "cs", "sound/"..codename.."/cs/sl-m-velkolepe.ogg", "font_small", "Vypadá to velkolepě.")
 dialog_addDialog("sl-m-velkolepe", "fr", "", "font_small", "C'est magnifique.")
+dialog_addDialog("sl-m-velkolepe", "de", "", "font_small", "Sieht prächtig aus.")
 
 dialog_addDialog("sl-v-vkapse", "en", "", "font_big", "Now we only have to find out why it sank and we are done.")
 dialog_addDialog("sl-v-vkapse", "cs", "sound/"..codename.."/cs/sl-v-vkapse.ogg", "font_big", "Teď už musíme jen zjistit, proč se vlastně potopilo a máme to v kapse.")
 dialog_addDialog("sl-v-vkapse", "fr", "", "font_big", "Il ne nous reste plus qu'à trouver pourquoi elle a sombré et c'est bon.")
+dialog_addDialog("sl-v-vkapse", "de", "", "font_big", "Jetzt müssen wir nur noch herausfinden, warum sie versunken ist und wir sind fertig.")
 
 dialog_addDialog("sl-m-trvat", "en", "", "font_small", "But it may take us a little while.")
 dialog_addDialog("sl-m-trvat", "cs", "sound/"..codename.."/cs/sl-m-trvat.ogg", "font_small", "Ale možná nám to bude ještě chvíli trvat.")
 dialog_addDialog("sl-m-trvat", "fr", "", "font_small", "Mais ça va nous prendre du temps.")
+dialog_addDialog("sl-m-trvat", "de", "", "font_small", "Dafür brauchen wir aber ein Weilchen.")
 
 dialog_addDialog("sl-v-nechme", "en", "", "font_big", "Leave me alone. I adore the classic beauty of this temple.")
 dialog_addDialog("sl-v-nechme", "cs", "sound/"..codename.."/cs/sl-v-nechme.ogg", "font_big", "Nech mě. Kochám se antickou dokonalostí tohoto chrámu.")
 dialog_addDialog("sl-v-nechme", "fr", "", "font_big", "Laisse moi tranquille. J'adore la beauté classique de ce temple.")
+dialog_addDialog("sl-v-nechme", "de", "", "font_big", "Lass mich in Ruhe. Ich bewundere die klassische Schönheit dieses Tempels.")
 
 dialog_addDialog("sl-m-sedi", "en", "", "font_small", "Who is sitting on that chair?")
 dialog_addDialog("sl-m-sedi", "cs", "sound/"..codename.."/cs/sl-m-sedi.ogg", "font_small", "Kdo to tam sedí na židli?")
 dialog_addDialog("sl-m-sedi", "fr", "", "font_small", "Qui est assis sur la chaise ?")
+dialog_addDialog("sl-m-sedi", "de", "", "font_small", "Wer sitzt dort auf dem Sessel?")
 
 dialog_addDialog("sl-v-feidios", "en", "", "font_big", "It`s a copy of Feidios` Zeus. One of the seven wonders of the world.")
 dialog_addDialog("sl-v-feidios", "cs", "sound/"..codename.."/cs/sl-v-feidios.ogg", "font_big", "To je kopie Feidiova Dia. Jeden ze sedmi divů světa.")
 dialog_addDialog("sl-v-feidios", "fr", "", "font_big", "C'est une copie du Zeus de Feidios. Une des sept merveilles du monde.")
+dialog_addDialog("sl-v-feidios", "de", "", "font_big", "Eine Kopie von Feidios Zeus. Eins der sieben Weltwunder.")
 
 dialog_addDialog("sl-m-tehdy", "en", "", "font_small", "Hmm. It was another age.")
 dialog_addDialog("sl-m-tehdy", "cs", "sound/"..codename.."/cs/sl-m-tehdy.ogg", "font_small", "No. Tehdy byla jiná doba.")
 dialog_addDialog("sl-m-tehdy", "fr", "", "font_small", "Hmm. Il est d'une autre ère.")
+dialog_addDialog("sl-m-tehdy", "de", "", "font_small", "Hmm. Das war ein anderes Zeitalter.")
 
 dialog_addDialog("sl-v-opatrne", "en", "", "font_big", "Be careful! Try not to damage it!")
 dialog_addDialog("sl-v-opatrne", "cs", "sound/"..codename.."/cs/sl-v-opatrne.ogg", "font_big", "Opatrně! Ať to nepoškodíš.")
 dialog_addDialog("sl-v-opatrne", "fr", "", "font_big", "Attention ! Essaie de ne pas le casser !")
+dialog_addDialog("sl-v-opatrne", "de", "", "font_big", "Sei vorsichtig! Mach es nicht kaputt!")
 
 dialog_addDialog("sl-v-skoda", "en", "", "font_big", "Such a waste!")
 dialog_addDialog("sl-v-skoda", "cs", "sound/"..codename.."/cs/sl-v-skoda.ogg", "font_big", "Taková škoda!")
 dialog_addDialog("sl-v-skoda", "fr", "", "font_big", "Quel gâchis !")
+dialog_addDialog("sl-v-skoda", "de", "", "font_big", "Was für ein Verlust!")
 
 dialog_addDialog("sl-v-pust", "en", "", "font_big", "And now ease it down, carefully.")
 dialog_addDialog("sl-v-pust", "cs", "sound/"..codename.."/cs/sl-v-pust.ogg", "font_big", "Teď to opatrně pusť.")
 dialog_addDialog("sl-v-pust", "fr", "", "font_big", "Et maintenant fais la descendre, tout doucement.")
+dialog_addDialog("sl-v-pust", "de", "", "font_big", "Und jetzt lass es vorsichtig wieder runter!")
 
 dialog_addDialog("sl-m-jekot", "en", "sound/"..codename.."/en/sl-m-jekot.ogg")
 
 dialog_addDialog("sl-v-barbarka", "en", "", "font_big", "You barbarian! Can`t you be a little bit cautious?!")
 dialog_addDialog("sl-v-barbarka", "cs", "sound/"..codename.."/cs/sl-v-barbarka.ogg", "font_big", "Barbarko! Nemůžeš si dát trochu pozor?!")
 dialog_addDialog("sl-v-barbarka", "fr", "", "font_big", "Vandale ! Tu ne peux pas faire un petit peu attention ?!")
+dialog_addDialog("sl-v-barbarka", "de", "", "font_big", "Du Barbar! Kannst Du nicht ein wenig vorsichtiger sein?!")
 
 dialog_addDialog("sl-m-nelibila", "en", "", "font_small", "I didn`t like this sculpture anyway.")
 dialog_addDialog("sl-m-nelibila", "cs", "sound/"..codename.."/cs/sl-m-nelibila.ogg", "font_small", "Stejně se mi ta socha nelíbila.")
 dialog_addDialog("sl-m-nelibila", "fr", "", "font_small", "Je n'aimais pas cette statue de toute façon.")
+dialog_addDialog("sl-m-nelibila", "de", "", "font_small", "Ich konnte die Skulptur sowieso nicht leiden.")
 
 
