32e4420637d2f495ed097d229213ef86f2526fc8
ronnystandtke <ronnystandtke>
2004-08-31 06:40:37 +0000

    added German translation

--- script/pyramid/dialogs.lua
+++ script/pyramid/dialogs.lua
@@ -1,49 +1,61 @@
 dialog_addDialog("pyr-m-kam", "en", "", "font_small", "Where are we now?")
 dialog_addDialog("pyr-m-kam", "cs", "sound/"..codename.."/cs/pyr-m-kam.ogg", "font_small", "Kam jsme se to dostali?")
 dialog_addDialog("pyr-m-kam", "fr", "", "font_small", "Où sommes-nous maintenant ?")
+dialog_addDialog("pyr-m-kam", "de", "", "font_small", "Wo sind wir jetzt?")
 
 dialog_addDialog("pyr-v-vsim", "en", "", "font_big", "The Pyramids... Notice how the classical motifs mix in this city.")
 dialog_addDialog("pyr-v-vsim", "cs", "sound/"..codename.."/cs/pyr-v-vsim.ogg", "font_big", "Pyramidy... Povšimni si, jak se v tomto městě mísí antické motivy.")
 dialog_addDialog("pyr-v-vsim", "fr", "", "font_big", "Les pyramides... Regarde comme les motifs classiques se mélangent dans la cité.")
+dialog_addDialog("pyr-v-vsim", "de", "", "font_big", "Die Pyramiden... Sieh nur, wie sich die klassischen Motive in dieser Stadt vermischen.")
 
 dialog_addDialog("pyr-m-plaz", "en", "", "font_small", "What is that crawling around over there?")
 dialog_addDialog("pyr-m-plaz", "cs", "sound/"..codename.."/cs/pyr-m-plaz.ogg", "font_small", "Co se to tam plazí?")
 dialog_addDialog("pyr-m-plaz", "fr", "", "font_small", "C'est quoi cette reptation à l'extérieur ?")
+dialog_addDialog("pyr-m-plaz", "de", "", "font_small", "Was krabbelt dort drüben?")
 
 dialog_addDialog("pyr-v-druha", "en", "", "font_big", "You can`t see it from here? It`s on the other side of wall.")
 dialog_addDialog("pyr-v-druha", "cs", "sound/"..codename.."/cs/pyr-v-druha.ogg", "font_big", "To přece nevidíš. Vždyť je to na druhé straně zdi.")
 dialog_addDialog("pyr-v-druha", "fr", "", "font_big", "Tu peux le voir d'ici ? C'est de l'autre coté du mur.")
+dialog_addDialog("pyr-v-druha", "de", "", "font_big", "Kannst Du es von da aus nicht sehen? Es ist auf der anderen Seite der Mauer.")
 
 dialog_addDialog("pyr-m-nudi", "en", "", "font_small", "Look, the woman is bored!")
 dialog_addDialog("pyr-m-nudi", "cs", "sound/"..codename.."/cs/pyr-m-nudi.ogg", "font_small", "Podívej, ta ženská se snad nudí!")
 dialog_addDialog("pyr-m-nudi", "fr", "", "font_small", "Regarde, la femme est ennuyée.")
+dialog_addDialog("pyr-m-nudi", "de", "", "font_small", "Schau, die Frau ist gelangweilt!")
 
 dialog_addDialog("pyr-v-sark", "en", "", "font_big", "Do you think that this is taking us too long?")
 dialog_addDialog("pyr-v-sark", "cs", "sound/"..codename.."/cs/pyr-v-sark.ogg", "font_big", "Tak tobě se zdá, že to trvá dlouho?")
 dialog_addDialog("pyr-v-sark", "fr", "", "font_big", "Tu penses qu'elle nous a trop parlé ?")
+dialog_addDialog("pyr-v-sark", "de", "", "font_big", "Denkst Du, wir brauchen hierfür zu lange?")
 
 dialog_addDialog("pyr-m-zkus", "en", "", "font_small", "Try it yourself, if you`re so clever.")
 dialog_addDialog("pyr-m-zkus", "cs", "sound/"..codename.."/cs/pyr-m-zkus.ogg", "font_small", "Zkus si to sama, když jsi tak chytrá.")
 dialog_addDialog("pyr-m-zkus", "fr", "", "font_small", "Essaye toi même, si tu es si intelligent.")
+dialog_addDialog("pyr-m-zkus", "de", "", "font_small", "Versuch es selbst, wenn Du so schlau bist!")
 
 dialog_addDialog("pyr-m-comy", "en", "", "font_small", "What should we say?")
 dialog_addDialog("pyr-m-comy", "cs", "sound/"..codename.."/cs/pyr-m-comy.ogg", "font_small", "A co máme říkat my?")
 dialog_addDialog("pyr-m-comy", "fr", "", "font_small", "Que devons-nous dire ?")
+dialog_addDialog("pyr-m-comy", "de", "", "font_small", "Was sollen wir sagen?")
 
 dialog_addDialog("pyr-m-nic", "en", "", "font_small", "You don`t have to carry anything.")
 dialog_addDialog("pyr-m-nic", "cs", "sound/"..codename.."/cs/pyr-m-nic.ogg", "font_small", "Ty tu nic nosit nemusíš.")
 dialog_addDialog("pyr-m-nic", "fr", "", "font_small", "Tu n'es pas obligé de porter quoi que ce soit.")
+dialog_addDialog("pyr-m-nic", "de", "", "font_small", "Du musst nichts tragen.")
 
 dialog_addDialog("pyr-v-sfing", "en", "", "font_big", "Don`t be afraid.")
 dialog_addDialog("pyr-v-sfing", "cs", "sound/"..codename.."/cs/pyr-v-sfing.ogg", "font_big", "Neboj se.")
 dialog_addDialog("pyr-v-sfing", "fr", "", "font_big", "N'ai pas peur.")
+dialog_addDialog("pyr-v-sfing", "de", "", "font_big", "Hab keine Angst.")
 
 dialog_addDialog("pyr-m-dest", "en", "", "font_small", "What is it written on these tablets?")
 dialog_addDialog("pyr-m-dest", "cs", "sound/"..codename.."/cs/pyr-m-dest.ogg", "font_small", "Co je asi napsáno na těch destičkách?")
 dialog_addDialog("pyr-m-dest", "fr", "", "font_small", "Qu'est ce qu'il y a d'écrit sur ces tablettes ?")
+dialog_addDialog("pyr-m-dest", "de", "", "font_small", "Was steht auf diesen Täfelchen?")
 
 dialog_addDialog("pyr-v-sbohem", "en", "", "font_big", "So long and thanks for all the fish.")
 dialog_addDialog("pyr-v-sbohem", "cs", "sound/"..codename.."/cs/pyr-v-sbohem.ogg", "font_big", "Sbohem a dík za všechny ty ryby.")
 dialog_addDialog("pyr-v-sbohem", "fr", "", "font_big", "Et ainsi de suite et merci à tous les poissons.")
+dialog_addDialog("pyr-v-sbohem", "de", "", "font_big", "Macht`s gut und danke für den Fisch.")
 
 
