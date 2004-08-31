eeb79acb2d3f435dee4a4741e1135bbe783a6c85
ronnystandtke <ronnystandtke>
2004-08-31 06:28:27 +0000

    added German translation

--- script/pavement/dialogs.lua
+++ script/pavement/dialogs.lua
@@ -1,98 +1,123 @@
 dialog_addDialog("dir-m-rada0", "en", "", "font_small", "Stuff yourself with your advice.")
 dialog_addDialog("dir-m-rada0", "cs", "sound/"..codename.."/cs/dir-m-rada0.ogg", "font_small", "Nech si ty rady.")
 dialog_addDialog("dir-m-rada0", "fr", "", "font_small", "Garde tes conseils pour toi.")
+dialog_addDialog("dir-m-rada0", "de", "", "font_small", "Behalte Deine Ratschläge für Dich!")
 
 dialog_addDialog("dir-m-rada1", "en", "", "font_small", "We know that very well.")
 dialog_addDialog("dir-m-rada1", "cs", "sound/"..codename.."/cs/dir-m-rada1.ogg", "font_small", "To víme i bez tebe.")
 dialog_addDialog("dir-m-rada1", "fr", "", "font_small", "Nous savons ça parfaitement.")
+dialog_addDialog("dir-m-rada1", "de", "", "font_small", "Das wissen wir auch!")
 
 dialog_addDialog("dir-m-rada2", "en", "", "font_small", "Get lost.")
 dialog_addDialog("dir-m-rada2", "cs", "sound/"..codename.."/cs/dir-m-rada2.ogg", "font_small", "Běž se vycpat.")
 dialog_addDialog("dir-m-rada2", "fr", "", "font_small", "Oublie moi.")
+dialog_addDialog("dir-m-rada2", "de", "", "font_small", "Hau ab!")
 
 dialog_addDialog("dir-m-rada3", "en", "", "font_small", "My patience is running out.")
 dialog_addDialog("dir-m-rada3", "cs", "sound/"..codename.."/cs/dir-m-rada3.ogg", "font_small", "Pomalu mi dochází trpělivost.")
 dialog_addDialog("dir-m-rada3", "fr", "", "font_small", "Ma patience est à bout.")
+dialog_addDialog("dir-m-rada3", "de", "", "font_small", "Meine Geduld ist bald am Ende.")
 
 dialog_addDialog("dir-m-rada4", "en", "", "font_small", "Arrgh... I`ll smash it to pieces.")
 dialog_addDialog("dir-m-rada4", "cs", "sound/"..codename.."/cs/dir-m-rada4.ogg", "font_small", "Grrr... asi ji brzy něčím majznu.")
 dialog_addDialog("dir-m-rada4", "fr", "", "font_small", "Arrgh... Je vais le casser en petits morceaux.")
+dialog_addDialog("dir-m-rada4", "de", "", "font_small", "Arrgh... Ich werde ihn in Stücke hauen!")
 
 dialog_addDialog("dir-v-rada0", "en", "", "font_big", "Ignore it, let it talk.")
 dialog_addDialog("dir-v-rada0", "cs", "sound/"..codename.."/cs/dir-v-rada0.ogg", "font_big", "Nevšímej si jí, ať si tlachá.")
 dialog_addDialog("dir-v-rada0", "fr", "", "font_big", "Ignore le, laisse le causer.")
+dialog_addDialog("dir-v-rada0", "de", "", "font_big", "Ignoriere ihn. Lass ihn reden.")
 
 dialog_addDialog("dir-v-rada1", "en", "", "font_big", "Let us think.")
 dialog_addDialog("dir-v-rada1", "cs", "sound/"..codename.."/cs/dir-v-rada1.ogg", "font_big", "Nech nás v klidu přemýšlet.")
 dialog_addDialog("dir-v-rada1", "fr", "", "font_big", "Réfléchissons.")
+dialog_addDialog("dir-v-rada1", "de", "", "font_big", "Lass uns nachdenken.")
 
 dialog_addDialog("dir-v-rada2", "en", "", "font_big", "This was some advice, again.")
 dialog_addDialog("dir-v-rada2", "cs", "sound/"..codename.."/cs/dir-v-rada2.ogg", "font_big", "To jsi nám zase poradila.")
 dialog_addDialog("dir-v-rada2", "fr", "", "font_big", "Ce doit être quelque indice, à nouveau.")
+dialog_addDialog("dir-v-rada2", "de", "", "font_big", "Das war ja wieder ein Ratschlag.")
 
 dialog_addDialog("dir-v-rada3", "en", "", "font_big", "I am sorry I have no ears. I could plug them.")
 dialog_addDialog("dir-v-rada3", "cs", "sound/"..codename.."/cs/dir-v-rada3.ogg", "font_big", "Ach jo, škoda že nemám uši. Zacpal bych si je.")
 dialog_addDialog("dir-v-rada3", "fr", "", "font_big", "Je suis désolé, je n'ai pas d'oreilles. Je pourrais les boucher.")
+dialog_addDialog("dir-v-rada3", "de", "", "font_big", "Schade, dass ich keine Ohren habe. Die könnte ich zuhalten.")
 
 dialog_addDialog("dir-v-rada4", "en", "", "font_big", "I am fed up to my gills with its nonsenses.")
 dialog_addDialog("dir-v-rada4", "cs", "sound/"..codename.."/cs/dir-v-rada4.ogg", "font_big", "Už těch jejích nesmyslů mám po krk.")
 dialog_addDialog("dir-v-rada4", "fr", "", "font_big", "J'y perds mes branchies avec ces non-sens.")
+dialog_addDialog("dir-v-rada4", "de", "", "font_big", "Dieser Unsinn steht mir bis zu den Kiemen!")
 
 dialog_addDialog("dir-hs-uvod0", "en", "", "font_statue", "It would be easiest if you")
 dialog_addDialog("dir-hs-uvod0", "cs", "sound/"..codename.."/cs/dir-hs-uvod0.ogg", "font_statue", "Nejjednodušší by bylo")
 dialog_addDialog("dir-hs-uvod0", "fr", "", "font_statue", "Ca serait plus facile si vous...")
+dialog_addDialog("dir-hs-uvod0", "de", "", "font_statue", "Am einfachsten wäre es, wenn ihr")
 
 dialog_addDialog("dir-hs-uvod1", "en", "", "font_statue", "You would be better off if you")
 dialog_addDialog("dir-hs-uvod1", "cs", "sound/"..codename.."/cs/dir-hs-uvod1.ogg", "font_statue", "Určitě by pomohlo")
 dialog_addDialog("dir-hs-uvod1", "fr", "", "font_statue", "Vous serez meilleurs si vous...")
+dialog_addDialog("dir-hs-uvod1", "de", "", "font_statue", "Ihr wärt besser dran, wenn ihr")
 
 dialog_addDialog("dir-hs-uvod2", "en", "", "font_statue", "It would help you if you")
 dialog_addDialog("dir-hs-uvod2", "cs", "sound/"..codename.."/cs/dir-hs-uvod2.ogg", "font_statue", "Možná by nebylo špatné")
 dialog_addDialog("dir-hs-uvod2", "fr", "", "font_statue", "Ca vous aiderait si vous...")
+dialog_addDialog("dir-hs-uvod2", "de", "", "font_statue", "Es würde helfen, wenn ihr")
 
 dialog_addDialog("dir-hs-uvod3", "en", "", "font_statue", "I have an idea. What if you")
 dialog_addDialog("dir-hs-uvod3", "cs", "sound/"..codename.."/cs/dir-hs-uvod3.ogg", "font_statue", "Mám takový nápad. Co zkusit")
 dialog_addDialog("dir-hs-uvod3", "fr", "", "font_statue", "J'ai une idée. Si vous...")
+dialog_addDialog("dir-hs-uvod3", "de", "", "font_statue", "Ich habe eine Idee. Es wäre gut, wenn ihr")
 
 dialog_addDialog("dir-hs-uvod4", "en", "", "font_statue", "Sorry to interrupt, but what if you")
 dialog_addDialog("dir-hs-uvod4", "cs", "sound/"..codename.."/cs/dir-hs-uvod4.ogg", "font_statue", "Promiňte, že vám do toho mluvím, ale co takhle")
 dialog_addDialog("dir-hs-uvod4", "fr", "", "font_statue", "Désolé d'interrompre, mais si vous...")
+dialog_addDialog("dir-hs-uvod4", "de", "", "font_statue", "Entschuldigt wenn ich unterbreche. Es wäre gut, wenn ihr")
 
 dialog_addDialog("dir-hs-konec0", "en", "", "font_statue", "demolish some wall.")
 dialog_addDialog("dir-hs-konec0", "cs", "sound/"..codename.."/cs/dir-hs-konec0.ogg", "font_statue", "vybourat nějakou tu zeď.")
 dialog_addDialog("dir-hs-konec0", "fr", "", "font_statue", "Détruisez quelque mur.")
+dialog_addDialog("dir-hs-konec0", "de", "", "font_statue", "ein paar Mauern niederreißt.")
+dialog_addDialog("dir-hs-konec0", "de_CH", "", "font_statue", "ein paar Mauern niederreisst.")
 
 dialog_addDialog("dir-hs-konec1", "en", "", "font_statue", "break off few stones from the wall and plug the holes with them.")
 dialog_addDialog("dir-hs-konec1", "cs", "sound/"..codename.."/cs/dir-hs-konec1.ogg", "font_statue", "vylomit pár kamenů ze zdi a ucpat ty díry.")
 dialog_addDialog("dir-hs-konec1", "fr", "", "font_statue", "Enlever quelques pierres du mur et boucher les trous avec.")
+dialog_addDialog("dir-hs-konec1", "de", "", "font_statue", "ein paar Steine aus der Mauer brecht und die Löcher damit stopft.")
 
 dialog_addDialog("dir-hs-konec2", "en", "", "font_statue", "plug those holes with something so that that hooked steel wouldn`t get stuck there.")
 dialog_addDialog("dir-hs-konec2", "cs", "sound/"..codename.."/cs/dir-hs-konec2.ogg", "font_statue", "něčím ucpat ty díry, aby vám tam nezapadla ta zahnutá ocel.")
 dialog_addDialog("dir-hs-konec2", "fr", "", "font_statue", "Boucher ces trous avec quelque chose, ainsi ce clou ne gênera plus personne.")
+dialog_addDialog("dir-hs-konec2", "de", "", "font_statue", "diese Löcher stopft, so dass dieser gebogene Stahl nicht stecken bleibt.")
 
 dialog_addDialog("dir-hs-konec3", "en", "", "font_statue", "smash that pillar by the exit.")
 dialog_addDialog("dir-hs-konec3", "cs", "sound/"..codename.."/cs/dir-hs-konec3.ogg", "font_statue", "rozbít ten sloupek u východu.")
 dialog_addDialog("dir-hs-konec3", "fr", "", "font_statue", "Jette ce pilier par la sortie.")
+dialog_addDialog("dir-hs-konec3", "de", "", "font_statue", "die Säule am Ausgang zertrümmert.")
 
 dialog_addDialog("dir-hs-konec4", "en", "", "font_statue", "saw off some of that hooked steel.")
 dialog_addDialog("dir-hs-konec4", "cs", "sound/"..codename.."/cs/dir-hs-konec4.ogg", "font_statue", "uříznout kus té zahnuté oceli.")
 dialog_addDialog("dir-hs-konec4", "fr", "", "font_statue", "Faites attention aux clous.")
+dialog_addDialog("dir-hs-konec4", "de", "", "font_statue", "etwas von dem gebogenen Stahl absägt.")
 
 dialog_addDialog("dir-hs-konec5", "en", "", "font_statue", "rearrange the objects so that you can swim out.")
 dialog_addDialog("dir-hs-konec5", "cs", "sound/"..codename.."/cs/dir-hs-konec5.ogg", "font_statue", "nějak ty předměty přeskládat, abyste mohly odjet z místnosti.")
 dialog_addDialog("dir-hs-konec5", "fr", "", "font_statue", "Réarrangez les objets ainsi vous pourrez nagez hors du tableau.")
+dialog_addDialog("dir-hs-konec5", "de", "", "font_statue", "die Gegenstände so umordnet, dass ihr rausschwimmen könnt.")
 
 dialog_addDialog("dir-hs-konec6", "en", "", "font_statue", "solve it somehow and get to the next level.")
 dialog_addDialog("dir-hs-konec6", "cs", "sound/"..codename.."/cs/dir-hs-konec6.ogg", "font_statue", "celé to nějak vyřešit a pustit se do dalších místností.")
 dialog_addDialog("dir-hs-konec6", "fr", "", "font_statue", "Trouvez la solution et allez au niveau suivant.")
+dialog_addDialog("dir-hs-konec6", "de", "", "font_statue", "es irgendwie löst und in die nächste Ebene kommt.")
 
 dialog_addDialog("dir-hs-konec7", "en", "", "font_statue", "start to think about it really hard.")
 dialog_addDialog("dir-hs-konec7", "cs", "sound/"..codename.."/cs/dir-hs-konec7.ogg", "font_statue", "opravdu pořádně se nad tím zamyslet.")
 dialog_addDialog("dir-hs-konec7", "fr", "", "font_statue", "Vous commencez à penser que c'est vraiment dur.")
+dialog_addDialog("dir-hs-konec7", "de", "", "font_statue", "anfangt, intensiv über eine Lösung nachzudenken.")
 
 dialog_addDialog("dir-hs-konec8", "en", "", "font_statue", "swim out through that hole on the left.")
 dialog_addDialog("dir-hs-konec8", "cs", "sound/"..codename.."/cs/dir-hs-konec8.ogg", "font_statue", "nějak zkusit odjet tou dírou nalevo.")
 dialog_addDialog("dir-hs-konec8", "fr", "", "font_statue", "Nagez à travers ce trou, sur la gauche.")
+dialog_addDialog("dir-hs-konec8", "de", "", "font_statue", "durch das Loch auf der linken Seite schwimmt.")
 
 dialog_addDialog("k1-chob-1", "en", "sound/"..codename.."/en/k1-chob-1.ogg")
 
