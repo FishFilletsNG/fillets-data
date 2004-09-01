9cb6e80ec9dc4588b4dc9638d2150b56f2bb02f7
ronnystandtke <ronnystandtke>
2004-09-01 17:00:28 +0000

    added German translation

--- script/tetris/dialogs.lua
+++ script/tetris/dialogs.lua
@@ -1,61 +1,77 @@
 dialog_addDialog("tet-m-vypadala", "en", "", "font_small", "So this is how the most successful game of all times looked like.")
 dialog_addDialog("tet-m-vypadala", "cs", "sound/"..codename.."/cs/tet-m-vypadala.ogg", "font_small", "Tak takhle vypadala nejúspěšnější hra všech dob.")
 dialog_addDialog("tet-m-vypadala", "fr", "", "font_small", "Donc, voici à quoi ressemble le jeu le plus populaire de tout les temps.")
+dialog_addDialog("tet-m-vypadala", "de", "", "font_small", "So sah also das erfolgreichste Spiel aller Zeiten aus.")
 
 dialog_addDialog("tet-v-ucta", "en", "", "font_big", "Only the deep respect for its memory prevents me from saying how it looks like...")
 dialog_addDialog("tet-v-ucta", "cs", "sound/"..codename.."/cs/tet-v-ucta.ogg", "font_big", "Jenom úcta k její památce mi brání v tom, abych řekl, jak to vypadá...")
 dialog_addDialog("tet-v-ucta", "fr", "", "font_big", "Seulement le profond respect pour sa mémoire m'empêche de dire à quoi il ressemble.")
+dialog_addDialog("tet-v-ucta", "de", "", "font_big", "Nur der tiefe Respekt vor seinem Andenken hält mich davon ab, zu sagen wie es aussieht...")
 
 dialog_addDialog("tet-m-usudek", "en", "", "font_small", "But everybody can see it for himself.")
 dialog_addDialog("tet-m-usudek", "cs", "sound/"..codename.."/cs/tet-m-usudek.ogg", "font_small", "Ale každý si může udělat úsudek sám.")
 dialog_addDialog("tet-m-usudek", "fr", "", "font_small", "Mais tout le monde peux s'en rendre compte.")
+dialog_addDialog("tet-m-usudek", "de", "", "font_small", "Aber jeder kann es selbst sehen.")
 
 dialog_addDialog("tet-v-myslim", "en", "", "font_big", "Do you know what I have in mind?")
 dialog_addDialog("tet-v-myslim", "cs", "sound/"..codename.."/cs/tet-v-myslim.ogg", "font_big", "Víš, co si myslím?")
 dialog_addDialog("tet-v-myslim", "fr", "", "font_big", "Sais-tu ce que j'ai en tête ?")
+dialog_addDialog("tet-v-myslim", "de", "", "font_big", "Weißt Du was ich denke?")
+dialog_addDialog("tet-v-myslim", "de_CH", "", "font_big", "Weisst Du was ich denke?")
 
 dialog_addDialog("tet-m-ano", "en", "", "font_small", "Well?")
 dialog_addDialog("tet-m-ano", "cs", "sound/"..codename.."/cs/tet-m-ano.ogg", "font_small", "Ano?")
 dialog_addDialog("tet-m-ano", "fr", "", "font_small", "Et bien ?")
+dialog_addDialog("tet-m-ano", "de", "", "font_small", "Was?")
 
 dialog_addDialog("tet-v-lepsi", "en", "", "font_big", "I think we could do a better tetris than this room.")
 dialog_addDialog("tet-v-lepsi", "cs", "sound/"..codename.."/cs/tet-v-lepsi.ogg", "font_big", "Dokázali bychom udělat lepší tetris, než je tahle místnost.")
 dialog_addDialog("tet-v-lepsi", "fr", "", "font_big", "Je pense que nous pouvons faire un meilleur tetris que ça.")
+dialog_addDialog("tet-v-lepsi", "de", "", "font_big", "Ich denke, wir könnten ein besseres Tetris machen, als diesen Raum.")
 
 dialog_addDialog("tet-m-jaklepsi", "en", "", "font_small", "What do you mean, better?")
 dialog_addDialog("tet-m-jaklepsi", "cs", "sound/"..codename.."/cs/tet-m-jaklepsi.ogg", "font_small", "Jak lepší?")
 dialog_addDialog("tet-m-jaklepsi", "fr", "", "font_small", "Qu'entends-tu par meilleur ?")
+dialog_addDialog("tet-m-jaklepsi", "de", "", "font_small", "Wie \"besser\"?")
 
 dialog_addDialog("tet-v-hybat", "en", "", "font_big", "At least, you won`t have to move all the pieces to the right.")
 dialog_addDialog("tet-v-hybat", "cs", "sound/"..codename.."/cs/tet-v-hybat.ogg", "font_big", "No, přinejmenším tak, aby se tam dalo hýbat kostkou nejen doprava.")
 dialog_addDialog("tet-v-hybat", "fr", "", "font_big", "Au moins, tu n'as pas à pousser toutes les pièce sur la droite.")
+dialog_addDialog("tet-v-hybat", "de", "", "font_big", "Zumindest, dass man nicht alle Teile nach rechts bewegen müsste.")
 
 dialog_addDialog("tet-m-predmety", "en", "", "font_small", "The truth is we have an amply supply of suitable objects here.")
 dialog_addDialog("tet-m-predmety", "cs", "sound/"..codename.."/cs/tet-m-predmety.ogg", "font_small", "Pravda je, že tu máme spoustu vhodných předmětů.")
 dialog_addDialog("tet-m-predmety", "fr", "", "font_small", "En vérité, nous avons amplement tout ce dont nous avons besoin.")
+dialog_addDialog("tet-m-predmety", "de", "", "font_small", "Die Wahrheit ist, dass wir hier reichlich passende Teile haben.")
 
 dialog_addDialog("tet-v-uprava", "en", "", "font_big", "I think that little patch of the code can give the player the possibility to enjoy some original tetris.")
 dialog_addDialog("tet-v-uprava", "cs", "sound/"..codename.."/cs/tet-v-uprava.ogg", "font_big", "Myslím, že by stačila jen malá úprava kódu, aby si hráč mohl vychutnat trochu originálního tetrisu.")
 dialog_addDialog("tet-v-uprava", "fr", "", "font_big", "Je pense que ce patch du jeu permettra au joueur d'apprécier le tetris original.")
+dialog_addDialog("tet-v-uprava", "de", "", "font_big", "Ich denke, dass ein kleines Programmstück dem Spieler die Möglichkeit geben kann, sich an einem Original-Tetris zu erfreuen.")
 
 dialog_addDialog("tet-m-program", "en", "", "font_small", "Okay, try to program it.")
 dialog_addDialog("tet-m-program", "cs", "sound/"..codename.."/cs/tet-m-program.ogg", "font_small", "Tak to zkus naprogramovat.")
 dialog_addDialog("tet-m-program", "fr", "", "font_small", "Okay, Essaye de l'installer.")
+dialog_addDialog("tet-m-program", "de", "", "font_small", "Gut, versuche es zu programmieren.")
 
 dialog_addDialog("tet-m-pozor", "en", "", "font_small", "Be careful, not to hurt us.")
 dialog_addDialog("tet-m-pozor", "cs", "sound/"..codename.."/cs/tet-m-pozor.ogg", "font_small", "Dej si pozor, ať nám něco neuděláš.")
 dialog_addDialog("tet-m-pozor", "fr", "", "font_small", "Fais attention, nous sommes fragile.")
+dialog_addDialog("tet-m-pozor", "de", "", "font_small", "Pass auf, dass Du uns nicht wehtust.")
 
 dialog_addDialog("tet-v-hotovo", "en", "", "font_big", "Okay, I`m done. Input XTETRIS and you can get to it. Control the cubes by arrow keys or numpad.")
 dialog_addDialog("tet-v-hotovo", "cs", "sound/"..codename.."/cs/tet-v-hotovo.ogg", "font_big", "A je to hotovo. Napiš XTETRIS a můžeš se dát do toho. Ovládej to šipkama nebo numerickou klávesnicí.")
 dialog_addDialog("tet-v-hotovo", "fr", "", "font_big", "Okay, c'est fait. Entre XTETRIS pour y accéder. Dirige les cubes par les flèches ou le pavé numérique.")
+dialog_addDialog("tet-v-hotovo", "de", "", "font_big", "Gut, Ich bin fertig. Gib XTETRIS ein und Du kannst loslegen. Steuere die Teile mit den Pfeiltasten oder dem Ziffernblock.")
 
 dialog_addDialog("tet-v-kostky", "en", "", "font_big", "We will have to stack these pieces more efficiently, perhaps.")
 dialog_addDialog("tet-v-kostky", "cs", "sound/"..codename.."/cs/tet-v-kostky.ogg", "font_big", "Asi tam budeme muset ty kostky naskládat úsporněji.")
 dialog_addDialog("tet-v-kostky", "fr", "", "font_big", "Nous devons ranger ces pièces de manière plus efficace, peut-être.")
+dialog_addDialog("tet-v-kostky", "de", "", "font_big", "Wir müssen die Teile wahrscheinlich effizienter stapeln.")
 
 dialog_addDialog("tet-m-lepe", "en", "", "font_small", "Hmm... I should have arranged it better.")
 dialog_addDialog("tet-m-lepe", "cs", "sound/"..codename.."/cs/tet-m-lepe.ogg", "font_small", "Hmmm... měla jsem to tam naskládat nějak lépe.")
 dialog_addDialog("tet-m-lepe", "fr", "", "font_small", "Hmm... Je devrais mieux empiler ça.")
+dialog_addDialog("tet-m-lepe", "de", "", "font_small", "Hmm... Ich hätte es besser anordnen sollen.")
 
 
