c47ded7a62841a9f266e7c1b32bea23ac10f0f88
ronnystandtke <ronnystandtke>
2004-09-01 06:51:36 +0000

    added German translation

--- script/reactor/dialogs.lua
+++ script/reactor/dialogs.lua
@@ -1,70 +1,89 @@
 dialog_addDialog("rea-m-proboha", "en", "", "font_small", "My God, can you see that pink thing over there?")
 dialog_addDialog("rea-m-proboha", "cs", "sound/"..codename.."/cs/rea-m-proboha.ogg", "font_small", "Proboha, vidíš támhletu růžovou věc?")
 dialog_addDialog("rea-m-proboha", "fr", "", "font_small", "Mon dieu, peux-tu voir cette chose rose ?")
+dialog_addDialog("rea-m-proboha", "de", "", "font_small", "Um Himmels Willen, siehst Du dieses rosa Ding da drüben?")
 
 dialog_addDialog("rea-v-coto", "en", "", "font_big", "What could it be?")
 dialog_addDialog("rea-v-coto", "cs", "sound/"..codename.."/cs/rea-v-coto.ogg", "font_big", "Co to může být?")
 dialog_addDialog("rea-v-coto", "fr", "", "font_big", "Qu'est-ce que ça peut être ?")
+dialog_addDialog("rea-v-coto", "de", "", "font_big", "Was könnte es sein?")
 
 dialog_addDialog("rea-m-nevim", "en", "", "font_small", "I have no idea, but it`s disgusting.")
 dialog_addDialog("rea-m-nevim", "cs", "sound/"..codename.."/cs/rea-m-nevim.ogg", "font_small", "Nevím, ale je to odporné.")
 dialog_addDialog("rea-m-nevim", "fr", "", "font_small", "Aucune idée, mais c'est dégoutant.")
+dialog_addDialog("rea-m-nevim", "de", "", "font_small", "Ich habe keine Ahnung, aber es ist eklig.")
 
 dialog_addDialog("rea-m-comyslis", "en", "", "font_small", "What do you think about these rods?")
 dialog_addDialog("rea-m-comyslis", "cs", "sound/"..codename.."/cs/rea-m-comyslis.ogg", "font_small", "Co si myslíš o těch tyčích?")
 dialog_addDialog("rea-m-comyslis", "fr", "", "font_small", "Que penses-tu de ces tiges ?")
+dialog_addDialog("rea-m-comyslis", "de", "", "font_small", "Was denkst Du über diese Stäbe?")
 
 dialog_addDialog("rea-v-patrne", "en", "", "font_big", "They look like they`re some kind of fuel rods or maybe regulators for the nuclear reactor.")
 dialog_addDialog("rea-v-patrne", "cs", "sound/"..codename.."/cs/rea-v-patrne.ogg", "font_big", "Tohle jsou patrně palivové nebo regulační tyče reaktoru.")
 dialog_addDialog("rea-v-patrne", "fr", "", "font_big", "Elles ressemblent à des barres énergétiques ou peut-être des régulateurs pour le réacteur nucléaire.")
+dialog_addDialog("rea-v-patrne", "de", "", "font_big", "Die sehen wie Brennstäbe oder Regulatoren für den Kernreaktor aus.")
 
 dialog_addDialog("rea-v-ledaze", "en", "", "font_big", "Unless, of course, this reactor works on some completely different principle.")
 dialog_addDialog("rea-v-ledaze", "cs", "sound/"..codename.."/cs/rea-v-ledaze.ogg", "font_big", "Ledaže by ten reaktor pracoval na úplně jiném principu.")
 dialog_addDialog("rea-v-ledaze", "fr", "", "font_big", "A moins que bien sûr, ce réacteur ne fonctionne sur un principe complètement différent.")
+dialog_addDialog("rea-v-ledaze", "de", "", "font_big", "Es sei denn, der Reaktor arbeitet nach einem ganz anderen Prinzip.")
 
 dialog_addDialog("rea-m-mohl", "en", "", "font_small", "It could, of course, be a nucleo-particle megadisruptor with ultramezon colliders.")
 dialog_addDialog("rea-m-mohl", "cs", "sound/"..codename.."/cs/rea-m-mohl.ogg", "font_small", "Mohl by to být nukleopartikulární megadisruptor s ultramezonovými urychlovači.")
 dialog_addDialog("rea-m-mohl", "fr", "", "font_small", "Il pourrait bien sûr être un mégagénérateur d'exploion de particules nucléaires à hautes vélocités.")
+dialog_addDialog("rea-m-mohl", "de", "", "font_small", "Es könnte ja ein Kernteilchen-Megaspalter mit Ultramesonen-Beschleuniger sein.")
 
 dialog_addDialog("rea-v-tozni", "en", "", "font_big", "That sounds reasonable...")
 dialog_addDialog("rea-v-tozni", "cs", "sound/"..codename.."/cs/rea-v-tozni.ogg", "font_big", "To zní pravděpodobně...")
 dialog_addDialog("rea-v-tozni", "fr", "", "font_big", "Ca semble raisonnable...")
+dialog_addDialog("rea-v-tozni", "de", "", "font_big", "Ja, wahrscheinlich...")
 
 dialog_addDialog("rea-m-anebo", "en", "", "font_small", "Then again, maybe this isn`t a nuclear reactor.")
 dialog_addDialog("rea-m-anebo", "cs", "sound/"..codename.."/cs/rea-m-anebo.ogg", "font_small", "A nebo to není vůbec reaktor.")
 dialog_addDialog("rea-m-anebo", "fr", "", "font_small", "Alors, ce n'est peut-être pas un réacteur nucléaire.")
+dialog_addDialog("rea-m-anebo", "de", "", "font_small", "Aber vielleicht ist es kein Kernreaktor.")
 
 dialog_addDialog("rea-v-acoby", "en", "", "font_big", "So what is it, then?")
 dialog_addDialog("rea-v-acoby", "cs", "sound/"..codename.."/cs/rea-v-acoby.ogg", "font_big", "A co by to bylo?")
 dialog_addDialog("rea-v-acoby", "fr", "", "font_big", "Donc, qu'est-ce que c'est ?")
+dialog_addDialog("rea-v-acoby", "de", "", "font_big", "Was soll es sonst sein?")
 
 dialog_addDialog("rea-m-cojavim", "en", "", "font_small", "How should I know? A cinematoscope? A child`s toy? A shooting gallery?")
 dialog_addDialog("rea-m-cojavim", "cs", "sound/"..codename.."/cs/rea-m-cojavim.ogg", "font_small", "Co já vím? Barevná hudba? Dětská hračka? Střelnice?")
 dialog_addDialog("rea-m-cojavim", "fr", "", "font_small", "Comment le saurais-je ? Un cinéma ? Un jouet pour enfant ? un stand de tir ?")
+dialog_addDialog("rea-m-cojavim", "de", "", "font_small", "Woher soll ich das wissen? Ein Kino? Ein Kinderspielzeug? Ein Schießstand?")
+dialog_addDialog("rea-m-cojavim", "de_CH", "", "font_small", "Woher soll ich das wissen? Ein Kino? Ein Kinderspielzeug? Ein Schiessstand?")
 
 dialog_addDialog("rea-v-radeji", "en", "", "font_big", "I`d rather stick to a more rational explanation.")
 dialog_addDialog("rea-v-radeji", "cs", "sound/"..codename.."/cs/rea-v-radeji.ogg", "font_big", "Raději bych se držel nějakého rozumného vysvětlení.")
 dialog_addDialog("rea-v-radeji", "fr", "", "font_big", "Je préfèrerais une explication plus rationnelle.")
+dialog_addDialog("rea-v-radeji", "de", "", "font_big", "Ich bleibe lieber bei einer vernünftigeren Erklärung.")
 
 dialog_addDialog("rea-m-jakmuzes", "en", "", "font_small", "But how do you know what`s a rational explanation and what isn`t?")
 dialog_addDialog("rea-m-jakmuzes", "cs", "sound/"..codename.."/cs/rea-m-jakmuzes.ogg", "font_small", "Jak můžeš vědět, co je rozumné vysvětlení?")
 dialog_addDialog("rea-m-jakmuzes", "fr", "", "font_small", "Mais comment reconnais-tu une explication rationnelle de ce qui n'en est pas ?")
+dialog_addDialog("rea-m-jakmuzes", "de", "", "font_small", "Aber woher willst Du wissen, welche Erklärung vernünftig ist und welche nicht?")
 
 dialog_addDialog("rea-v-kolik", "en", "", "font_big", "Well just how many flying saucers, in your opinion, have a shooting gallery on board? And how many have a power plant?")
 dialog_addDialog("rea-v-kolik", "cs", "sound/"..codename.."/cs/rea-v-kolik.ogg", "font_big", "Kolik procent létajících talířů má podle tebe na palubě střelnici? A kolik reaktor?")
 dialog_addDialog("rea-v-kolik", "fr", "", "font_big", "Et bien, d'après toi, combien de soucoupe volante ont à bord un stand de tir? Et combien un générateur éléctrique ?")
+dialog_addDialog("rea-v-kolik", "de", "", "font_big", "Naja, wie viele fliegenden Untertassen haben Deiner Meinung nach einen Schießstand an Bord? Und wie viele haben einen Reaktor?")
+dialog_addDialog("rea-v-kolik", "de_CH", "", "font_big", "Naja, wie viele fliegenden Untertassen haben Deiner Meinung nach einen Schiessstand an Bord? Und wie viele haben einen Reaktor?")
 
 dialog_addDialog("rea-v-takvidis", "en", "", "font_big", "You see?")
 dialog_addDialog("rea-v-takvidis", "cs", "sound/"..codename.."/cs/rea-v-takvidis.ogg", "font_big", "Tak vidíš.")
 dialog_addDialog("rea-v-takvidis", "fr", "", "font_big", "Tu vois ?")
+dialog_addDialog("rea-v-takvidis", "de", "", "font_big", "Siehst Du?")
 
 dialog_addDialog("rea-m-doufam", "en", "", "font_small", "I hope we don`t start some upleasant reaction.")
 dialog_addDialog("rea-m-doufam", "cs", "sound/"..codename.."/cs/rea-m-doufam.ogg", "font_small", "Doufám, že tím nezpůsobíme nějakou nežádoucí reakci.")
 dialog_addDialog("rea-m-doufam", "fr", "", "font_small", "J'espère que nous n'avons pas déclencher de réaction.")
+dialog_addDialog("rea-m-doufam", "de", "", "font_small", "Ich hoffe, wir starten keine unangenehme Reaktion.")
 
 dialog_addDialog("rea-v-nemudruj", "en", "", "font_big", "Don`t think. Just push the rods.")
 dialog_addDialog("rea-v-nemudruj", "cs", "sound/"..codename.."/cs/rea-v-nemudruj.ogg", "font_big", "Nemudruj a zasouvej tyče.")
 dialog_addDialog("rea-v-nemudruj", "fr", "", "font_big", "Ne pense pas. Pousse simplement les tiges.")
+dialog_addDialog("rea-v-nemudruj", "de", "", "font_big", "Denk nicht! Schieb einfach die Stäbe!.")
 
 dialog_addDialog("rea-x-pldik", "en", "sound/"..codename.."/en/rea-x-pldik.ogg", "font_pink", "MUAHHH... UAHH... UUUH...")
 
