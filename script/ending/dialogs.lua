ff683ef40b3faa7613117364e0a060b21195bfeb
fidlej <fidlej>
2004-09-07 22:56:00 +0000

    Added level ending.

---
+++ script/ending/dialogs.lua
@@ -0,0 +1,149 @@
+dialog_addDialog("z-v-doma", "en", "", "font_big", "I am glad to be home again.")
+dialog_addDialog("z-v-doma", "cs", "sound/"..codename.."/cs/z-v-doma.ogg", "font_big", "Jsem rád, že jsme zase doma.")
+
+dialog_addDialog("z-m-pocit", "en", "", "font_small", "I have such a good feeling of the work well done.")
+dialog_addDialog("z-m-pocit", "cs", "sound/"..codename.."/cs/z-m-pocit.ogg", "font_small", "Mám takový pěkný pocit dobře odvedené práce.")
+
+dialog_addDialog("z-v-sef", "en", "", "font_big", "The boss called a moment ago. He was very satisfied with our performance.")
+dialog_addDialog("z-v-sef", "cs", "sound/"..codename.."/cs/z-v-sef.ogg", "font_big", "Před chvílí volal šéf. Byl s námi moc spokojen.")
+
+dialog_addDialog("z-m-nemluv", "en", "", "font_small", "Don`t mention the boss to me. He sits somewhere in his cozy office and we have to do all the dirty work.")
+dialog_addDialog("z-m-nemluv", "cs", "sound/"..codename.."/cs/z-m-nemluv.ogg", "font_small", "O šéfovi mi ani nemluv. On si sedí někde v teploučku, zatím co my jsme se dřely.")
+
+dialog_addDialog("z-v-slyset", "en", "", "font_big", "Shhh, he could hear us.")
+dialog_addDialog("z-v-slyset", "cs", "sound/"..codename.."/cs/z-v-slyset.ogg", "font_big", "Pssst, může nás slyšet.")
+
+dialog_addDialog("z-m-netusi", "en", "", "font_small", "How could he? He surely doesn`t even know how long it took us.")
+dialog_addDialog("z-m-netusi", "cs", "sound/"..codename.."/cs/z-m-netusi.ogg", "font_small", "Jak by nás mohl slyšet. Určitě ani netuší, jak dlouho nám to trvalo.")
+
+dialog_addDialog("z-m-dlouho", "en", "", "font_small", "Really? It didn`t seem that long to me.")
+dialog_addDialog("z-m-dlouho", "cs", "sound/"..codename.."/cs/z-m-dlouho.ogg", "font_small", "Jé, tak dlouho mi to ani nepřipadlo.")
+
+dialog_addDialog("z-v-pozdrav", "en", "", "font_big", "Dear player. We would like to communicate to you the high regards of the authors of this game. If you didn`t solve it by some cheat, you are really very, very good. It`s a pity we cannot see you from here, but...")
+dialog_addDialog("z-v-pozdrav", "cs", "sound/"..codename.."/cs/z-v-pozdrav.ogg", "font_big", "Hráči, máme ti vyřídit pozdrav a uznání od autorů hry. Jestli jsi to neřešil nějakým podvodem, tak jsi opravdu dobrej. Nebo dokonce dobrá? My tě tady odsud moc nevidíme.")
+
+dialog_addDialog("z-m-oblicej", "en", "", "font_small", "Could you please put your face in front of the hard disk LED? You might not know it but it is a kind of computer`s eye through which...")
+dialog_addDialog("z-m-oblicej", "cs", "sound/"..codename.."/cs/z-m-oblicej.ogg", "font_small", "Mohl bys dát obličej před kontrolku harddisku? Možná to nevíš, ale to je takové oko počítače, kterým tě...")
+
+dialog_addDialog("z-v-forky", "en", "", "font_big", "Stop this jokes of yours. This is a very special moment. So:")
+dialog_addDialog("z-v-forky", "cs", "sound/"..codename.."/cs/z-v-forky.ogg", "font_big", "Nech už těch fórků. Teď je slavnostní chvíle. Takže:")
+
+dialog_addDialog("z-o-blahoprejeme", "en", "", "font_both", "CONGRATULATIONS!!!")
+dialog_addDialog("z-o-blahoprejeme", "cs", "sound/"..codename.."/cs/z-o-blahoprejeme.ogg", "font_both", "BLAHOPŘEJEME!!!")
+
+dialog_addDialog("z-c-1", "en", "")
+dialog_addDialog("z-c-1", "cs", "sound/"..codename.."/cs/z-c-1.ogg")
+
+dialog_addDialog("z-c-2", "en", "")
+dialog_addDialog("z-c-2", "cs", "sound/"..codename.."/cs/z-c-2.ogg")
+
+dialog_addDialog("z-c-3", "en", "")
+dialog_addDialog("z-c-3", "cs", "sound/"..codename.."/cs/z-c-3.ogg")
+
+dialog_addDialog("z-c-4", "en", "")
+dialog_addDialog("z-c-4", "cs", "sound/"..codename.."/cs/z-c-4.ogg")
+
+dialog_addDialog("z-c-5", "en", "")
+dialog_addDialog("z-c-5", "cs", "sound/"..codename.."/cs/z-c-5.ogg")
+
+dialog_addDialog("z-c-6", "en", "")
+dialog_addDialog("z-c-6", "cs", "sound/"..codename.."/cs/z-c-6.ogg")
+
+dialog_addDialog("z-c-7", "en", "")
+dialog_addDialog("z-c-7", "cs", "sound/"..codename.."/cs/z-c-7.ogg")
+
+dialog_addDialog("z-c-8", "en", "")
+dialog_addDialog("z-c-8", "cs", "sound/"..codename.."/cs/z-c-8.ogg")
+
+dialog_addDialog("z-c-9", "en", "")
+dialog_addDialog("z-c-9", "cs", "sound/"..codename.."/cs/z-c-9.ogg")
+
+dialog_addDialog("z-c-10", "en", "")
+dialog_addDialog("z-c-10", "cs", "sound/"..codename.."/cs/z-c-10.ogg")
+
+dialog_addDialog("z-c-11", "en", "")
+dialog_addDialog("z-c-11", "cs", "sound/"..codename.."/cs/z-c-11.ogg")
+
+dialog_addDialog("z-c-12", "en", "")
+dialog_addDialog("z-c-12", "cs", "sound/"..codename.."/cs/z-c-12.ogg")
+
+dialog_addDialog("z-c-13", "en", "")
+dialog_addDialog("z-c-13", "cs", "sound/"..codename.."/cs/z-c-13.ogg")
+
+dialog_addDialog("z-c-14", "en", "")
+dialog_addDialog("z-c-14", "cs", "sound/"..codename.."/cs/z-c-14.ogg")
+
+dialog_addDialog("z-c-15", "en", "")
+dialog_addDialog("z-c-15", "cs", "sound/"..codename.."/cs/z-c-15.ogg")
+
+dialog_addDialog("z-c-16", "en", "")
+dialog_addDialog("z-c-16", "cs", "sound/"..codename.."/cs/z-c-16.ogg")
+
+dialog_addDialog("z-c-17", "en", "")
+dialog_addDialog("z-c-17", "cs", "sound/"..codename.."/cs/z-c-17.ogg")
+
+dialog_addDialog("z-c-18", "en", "")
+dialog_addDialog("z-c-18", "cs", "sound/"..codename.."/cs/z-c-18.ogg")
+
+dialog_addDialog("z-c-19", "en", "")
+dialog_addDialog("z-c-19", "cs", "sound/"..codename.."/cs/z-c-19.ogg")
+
+dialog_addDialog("z-c-20", "en", "")
+dialog_addDialog("z-c-20", "cs", "sound/"..codename.."/cs/z-c-20.ogg")
+
+dialog_addDialog("z-c-30", "en", "")
+dialog_addDialog("z-c-30", "cs", "sound/"..codename.."/cs/z-c-30.ogg")
+
+dialog_addDialog("z-c-40", "en", "")
+dialog_addDialog("z-c-40", "cs", "sound/"..codename.."/cs/z-c-40.ogg")
+
+dialog_addDialog("z-c-50", "en", "")
+dialog_addDialog("z-c-50", "cs", "sound/"..codename.."/cs/z-c-50.ogg")
+
+dialog_addDialog("z-c-60", "en", "")
+dialog_addDialog("z-c-60", "cs", "sound/"..codename.."/cs/z-c-60.ogg")
+
+dialog_addDialog("z-c-70", "en", "")
+dialog_addDialog("z-c-70", "cs", "sound/"..codename.."/cs/z-c-70.ogg")
+
+dialog_addDialog("z-c-80", "en", "")
+dialog_addDialog("z-c-80", "cs", "sound/"..codename.."/cs/z-c-80.ogg")
+
+dialog_addDialog("z-c-90", "en", "")
+dialog_addDialog("z-c-90", "cs", "sound/"..codename.."/cs/z-c-90.ogg")
+
+dialog_addDialog("z-c-100", "en", "")
+dialog_addDialog("z-c-100", "cs", "sound/"..codename.."/cs/z-c-100.ogg")
+
+dialog_addDialog("z-c-200", "en", "")
+dialog_addDialog("z-c-200", "cs", "sound/"..codename.."/cs/z-c-200.ogg")
+
+dialog_addDialog("z-c-sta", "en", "")
+dialog_addDialog("z-c-sta", "cs", "sound/"..codename.."/cs/z-c-sta.ogg")
+
+dialog_addDialog("z-c-set", "en", "")
+dialog_addDialog("z-c-set", "cs", "sound/"..codename.."/cs/z-c-set.ogg")
+
+dialog_addDialog("z-c-tisic", "en", "")
+dialog_addDialog("z-c-tisic", "cs", "sound/"..codename.."/cs/z-c-tisic.ogg")
+
+dialog_addDialog("z-c-tisice", "en", "")
+dialog_addDialog("z-c-tisice", "cs", "sound/"..codename.."/cs/z-c-tisice.ogg")
+
+dialog_addDialog("z-c-hodin", "en", "", "font_white", "To be specific, it took you %1 hours!")
+dialog_addDialog("z-c-hodin", "cs", "sound/"..codename.."/cs/z-c-hodin.ogg", "font_white", "Konkrétně %1 hodin!")
+
+dialog_addDialog("z-c-konkretne", "en", "")
+dialog_addDialog("z-c-konkretne", "cs", "sound/"..codename.."/cs/z-c-konkretne.ogg")
+
+dialog_addDialog("bar-x-suck0", "en", "sound/"..codename.."/en/bar-x-suck0.ogg")
+
+dialog_addDialog("bar-x-suck1", "en", "sound/"..codename.."/en/bar-x-suck1.ogg")
+
+dialog_addDialog("bar-x-suck2", "en", "sound/"..codename.."/en/bar-x-suck2.ogg")
+
+dialog_addDialog("bar-x-suck3", "en", "sound/"..codename.."/en/bar-x-suck3.ogg")
+
+dialog_addDialog("bar-x-suckano", "en", "sound/"..codename.."/en/bar-x-suckano.ogg")
+
+
