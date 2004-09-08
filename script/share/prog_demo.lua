55abc0515c0e91774e964ea4c81942eb8d85c628
fidlej <fidlej>
2004-09-08 13:33:53 +0000

    Subtitles are scrolling non-stop on final poster.

--- script/share/prog_demo.lua
+++ script/share/prog_demo.lua
@@ -4,8 +4,11 @@ function planDelay(delay)
         return count >= delay
     end)
 end
+--NOTE: hack, dialog with space as filler
+dialog_addDialog("dlg-x-STOP", "en", "", "font_poster", " ")
 function planStop()
     game_planAction(function(count)
+        model_talk(1, "dlg-x-STOP")
         return false
     end)
 end
