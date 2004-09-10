ce5083c6a87f0178bd97c96a44a1e0c38f1a3773
ronnystandtke <ronnystandtke>
2004-09-10 20:33:43 +0000

    added German translation

--- script/chest/dialogs.lua
+++ script/chest/dialogs.lua
@@ -1,70 +1,94 @@
 dialog_addDialog("tru-v-nasly", "en", "", "font_big", "So we have found it finally.")
 dialog_addDialog("tru-v-nasly", "cs", "sound/"..codename.."/cs/tru-v-nasly.ogg", "font_big", "Tak jsme to konečně našly.")
+dialog_addDialog("tru-v-nasly", "de", "", "font_big", "Haben wir sie endlich gefunden.")
 
 dialog_addDialog("tru-m-co", "en", "", "font_small", "And what?")
 dialog_addDialog("tru-m-co", "cs", "sound/"..codename.."/cs/tru-m-co.ogg", "font_small", "Co?")
+dialog_addDialog("tru-m-co", "de", "", "font_small", "Was?")
 
 dialog_addDialog("tru-v-poklad", "en", "", "font_big", "Captain Silver`s treasure, of course.")
 dialog_addDialog("tru-v-poklad", "cs", "sound/"..codename.."/cs/tru-v-poklad.ogg", "font_big", "No přeci poklady kapitána Silvera.")
+dialog_addDialog("tru-v-poklad", "de", "", "font_big", "Na Kapitän Silvers Schatz!")
 
 dialog_addDialog("tru-v-gral", "en", "", "font_big", "The Holy Grail, of course.")
 dialog_addDialog("tru-v-gral", "cs", "sound/"..codename.."/cs/tru-v-gral.ogg", "font_big", "No přeci svatý grál.")
+dialog_addDialog("tru-v-gral", "de", "", "font_big", "Na den heiligen Gral!")
 
 dialog_addDialog("tru-v-vkupe", "en", "", "font_big", "It`s neatly in one place: the treasure and the Grail.")
 dialog_addDialog("tru-v-vkupe", "cs", "sound/"..codename.."/cs/tru-v-vkupe.ogg", "font_big", "Tak to tu máme pěkně v kupě: poklady i Grál.")
+dialog_addDialog("tru-v-vkupe", "de", "", "font_big", "Alles schön auf einem Haufen: der Schatz und der Gral.")
 
 dialog_addDialog("tru-m-zrada", "en", "", "font_small", "I think it`s much too easy to be true.")
 dialog_addDialog("tru-m-zrada", "cs", "sound/"..codename.."/cs/tru-m-zrada.ogg", "font_small", "Jen aby v tom nebyla nějaká zrada.")
+dialog_addDialog("tru-m-zrada", "de", "", "font_small", "Ich glaube, das ist zu einfach, um war zu sein.")
 
 dialog_addDialog("tru-m-oznamit", "en", "", "font_small", "I think that if it really was our goal, the game would let the player know.")
 dialog_addDialog("tru-m-oznamit", "cs", "sound/"..codename.."/cs/tru-m-oznamit.ogg", "font_small", "Já myslím, že kdyby to bylo skutečně ono, hra by to hráči nějak výrazně oznámila.")
+dialog_addDialog("tru-m-oznamit", "de", "", "font_small", "Ich denke, wenn das wirklich unser Ziel wäre, würde das Programm es den Spieler wissen lassen.")
 
 dialog_addDialog("tru-v-stacit", "en", "", "font_big", "But I have let him know. This could be enough.")
 dialog_addDialog("tru-v-stacit", "cs", "sound/"..codename.."/cs/tru-v-stacit.ogg", "font_big", "Však jsem mu to řekl. To by mu mohlo stačit.")
+dialog_addDialog("tru-v-stacit", "de", "", "font_big", "Aber ich habe es ihn wissen lassen. Das könnte reichen.")
 
 dialog_addDialog("tru-m-zpochybnit", "en", "", "font_small", "But I have casted doubts on it. This could confuse him.")
 dialog_addDialog("tru-m-zpochybnit", "cs", "sound/"..codename.."/cs/tru-m-zpochybnit.ogg", "font_small", "Jenže já jsem to zpochybnila. To by jej mohlo zmást.")
+dialog_addDialog("tru-m-zpochybnit", "de", "", "font_small", "Aber ich habe es angezweifelt. Das könnte ihn durcheinanderbringen.")
 
 dialog_addDialog("tru-v-nejspis", "en", "", "font_big", "Well, all right. PROBABLY this is not what we are looking for.")
 dialog_addDialog("tru-v-nejspis", "cs", "sound/"..codename.."/cs/tru-v-nejspis.ogg", "font_big", "No tak dobře. Tak to NEJSPÍŠ ještě není ono.")
+dialog_addDialog("tru-v-nejspis", "de", "", "font_big", "Hmm, na gut. WAHRSCHEINLICH ist das nicht, wonach wir suchen.")
 
 dialog_addDialog("tru-m-nejistota", "en", "", "font_small", "This is better. There remains a bit of uncertainty.")
 dialog_addDialog("tru-m-nejistota", "cs", "sound/"..codename.."/cs/tru-m-nejistota.ogg", "font_small", "Tak je to lepší. Zůstává v tom střípek nejistoty.")
+dialog_addDialog("tru-m-nejistota", "de", "", "font_small", "So ist es besser. Das hinterlässt eine bisschen Unsicherheit.")
 
 dialog_addDialog("tru-m-truhla0", "en", "", "font_small", "Couldn`t we close that chest?")
 dialog_addDialog("tru-m-truhla0", "cs", "sound/"..codename.."/cs/tru-m-truhla0.ogg", "font_small", "Nemůže se ta truhla najednou zavřít?")
+dialog_addDialog("tru-m-truhla0", "de", "", "font_small", "Können wir die Truhe nicht zumachen?")
 
 dialog_addDialog("tru-m-truhla1", "en", "", "font_small", "What if we try to close that chest?")
 dialog_addDialog("tru-m-truhla1", "cs", "sound/"..codename.."/cs/tru-m-truhla1.ogg", "font_small", "Co kdybychom zkusili tu truhlu zavřít?")
+dialog_addDialog("tru-m-truhla1", "de", "", "font_small", "Wollen wir versuchen, die Truhe zu schließen?")
+dialog_addDialog("tru-m-truhla1", "de_CH", "", "font_small", "Wollen wir versuchen, die Truhe zu schliessen?")
 
 dialog_addDialog("tru-v-truhla0", "en", "", "font_big", "It would be much easier with the closed chest.")
 dialog_addDialog("tru-v-truhla0", "cs", "sound/"..codename.."/cs/tru-v-truhla0.ogg", "font_big", "Se zavřenou truhlou by bylo všechno jednodušší.")
+dialog_addDialog("tru-v-truhla0", "de", "", "font_big", "Mit der geschlossenen Truhe wäre es viel einfacher.")
 
 dialog_addDialog("tru-v-truhla1", "en", "", "font_big", "Try to push its lid.")
 dialog_addDialog("tru-v-truhla1", "cs", "sound/"..codename.."/cs/tru-v-truhla1.ogg", "font_big", "Zkus jí strčit do víka.")
+dialog_addDialog("tru-v-truhla1", "de", "", "font_big", "Versuch mal, den Deckel zu bewegen.")
 
 dialog_addDialog("tru-m-vzit0", "en", "", "font_small", "Won`t we take something with us?")
 dialog_addDialog("tru-m-vzit0", "cs", "sound/"..codename.."/cs/tru-m-vzit0.ogg", "font_small", "Nevezmeme si s sebou něco?")
+dialog_addDialog("tru-m-vzit0", "de", "", "font_small", "Wollen wir nicht etwas mitnehmen?")
 
 dialog_addDialog("tru-m-vzit1", "en", "", "font_small", "What about taking some treasure with us?")
 dialog_addDialog("tru-m-vzit1", "cs", "sound/"..codename.."/cs/tru-m-vzit1.ogg", "font_small", "Co si takhle vzít nějaký poklad s sebou?")
+dialog_addDialog("tru-m-vzit1", "de", "", "font_small", "Wie wäre es, wenn wir ein paar Schätze mitnehmen?")
 
 dialog_addDialog("tru-m-vzit2", "en", "", "font_small", "Ain`t you sorry to leave all these treasures behind?")
 dialog_addDialog("tru-m-vzit2", "cs", "sound/"..codename.."/cs/tru-m-vzit2.ogg", "font_small", "Není ti líto, že tu necháme všechny ty poklady?")
+dialog_addDialog("tru-m-vzit2", "de", "", "font_small", "Tut es dir nicht leid, die ganzen Schätze zurückzulassen?")
 
 dialog_addDialog("tru-v-vzit0", "en", "", "font_big", "We will be glad if we manage to get out ourselves.")
 dialog_addDialog("tru-v-vzit0", "cs", "sound/"..codename.."/cs/tru-v-vzit0.ogg", "font_big", "Budeme rádi, když odjedem aspoň my samy.")
+dialog_addDialog("tru-v-vzit0", "de", "", "font_big", "Wir werden froh sein, wenn wir selbst hier rauskommen.")
 
 dialog_addDialog("tru-v-vzit1", "en", "", "font_big", "Don`t forget our mission.")
 dialog_addDialog("tru-v-vzit1", "cs", "sound/"..codename.."/cs/tru-v-vzit1.ogg", "font_big", "Nezapomeň, že máme poslání.")
+dialog_addDialog("tru-v-vzit1", "de", "", "font_big", "Vergiss nicht unseren Auftrag.")
 
 dialog_addDialog("tru-v-vzit2", "en", "", "font_big", "A treasure would only hinder us.")
 dialog_addDialog("tru-v-vzit2", "cs", "sound/"..codename.."/cs/tru-v-vzit2.ogg", "font_big", "Poklady by nás jenom zdržovaly.")
+dialog_addDialog("tru-v-vzit2", "de", "", "font_big", "Ein Schatz würde uns nur behindern.")
 
 dialog_addDialog("tru-v-zrak", "en", "", "font_big", "All this glint is a sight for the sore eyes.")
 dialog_addDialog("tru-v-zrak", "cs", "sound/"..codename.."/cs/tru-v-zrak.ogg", "font_big", "Už mě z toho lesku přechází zrak.")
+dialog_addDialog("tru-v-zrak", "de", "", "font_big", "All dieses Glitzern ist eine Augenweide.")
 
 dialog_addDialog("tru-m-trpyt", "en", "", "font_small", "Everything here gleams so. It makes me dizzy.")
 dialog_addDialog("tru-m-trpyt", "cs", "sound/"..codename.."/cs/tru-m-trpyt.ogg", "font_small", "Všechno se tu tak třpytí. Až z toho oči přecházejí.")
+dialog_addDialog("tru-m-trpyt", "de", "", "font_small", "Hier schimmert alles so. Mir wird ganz schwindelig.")
 
 
