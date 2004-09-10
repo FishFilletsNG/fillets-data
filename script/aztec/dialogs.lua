fb5db329c4039b9d80efb6b9c5922227b14297fd
ronnystandtke <ronnystandtke>
2004-09-10 20:48:41 +0000

    added German translation

--- script/aztec/dialogs.lua
+++ script/aztec/dialogs.lua
@@ -1,62 +1,77 @@
 dialog_addDialog("bot-m-vidis", "en", "", "font_small", "Can you see that seahorse?")
 dialog_addDialog("bot-m-vidis", "cs", "sound/"..codename.."/cs/bot-m-vidis.ogg", "font_small", "Vidíš toho koníka?")
 dialog_addDialog("bot-m-vidis", "fr", "", "font_small", "Peux-tu voir cet hypocampe ?")
+dialog_addDialog("bot-m-vidis", "de", "", "font_small", "Siehst du das Seepferdchen?")
 
 dialog_addDialog("bot-v-uveznen0", "en", "", "font_big", "It is blocked by amphoras.")
 dialog_addDialog("bot-v-uveznen0", "cs", "sound/"..codename.."/cs/bot-v-uveznen0.ogg", "font_big", "Je uvězněný mezi amforami.")
 dialog_addDialog("bot-v-uveznen0", "fr", "", "font_big", "Il est bloqué par les amphores.")
+dialog_addDialog("bot-v-uveznen0", "de", "", "font_big", "Es ist von Amphoren verschüttet.")
 
 dialog_addDialog("bot-v-uveznen1", "en", "", "font_big", "It got itself drunk among amphoras.")
 dialog_addDialog("bot-v-uveznen1", "cs", "sound/"..codename.."/cs/bot-v-uveznen1.ogg", "font_big", "Opíjel se mezi amforami.")
 dialog_addDialog("bot-v-uveznen1", "fr", "", "font_big", "Il s'est devenu saoul au milieu des amphores.")
+dialog_addDialog("bot-v-uveznen1", "de", "", "font_big", "Es hat sich zwischen Amphoren betrunken.")
 
 dialog_addDialog("bot-m-zajem", "en", "", "font_small", "I wonder if there is anything left in them.")
 dialog_addDialog("bot-m-zajem", "cs", "sound/"..codename.."/cs/bot-m-zajem.ogg", "font_small", "Zajímalo by mě, jestli v nich ještě něco je.")
 dialog_addDialog("bot-m-zajem", "fr", "", "font_small", "Je me demande si il a laissé quelque chose dedans.")
+dialog_addDialog("bot-m-zajem", "de", "", "font_small", "Ich frage mich, ob in denen noch was drin ist.")
 
 dialog_addDialog("bot-v-podivat", "en", "", "font_big", "You probably have to go check for yourself.")
 dialog_addDialog("bot-v-podivat", "cs", "sound/"..codename.."/cs/bot-v-podivat.ogg", "font_big", "Asi se stejně budeš muset jít podívat.")
 dialog_addDialog("bot-v-podivat", "fr", "", "font_big", "Tu vas devoir vérifier par toi même.")
+dialog_addDialog("bot-v-podivat", "de", "", "font_big", "Das musst du wahrscheinlich selbst überprüfen.")
 
 dialog_addDialog("bot-m-vidim", "en", "", "font_small", "Finally, I can see some new kind of skull.")
 dialog_addDialog("bot-m-vidim", "cs", "sound/"..codename.."/cs/bot-m-vidim.ogg", "font_small", "Konečně vidím nějakou novou lebku.")
 dialog_addDialog("bot-m-vidim", "fr", "", "font_small", "Finallement, je peux voir de nouveau type de crâne.")
+dialog_addDialog("bot-m-vidim", "de", "", "font_small", "Endlich sehe ich mal einen anderen Schädel.")
 
 dialog_addDialog("bot-v-vsim", "en", "", "font_big", "Did you notice that totem? This is Mexican god Shelloxuatl.")
 dialog_addDialog("bot-v-vsim", "cs", "sound/"..codename.."/cs/bot-v-vsim.ogg", "font_big", "Všimla sis toho totemu? To je mexický bůh Škleboxuatl.")
 dialog_addDialog("bot-v-vsim", "fr", "", "font_big", "Tu as remarqué ce totem ? C'est le dieux mexicain Shelloxuatl.")
+dialog_addDialog("bot-v-vsim", "de", "", "font_big", "Das du das Totem gesehen? Das ist der mexikanische Gott Shelloxuatl.")
 
 dialog_addDialog("bot-m-vypada", "en", "", "font_small", "It looks like that.")
 dialog_addDialog("bot-m-vypada", "cs", "sound/"..codename.."/cs/bot-m-vypada.ogg", "font_small", "Vypadá na to.")
 dialog_addDialog("bot-m-vypada", "fr", "", "font_small", "Il ressemble à ça.")
+dialog_addDialog("bot-m-vypada", "de", "", "font_small", "So sieht es aus.")
 
 dialog_addDialog("bot-m-padaji", "en", "", "font_small", "These amphores fall unbearably slow.")
 dialog_addDialog("bot-m-padaji", "cs", "sound/"..codename.."/cs/bot-m-padaji.ogg", "font_small", "Ty amfory padají nelidsky pomalu.")
 dialog_addDialog("bot-m-padaji", "fr", "", "font_small", "La chutte de ces amphores est insupportablement lente.")
+dialog_addDialog("bot-m-padaji", "de", "", "font_small", "Diese Amphoren fallen ja unmenschlich langsam.")
 
 dialog_addDialog("bot-v-vsak0", "en", "", "font_big", "Well, you are not a bear, after all.")
 dialog_addDialog("bot-v-vsak0", "cs", "sound/"..codename.."/cs/bot-v-vsak0.ogg", "font_big", "Však také nejsi člověk.")
 dialog_addDialog("bot-v-vsak0", "fr", "", "font_big", "Et bien, tu n'es pas un ours après tout.")
+dialog_addDialog("bot-v-vsak0", "de", "", "font_big", "Na und, Du bist ja auch un-menschlich.")
 
 dialog_addDialog("bot-v-vsak1", "en", "", "font_big", "Don`t forget we are under water.")
 dialog_addDialog("bot-v-vsak1", "cs", "sound/"..codename.."/cs/bot-v-vsak1.ogg", "font_big", "Však jsme také pod vodou.")
 dialog_addDialog("bot-v-vsak1", "fr", "", "font_big", "N'oublie pas que nous sommes sous l'eau.")
+dialog_addDialog("bot-v-vsak1", "de", "", "font_big", "Vergiss nicht, dass wir unter Wasser sind.")
 
 dialog_addDialog("bot-m-ble", "en", "", "font_small", "Yuck. The authors could spare us that animation.")
 dialog_addDialog("bot-m-ble", "cs", "sound/"..codename.."/cs/bot-m-ble.ogg", "font_small", "Blé... Tuhle animaci si autoři mohli odpustit.")
 dialog_addDialog("bot-m-ble", "fr", "", "font_small", "Argh. Les auteurs auraient put nous épargner cette animation.")
+dialog_addDialog("bot-m-ble", "de", "", "font_small", "Bäh. Die Animation hätten sich die Autoren sparen können.")
 
 dialog_addDialog("bot-v-totem", "en", "", "font_big", "This totem looks good to me.")
 dialog_addDialog("bot-v-totem", "cs", "sound/"..codename.."/cs/bot-v-totem.ogg", "font_big", "Mně se ten totem líbí.")
 dialog_addDialog("bot-v-totem", "fr", "", "font_big", "Ce totem me semble bien.")
+dialog_addDialog("bot-v-totem", "de", "", "font_big", "Dieses Totem gefällt mir.")
 
 dialog_addDialog("bot-v-lebka", "en", "", "font_big", "That skull seems to radiate something strange.")
 dialog_addDialog("bot-v-lebka", "cs", "sound/"..codename.."/cs/bot-v-lebka.ogg", "font_big", "Z té lebky vyzařuje něco tajemného.")
 dialog_addDialog("bot-v-lebka", "fr", "", "font_big", "Ce crâne semble iradier quelque chose d'étrange.")
+dialog_addDialog("bot-v-lebka", "de", "", "font_big", "Der Schädel scheint etwas seltsames auszustrahlen.")
 
 dialog_addDialog("bot-m-zivy", "en", "", "font_small", "Is it alive or is it some kind of spell?")
 dialog_addDialog("bot-m-zivy", "cs", "sound/"..codename.."/cs/bot-m-zivy.ogg", "font_small", "Je to živý, nebo je to kouzlo?")
 dialog_addDialog("bot-m-zivy", "fr", "", "font_small", "Est-il vivant ou est-ce une sorte de magie ?")
+dialog_addDialog("bot-m-zivy", "de", "", "font_small", "Lebt es oder ist es so eine Art Zauberei?")
 
 dialog_addDialog("bot-x-smich", "en", "sound/"..codename.."/en/bot-x-smich.ogg")
 
