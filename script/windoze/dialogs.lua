daace02764c965a6b4282ffe7ee43e39687fb93d
ronnystandtke <ronnystandtke>
2004-09-14 06:20:40 +0000

    added German translation

--- script/windoze/dialogs.lua
+++ script/windoze/dialogs.lua
@@ -1,94 +1,128 @@
 dialog_addDialog("win-v-plocha", "en", "", "font_big", "This is how it looks when you don`t keep your desktop tidy.")
 dialog_addDialog("win-v-plocha", "cs", "sound/"..codename.."/cs/win-v-plocha.ogg", "font_big", "Tak to vypadá, když si někdo neudržuje na ploše pořádek.")
+dialog_addDialog("win-v-plocha", "de", "", "font_big", "So sieht es aus, wenn man seine Arbeitsfläche nicht aufräumt.")
 
 dialog_addDialog("win-m-costim0", "en", "", "font_small", "Wouldn`t it help us if we select Desktop/Line up Icons?")
 dialog_addDialog("win-m-costim0", "cs", "sound/"..codename.."/cs/win-m-costim0.ogg", "font_small", "Hele, nepomohlo by nám, kdybychom zvolili funkci: Vyrovnat ikony?")
+dialog_addDialog("win-m-costim0", "de", "", "font_small", "Würde es uns nicht helfen, wenn wir Arbeitsfläche/Symbole anordnen auswählen?")
 
 dialog_addDialog("win-m-costim1", "en", "", "font_small", "What if we try to minimize the biggest windows?")
 dialog_addDialog("win-m-costim1", "cs", "sound/"..codename.."/cs/win-m-costim1.ogg", "font_small", "Co kdybychom zkusili ta největší okna minimalizovat?")
+dialog_addDialog("win-m-costim1", "de", "", "font_small", "Wollen wir versuchen, die größten Fenster zu schließen?")
+dialog_addDialog("win-m-costim1", "de_CH", "", "font_small", "Wollen wir versuchen, die grössten Fenster zu schliessen?")
 
 dialog_addDialog("win-m-costim2", "en", "", "font_small", "What if we try to close some of these windows?")
 dialog_addDialog("win-m-costim2", "cs", "sound/"..codename.."/cs/win-m-costim2.ogg", "font_small", "Co kdybychom některá z těch oken zavřeli?")
+dialog_addDialog("win-m-costim2", "de", "", "font_small", "Wollen wir versuchen, einige der Fenster zu schließen?")
+dialog_addDialog("win-m-costim2", "de_CH", "", "font_small", "Wollen wir versuchen, einige der Fenster zu schliessen?")
 
 dialog_addDialog("win-m-costim3", "en", "", "font_small", "What about shutting down the whole thing and switch to command prompt only?")
 dialog_addDialog("win-m-costim3", "cs", "sound/"..codename.."/cs/win-m-costim3.ogg", "font_small", "Co takhle to celé vypnout a nabootovat v DOSu?")
+dialog_addDialog("win-m-costim3", "de", "", "font_small", "Wie wäre es, wenn wir das ganze Ding runterfahren und zur Kommandozeile umschalten?")
 
 dialog_addDialog("win-m-costim4", "en", "", "font_small", "What about giving up altogether and swimming to the pub instead?")
 dialog_addDialog("win-m-costim4", "cs", "sound/"..codename.."/cs/win-m-costim4.ogg", "font_small", "Co takhle se na to vykašlat a odplavat do hospody?")
+dialog_addDialog("win-m-costim4", "de", "", "font_small", "Wie wäre es, wenn wir einfach aufgeben und stattdessen in die Kneipe schwimmen?")
 
 dialog_addDialog("win-v-pocitala", "en", "", "font_big", "Did you count these windows around us?")
 dialog_addDialog("win-v-pocitala", "cs", "sound/"..codename.."/cs/win-v-pocitala.ogg", "font_big", "Počítala jsi ty okna všude kolem?")
+dialog_addDialog("win-v-pocitala", "de", "", "font_big", "Hast du die Fenster um uns herum gezählt?")
 
 dialog_addDialog("win-m-nemusim", "en", "", "font_small", "I don`t need to count them. There are ninety five of them.")
 dialog_addDialog("win-m-nemusim", "cs", "sound/"..codename.."/cs/win-m-nemusim.ogg", "font_small", "To nemusím počítat. Je jich devadesát pět.")
+dialog_addDialog("win-m-nemusim", "de", "", "font_small", "Die muss ich nicht zählen. Es sind fünfundneunzig.")
 
 dialog_addDialog("win-m-blok", "en", "", "font_small", "Can you see that Notepad? Now it`s good time to send a message to the player!")
 dialog_addDialog("win-m-blok", "cs", "sound/"..codename.."/cs/win-m-blok.ogg", "font_small", "Hele, vidíš ten Poznámkový blok? Vhodná chvíle poslat hráči nějaký vzkaz!")
+dialog_addDialog("win-m-blok", "de", "", "font_small", "Siehst du den Notizblock? Das ist die Gelegeheit, dem Spieler eine Nachricht zu schicken!")
 
 dialog_addDialog("win-v-premyslej", "en", "", "font_big", "Stop that folly and try to think instead.")
 dialog_addDialog("win-v-premyslej", "cs", "sound/"..codename.."/cs/win-v-premyslej.ogg", "font_big", "Nech těch hloupostí a raději přemýšlej.")
+dialog_addDialog("win-v-premyslej", "de", "", "font_big", "Hör mit dem Blödsinn auf und versuch lieber nachzudenken!")
 
 dialog_addDialog("win-m-dira", "en", "", "font_small", "This system has to have a hole somewhere.")
 dialog_addDialog("win-m-dira", "cs", "sound/"..codename.."/cs/win-m-dira.ogg", "font_small", "Někde ten systém přece musí mít díru.")
+dialog_addDialog("win-m-dira", "de", "", "font_small", "Das System muss irgendwo ein Loch haben.")
 
 dialog_addDialog("win-v-tamhle", "en", "", "font_big", "Surely, it`s in the lower right corner.")
 dialog_addDialog("win-v-tamhle", "cs", "sound/"..codename.."/cs/win-v-tamhle.ogg", "font_big", "Jistě, támhle vpravo dole.")
+dialog_addDialog("win-v-tamhle", "de", "", "font_big", "Sicher, es ist unten rechts.")
 
 dialog_addDialog("win-m-okno", "en", "", "font_small", "Can you see this big window on the right?!")
 dialog_addDialog("win-m-okno", "cs", "sound/"..codename.."/cs/win-m-okno.ogg", "font_small", "Vidíš to velké okno napravo?!")
+dialog_addDialog("win-m-okno", "de", "", "font_small", "Siehst Du das rechts das große Fenster?!")
 
 dialog_addDialog("win-v-hra", "en", "", "font_big", "Oh, no! This must be the original version of this game.")
 dialog_addDialog("win-v-hra", "cs", "sound/"..codename.."/cs/win-v-hra.ogg", "font_big", "No to snad ne! To je původní verze této hry.")
+dialog_addDialog("win-v-hra", "de", "", "font_big", "Oh, nein! Das muss die Originalversion dieses Spiels sein.")
 
 dialog_addDialog("win-m-chodila", "en", "", "font_small", "It ran on the antediluvial machines, in standard VGA resolution...")
 dialog_addDialog("win-m-chodila", "cs", "sound/"..codename.."/cs/win-m-chodila.ogg", "font_small", "Ta chodila na předpotopních mašinách, ve standardním VGA rozlišení...")
+dialog_addDialog("win-m-chodila", "de", "", "font_small", "Es ist auf vorsintflutlichen Rechnern gelaufen, in Standard-VGA-Auflösung...")
 
 dialog_addDialog("win-v-nic0", "en", "", "font_big", "No animation...")
 dialog_addDialog("win-v-nic0", "cs", "sound/"..codename.."/cs/win-v-nic0.ogg", "font_big", "Žádné animace...")
+dialog_addDialog("win-v-nic0", "de", "", "font_big", "Keine Animationen...")
 
 dialog_addDialog("win-m-nic1", "en", "", "font_small", "No talking...")
 dialog_addDialog("win-m-nic1", "cs", "sound/"..codename.."/cs/win-m-nic1.ogg", "font_small", "Žádné namluvené texty...")
+dialog_addDialog("win-m-nic1", "de", "", "font_small", "Kein Text...")
 
 dialog_addDialog("win-v-nic2", "en", "", "font_big", "No music...")
 dialog_addDialog("win-v-nic2", "cs", "sound/"..codename.."/cs/win-v-nic2.ogg", "font_big", "Žádná hudba...")
+dialog_addDialog("win-v-nic2", "de", "", "font_big", "Keine Musik...")
 
 dialog_addDialog("win-m-nic3", "en", "", "font_small", "Only a beep from time to time...")
 dialog_addDialog("win-m-nic3", "cs", "sound/"..codename.."/cs/win-m-nic3.ogg", "font_small", "Jen sem tam nějaké pípnutí...")
+dialog_addDialog("win-m-nic3", "de", "", "font_small", "Nur ab und zu ein Piepser...")
 
 dialog_addDialog("win-v-hav", "en", "", "font_big", "But fortunately the authors got back to it and gave it this modern facelift.")
 dialog_addDialog("win-v-hav", "cs", "sound/"..codename.."/cs/win-v-hav.ogg", "font_big", "Ještě, že se k ní autoři po létech vrátili a dali jí tento moderní háv.")
+dialog_addDialog("win-v-hav", "de", "", "font_big", "Aber glücklicherweise haben die Autoren das Spiel nochmal aufgemöbelt.")
 
 dialog_addDialog("win-m-zahrat", "en", "", "font_small", "But I`d like to play it sometime, anyway!")
 dialog_addDialog("win-m-zahrat", "cs", "sound/"..codename.."/cs/win-m-zahrat.ogg", "font_small", "Stejně bych si to někdy zkusila zahrát!")
+dialog_addDialog("win-m-zahrat", "de", "", "font_small", "Aber ich würde es trotzdem irgendwann mal spielen!")
 
 dialog_addDialog("win-m-vga", "en", "", "font_small", "I can`t move this window. Down there, it`s a steel cylinder, though it`s only in sixteen color VGA.")
 dialog_addDialog("win-m-vga", "cs", "sound/"..codename.."/cs/win-m-vga.ogg", "font_small", "Já s tímhle oknem nepohnu. Támhle dole, to je ocelový válec, i když jen v šestnáctibarevném VGA.")
+dialog_addDialog("win-m-vga", "de", "", "font_small", "Ich kann dieses Fenster nicht bewegen. Da unten ist ein Stahlzylinder, obwohl nur in 16-Farben-VGA.")
 
 dialog_addDialog("win-v-pockej", "en", "", "font_big", "Wait a moment, player. We have to make something clear. These two fish, they are our younger selves.")
 dialog_addDialog("win-v-pockej", "cs", "sound/"..codename.."/cs/win-v-pockej.ogg", "font_big", "Počkej, hráči. Musím si něco ujasnit. Támhle ty dvě rybičky, to jsme přece my zamlada.")
+dialog_addDialog("win-v-pockej", "de", "", "font_big", "Einen Moment, Spieler. Wir müssen etwas klarstellen. Diese beiden Fische sind unsere jüngeren Egos.")
 
 dialog_addDialog("win-m-zavrene", "en", "", "font_small", "You are right. They are closed there, poor souls.")
 dialog_addDialog("win-m-zavrene", "cs", "sound/"..codename.."/cs/win-m-zavrene.ogg", "font_small", "Máš pravdu. Jsou tam chudinky zavřené.")
+dialog_addDialog("win-m-zavrene", "de", "", "font_small", "Du hast Recht. Die armen Seelen sind dort eingeschlossen.")
 
 dialog_addDialog("win-v-osvobodit", "en", "", "font_big", "We can`t leave them there. We have to get them out!")
 dialog_addDialog("win-v-osvobodit", "cs", "sound/"..codename.."/cs/win-v-osvobodit.ogg", "font_big", "Přece je tam nenecháme! Musíme je osvobodit.")
+dialog_addDialog("win-v-osvobodit", "de", "", "font_big", "Wir können Sie dort nicht lassen. Wir müssen Sie rausholen!")
 
 dialog_addDialog("win-m-ven", "en", "", "font_small", "Yes, but the player has to get us out.")
 dialog_addDialog("win-m-ven", "cs", "sound/"..codename.."/cs/win-m-ven.ogg", "font_small", "No jo, ale hráč má za úkol dostat ven nás.")
+dialog_addDialog("win-m-ven", "de", "", "font_small", "Ja, aber der Spieler muss uns rausholen.")
 
 dialog_addDialog("win-v-citim", "en", "", "font_big", "So we`ll go on strike. Personally, I fully sympathize with the bigger fish inside.")
 dialog_addDialog("win-v-citim", "cs", "sound/"..codename.."/cs/win-v-citim.ogg", "font_big", "Tak budeme stávkovat. Já osobně plně cítím s tou velkou rybičkou uvnitř.")
+dialog_addDialog("win-v-citim", "de", "", "font_big", "Also streiken wir. Ich persönlich fühle übrigens sehr mit dem größeren Fisch da drin.")
+dialog_addDialog("win-v-citim", "de_CH", "", "font_big", "Also streiken wir. Ich persönlich fühle übrigens sehr mit dem grösseren Fisch da drin.")
 
 dialog_addDialog("win-m-vzit", "en", "", "font_small", "You`re right. I can imagine what the smaller one feels.")
 dialog_addDialog("win-m-vzit", "cs", "sound/"..codename.."/cs/win-m-vzit.ogg", "font_small", "Máš pravdu. Já se také dokážu vžít do té malé.")
+dialog_addDialog("win-m-vzit", "de", "", "font_small", "Du hast Recht. Ich kann mir vorstellen, was der Kleinere fühlt .")
 
 dialog_addDialog("win-v-nehrajem", "en", "", "font_big", "We are not going to play, until you save those two, player.")
 dialog_addDialog("win-v-nehrajem", "cs", "sound/"..codename.."/cs/win-v-nehrajem.ogg", "font_big", "Dál nehrajeme, dokud ty dvě nezachráníš, hráči.")
+dialog_addDialog("win-v-nehrajem", "de", "", "font_big", "Spieler, wir machen erst weiter, wenn du die zwei gerettet hast!")
 
 dialog_addDialog("win-v-real", "en", "", "font_big", "Goodness, that is a realistic game!")
 dialog_addDialog("win-v-real", "cs", "sound/"..codename.."/cs/win-v-real.ogg", "font_big", "Hergot, to je ale realistická hra!")
+dialog_addDialog("win-v-real", "de", "", "font_big", "Meine Güte, ist das ein realistisches Spiel!")
 
 dialog_addDialog("win-m-jejda", "en", "", "font_small", "Oh my, he took the game too seriously!")
 dialog_addDialog("win-m-jejda", "cs", "sound/"..codename.."/cs/win-m-jejda.ogg", "font_small", "Jejda, ten se do toho nějak vžil!")
+dialog_addDialog("win-m-jejda", "de", "", "font_small", "Oh je, er hat das Spiel zu ernst genommen!")
 
 
