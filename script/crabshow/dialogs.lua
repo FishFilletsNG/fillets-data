cd60d6e53fd81c8ba499d7220d17e7d7e8db88b7
ronnystandtke <ronnystandtke>
2004-09-21 19:49:55 +0000

    added German translation

--- script/crabshow/dialogs.lua
+++ script/crabshow/dialogs.lua
@@ -1,89 +1,112 @@
 dialog_addDialog("sec-v-ven0", "en", "", "font_big", "I am closed here.")
 dialog_addDialog("sec-v-ven0", "cs", "sound/"..codename.."/cs/sec-v-ven0.ogg", "font_big", "Jsem tu zavřený.")
 dialog_addDialog("sec-v-ven0", "fr", "", "font_big", "Là, je suis enfermé.")
+dialog_addDialog("sec-v-ven0", "de", "", "font_big", "Ich bin hier eingeschlossen.")
 
 dialog_addDialog("sec-v-ven1", "en", "", "font_big", "I want to get out.")
 dialog_addDialog("sec-v-ven1", "cs", "sound/"..codename.."/cs/sec-v-ven1.ogg", "font_big", "Já chci ven.")
 dialog_addDialog("sec-v-ven1", "fr", "", "font_big", "Je veux sortir.")
+dialog_addDialog("sec-v-ven1", "de", "", "font_big", "Ich will raus.")
 
 dialog_addDialog("sec-v-ven2", "en", "", "font_big", "How I can get out of here?")
 dialog_addDialog("sec-v-ven2", "cs", "sound/"..codename.."/cs/sec-v-ven2.ogg", "font_big", "Jak se odtud mám dostat?")
 dialog_addDialog("sec-v-ven2", "fr", "", "font_big", "Comment vais-je sortir d'ici ?")
+dialog_addDialog("sec-v-ven2", "de", "", "font_big", "Wie komme ich hier raus?")
 
 dialog_addDialog("sec-m-ven0", "en", "", "font_small", "See, now you got out.")
 dialog_addDialog("sec-m-ven0", "cs", "sound/"..codename.."/cs/sec-m-ven0.ogg", "font_small", "No vidíš, jsi venku.")
 dialog_addDialog("sec-m-ven0", "fr", "", "font_small", "Tu vois, maintenant tu es dehors.")
+dialog_addDialog("sec-m-ven0", "de", "", "font_small", "Schau, jetzt bist du draußen.")
+dialog_addDialog("sec-m-ven0", "de_CH", "", "font_small", "Schau, jetzt bist du draussen.")
 
 dialog_addDialog("sec-m-ven1", "en", "", "font_small", "So, we are together again.")
 dialog_addDialog("sec-m-ven1", "cs", "sound/"..codename.."/cs/sec-m-ven1.ogg", "font_small", "Tak, a jsme zase spolu.")
 dialog_addDialog("sec-m-ven1", "fr", "", "font_small", "De nouveau ensemble.")
+dialog_addDialog("sec-m-ven1", "de", "", "font_small", "So, wir sind wieder zusammen.")
 
 dialog_addDialog("sec-v-zavreny", "en", "", "font_big", "I am closed in a room without doors!")
 dialog_addDialog("sec-v-zavreny", "cs", "sound/"..codename.."/cs/sec-v-zavreny.ogg", "font_big", "Jsem zavřený v místnosti bez dveří!")
 dialog_addDialog("sec-v-zavreny", "fr", "", "font_big", "Je suis enfermé dans une pièce sans porte !")
+dialog_addDialog("sec-v-zavreny", "de", "", "font_big", "Ich bin in einem Raum ohne Türen eingeschlossen!")
 
 dialog_addDialog("sec-m-kamen", "en", "", "font_small", "Try to loosen some stone in the wall.")
 dialog_addDialog("sec-m-kamen", "cs", "sound/"..codename.."/cs/sec-m-kamen.ogg", "font_small", "Zkus, jestli neuvolníš nějaký kámen ve zdi!")
 dialog_addDialog("sec-m-kamen", "fr", "", "font_small", "Essaie de détacher des pierres du mur.")
+dialog_addDialog("sec-m-kamen", "de", "", "font_small", "Versuche, einen Stein in der Mauer zu lockern.")
 
 dialog_addDialog("sec-m-krab", "en", "", "font_small", "Isn`t that crab little bit strange?")
 dialog_addDialog("sec-m-krab", "cs", "sound/"..codename.."/cs/sec-m-krab.ogg", "font_small", "Nezdá se ti ten krab poněkud úchylný?")
 dialog_addDialog("sec-m-krab", "fr", "", "font_small", "Ce crabe, n'est-il pas un petit peu étrange ?")
+dialog_addDialog("sec-m-krab", "de", "", "font_small", "Ist die Krabbe nicht ein wenig seltsam?")
 
 dialog_addDialog("sec-v-ktery", "en", "", "font_big", "Which one do you mean?")
 dialog_addDialog("sec-v-ktery", "cs", "sound/"..codename.."/cs/sec-v-ktery.ogg", "font_big", "A kterého myslíš?")
 dialog_addDialog("sec-v-ktery", "fr", "", "font_big", "Du quel tu veux parler ?")
+dialog_addDialog("sec-v-ktery", "de", "", "font_big", "Welche meinst du?")
 
 dialog_addDialog("sec-m-dole0", "en", "", "font_small", "The upper one, of course.")
 dialog_addDialog("sec-m-dole0", "cs", "sound/"..codename.."/cs/sec-m-dole0.ogg", "font_small", "Toho nahoře, přece.")
 dialog_addDialog("sec-m-dole0", "fr", "", "font_small", "Celui qui est tout en haut, bien sûr.")
+dialog_addDialog("sec-m-dole0", "de", "", "font_small", "Natürlich die obere.")
 
 dialog_addDialog("sec-m-dole1", "en", "", "font_small", "The lower one, of course.")
 dialog_addDialog("sec-m-dole1", "cs", "sound/"..codename.."/cs/sec-m-dole1.ogg", "font_small", "Samozřejmě, že toho dole.")
 dialog_addDialog("sec-m-dole1", "fr", "", "font_small", "Celui qui est tout en bas, bien sûr.")
+dialog_addDialog("sec-m-dole1", "de", "", "font_small", "Natürlich die untere.")
 
 dialog_addDialog("sec-v-normalni0", "en", "", "font_big", "I think it`s normal.")
 dialog_addDialog("sec-v-normalni0", "cs", "sound/"..codename.."/cs/sec-v-normalni0.ogg", "font_big", "Mně připadne normální.")
 dialog_addDialog("sec-v-normalni0", "fr", "", "font_big", "Je le trouve normal.")
+dialog_addDialog("sec-v-normalni0", "de", "", "font_big", "Ich denke, sie ist normal.")
 
 dialog_addDialog("sec-v-normalni1", "en", "", "font_big", "Yes, it`s little weird.")
 dialog_addDialog("sec-v-normalni1", "cs", "sound/"..codename.."/cs/sec-v-normalni1.ogg", "font_big", "Je trochu zvláštní...")
 dialog_addDialog("sec-v-normalni1", "fr", "", "font_big", "Oui il paraît dérangé.")
+dialog_addDialog("sec-v-normalni1", "de", "", "font_big", "Ja, die ist ein wenig verrückt.")
 
 dialog_addDialog("sec-m-balonky", "en", "", "font_small", "We should be glad we got all those balls.")
 dialog_addDialog("sec-m-balonky", "cs", "sound/"..codename.."/cs/sec-m-balonky.ogg", "font_small", "Ještě, že tu jsou všechny ty balónky.")
 dialog_addDialog("sec-m-balonky", "fr", "", "font_small", "Nous devrions être content d'avoir toutes ces balles.")
+dialog_addDialog("sec-m-balonky", "de", "", "font_small", "Ein Glück, dass wir so viele Bälle haben.")
 
 dialog_addDialog("sec-v-mesto", "en", "", "font_big", "This city looks suspiciously well preserved...")
 dialog_addDialog("sec-v-mesto", "cs", "sound/"..codename.."/cs/sec-v-mesto.ogg", "font_big", "To město je podezřele zachovalé...")
 dialog_addDialog("sec-v-mesto", "fr", "", "font_big", "La cité semble étrangement bien conservée.")
+dialog_addDialog("sec-v-mesto", "de", "", "font_big", "Diese Stadt sieht verdächtig gut erhalten aus...")
 
 dialog_addDialog("sec-v-pocit", "en", "", "font_big", "I have a feeling that I am missing something.")
 dialog_addDialog("sec-v-pocit", "cs", "sound/"..codename.."/cs/sec-v-pocit.ogg", "font_big", "Mám pocit, že nám něco uniká.")
 dialog_addDialog("sec-v-pocit", "fr", "", "font_big", "J'ai l'impression que j'oublie quelque chose.")
+dialog_addDialog("sec-v-pocit", "de", "", "font_big", "Ich hab so ein komisches Gefühl.")
 
 dialog_addDialog("sec-m-pocity", "en", "", "font_small", "Your feeling... think about how to get us out.")
 dialog_addDialog("sec-m-pocity", "cs", "sound/"..codename.."/cs/sec-m-pocity.ogg", "font_small", "Tvoje pocity... radši přemýšlej jak ven.")
 dialog_addDialog("sec-m-pocity", "fr", "", "font_small", "Tes intuitions... Réfléchit plutôt à comment sortir.")
+dialog_addDialog("sec-m-pocity", "de", "", "font_small", "Deine Gefühle... denk darüber nach, wie wir hier rauskommen.")
 
 dialog_addDialog("sec-v-pockej", "en", "", "font_big", "No, wait, watch all these crabs for a moment... look into their eyes...")
 dialog_addDialog("sec-v-pockej", "cs", "sound/"..codename.."/cs/sec-v-pockej.ogg", "font_big", "Ne, počkej, sleduj chvíli všechny ty kraby... podívej se na jejich oči...")
 dialog_addDialog("sec-v-pockej", "fr", "", "font_big", "Non, attends, regarde ces crabes un moment... Dans leurs yeux...")
+dialog_addDialog("sec-v-pockej", "de", "", "font_big", "Nein, warte, schau dir mal kurz die Krabben an... schau in ihre Augen...")
 
 dialog_addDialog("sec-v-oci", "en", "", "font_big", "Their eyes move in accord with each other. As if they are controlled by some common superconsciousness.")
 dialog_addDialog("sec-v-oci", "cs", "sound/"..codename.."/cs/sec-v-oci.ogg", "font_big", "Jejich oči se pohybují společně. Jako by je řídila nějaká společná superinteligence.")
 dialog_addDialog("sec-v-oci", "fr", "", "font_big", "Leurs yeux bougent en harmonie les uns avec les autres. Comme s'ils étaient contrôlés par une conscience plus forte.")
+dialog_addDialog("sec-v-oci", "de", "", "font_big", "Ihre Augen bewegen sich alle gleich. Als wenn sie durch ein gemeinsames Überbewusstsein kontrolliert werden.")
 
 dialog_addDialog("sec-m-program", "en", "", "font_small", "But of course, they are controlled by computer program. Just like the undulating of water and your silly talk.")
 dialog_addDialog("sec-m-program", "cs", "sound/"..codename.."/cs/sec-m-program.ogg", "font_small", "Jistě, řídí je počítačový program. Stejně jako to vlnění vody kolem a tvoje hloupé řeči.")
 dialog_addDialog("sec-m-program", "fr", "", "font_small", "Mais bien sûr, ils sont contrôlés par le programme. Juste comme l'ondulation de l'eau et tes stupides paroles.")
+dialog_addDialog("sec-m-program", "de", "", "font_small", "Sie werden durch ein Programm gesteuert. Genauso wie die Wellen im Wasser und dein blödes Gequatsche.")
 
 dialog_addDialog("sec-v-socha", "en", "", "font_big", "Did we ever see a sculpture that would be silent for so long?")
 dialog_addDialog("sec-v-socha", "cs", "sound/"..codename.."/cs/sec-v-socha.ogg", "font_big", "To se ještě nestalo, aby ta socha celou dobu mlčela.")
 dialog_addDialog("sec-v-socha", "fr", "", "font_big", "Tu as déjà vu une statue qui reste silencieuse aussi longtemps ?")
+dialog_addDialog("sec-v-socha", "de", "", "font_big", "Haben wir je eine Skulptur gesehen, die so lange ruhig war?")
 
 dialog_addDialog("sec-m-situace", "en", "", "font_small", "I think it`s only waiting for the best opportunity.")
 dialog_addDialog("sec-m-situace", "cs", "sound/"..codename.."/cs/sec-m-situace.ogg", "font_small", "Myslím, že jen vyčkává vhodné situace.")
 dialog_addDialog("sec-m-situace", "fr", "", "font_small", "Je pense qu'elle attend pour une meilleure opportunité.")
+dialog_addDialog("sec-m-situace", "de", "", "font_small", "Ich denke, sie wartet nur auf die beste Gelegenheit.")
 
 
