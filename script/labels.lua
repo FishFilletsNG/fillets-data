1da9355ee5567d4aff88ed52cd64a5a6a5a4daf5
fidlej <fidlej>
2004-09-24 17:04:21 +0000

    Info about best solver is under pedometer.

---
+++ script/labels.lua
@@ -0,0 +1,9 @@
+
+label_text("solver_title", "en", "Best solver");
+label_text("solver_title", "cs", "Nejlepší řešitel");
+
+label_text("solver_best", "en", "%1 ... %2");
+
+label_text("solver_check", "en", "Check http://fillets.sf.net/halloffame.php for newer statistics.");
+label_text("solver_check", "cs", "Navštiv http://fillets.sf.net/halloffame.php pro novější statistiky.");
+
