86eded031de8b928557ed243fcad150130830a30
mrpaja <mrpaja>
2004-09-24 18:11:36 +0000

    change labels

--- script/labels.lua
+++ script/labels.lua
@@ -1,9 +1,13 @@
 
-label_text("solver_title", "en", "Best solver");
-label_text("solver_title", "cs", "Nejlepší řešitel");
+label_text("solver_worse1", "en", "Best solution:      %1");
+label_text("solver_worse1", "cs", "Nejlepší řešení:    %1");
 
-label_text("solver_best", "en", "%1 ... %2");
+label_text("solver_worse2", "en", "Solved by %2");
+label_text("solver_worse2", "cs", "Dohrál %2");
 
-label_text("solver_check", "en", "Check http://fillets.sf.net/halloffame.php for newer statistics.");
-label_text("solver_check", "cs", "Navštiv http://fillets.sf.net/halloffame.php pro novější statistiky.");
+label_text("solver_better1", "en", "You have BETTER solution!");
+label_text("solver_better1", "cs", "Vymyslel jsi LEPŠÍ řešení!");
+
+label_text("solver_better2", "en", "See http://fillets.sf.net/halloffame.php");
+label_text("solver_better2", "cs", "Koukni na http://fillets.sf.net/halloffame.php");
 
