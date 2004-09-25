81d708f8f89bffb506131e4397a6563c9616e9b7
fidlej <fidlej>
2004-09-25 01:16:24 +0000

    Implemented real help screen. Use F1 and translate script/labels.lua.

--- script/labels.lua
+++ script/labels.lua
@@ -11,3 +11,33 @@ label_text("solver_better1", "cs", "Vymyslel jsi LEPŠÍ řešení!");
 label_text("solver_better2", "en", "See http://fillets.sf.net/halloffame.php");
 label_text("solver_better2", "cs", "Koukni na http://fillets.sf.net/halloffame.php");
 
+
+label_text("help", "en",
+[[
+                HELP
+restart level - Backspace
+help - F1
+save game - F2
+load game - F3
+show/hide move counter - F5
+show/hide subtitles - F6
+game menu - F10
+fullscreen - F11
+faster game - Shift
+]])
+label_text("help", "cs",
+[[
+              NÁPOVĚDA
+opakuj místnost - Backspace
+nápověda - F1
+ulož hru - F2
+načti hru - F3
+zobraz/schovej počítadlo tahů - F5
+zobraz/schovej titulky - F6
+herní nastavení - F10
+celá obrazovka - F11
+rychlejší hra - Shift
+]])
+
+
+
