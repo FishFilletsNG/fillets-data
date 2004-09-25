16f46b68800f8b3311b9c86912a3a20aa2152d5b
fidlej <fidlej>
2004-09-25 16:24:06 +0000

    Added menu tooltips.

--- script/labels.lua
+++ script/labels.lua
@@ -1,4 +1,5 @@
 
+-- pedometer
 label_text("solver_worse1", "en", "Best solution:      %1");
 label_text("solver_worse1", "cs", "Nejlepší řešení:    %1");
 
@@ -11,7 +12,23 @@ label_text("solver_better1", "cs", "Vymyslel jsi LEPŠÍ řešení!");
 label_text("solver_better2", "en", "See http://fillets.sf.net/halloffame.php");
 label_text("solver_better2", "cs", "Koukni na http://fillets.sf.net/halloffame.php");
 
+-- menu
+label_text("menu_sound", "en", "Sound volume");
+label_text("menu_sound", "cs", "Hlasitost zvuku");
 
+label_text("menu_music", "en", "Music volume");
+label_text("menu_music", "cs", "Hlasitost hudby");
+
+label_text("menu_lang", "en", "Choose language");
+label_text("menu_lang", "cs", "Zvol jazyk");
+
+label_text("menu_subtitles", "en", "Show subtitles?");
+label_text("menu_subtitles", "cs", "Zobrazovat titulky?");
+
+label_text("menu_back", "en", "Back");
+label_text("menu_back", "cs", "Zpět");
+
+-- help
 label_text("help", "en",
 [[
                 HELP
