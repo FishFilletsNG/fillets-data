5d12dbcd0594386e73ab1b84eb2948a4cedf6458
fidlej <fidlej>
2004-09-26 12:40:34 +0000

    Multi line paragraph is used for solver info.

--- script/labels.lua
+++ script/labels.lua
@@ -1,16 +1,26 @@
 
 -- pedometer
-label_text("solver_worse1", "en", "Best solution:      %1");
-label_text("solver_worse1", "cs", "Nejlepší řešení:    %1");
+label_text("solver_worse", "en",
+[[
+Best solution:      %1"
+Solved by %2
+]])
+label_text("solver_worse", "cs",
+[[
+Nejlepší řešení:    %1
+Dohrál %2
+]])
 
-label_text("solver_worse2", "en", "Solved by %2");
-label_text("solver_worse2", "cs", "Dohrál %2");
-
-label_text("solver_better1", "en", "You have BETTER solution!");
-label_text("solver_better1", "cs", "Vymyslel jsi LEPŠÍ řešení!");
-
-label_text("solver_better2", "en", "See http://fillets.sf.net/halloffame.php");
-label_text("solver_better2", "cs", "Koukni na http://fillets.sf.net/halloffame.php");
+label_text("solver_better", "en",
+[[
+You have BETTER solution!
+See http://fillets.sf.net/halloffame.php
+]])
+label_text("solver_better", "cs",
+[[
+Vymyslel jsi LEPŠÍ řešení!
+Koukni na http://fillets.sf.net/halloffame.php
+]])
 
 -- menu
 label_text("menu_sound", "en", "Sound volume");
@@ -55,6 +65,7 @@ herní nastavení - F10
 celá obrazovka - F11
 rychlejší hra - Shift
 ]])
+
 
 
 
