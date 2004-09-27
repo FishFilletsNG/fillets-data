351ca40be5bf27fa0cc2832cfe756db641181e8a
mrpaja <mrpaja>
2004-09-27 02:21:34 +0000

    swiss flag for menu

--- script/select_lang.lua
+++ script/select_lang.lua
@@ -3,4 +3,5 @@ select_addFlag("en", "images/menu/flags/en.png")
 select_addFlag("cs", "images/menu/flags/cs.png")
 select_addFlag("fr", "images/menu/flags/fr.png")
 select_addFlag("de", "images/menu/flags/de.png")
+select_addFlag("de_ch", "images/menu/flags/de_ch.png")
 
