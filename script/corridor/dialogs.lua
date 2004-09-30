b38b348b08ece0973190ad50ea665186ced2241e
mrpaja <mrpaja>
2004-09-30 13:49:19 +0000

    french translation update

--- script/corridor/dialogs.lua
+++ script/corridor/dialogs.lua
@@ -1,139 +1,185 @@
 dialog_addDialog("ch-m-rozsvit0", "en", "", "font_small", "Turn on the lights, NOW!")
 dialog_addDialog("ch-m-rozsvit0", "cs", "sound/"..codename.."/cs/ch-m-rozsvit0.ogg", "font_small", "Okamžitě zase rozsviť!")
+dialog_addDialog("ch-m-rozsvit0", "fr", "", "font_small", "Allume la lumière, MAINTENANT !")
 
 dialog_addDialog("ch-m-rozsvit1", "en", "", "font_small", "What do you think you are doing?")
 dialog_addDialog("ch-m-rozsvit1", "cs", "sound/"..codename.."/cs/ch-m-rozsvit1.ogg", "font_small", "Co děláš, rozsviť!")
+dialog_addDialog("ch-m-rozsvit1", "fr", "", "font_small", "Qu'essaies-tu de faire ?")
 
 dialog_addDialog("ch-m-rozsvit2", "en", "", "font_small", "Cut it out and turn on the lights.")
 dialog_addDialog("ch-m-rozsvit2", "cs", "sound/"..codename.."/cs/ch-m-rozsvit2.ogg", "font_small", "Jé, nech si ty srandičky! Rozsviť!")
+dialog_addDialog("ch-m-rozsvit2", "fr", "", "font_small", "Arrête ça et allume la lumière.")
 
 dialog_addDialog("ch-v-pockej0", "en", "", "font_big", "Wait, I`ll try to find the switch.")
 dialog_addDialog("ch-v-pockej0", "cs", "sound/"..codename.."/cs/ch-v-pockej0.ogg", "font_big", "Počkej, zkusím dotápat k vypínači.")
+dialog_addDialog("ch-v-pockej0", "fr", "", "font_big", "Attends, j'essaie de trouver l'interrupteur.")
 
 dialog_addDialog("ch-v-pockej1", "en", "", "font_big", "I can`t. The dang switch fell down.")
 dialog_addDialog("ch-v-pockej1", "cs", "sound/"..codename.."/cs/ch-v-pockej1.ogg", "font_big", "Nejde to, ten zatracenej vypínač spadnul.")
+dialog_addDialog("ch-v-pockej1", "fr", "", "font_big", "Je ne peux pas. l'interrupteur est tombé.")
 
 dialog_addDialog("ch-v-pockej2", "en", "", "font_big", "Turn on the lights yourself, if you`re so smart.")
 dialog_addDialog("ch-v-pockej2", "cs", "sound/"..codename.."/cs/ch-v-pockej2.ogg", "font_big", "Rozsviť si sama, když jsi tak chytrá.")
+dialog_addDialog("ch-v-pockej2", "fr", "", "font_big", "Allume la lumière toi même, si tu es si préssée.")
 
 dialog_addDialog("ch-v-halo0", "en", "", "font_big", "Where are you?")
 dialog_addDialog("ch-v-halo0", "cs", "sound/"..codename.."/cs/ch-v-halo0.ogg", "font_big", "Kde jsi?")
+dialog_addDialog("ch-v-halo0", "fr", "", "font_big", "Où est-ce que tu es ?")
 
 dialog_addDialog("ch-v-halo1", "en", "", "font_big", "Hello-o-o.")
 dialog_addDialog("ch-v-halo1", "cs", "sound/"..codename.."/cs/ch-v-halo1.ogg", "font_big", "Haló.")
+dialog_addDialog("ch-v-halo1", "fr", "", "font_big", "Hello-o-o.")
 
 dialog_addDialog("ch-v-halo2", "en", "", "font_big", "Say something.")
 dialog_addDialog("ch-v-halo2", "cs", "sound/"..codename.."/cs/ch-v-halo2.ogg", "font_big", "Ozvi se.")
+dialog_addDialog("ch-v-halo2", "fr", "", "font_big", "Dis quelque chose.")
 
 dialog_addDialog("ch-m-tady0", "en", "", "font_small", "Here.")
 dialog_addDialog("ch-m-tady0", "cs", "sound/"..codename.."/cs/ch-m-tady0.ogg", "font_small", "Tady.")
+dialog_addDialog("ch-m-tady0", "fr", "", "font_small", "Ici.")
 
 dialog_addDialog("ch-m-tady1", "en", "", "font_small", "Here I am.")
 dialog_addDialog("ch-m-tady1", "cs", "sound/"..codename.."/cs/ch-m-tady1.ogg", "font_small", "Tady jsem.")
+dialog_addDialog("ch-m-tady1", "fr", "", "font_small", "Je suis là.")
 
 dialog_addDialog("ch-m-tady2", "en", "", "font_small", "I am right over here.")
 dialog_addDialog("ch-m-tady2", "cs", "sound/"..codename.."/cs/ch-m-tady2.ogg", "font_small", "Já jsem tady.")
+dialog_addDialog("ch-m-tady2", "fr", "", "font_small", "Je suis là voyons.")
 
 dialog_addDialog("ch-m-bojim0", "en", "", "font_small", "Where are you, I am afraid.")
 dialog_addDialog("ch-m-bojim0", "cs", "sound/"..codename.."/cs/ch-m-bojim0.ogg", "font_small", "Kde jsi, mám strach...")
+dialog_addDialog("ch-m-bojim0", "fr", "", "font_small", "Où es-tu, j'ai peur.")
 
 dialog_addDialog("ch-m-bojim1", "en", "", "font_small", "I`m afraid of the dark.")
 dialog_addDialog("ch-m-bojim1", "cs", "sound/"..codename.."/cs/ch-m-bojim1.ogg", "font_small", "Já se tmy bojím.")
+dialog_addDialog("ch-m-bojim1", "fr", "", "font_small", "J'ai peur du noir.")
 
 dialog_addDialog("ch-m-bojim2", "en", "", "font_small", "Please don`t leave. It`s so dark in here.")
 dialog_addDialog("ch-m-bojim2", "cs", "sound/"..codename.."/cs/ch-m-bojim2.ogg", "font_small", "Neodcházej, je tady strašná tma...")
+dialog_addDialog("ch-m-bojim2", "fr", "", "font_small", "S'il te plaît ne pars pas. Il fait si noir ici.")
 
 dialog_addDialog("ch-v-neboj0", "en", "", "font_big", "Don`t be afraid. I`m here.")
 dialog_addDialog("ch-v-neboj0", "cs", "sound/"..codename.."/cs/ch-v-neboj0.ogg", "font_big", "Neboj, já jsem tady.")
+dialog_addDialog("ch-v-neboj0", "fr", "", "font_big", "N'aie pas peur. Je suis là.")
 
 dialog_addDialog("ch-v-neboj1", "en", "", "font_big", "Stop whining. We`ll manage.")
 dialog_addDialog("ch-v-neboj1", "cs", "sound/"..codename.."/cs/ch-v-neboj1.ogg", "font_big", "Nefňukej, to zvládnem.")
+dialog_addDialog("ch-v-neboj1", "fr", "", "font_big", "Arrête de geindre. Nous allons nous en sortir.")
 
 dialog_addDialog("ch-v-neboj2", "en", "", "font_big", "There`s nothing to be afraid of.")
 dialog_addDialog("ch-v-neboj2", "cs", "sound/"..codename.."/cs/ch-v-neboj2.ogg", "font_big", "Ničeho se neboj.")
+dialog_addDialog("ch-v-neboj2", "fr", "", "font_big", "Il n'y a pas de quoi avoir peur.")
 
 dialog_addDialog("ch-m-doufam", "en", "", "font_small", "I hope it doesn`t kick me...")
 dialog_addDialog("ch-m-doufam", "cs", "sound/"..codename.."/cs/ch-m-doufam.ogg", "font_small", "Doufám, že mě to nekopne...")
+dialog_addDialog("ch-m-doufam", "fr", "", "font_small", "J'espère qu'il ne va pas me donner un coup de pied.")
 
 dialog_addDialog("ch-m-blik0", "en", "", "font_small", "This flickering light is beginning to make my eyes hurt.")
 dialog_addDialog("ch-m-blik0", "cs", "sound/"..codename.."/cs/ch-m-blik0.ogg", "font_small", "Už mě z toho blikání bolí oči.")
+dialog_addDialog("ch-m-blik0", "fr", "", "font_small", "Cette lumière clignotante me blesse les yeux.")
 
 dialog_addDialog("ch-m-blik1", "en", "", "font_small", "I never thought I`d be so glad to see you again.")
 dialog_addDialog("ch-m-blik1", "cs", "sound/"..codename.."/cs/ch-m-blik1.ogg", "font_small", "Nikdy bych neřekla, že budu tak ráda, že tě vidím.")
+dialog_addDialog("ch-m-blik1", "fr", "", "font_small", "Je n'imaginais pas être si contente de te revoir.")
 
 dialog_addDialog("ch-m-blik2", "en", "", "font_small", "Finally, the lights are back on...")
 dialog_addDialog("ch-m-blik2", "cs", "sound/"..codename.."/cs/ch-m-blik2.ogg", "font_small", "Konečně zase světlo...")
+dialog_addDialog("ch-m-blik2", "fr", "", "font_small", "Finallement, la lumière est revenue.")
 
 dialog_addDialog("ch-v-pozor", "en", "", "font_big", "Be careful. It bites!")
 dialog_addDialog("ch-v-pozor", "cs", "sound/"..codename.."/cs/ch-v-pozor.ogg", "font_big", "Pozor, ať tě nekousne!")
+dialog_addDialog("ch-v-pozor", "fr", "", "font_big", "Attention. Ca mords.")
 
 dialog_addDialog("ch-v-robopes", "en", "", "font_big", "Look! That must be... ROBODOG!")
 dialog_addDialog("ch-v-robopes", "cs", "sound/"..codename.."/cs/ch-v-robopes.ogg", "font_big", "To je... to je snad... ROBOPES!")
+dialog_addDialog("ch-v-robopes", "fr", "", "font_big", "Regarde ! Ce doit être... ROBOCHIEN !")
 
 dialog_addDialog("ch-m-ten", "en", "", "font_small", "What? THAT robodog?!?")
 dialog_addDialog("ch-m-ten", "cs", "sound/"..codename.."/cs/ch-m-ten.ogg", "font_small", "Cože, TEN robopes?!")
+dialog_addDialog("ch-m-ten", "fr", "", "font_small", "Pardon ? CA robochien ?!?")
 
 dialog_addDialog("ch-v-zapada", "en", "", "font_big", "Well now it all makes sense.")
 dialog_addDialog("ch-v-zapada", "cs", "sound/"..codename.."/cs/ch-v-zapada.ogg", "font_big", "Tak teď už do sebe všechno jasně zapadá.")
+dialog_addDialog("ch-v-zapada", "fr", "", "font_big", "Et bien, maintenant je comprends tout.")
 
 dialog_addDialog("ch-m-odpoved0", "en", "", "font_small", "Including the Cuban missile crisis and Nostradamus` prophecies.")
 dialog_addDialog("ch-m-odpoved0", "cs", "sound/"..codename.."/cs/ch-m-odpoved0.ogg", "font_small", "Včetně karibské krize a Nostradamova proroctví.")
+dialog_addDialog("ch-m-odpoved0", "fr", "", "font_small", "Tu inclus la crise des missiles de Cuba et les prophécies de Nostradamus.")
 
 dialog_addDialog("ch-m-odpoved1", "en", "", "font_small", "Including the murder of John Lennon and the Tunguzian meteor.")
 dialog_addDialog("ch-m-odpoved1", "cs", "sound/"..codename.."/cs/ch-m-odpoved1.ogg", "font_small", "Včetně vraždy Johna Lennona a pádu tunguzského meteoritu.")
+dialog_addDialog("ch-m-odpoved1", "fr", "", "font_small", "Tu inclus le meurtre de John Lennon et la météorite de Tunguza.")
 
 dialog_addDialog("ch-m-odpoved2", "en", "", "font_small", "Including the Challenger explosion and the Battle of Bull Run.")
 dialog_addDialog("ch-m-odpoved2", "cs", "sound/"..codename.."/cs/ch-m-odpoved2.ogg", "font_small", "Včetně havárie raketoplánu Challenger a bitvy u Chlumce.")
+dialog_addDialog("ch-m-odpoved2", "fr", "", "font_small", "Tu inclus l'explosion de la navette Challenger et le mythe du King.")
 
 dialog_addDialog("ch-m-odpoved3", "en", "", "font_small", "Including the Chernobyl meltdown and the wreck of Noah`s Ark.")
 dialog_addDialog("ch-m-odpoved3", "cs", "sound/"..codename.."/cs/ch-m-odpoved3.ogg", "font_small", "Včetně havárie v Černobylu a ztroskotání Noemovy archy.")
+dialog_addDialog("ch-m-odpoved3", "fr", "", "font_small", "Tu inclus la catastrophe de TChernobyl et l'épave de l'arche de Noé.")
 
 dialog_addDialog("ch-v-smysl", "en", "", "font_big", "Now I can see the rationale behind this game.")
 dialog_addDialog("ch-v-smysl", "cs", "sound/"..codename.."/cs/ch-v-smysl.ogg", "font_big", "Už je mi také jasný smysl této hry.")
+dialog_addDialog("ch-v-smysl", "fr", "", "font_big", "Maintenant je peux voir le coté rationel de ce jeu.")
 
 dialog_addDialog("ch-m-vubec", "en", "", "font_small", "And also the meaning of life, the universe and everything.")
 dialog_addDialog("ch-m-vubec", "cs", "sound/"..codename.."/cs/ch-m-vubec.ogg", "font_small", "A taky smysl života, vesmíru a vůbec.")
+dialog_addDialog("ch-m-vubec", "fr", "", "font_small", "Et aussi le sens de la vie, de l'univers et du reste.")
 
 dialog_addDialog("ch-x-click1", "en", "sound/"..codename.."/en/ch-x-click1.ogg", "font_darkgrey", "CLICK")
 dialog_addDialog("ch-x-click1", "cs", "sound/"..codename.."/en/ch-x-click1.ogg", "font_darkgrey", "CVAK")
+dialog_addDialog("ch-x-click1", "fr", "sound/"..codename.."/en/ch-x-click1.ogg", "font_darkgrey", "CLICK")
 
 dialog_addDialog("ch-x-click2", "en", "sound/"..codename.."/en/ch-x-click2.ogg", "font_lightgrey", "CLICK")
 dialog_addDialog("ch-x-click2", "cs", "sound/"..codename.."/en/ch-x-click2.ogg", "font_lightgrey", "CVAK")
+dialog_addDialog("ch-x-click2", "fr", "sound/"..codename.."/en/ch-x-click2.ogg", "font_lightgrey", "CLICK")
 
 dialog_addDialog("ch-r-nevsimej0", "en", "", "font_robodog", "Don`t listen to them, player. They are kidding you.")
 dialog_addDialog("ch-r-nevsimej0", "cs", "sound/"..codename.."/cs/ch-r-nevsimej0.ogg", "font_robodog", "Nevšímej si jich, hráči, dělají si z tebe legraci.")
+dialog_addDialog("ch-r-nevsimej0", "fr", "", "font_robodog", "Ne les écoute pas, joueur. Ils se moquent de toi.")
 
 dialog_addDialog("ch-r-nevsimej1", "en", "", "font_robodog", "Don`t listen to them, player. They are trying to be funny.")
 dialog_addDialog("ch-r-nevsimej1", "cs", "sound/"..codename.."/cs/ch-r-nevsimej1.ogg", "font_robodog", "Nevšímej si jich, hráči, tropí si z tebe žerty.")
+dialog_addDialog("ch-r-nevsimej1", "fr", "", "font_robodog", "Ne les écoute pas, joueur. Ils essaient d'être drôles.")
 
 dialog_addDialog("ch-r-nevsimej2", "en", "", "font_robodog", "Don`t listen to them, player. They are trying to fool you.")
 dialog_addDialog("ch-r-nevsimej2", "cs", "sound/"..codename.."/cs/ch-r-nevsimej2.ogg", "font_robodog", "Nevšímej si jich, hráči, tropí si z tebe řiť.")
+dialog_addDialog("ch-r-nevsimej2", "fr", "", "font_robodog", "Ne les écoute pas, joueur. Ils vont te rendre fou.")
 
 dialog_addDialog("ch-r-hracka", "en", "", "font_robodog", "I am but a humble cybernetic toy.")
 dialog_addDialog("ch-r-hracka", "cs", "sound/"..codename.."/cs/ch-r-hracka.ogg", "font_robodog", "Jsem jen obyčejná kybernetická hračka.")
+dialog_addDialog("ch-r-hracka", "fr", "", "font_robodog", "Je ne suis qu'un simple jouet cybernetique.")
 
 dialog_addDialog("ch-r-ikdyz0", "en", "", "font_robodog", "Though originally I was to be a scooter.")
 dialog_addDialog("ch-r-ikdyz0", "cs", "sound/"..codename.."/cs/ch-r-ikdyz0.ogg", "font_robodog", "I když původně jsem měl být skútr.")
+dialog_addDialog("ch-r-ikdyz0", "fr", "", "font_robodog", "Même si au départ je devais être un scooter.")
 
 dialog_addDialog("ch-r-ikdyz1", "en", "", "font_robodog", "Though originally I was to be a lawn-mower.")
 dialog_addDialog("ch-r-ikdyz1", "cs", "sound/"..codename.."/cs/ch-r-ikdyz1.ogg", "font_robodog", "I když původně jsem měl být sekačka na trávu.")
+dialog_addDialog("ch-r-ikdyz1", "fr", "", "font_robodog", "Même s'il était prévu que je sois une tondeuse à gazon.")
 
 dialog_addDialog("ch-r-ikdyz2", "en", "", "font_robodog", "Though originally I was to be a space orbital station.")
 dialog_addDialog("ch-r-ikdyz2", "cs", "sound/"..codename.."/cs/ch-r-ikdyz2.ogg", "font_robodog", "I když původně jsem měl být kosmická orbitální stanice.")
+dialog_addDialog("ch-r-ikdyz2", "fr", "", "font_robodog", "Même s'il était prévu que je sois une station orbitale.")
 
 dialog_addDialog("ch-r-ikdyz3", "en", "", "font_robodog", "Though orginally I was to be a piece of cake.")
 dialog_addDialog("ch-r-ikdyz3", "cs", "sound/"..codename.."/cs/ch-r-ikdyz3.ogg", "font_robodog", "I když původně jsem měl být rakvička se šlehačkou.")
+dialog_addDialog("ch-r-ikdyz3", "fr", "", "font_robodog", "Même si j'aurais dû être un morceau de gateau.")
 
 dialog_addDialog("ch-r-anavic0", "en", "", "font_robodog", "And I visited Cuba only once.")
 dialog_addDialog("ch-r-anavic0", "cs", "sound/"..codename.."/cs/ch-r-anavic0.ogg", "font_robodog", "A u té karibské krize jsem se jen nachomýtl.")
+dialog_addDialog("ch-r-anavic0", "fr", "", "font_robodog", "Et je n'ai visité Cuba qu'une seule fois.")
 
 dialog_addDialog("ch-r-anavic1", "en", "", "font_robodog", "And everybody knows somebody else murdered him, they can`t prove anything.")
 dialog_addDialog("ch-r-anavic1", "cs", "sound/"..codename.."/cs/ch-r-anavic1.ogg", "font_robodog", "A z té vraždy usvědčili přece někoho jiného, mně nic nedokážou, nikdo mě neviděl.")
+dialog_addDialog("ch-r-anavic1", "fr", "", "font_robodog", "Et tout le monde sait que quelqu'un d'autre l'a tué, il ne peuvent pas tout prouver.")
 
 dialog_addDialog("ch-r-anavic2", "en", "", "font_robodog", "And who cares for few soldiers.")
 dialog_addDialog("ch-r-anavic2", "cs", "sound/"..codename.."/cs/ch-r-anavic2.ogg", "font_robodog", "A pro těch pár sedláků se nikdo nezblázní.")
+dialog_addDialog("ch-r-anavic2", "fr", "", "font_robodog", "Et qui se soucie de quelques soldats.")
 
 dialog_addDialog("ch-r-anavic3", "en", "", "font_robodog", "And they told me that that regulatory rod isn`t important.")
 dialog_addDialog("ch-r-anavic3", "cs", "sound/"..codename.."/cs/ch-r-anavic3.ogg", "font_robodog", "A navíc říkali, že ta regulační tyč není v reaktoru důležitá.")
+dialog_addDialog("ch-r-anavic3", "fr", "", "font_robodog", "Et ils m'ont dit que ce bouton de régulation n'était pas important.")
 
 
--- script/propulsion/dialogs.lua
+++ script/propulsion/dialogs.lua
@@ -1,67 +1,89 @@
 dialog_addDialog("poh-v-takhle", "en", "", "font_big", "So that`s how it is!")
 dialog_addDialog("poh-v-takhle", "cs", "sound/"..codename.."/cs/poh-v-takhle.ogg", "font_big", "Tak takhle to je!")
+dialog_addDialog("poh-v-takhle", "fr", "", "font_big", "Alors c'est comme ça!")
 
 dialog_addDialog("poh-m-tosnadne", "en", "", "font_small", "Oh, no!")
 dialog_addDialog("poh-m-tosnadne", "cs", "sound/"..codename.."/cs/poh-m-tosnadne.ogg", "font_small", "To snad ne!")
+dialog_addDialog("poh-m-tosnadne", "fr", "", "font_small", "Oh, non!")
 
 dialog_addDialog("poh-v-biosila", "en", "", "font_big", "Biological power. We should have thought about it earlier. We had every hint.")
 dialog_addDialog("poh-v-biosila", "cs", "sound/"..codename.."/cs/poh-v-biosila.ogg", "font_big", "Biologická síla. To nás mělo napadnout dříve. Všechno k tomu směřovalo.")
+dialog_addDialog("poh-v-biosila", "fr", "", "font_big", "Energie biologique. Nous aurions dû y penser avant. Nous avions tous les indices.")
 
 dialog_addDialog("poh-m-reaktor", "en", "", "font_small", "And what was that nuclear power plant for?")
 dialog_addDialog("poh-m-reaktor", "cs", "sound/"..codename.."/cs/poh-m-reaktor.ogg", "font_small", "A k čemu byl ten reaktor?")
+dialog_addDialog("poh-m-reaktor", "fr", "", "font_small", "Et à quoi sert cette centrale nucléaire ?")
 
 dialog_addDialog("poh-v-automat", "en", "", "font_big", "Can`t you see? It powers that Coca-cola vending machine over there.")
 dialog_addDialog("poh-v-automat", "cs", "sound/"..codename.."/cs/poh-v-automat.ogg", "font_big", "Nevidíš? Napájí támhle ten automat na Coca-Colu.")
+dialog_addDialog("poh-v-automat", "fr", "", "font_big", "Tu ne vois pas ? Elle fournit le courant pour le distributeur de boissons là bas.")
 
 dialog_addDialog("poh-m-motor", "en", "", "font_small", "And what about that motor?")
 dialog_addDialog("poh-m-motor", "cs", "sound/"..codename.."/cs/poh-m-motor.ogg", "font_small", "A co ten motor?")
+dialog_addDialog("poh-m-motor", "fr", "", "font_small", "Et ce moteur ?")
 
 dialog_addDialog("poh-v-tocit", "en", "", "font_big", "It makes the level run around the display.")
 dialog_addDialog("poh-v-tocit", "cs", "sound/"..codename.."/cs/poh-v-tocit.ogg", "font_big", "Ten točí místností dokola po obrazovce.")
+dialog_addDialog("poh-v-tocit", "fr", "", "font_big", "Il fait tourner le niveau à l'écran.")
 
 dialog_addDialog("poh-v-neuveri", "en", "", "font_big", "Nobody is going to believe us.")
 dialog_addDialog("poh-v-neuveri", "cs", "sound/"..codename.."/cs/poh-v-neuveri.ogg", "font_big", "Tak tohle nám nikdo neuvěří.")
+dialog_addDialog("poh-v-neuveri", "fr", "", "font_big", "Personne ne va nous croire.")
 
 dialog_addDialog("poh-m-projekt", "en", "", "font_small", "Maybe yes. FDTO works on a project to make use of bioenergy.")
 dialog_addDialog("poh-m-projekt", "cs", "sound/"..codename.."/cs/poh-m-projekt.ogg", "font_small", "Možná ano. FDTO má rozpracovaný projekt na využití bioenergie.")
+dialog_addDialog("poh-m-projekt", "fr", "", "font_small", "Peut-être que si. FDTO travaille sur un projet qui permettra l'utilisation de la bio-énergie.")
 
 dialog_addDialog("poh-v-zarizeni", "en", "", "font_big", "We have to get out this device.")
 dialog_addDialog("poh-v-zarizeni", "cs", "sound/"..codename.."/cs/poh-v-zarizeni.ogg", "font_big", "Musíme dostat ven kus toho zařízení.")
+dialog_addDialog("poh-v-zarizeni", "fr", "", "font_big", "Nous devons emmener cet objet.")
 
 dialog_addDialog("poh-m-sest", "en", "", "font_small", "If I can see it correctly, it is composed of six parts.")
 dialog_addDialog("poh-m-sest", "cs", "sound/"..codename.."/cs/poh-m-sest.ogg", "font_small", "Pokud dobře vidím, skládá se ze šesti částí.")
+dialog_addDialog("poh-m-sest", "fr", "", "font_small", "Si je ne me trompe pas, il se compose de six parties.")
 
 dialog_addDialog("poh-v-klec", "en", "", "font_big", "The cage in the middle is probably the most important.")
 dialog_addDialog("poh-v-klec", "cs", "sound/"..codename.."/cs/poh-v-klec.ogg", "font_big", "Nejdůležitější asi bude ta prostřední klec.")
+dialog_addDialog("poh-v-klec", "fr", "", "font_big", "La cage du milieu est sans doute la plus importante.")
 
 dialog_addDialog("poh-m-dobre", "en", "", "font_small", "Okay, we shall take this one.")
 dialog_addDialog("poh-m-dobre", "cs", "sound/"..codename.."/cs/poh-m-dobre.ogg", "font_small", "Dobře, tu vezmeme.")
+dialog_addDialog("poh-m-dobre", "fr", "", "font_small", "Okay, emportons celle-ci.")
 
 dialog_addDialog("poh-v-forma", "en", "", "font_big", "Look, there is some strange life form, over there. Won`t we try to contact them?")
 dialog_addDialog("poh-v-forma", "cs", "sound/"..codename.."/cs/poh-v-forma.ogg", "font_big", "Hele, támhleto je nějaká zvláštní forma života. Nezkusíme navázat kontakt?")
+dialog_addDialog("poh-v-forma", "fr", "", "font_big", "Regarde, il y a des formes de vie étranges plus loin. Nous devons essayer de les contacter, non ?")
 
 dialog_addDialog("poh-m-princip", "en", "", "font_small", "Our mission was clear - principles and construction of the interstellar propulsion. Not a single word about contact.")
 dialog_addDialog("poh-m-princip", "cs", "sound/"..codename.."/cs/poh-m-princip.ogg", "font_small", "Náš úkol zněl jasně - principy a konstrukce mezihvězdných pohonů. O kontaktu ani slovo.")
+dialog_addDialog("poh-m-princip", "fr", "", "font_small", "Notre mission est claire - principes et tout ce qui concerne la propulsion interstélaire. Pas un mot à propos de contact.")
 
 dialog_addDialog("poh-v-pomoct", "en", "", "font_big", "Well. But they could at least help us.")
 dialog_addDialog("poh-v-pomoct", "cs", "sound/"..codename.."/cs/poh-v-pomoct.ogg", "font_big", "Tak dobře. Ale mohli by nám aspoň pomoct.")
+dialog_addDialog("poh-v-pomoct", "fr", "", "font_big", "Bien. Mais il pourrait au moins nous aider.")
 
 dialog_addDialog("poh-m-dobryden0", "en", "", "font_small", "Good afternoon, excuse me, please, couldn`t I loot your drive?")
 dialog_addDialog("poh-m-dobryden0", "cs", "sound/"..codename.."/cs/poh-m-dobryden0.ogg", "font_small", "Dobrý den, nezlobte se, nepomohl byste nám vybrakovat váš motor?")
+dialog_addDialog("poh-m-dobryden0", "fr", "", "font_small", "Bonjour, excusez moi, s'il vous plaît, ne pourrais-je pas détruire votre vaisseau.")
 
 dialog_addDialog("poh-m-dobryden1", "en", "", "font_small", "Hmm, good afternoon sir, could I ask you to help us to dismantle your propulsion unit, please?")
 dialog_addDialog("poh-m-dobryden1", "cs", "sound/"..codename.."/cs/poh-m-dobryden1.ogg", "font_small", "Ehm, dobrý den, mohla bych vás požádat, abyste nám pomohl rozbít vaši pohonnou jednotku?")
+dialog_addDialog("poh-m-dobryden1", "fr", "", "font_small", "Hmm, bonjour monsieur, Puis-je vous demander de nous aider à démonter votre moteur, s'il vous plaît ?")
 
 dialog_addDialog("poh-v-ukol", "en", "", "font_big", "Our goal in this level is to get out the cage with that animal.")
 dialog_addDialog("poh-v-ukol", "cs", "sound/"..codename.."/cs/poh-v-ukol.ogg", "font_big", "Naším úkolem v této místnosti je dostat ven tu klec s živočichem.")
+dialog_addDialog("poh-v-ukol", "fr", "", "font_big", "Notre but dans ce niveau est d'emporter la cage avec cet animal.")
 
 dialog_addDialog("poh-m-pohadali", "en", "", "font_small", "I have a feeling there was an argument between these two.")
 dialog_addDialog("poh-m-pohadali", "cs", "sound/"..codename.."/cs/poh-m-pohadali.ogg", "font_small", "Mám pocit, že ti dva se nějak pohádali.")
+dialog_addDialog("poh-m-pohadali", "fr", "", "font_small", "J'ai le sentiment que cet argument ne va pas nous rapprochez d'eux.")
 
 dialog_addDialog("poh-v-setkani", "en", "", "font_big", "I never imagined that meeting aliens would look this way.")
 dialog_addDialog("poh-v-setkani", "cs", "sound/"..codename.."/cs/poh-v-setkani.ogg", "font_big", "Tak jsem si setkání s mimozemšťany nepředstavoval.")
+dialog_addDialog("poh-v-setkani", "fr", "", "font_big", "Je n'ai jamais imaginé que ce rassemblement d'extraterrestres sera d'accord.")
 
 dialog_addDialog("poh-m-sestra", "en", "", "font_small", "And what have you expected? That they will kidnap your sister?")
 dialog_addDialog("poh-m-sestra", "cs", "sound/"..codename.."/cs/poh-m-sestra.ogg", "font_small", "A cos čekal? Že unesou tvou sestru?")
+dialog_addDialog("poh-m-sestra", "fr", "", "font_small", "Et qu'espèrais-tu ? Qu'ils vont kidnaper ta soeur ?")
 
 
