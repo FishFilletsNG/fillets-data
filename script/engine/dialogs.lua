7a15966eeebf19c533f3769fe3cb9b911aaaf3b8
mrpaja <mrpaja>
2004-10-01 18:00:12 +0000

    french translation update

--- script/engine/dialogs.lua
+++ script/engine/dialogs.lua
@@ -1,107 +1,133 @@
 dialog_addDialog("mot-m-info", "en", "", "font_small", "I think we can gain some information about the interstellar propulsion here.")
 dialog_addDialog("mot-m-info", "cs", "sound/"..codename.."/cs/mot-m-info.ogg", "font_small", "Myslím, že bychom tu mohli získat nějaké informace o mezihvězdném pohonu.")
+dialog_addDialog("mot-m-info", "fr", "", "font_small", "Je pense que nous pouvons glaner quelques informations à propos de la propulsion interstellaire ici.")
 dialog_addDialog("mot-m-info", "de", "", "font_small", "Ich glaube, hier können wir ein paar Informationen über den intergalaktischen Antrieb bekommen.")
 
 dialog_addDialog("mot-v-konvencni", "en", "", "font_big", "This looks more like a conventional propulsion for the landing craft.")
 dialog_addDialog("mot-v-konvencni", "cs", "sound/"..codename.."/cs/mot-v-konvencni.ogg", "font_big", "Tohle vypadá spíš na konvenční pohon pro výsadková vozidla.")
+dialog_addDialog("mot-v-konvencni", "fr", "", "font_big", "Ceci ressemble plus à la propulsion conventionnelle d'un véhicule terrestre.")
 dialog_addDialog("mot-v-konvencni", "de", "", "font_big", "Das sieht eher wie ein gewöhnlicher Antrieb für das Landungsschiff aus.")
 
 dialog_addDialog("mot-m-tak", "en", "", "font_small", "So. We found the drive. We have achieved one of the objectives of our mission.")
 dialog_addDialog("mot-m-tak", "cs", "sound/"..codename.."/cs/mot-m-tak.ogg", "font_small", "Tak. Našli jsme motor. Mohli bychom prohlásit jeden z našich úkolů za splněný.")
+dialog_addDialog("mot-m-tak", "fr", "", "font_small", "Enfin. Nous avons trouvé l'accès. Nous avons atteint un des objectifs de notre mission.")
 dialog_addDialog("mot-m-tak", "de", "", "font_small", "So. Wir haben den Antrieb gefunden. Wir haben ein Ziel unserer Aufgabe erreicht.")
 
 dialog_addDialog("mot-v-zavery", "en", "", "font_big", "Don`t be hasty. We didn`t search the whole wreck yet.")
 dialog_addDialog("mot-v-zavery", "cs", "sound/"..codename.."/cs/mot-v-zavery.ogg", "font_big", "Nedělej ukvapené závěry. Ještě jsme neprohlédli celý vrak.")
+dialog_addDialog("mot-v-zavery", "fr", "", "font_big", "Pas de précipitation. Nous n'avons pas encore fouillé toute l'épave.")
 dialog_addDialog("mot-v-zavery", "de", "", "font_big", "Nicht so voreilig. Wir haben noch nicht das ganze Wrack abgesucht.")
 
 dialog_addDialog("mot-m-akce0", "en", "", "font_small", "I am sorry that none of these technical marvels around us work.")
 dialog_addDialog("mot-m-akce0", "cs", "sound/"..codename.."/cs/mot-m-akce0.ogg", "font_small", "To je škoda, že všechna ta technika, kterou tu vidíme, už nefunguje.")
+dialog_addDialog("mot-m-akce0", "fr", "", "font_small", "Je suis déçue qu'aucune de ces merveilles technologiques ne marche.")
 dialog_addDialog("mot-m-akce0", "de", "", "font_small", "Schade, dass keins der technischen Wunderwerke um uns herum funktioniert.")
 
 dialog_addDialog("mot-m-akce1", "en", "", "font_small", "I`d like to see some of these extraterrestrial gizmos in action.")
 dialog_addDialog("mot-m-akce1", "cs", "sound/"..codename.."/cs/mot-m-akce1.ogg", "font_small", "Já bych tak chtěla vidět trochu té mimozemské techniky v akci.")
+dialog_addDialog("mot-m-akce1", "fr", "", "font_small", "J'aimerai voir quelques uns de ces gadgets extraterrestres en fonctionnement.")
 dialog_addDialog("mot-m-akce1", "de", "", "font_small", "Ich will mal eins dieser außerirdischen Dinger in Aktion erleben!.")
 dialog_addDialog("mot-m-akce1", "de_CH", "", "font_small", "Ich will mal eins dieser ausserirdischen Dinger in Aktion erleben!.")
 
 dialog_addDialog("mot-m-akce2", "en", "", "font_small", "I wonder if this motor could work under water.")
 dialog_addDialog("mot-m-akce2", "cs", "sound/"..codename.."/cs/mot-m-akce2.ogg", "font_small", "To by mě zajímalo, jestli by tenhle motor fungoval i pod vodou.")
+dialog_addDialog("mot-m-akce2", "fr", "", "font_small", "Je me demande si ce moteur peut tourner sous l'eau.")
 dialog_addDialog("mot-m-akce2", "de", "", "font_small", "Ich frage mich, ob dieser Motor unter Wasser funktioniert.")
 
 dialog_addDialog("mot-v-funkce0", "en", "", "font_big", "Maybe it`s better for us if it doesn`t work.")
 dialog_addDialog("mot-v-funkce0", "cs", "sound/"..codename.."/cs/mot-v-funkce0.ogg", "font_big", "Možná je pro nás lepší, že už tu nic nepracuje.")
+dialog_addDialog("mot-v-funkce0", "fr", "", "font_big", "Peut-être qu'il vaut mieux pour nous qu'il ne puisse pas.")
 dialog_addDialog("mot-v-funkce0", "de", "", "font_big", "Vielleicht ist es für uns besser, wenn er es nicht tut.")
 
 dialog_addDialog("mot-v-funkce1", "en", "", "font_big", "I am rather glad we cannot turn anything on here.")
 dialog_addDialog("mot-v-funkce1", "cs", "sound/"..codename.."/cs/mot-v-funkce1.ogg", "font_big", "Já jsem spíš rád, že tu nemůžeme nic zapnout.")
+dialog_addDialog("mot-v-funkce1", "fr", "", "font_big", "Je me réjouis que nous ne pouvons rien tourner ici.")
 dialog_addDialog("mot-v-funkce1", "de", "", "font_big", "Ich bin ziemlich froh, dass wir hier nichts anschalten können.")
 
 dialog_addDialog("mot-v-funkce2", "en", "", "font_big", "We should be happy that we didn`t switch anything on yet.")
 dialog_addDialog("mot-v-funkce2", "cs", "sound/"..codename.."/cs/mot-v-funkce2.ogg", "font_big", "Naše štěstí je, že tu nemůžeme nic zapnout.")
+dialog_addDialog("mot-v-funkce2", "fr", "", "font_big", "Nous devrions être content d'être incapable d'allumer quoique ce soit.")
 dialog_addDialog("mot-v-funkce2", "de", "", "font_big", "Wir sollten froh sein, dass wir hier noch nichts angeschaltet haben.")
 
 dialog_addDialog("mot-v-klic", "en", "", "font_big", "Careful with that wrench.")
 dialog_addDialog("mot-v-klic", "cs", "sound/"..codename.."/cs/mot-v-klic.ogg", "font_big", "Opatrně s tím klíčem.")
+dialog_addDialog("mot-v-klic", "fr", "", "font_big", "Attention avec cette clé.")
 dialog_addDialog("mot-v-klic", "de", "", "font_big", "Vorsicht mit dem Schraubenschlüssel.")
 
 dialog_addDialog("mot-m-ublizit", "en", "", "font_small", "I can`t harm anything here.")
 dialog_addDialog("mot-m-ublizit", "cs", "sound/"..codename.."/cs/mot-m-ublizit.ogg", "font_small", "Tady přece nemůžu ničemu ublížit.")
+dialog_addDialog("mot-m-ublizit", "fr", "", "font_small", "Je ne peux blesser personne ici.")
 dialog_addDialog("mot-m-ublizit", "de", "", "font_small", "Hier kann ich nichts kaputt machen.")
 
 dialog_addDialog("mot-m-zvuky0", "en", "", "font_small", "What have you done? Turn off that roar!")
 dialog_addDialog("mot-m-zvuky0", "cs", "sound/"..codename.."/cs/mot-m-zvuky0.ogg", "font_small", "Co jsi to udělal? Vypni ten randál!")
+dialog_addDialog("mot-m-zvuky0", "fr", "", "font_small", "Qu'as-tu fait ? Eteint ce fracas.")
 dialog_addDialog("mot-m-zvuky0", "de", "", "font_small", "Was hast du gemacht? Mach das Getöse aus!")
 
 dialog_addDialog("mot-m-zvuky1", "en", "", "font_small", "This is terrible! Turn it off before it`s too late!")
 dialog_addDialog("mot-m-zvuky1", "cs", "sound/"..codename.."/cs/mot-m-zvuky1.ogg", "font_small", "To jsou strašné zvuky! Vypni to dřív, než bude pozdě.")
+dialog_addDialog("mot-m-zvuky1", "fr", "", "font_small", "C'est horrible ! Eteint le avant qu'il ne soit trop tard !")
 dialog_addDialog("mot-m-zvuky1", "de", "", "font_small", "Das ist schrecklich! Mach das aus bevor es zu spät ist!")
 
 dialog_addDialog("mot-v-nemuzu0", "en", "", "font_big", "I can`t! I can`t take it out!")
 dialog_addDialog("mot-v-nemuzu0", "cs", "sound/"..codename.."/cs/mot-v-nemuzu0.ogg", "font_big", "Nemůžu! Jak to mám vytáhnout?!")
+dialog_addDialog("mot-v-nemuzu0", "fr", "", "font_big", "Je ne peux pas ! Impossible de la retirer !")
 dialog_addDialog("mot-v-nemuzu0", "de", "", "font_big", "Ich kann nicht! Ich krieg`s nicht aus!")
 
 dialog_addDialog("mot-v-nemuzu1", "en", "", "font_big", "I don`t know how! I can`t take it out!")
 dialog_addDialog("mot-v-nemuzu1", "cs", "sound/"..codename.."/cs/mot-v-nemuzu1.ogg", "font_big", "Nevím jak! Nedá se to vyndat!")
+dialog_addDialog("mot-v-nemuzu1", "fr", "", "font_big", "Je ne sais pas comment ! Impossible de la retirer !")
 dialog_addDialog("mot-v-nemuzu1", "de", "", "font_big", "Ich weiß nicht wie! Ich krieg`s nicht aus!")
 dialog_addDialog("mot-v-nemuzu1", "de_CH", "", "font_big", "Ich weiss nicht wie! Ich krieg`s nicht aus!")
 
 dialog_addDialog("mot-m-mayday", "en", "", "font_small", "Mayday! Mayday!")
 dialog_addDialog("mot-m-mayday", "cs", "sound/"..codename.."/cs/mot-m-mayday.ogg", "font_small", "Mayday! Mayday!")
+dialog_addDialog("mot-m-mayday", "fr", "", "font_small", "Mayday! Mayday!")
 dialog_addDialog("mot-m-mayday", "de", "", "font_small", "Mayday! Mayday!")
 
 dialog_addDialog("mot-v-konecne0", "en", "", "font_big", "Finally.")
 dialog_addDialog("mot-v-konecne0", "cs", "sound/"..codename.."/cs/mot-v-konecne0.ogg", "font_big", "Konečně.")
+dialog_addDialog("mot-v-konecne0", "fr", "", "font_big", "Finallement.")
 dialog_addDialog("mot-v-konecne0", "de", "", "font_big", "Endlich.")
 
 dialog_addDialog("mot-v-konecne1", "en", "", "font_big", "What a relief.")
 dialog_addDialog("mot-v-konecne1", "cs", "sound/"..codename.."/cs/mot-v-konecne1.ogg", "font_big", "To se mi ulevilo")
+dialog_addDialog("mot-v-konecne1", "fr", "", "font_big", "Quel soulagement.")
 dialog_addDialog("mot-v-konecne1", "de", "", "font_big", "Was für eine Erleichterung.")
 
 dialog_addDialog("mot-v-zvuky0", "en", "", "font_big", "What are you doing? Where are we going?")
 dialog_addDialog("mot-v-zvuky0", "cs", "sound/"..codename.."/cs/mot-v-zvuky0.ogg", "font_big", "Co to děláš? Kam to jedeme?")
+dialog_addDialog("mot-v-zvuky0", "fr", "", "font_big", "Qu'est-ce que tu fais ? Où est-ce que l'on va ?")
 dialog_addDialog("mot-v-zvuky0", "de", "", "font_big", "Was machst du? Wohin gehen wir?")
 
 dialog_addDialog("mot-v-zvuky1", "en", "", "font_big", "What have you activated? Where is it taking us?")
 dialog_addDialog("mot-v-zvuky1", "cs", "sound/"..codename.."/cs/mot-v-zvuky1.ogg", "font_big", "Co jsi to aktivovala? Kam nás to veze?")
+dialog_addDialog("mot-v-zvuky1", "fr", "", "font_big", "Qu'est-ce que tu as activé ? Où est-ce qu'il nous emmène ?")
 dialog_addDialog("mot-v-zvuky1", "de", "", "font_big", "Was hast du angeschaltet? Wohin bringt es uns?")
 
 dialog_addDialog("mot-m-nemuzu0", "en", "", "font_small", "How can I turn it off?!")
 dialog_addDialog("mot-m-nemuzu0", "cs", "sound/"..codename.."/cs/mot-m-nemuzu0.ogg", "font_small", "Kde se to vypíná?!")
+dialog_addDialog("mot-m-nemuzu0", "fr", "", "font_small", "Comment puis-je l'arrêter ?!")
 dialog_addDialog("mot-m-nemuzu0", "de", "", "font_small", "Wie kann ich es ausschalten?!")
 
 dialog_addDialog("mot-m-nemuzu1", "en", "", "font_small", "I can`t turn it off!")
 dialog_addDialog("mot-m-nemuzu1", "cs", "sound/"..codename.."/cs/mot-m-nemuzu1.ogg", "font_small", "Nedá se to vypnout!")
+dialog_addDialog("mot-m-nemuzu1", "fr", "", "font_small", "Je ne peux pas l'arrêter !")
 dialog_addDialog("mot-m-nemuzu1", "de", "", "font_small", "Ich kann es nicht ausschalten!")
 
 dialog_addDialog("mot-m-konecne0", "en", "", "font_small", "Thanks.")
 dialog_addDialog("mot-m-konecne0", "cs", "sound/"..codename.."/cs/mot-m-konecne0.ogg", "font_small", "Díky.")
+dialog_addDialog("mot-m-konecne0", "fr", "", "font_small", "Merci.")
 dialog_addDialog("mot-m-konecne0", "de", "", "font_small", "Danke.")
 
 dialog_addDialog("mot-m-konecne1", "en", "", "font_small", "Finally.")
 dialog_addDialog("mot-m-konecne1", "cs", "sound/"..codename.."/cs/mot-m-konecne1.ogg", "font_small", "Konečně.")
+dialog_addDialog("mot-m-konecne1", "fr", "", "font_small", "Finallemment.")
 dialog_addDialog("mot-m-konecne1", "de", "", "font_small", "Endlich.")
 
 dialog_addDialog("mot-v-znovu0", "en", "", "font_big", "I am only afraid we`ll have to turn it on again.")
 dialog_addDialog("mot-v-znovu0", "cs", "sound/"..codename.."/cs/mot-v-znovu0.ogg", "font_big", "Bojím se, že to budeme muset zapnout znovu.")
+dialog_addDialog("mot-v-znovu0", "fr", "", "font_big", "J'ai seulement peur que nous le rallumions.")
 dialog_addDialog("mot-v-znovu0", "de", "", "font_big", "Ich fürchte nur, wir müssen es nochmal anschalten.")
 
 dialog_addDialog("mot-x-motor", "en", "sound/"..codename.."/en/mot-x-motor.ogg")
--- script/steel/dialogs.lua
+++ script/steel/dialogs.lua
@@ -1,9 +1,11 @@
 dialog_addDialog("steel-m-0", "en", "", "font_small", "It`s so quiet here...")
 dialog_addDialog("steel-m-0", "cs", "sound/"..codename.."/cs/steel-m-0.ogg", "font_small", "Tady je ale ticho...")
+dialog_addDialog("steel-m-0", "fr", "", "font_small", "C'est si calme ici...")
 dialog_addDialog("steel-m-0", "de", "", "font_small", "Hier ist es so ruhig...")
 
 dialog_addDialog("steel-m-1", "en", "", "font_small", "Boy is it quiet...")
 dialog_addDialog("steel-m-1", "cs", "sound/"..codename.."/cs/steel-m-1.ogg", "font_small", "Tady je takový ticho...")
+dialog_addDialog("steel-m-1", "fr", "", "font_small", "Garçon, ce silence est-il...")
 dialog_addDialog("steel-m-1", "de", "", "font_small", "Boah, ist das ruhig...")
 
 dialog_addDialog("steel-x-redalert", "en", "sound/"..codename.."/en/steel-x-redalert.ogg")
