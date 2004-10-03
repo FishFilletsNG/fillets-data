c90bd34ecc4b39a0fff29836b11cc05643154013
ronnystandtke <ronnystandtke>
2004-10-03 14:19:36 +0000

    added German translation

--- script/barrel/demo_poster.lua
+++ script/barrel/demo_poster.lua
@@ -1,14 +1,21 @@
 dialog_addDialog("dlg-x-poster1", "en", "", "font_poster", "Greetings from the rubbish heap to our beloved boss.")
 dialog_addDialog("dlg-x-poster1", "cs", "", "font_poster", "")
+dialog_addDialog("dlg-x-poster1", "de", "", "font_poster", "Viele Grüße vom Müllhaufen an unseren geliebten Chef.")
+dialog_addDialog("dlg-x-poster1", "de_CH", "", "font_poster", "Viele Grüsse vom Müllhaufen an unseren geliebten Chef.")
 
 dialog_addDialog("dlg-x-poster2", "en", "", "font_poster", "Yours sincerely")
 dialog_addDialog("dlg-x-poster2", "cs", "", "font_poster", "")
+dialog_addDialog("dlg-x-poster2", "de", "", "font_poster", "Mit freundlichen Grüßen")
+dialog_addDialog("dlg-x-poster2", "de_CH", "", "font_poster", "Mit freundlichen Grüssen")
 
 dialog_addDialog("dlg-x-poster3", "en", "", "font_poster", "Agents")
 dialog_addDialog("dlg-x-poster3", "cs", "", "font_poster", "")
+dialog_addDialog("dlg-x-poster3", "de", "", "font_poster", "Agenten")
 
 dialog_addDialog("dlg-x-poster4", "en", "", "font_poster", "PS: It's great to be here. We irradiate ourselves and swim in the sea a lot. We have many new friends.")
 dialog_addDialog("dlg-x-poster4", "cs", "", "font_poster", "")
+dialog_addDialog("dlg-x-poster4", "de", "", "font_poster", "PS: Hier ist es großartig. Wir strahlen übers ganze Gesicht und schwimmen oft im Meer. Wir haben viele neue Freunde.")
+dialog_addDialog("dlg-x-poster4", "de_CH", "", "font_poster", "PS: Hier ist es grossartig. Wir strahlen übers ganze Gesicht und schwimmen oft im Meer. Wir haben viele neue Freunde.")
 
 -- -----------------------------------------------------------------
 file_include("script/share/prog_demo.lua")
--- script/barrel/dialogs.lua
+++ script/barrel/dialogs.lua
@@ -1,126 +1,159 @@
 dialog_addDialog("bar-v-videt0", "en", "", "font_big", "Somebody from Greenpeace should see this.")
 dialog_addDialog("bar-v-videt0", "cs", "sound/"..codename.."/cs/bar-v-videt0.ogg", "font_big", "To by měli vidět lidi z Greenpeace.")
 dialog_addDialog("bar-v-videt0", "fr", "", "font_big", "Un membre de Greenpeace devrait voir ça.")
+dialog_addDialog("bar-v-videt0", "de", "", "font_big", "Das sollte mal jemand von Greenpeace sehen.")
 
 dialog_addDialog("bar-m-videt1", "en", "", "font_small", "Somebody from Three Miles Island should see this.")
 dialog_addDialog("bar-m-videt1", "cs", "sound/"..codename.."/cs/bar-m-videt1.ogg", "font_small", "To by měli vidět lidi z Temelína.")
 dialog_addDialog("bar-m-videt1", "fr", "", "font_small", "Les habitants des îles alentours devraient voir ça.")
+dialog_addDialog("bar-m-videt1", "de", "", "font_small", "Das sollte mal jemand von den Castor-Leuten sehen.")
 
 dialog_addDialog("bar-v-co", "en", "", "font_big", "What are we supposed to do with it?")
 dialog_addDialog("bar-v-co", "cs", "sound/"..codename.."/cs/bar-v-co.ogg", "font_big", "Ale co s tím budeme dělat?")
 dialog_addDialog("bar-v-co", "fr", "", "font_big", "Qu'est-ce que nous allons faire ?")
+dialog_addDialog("bar-v-co", "de", "", "font_big", "Was sollen wir damit tun?")
 
 dialog_addDialog("bar-m-pobit", "en", "", "font_small", "We have to exterminate all of them. Such abominations have no right to breath the same air... er, water, as we do.")
 dialog_addDialog("bar-m-pobit", "cs", "sound/"..codename.."/cs/bar-m-pobit.ogg", "font_small", "Musíme je všechny pobít. Takové stvůry nemají právo dýchat stejný vzduch.... é, vodu, jako my.")
 dialog_addDialog("bar-m-pobit", "fr", "", "font_small", "Nous devons les exterminer toutes. De telles abominations n'ont pas à respirer le même air..., la même eau que nous.")
+dialog_addDialog("bar-m-pobit", "de", "", "font_small", "Wir müssen alle von denen auslöschen. Solche Monster haben nicht das Recht, die gleiche Luft... äh, das gleiche Wasser wie wir zu atmen.")
 
 dialog_addDialog("bar-v-priciny", "en", "", "font_big", "I think we have to eliminate the cause and not the results.")
 dialog_addDialog("bar-v-priciny", "cs", "sound/"..codename.."/cs/bar-v-priciny.ogg", "font_big", "Já si myslím, že musíme odstraňovat příčiny a ne následky.")
 dialog_addDialog("bar-v-priciny", "fr", "", "font_big", "Je pense que nous devrions éliminer la cause et non les conséquences.")
+dialog_addDialog("bar-v-priciny", "de", "", "font_big", "Ich denke, wir müssen die Ursache beseitigen, nicht die Folgen.")
 
 dialog_addDialog("bar-m-no", "en", "", "font_small", "Well, you may be right.")
 dialog_addDialog("bar-m-no", "cs", "sound/"..codename.."/cs/bar-m-no.ogg", "font_small", "No, možná máš pravdu.")
 dialog_addDialog("bar-m-no", "fr", "", "font_small", "Et bien, tu dois avoir raison.")
+dialog_addDialog("bar-m-no", "de", "", "font_small", "Na gut, du hast ja Recht.")
 
 dialog_addDialog("bar-v-sud", "en", "", "font_big", "And the cause is doubtless this giant barrel. We have to get it out somehow.")
 dialog_addDialog("bar-v-sud", "cs", "sound/"..codename.."/cs/bar-v-sud.ogg", "font_big", "A příčina je nepochybně tento olbřímí sud. Musíme ho nějak dostat ven.")
 dialog_addDialog("bar-v-sud", "fr", "", "font_big", "Et la cause est sans aucun doute ce tonneau géant. Nous devons le faire sortir d'ici.")
+dialog_addDialog("bar-v-sud", "de", "", "font_big", "Und die Ursache ist zweifellos dieser riesige Behälter. Wir müssen ihn hier irgendwie rausbekommen.")
 
 dialog_addDialog("bar-m-panb", "en", "", "font_small", "Yes. Let`s get to work. We can drop it on the Mr. B.`s front yard afterwards.")
 dialog_addDialog("bar-m-panb", "cs", "sound/"..codename.."/cs/bar-m-panb.ogg", "font_small", "Ano. Tak se dejme do práce. Ať ho můžeme hodit panu B. na dvorek.")
 dialog_addDialog("bar-m-panb", "fr", "", "font_small", "Oui. Allons-y. Nous pouvons le jeter sur la pelouse de M. B. après tout.")
+dialog_addDialog("bar-m-panb", "de", "", "font_small", "Ja. An die Arbeit. Wir können ihn dann auf den Hof des Herrn B. werfen.")
 
 dialog_addDialog("bar-m-rada", "en", "", "font_small", "I think I grow to like them.")
 dialog_addDialog("bar-m-rada", "cs", "sound/"..codename.."/cs/bar-m-rada.ogg", "font_small", "Vlastně je začínám mít docela ráda.")
 dialog_addDialog("bar-m-rada", "fr", "", "font_small", "Je commence à aimer ça.")
+dialog_addDialog("bar-m-rada", "de", "", "font_small", "Ich fange an, sie zu mögen.")
 
 dialog_addDialog("bar-v-kdyby0", "en", "", "font_big", "If only they wouldn`t stand in our way so much.")
 dialog_addDialog("bar-v-kdyby0", "cs", "sound/"..codename.."/cs/bar-v-kdyby0.ogg", "font_big", "Jen kdyby se nám tu tolik nepletli.")
-dialog_addDialog("bar-v-kdyby0", "fr", "", "font_big", "Si seulement ils n'étaient pas tant sur notre chemin."à
+dialog_addDialog("bar-v-kdyby0", "fr", "", "font_big", "Si seulement ils n'étaient pas tant sur notre chemin.")
+dialog_addDialog("bar-v-kdyby0", "de", "", "font_big", "Wenn sie nur nicht so im Weg sein würden.")
 
 dialog_addDialog("bar-v-kdyby1", "en", "", "font_big", "If only they were where we need them.")
 dialog_addDialog("bar-v-kdyby1", "cs", "sound/"..codename.."/cs/bar-v-kdyby1.ogg", "font_big", "Jen kdyby byli tam, kde je potřebujeme.")
 dialog_addDialog("bar-v-kdyby1", "fr", "", "font_big", "Si seulement ils étaient où nous avons besoin d'eux.")
+dialog_addDialog("bar-v-kdyby1", "de", "", "font_big", "Wenn sie nur da wären, wo wir sie brauchen.")
 
 dialog_addDialog("bar-v-sbirka", "en", "", "font_big", "Such a lovely collection of monstrosities. I`ll be sorry to leave.")
 dialog_addDialog("bar-v-sbirka", "cs", "sound/"..codename.."/cs/bar-v-sbirka.ogg", "font_big", "Ta milá sbírka zrůdiček. Bude mi líto odsud odejít.")
 dialog_addDialog("bar-v-sbirka", "fr", "", "font_big", "Une collection de monstruosité si tendre. Ca me désole de les abandonner.")
+dialog_addDialog("bar-v-sbirka", "de", "", "font_big", "So eine hübsche Monstersammlung. Ich werde es bedauern, sie zurückzulassen.")
 
 dialog_addDialog("bar-m-dost0", "en", "", "font_small", "This won`t happen any time soon.")
 dialog_addDialog("bar-m-dost0", "cs", "sound/"..codename.."/cs/bar-m-dost0.ogg", "font_small", "Na to máš ještě dost času.")
 dialog_addDialog("bar-m-dost0", "fr", "", "font_small", "Bientôt ça n'arrivera plus.")
+dialog_addDialog("bar-m-dost0", "de", "", "font_small", "Das wird nicht so schnell passieren.")
 
 dialog_addDialog("bar-m-dost1", "en", "", "font_small", "You are going to have a plenty of time to enjoy them.")
 dialog_addDialog("bar-m-dost1", "cs", "sound/"..codename.."/cs/bar-m-dost1.ogg", "font_small", "Bojím se, že ti to ještě dlouho líto být nemusí.")
 dialog_addDialog("bar-m-dost1", "fr", "", "font_small", "Tu vas avoir tout le temps de les admirer.")
+dialog_addDialog("bar-m-dost1", "de", "", "font_small", "Du wirst viel Zeit haben, dich an ihnen zu erfreuen.")
 
 dialog_addDialog("bar-v-lih", "en", "", "font_big", "If we take them with us and store them in alcohol, we could found ourselves a freak show.")
 dialog_addDialog("bar-v-lih", "cs", "sound/"..codename.."/cs/bar-v-lih.ogg", "font_big", "Kdybychom si je vzali s sebou a naložili do lihu, tak si můžeme otevřít obludárium.")
 dialog_addDialog("bar-v-lih", "fr", "", "font_big", "Si nous les mettons dans l'alcool, nous pourrons avoir notre propre spectacle de l'horreur.")
+dialog_addDialog("bar-v-lih", "de", "", "font_big", "Wenn wir sie mitnehmen und in Alkohol einlegen, können wir unsere eigene Horrorshow aufmachen.")
 
 dialog_addDialog("bar-m-fdto", "en", "", "font_small", "And FDTO isn`t enough for you?")
 dialog_addDialog("bar-m-fdto", "cs", "sound/"..codename.."/cs/bar-m-fdto.ogg", "font_small", "To ti nestačí FDTO?")
 dialog_addDialog("bar-m-fdto", "fr", "", "font_small", "Et FDTO n'est pas suffisant pour toi ?")
+dialog_addDialog("bar-m-fdto", "de", "", "font_small", "Die FDTO reicht dir wohl nicht?")
 
 dialog_addDialog("bar-x-vypr", "en", "", "font_white", "I can hear everything!")
 dialog_addDialog("bar-x-vypr", "cs", "sound/"..codename.."/cs/bar-x-vypr.ogg", "font_white", "Všechno slyším!")
 dialog_addDialog("bar-x-vypr", "fr", "", "font_white", "Je peux tout entendre !")
+dialog_addDialog("bar-x-vypr", "de", "", "font_white", "Ich höre alles!")
 
 dialog_addDialog("bar-m-promin", "en", "", "font_small", "Sorry, boss. I didn`t mean to offend.")
 dialog_addDialog("bar-m-promin", "cs", "sound/"..codename.."/cs/bar-m-promin.ogg", "font_small", "Promiňte šéfe. Já to tak nemyslela.")
 dialog_addDialog("bar-m-promin", "fr", "", "font_small", "Désolée, boss. Je ne voulais pas offenser.")
+dialog_addDialog("bar-m-promin", "de", "", "font_small", "Tut mir leid Chef. War nicht so gemeint.")
 
 dialog_addDialog("bar-m-mutanti", "en", "", "font_small", "You know, I finally realized that even mutants have a soul.")
 dialog_addDialog("bar-m-mutanti", "cs", "sound/"..codename.."/cs/bar-m-mutanti.ogg", "font_small", "Víš, konečně jsem si uvědomila, že i mutanti mají duši.")
 dialog_addDialog("bar-m-mutanti", "fr", "", "font_small", "Vous savez, j'ai finalement réalisé que même les mutants ont une âme.")
+dialog_addDialog("bar-m-mutanti", "de", "", "font_small", "Weißt du, ich habe endlich begriffen, dass sogar Mutanten eine Seele haben.")
+dialog_addDialog("bar-m-mutanti", "de", "", "font_small", "Weisst du, ich habe endlich begriffen, dass sogar Mutanten eine Seele haben.")
 
 dialog_addDialog("bar-v-ufouni", "en", "", "font_big", "Maybe even UFOs can have a soul.")
 dialog_addDialog("bar-v-ufouni", "cs", "sound/"..codename.."/cs/bar-v-ufouni.ogg", "font_big", "Možná, že dokonce i ufouni mohou mít duši.")
 dialog_addDialog("bar-v-ufouni", "fr", "", "font_big", "Peut-être même les extra-terrestres peuvent avoir une âme.")
+dialog_addDialog("bar-v-ufouni", "de", "", "font_big", "Vielleicht können sogar UFOs eine Seele haben.")
 
 dialog_addDialog("bar-m-zmeni", "en", "", "font_small", "I think that this level is going to change us forever.")
 dialog_addDialog("bar-m-zmeni", "cs", "sound/"..codename.."/cs/bar-m-zmeni.ogg", "font_small", "Myslím, že tahle místnost nás změní navždy.")
 dialog_addDialog("bar-m-zmeni", "fr", "", "font_small", "Je pense que ce niveau va nous changer définitivement.")
+dialog_addDialog("bar-m-zmeni", "de", "", "font_small", "Ich denke, dass uns diese Ebene für immer verändert.")
 
 dialog_addDialog("bar-m-noha", "en", "", "font_small", "That foot is not only repulsive but also shameless. Just look at it.")
 dialog_addDialog("bar-m-noha", "cs", "sound/"..codename.."/cs/bar-m-noha.ogg", "font_small", "Ta noha je nejen odporná, ale i bezcharakterní. Podívej se na ni.")
 dialog_addDialog("bar-m-noha", "fr", "", "font_small", "Ce pied n'est pas que repoussant, il ne doit pas avoir honte. Regarde.")
+dialog_addDialog("bar-m-noha", "de", "", "font_small", "Dieser Fuß ist nicht nur abscheulich, sondern auch noch schamlos. Schau ihn dir nur an.")
+dialog_addDialog("bar-m-noha", "de_CH", "", "font_small", "Dieser Fuss ist nicht nur abscheulich, sondern auch noch schamlos. Schau ihn dir nur an.")
 
 dialog_addDialog("bar-v-pld", "en", "", "font_big", "Look at that pld. It looks the same and still it grows more and more repulsive.")
 dialog_addDialog("bar-v-pld", "cs", "sound/"..codename.."/cs/bar-v-pld.ogg", "font_big", "Podívej na toho plda. Vypadá pořád stejně a přitom je pořád odpornější.")
 dialog_addDialog("bar-v-pld", "fr", "", "font_big", "Jette un oeil à la chose. Il semble identique, et plus il grandit plus il est repoussant.")
+dialog_addDialog("bar-v-pld", "de", "", "font_big", "Schau dir dieses rosa Ding an. Es sieht zwar immernoch genauso aus aber es wird immer abscheulicher.")
 
 dialog_addDialog("bar-m-pudy", "en", "", "font_small", "But even it harbors some motherly instincts. Look how he cares about the little one.")
 dialog_addDialog("bar-m-pudy", "cs", "sound/"..codename.."/cs/bar-m-pudy.ogg", "font_small", "Ale i v něm dřímou mateřské pudy. Podívej, jak se stará o to maličké.")
 dialog_addDialog("bar-m-pudy", "fr", "", "font_small", "Mais même s'il fait preuve d'instincts maternelle, regarde comme il prend soin du petit.")
+dialog_addDialog("bar-m-pudy", "de", "", "font_small", "Aber sogar dieses Ding hat Muttergefühle. Schau nur, wie es sich um das Kleine kümmert.")
 
 dialog_addDialog("bar-v-traverza", "en", "", "font_big", "Yes. It almost killed it with iron beam.")
 dialog_addDialog("bar-v-traverza", "cs", "sound/"..codename.."/cs/bar-v-traverza.ogg", "font_big", "Ano. Přizabil ho traverzou.")
 dialog_addDialog("bar-v-traverza", "fr", "", "font_big", "Oui. Il l'écrase sous une poubelle en acier.")
+dialog_addDialog("bar-v-traverza", "de", "", "font_big", "Ja. Es hat es fast mit dem Stahlding erschlagen.")
 
 dialog_addDialog("bar-m-rybka", "en", "", "font_small", "This fish looks familiar to me.")
 dialog_addDialog("bar-m-rybka", "cs", "sound/"..codename.."/cs/bar-m-rybka.ogg", "font_small", "Ta rybka je mi odněkud povědomá.")
 dialog_addDialog("bar-m-rybka", "fr", "", "font_small", "Ce poisson m'est familier.")
+dialog_addDialog("bar-m-rybka", "de", "", "font_small", "Dieser Fisch kommt mir bekannt vor.")
 
 dialog_addDialog("bar-v-fotka", "en", "", "font_big", "Maybe it was on that photo from power plant.")
 dialog_addDialog("bar-v-fotka", "cs", "sound/"..codename.."/cs/bar-v-fotka.ogg", "font_big", "Možná, že byla na té fotografii z elekrárny.")
 dialog_addDialog("bar-v-fotka", "fr", "", "font_big", "Peut-être qu'il était sur la photo de la centrale électrique.")
+dialog_addDialog("bar-v-fotka", "de", "", "font_big", "Vielleicht war er auf dem Foto vom Kraftwerk.")
 
 dialog_addDialog("bar-v-krab", "en", "", "font_big", "The poor crab. So many pincers and no head. Although... maybe he is better off than we.")
 dialog_addDialog("bar-v-krab", "cs", "sound/"..codename.."/cs/bar-v-krab.ogg", "font_big", "Chudák ten krab. Tolik klepet a žádná hlava. I když... možná je mu takhle líp než nám.")
 dialog_addDialog("bar-v-krab", "fr", "", "font_big", "Pauvre crabe. Tant de pinces et pas de tête. Après tout... peut-être qu'il est meilleur que nous.")
+dialog_addDialog("bar-v-krab", "de", "", "font_big", "Die arme Krabbe. So viele Scheren und kein Kopf. Obwohl... vielleicht ist sie besser dran als wir.")
 
 dialog_addDialog("bar-m-kachna", "en", "", "font_small", "No man is alone with a little yellow duck. Not even fish.")
 dialog_addDialog("bar-m-kachna", "cs", "sound/"..codename.."/cs/bar-m-kachna.ogg", "font_small", "Se žlutou kachničkou není člověk nikdy sám. Ani ryba.")
 dialog_addDialog("bar-m-kachna", "fr", "", "font_small", "Aucun homme se sent seul avec un petit canard jaune. Pas même les poissons.")
+dialog_addDialog("bar-m-kachna", "de", "", "font_small", "Niemand ist alleine, wenn er eine kleine gelbe Ente hat. Nicht einmal Fische.")
 
 dialog_addDialog("bar-m-barel", "en", "", "font_small", "This time, our goal is to push out the barrel with that filth.")
 dialog_addDialog("bar-m-barel", "cs", "sound/"..codename.."/cs/bar-m-barel.ogg", "font_small", "Tentokrát je naším úkolem vytlačit ven barel s tím svinstvem.")
 dialog_addDialog("bar-m-barel", "fr", "", "font_small", "Cette fois, notre but est de faire sortir le tonneau.")
+dialog_addDialog("bar-m-barel", "de", "", "font_small", "Unser Ziel ist diesmal, diesen Abfallbehälter herauszuschieben.")
 
-dialog_addDialog("bar-v-genofond", "en", "", "font_big", "But we have to leave ere all those creatures hrere. They must not harm the genepool of the healthy population.")
+dialog_addDialog("bar-v-genofond", "en", "", "font_big", "But we have to leave all those creatures here. They must not harm the genepool of the healthy population.")
 dialog_addDialog("bar-v-genofond", "cs", "sound/"..codename.."/cs/bar-v-genofond.ogg", "font_big", "A všechny tvory tu musíme nechat! Ať nezkazí genofond zdravé populace.")
 dialog_addDialog("bar-v-genofond", "fr", "", "font_big", "Mais nous devons laisser ici toutes ces créatures. Elles ne doivent pas se mélanger à l'éco-système sain.")
+dialog_addDialog("bar-v-genofond", "de", "", "font_big", "Aber wir müssen alle diese Kreaturen hier lassen. Sie dürfen nicht den Genpool der gesunden Population schädigen.")
 
 dialog_addDialog("bar-x-kchkch", "en", "sound/"..codename.."/en/bar-x-kchkch.ogg")
 
