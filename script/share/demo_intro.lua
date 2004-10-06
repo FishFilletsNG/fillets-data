b7b67a0ca7d2f60f68c0e7d0d9fbc6ccf8d7790d
ronnystandtke <ronnystandtke>
2004-10-06 14:02:12 +0000

    added German translation

--- script/share/demo_intro.lua
+++ script/share/demo_intro.lua
@@ -1,6 +1,7 @@
 dialog_addDialog("dlg-x-poster1", "en", "", "font_poster", "Download video from http://fillets.sf.net/intro.avi")
 dialog_addDialog("dlg-x-poster1", "cs", "", "font_poster", "Video je na http://fillets.sf.net/intro.avi")
 dialog_addDialog("dlg-x-poster1", "fr", "", "font_poster", "Télécharger la vidéo à l'adresse http://fillets.sf.net/intro.avi")
+dialog_addDialog("dlg-x-poster1", "de", "", "font_poster", "Video bitte von http://fillets.sf.net/intro.avi herunterladen")
 
 -- -----------------------------------------------------------------
 file_include("script/share/prog_demo.lua")
