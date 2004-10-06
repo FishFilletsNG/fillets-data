8cba9127f0498d08b21ffa73b061fbf250ea5fb6
ronnystandtke <ronnystandtke>
2004-10-06 14:34:03 +0000

    added German translation

--- script/gods/demo_poster.lua
+++ script/gods/demo_poster.lua
@@ -1,14 +1,18 @@
 dialog_addDialog("dlg-x-poster1", "en", "", "font_poster", "Occupation of the captive we are sending to you is an ocean god. Except of the plane and ship disappearings (so called Sea Battle case) he is responsible for the other crimes as well, moving the continents (code name Run, continent, run) and meteorite in Tunguzka (code Jumping Jack) are among them.")
 dialog_addDialog("dlg-x-poster1", "cs", "", "font_poster", "")
 dialog_addDialog("dlg-x-poster1", "fr", "", "font_poster", "La profession du captif que nous vous avons envoyé est dieu de l'océan. En plus de la disparition des bateaux et avions (aussi appelé bataille navale) il est responsable de bien d'autres crimes, parmi eux le déplacement des continents (nom de code Run, continent, run) ou encore la météorite de Tunguzka (code Jumping Jack).")
+dialog_addDialog("dlg-x-poster1", "de", "", "font_poster", "Die Berufstätigkeit des Gefangenen, den wir ihnen schicken, ist Meeresgott. Außer des Verschwindens von Flugzeugen und Schiffen (dem sogenannten Schiffe-Versenken-Fall) ist er weiterer Verbrechen schuldig, darunter: Verschieben von Kontinenten (Kennzeichen Run,Continent,Run) und eines Meteoriten in Tunguska (Kennzeichen Jumping Jack).")
+dialog_addDialog("dlg-x-poster1", "de_CH", "", "font_poster", "Die Berufstätigkeit des Gefangenen, den wir ihnen schicken, ist Meeresgott. Ausser des Verschwindens von Flugzeugen und Schiffen (dem sogenannten Schiffe-Versenken-Fall) ist er weiterer Verbrechen schuldig, darunter: Verschieben von Kontinenten (Kennzeichen Run,Continent,Run) und eines Meteoriten in Tunguska (Kennzeichen Jumping Jack).")
 
 dialog_addDialog("dlg-x-poster2", "en", "", "font_poster", "We managed to intervene just in the nick of time: we have found a brand new unwrapped box with a table game called STAR WARS in the captive's house.")
 dialog_addDialog("dlg-x-poster2", "cs", "", "font_poster", "")
 dialog_addDialog("dlg-x-poster2", "fr", "", "font_poster", "Nous sommes intervenus juste à temps : nous avons trouvé tout juste déballée, une boite contenant le jeu de plateau STAR WARS dans la maison du prisonnier.")
+dialog_addDialog("dlg-x-poster2", "de", "", "font_poster", "Wir schafften es, im letzten Augenblick einzugreifen: Wir haben eine brandneue Schachtel mit dem Tischspiel \"STAR WARS\" im Haus des Gefangenen gefunden.")
 
 dialog_addDialog("dlg-x-poster3", "en", "", "font_poster", "You can find the records of his sea battles in the attachement.")
 dialog_addDialog("dlg-x-poster3", "cs", "", "font_poster", "")
 dialog_addDialog("dlg-x-poster3", "fr", "", "font_poster", "Vous trouverez les enregistrements de ses batailles navales en annexes.")
+dialog_addDialog("dlg-x-poster3", "de", "", "font_poster", "Sie finden die Aufzeichnungen seiner Seeschlachten im Anhang.")
 
 -- -----------------------------------------------------------------
 file_include("script/share/prog_demo.lua")
