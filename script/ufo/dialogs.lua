ec277cffc9a107c92f896ae5edf6c8ba5d4b0ec2
ronnystandtke <ronnystandtke>
2004-10-06 13:27:42 +0000

    added German translation

--- script/ufo/dialogs.lua
+++ script/ufo/dialogs.lua
@@ -1,64 +1,82 @@
 dialog_addDialog("ufo-v-znicilo", "en", "", "font_big", "We must be getting near the end of our mission. This is surely the UFO that destroyed our house.")
 dialog_addDialog("ufo-v-znicilo", "cs", "sound/"..codename.."/cs/ufo-v-znicilo.ogg", "font_big", "Blížíme se konci naší cesty: tohle je jistě to UFO, co nám zničilo domeček.")
 dialog_addDialog("ufo-v-znicilo", "fr", "", "font_big", "Nous devons être proche de la fin de notre mission. C'est probablement l'OVNI qui a détruit notre maison?")
+dialog_addDialog("ufo-v-znicilo", "de", "", "font_big", "Wir müssen nahe am Ziel unseres Auftrags sein. Das ist sicherlich das UFO, das unser Haus zerstört hat.")
 
 dialog_addDialog("ufo-m-osmy", "en", "", "font_small", "Are you sure? This is but the eighth level.")
 dialog_addDialog("ufo-m-osmy", "cs", "sound/"..codename.."/cs/ufo-m-osmy.ogg", "font_small", "Myslíš? Vždyť je to teprve osmý level od začátku.")
 dialog_addDialog("ufo-m-osmy", "fr", "", "font_small", "Tu es sûr ? C'est seulement le niveau huit.")
+dialog_addDialog("ufo-m-osmy", "de", "", "font_small", "Bist du sicher? Das ist erst die achte Ebene.")
 
 dialog_addDialog("ufo-m-valce", "en", "", "font_small", "These steel cylinders are pretty obnoxious.")
 dialog_addDialog("ufo-m-valce", "cs", "sound/"..codename.."/cs/ufo-m-valce.ogg", "font_small", "Ty ocelové válce mi jdou pěkně na nervy.")
 dialog_addDialog("ufo-m-valce", "fr", "", "font_small", "Ces cylindres en acier sont odieux.")
+dialog_addDialog("ufo-m-valce", "de", "", "font_small", "Diese Stahlzylinder sind ziemlich widerlich.")
 
 dialog_addDialog("ufo-m-moc", "en", "", "font_small", "There is just too much steel here.")
 dialog_addDialog("ufo-m-moc", "cs", "sound/"..codename.."/cs/ufo-m-moc.ogg", "font_small", "Oceli je tu tentokrát trochu moc.")
 dialog_addDialog("ufo-m-moc", "fr", "", "font_small", "Il y a seulement trop d'acier ici.")
+dialog_addDialog("ufo-m-moc", "de", "", "font_small", "Hier ist einfach zu viel Stahl.")
 
 dialog_addDialog("ufo-v-hur", "en", "", "font_big", "It could be worse. I for one can imagine myself a room where there is nothing but steel.")
 dialog_addDialog("ufo-v-hur", "cs", "sound/"..codename.."/cs/ufo-v-hur.ogg", "font_big", "Může být i hůř. Já si třeba dovedu představit místo, kde nebude nic než ocel.")
 dialog_addDialog("ufo-v-hur", "fr", "", "font_big", "Ca pourrait être pire. Pour ma part je peux très bien imaginer une pièce où il n'y aurait que de l'acier.")
+dialog_addDialog("ufo-v-hur", "de", "", "font_big", "Es könnte schlimmer sein. Ich kann mir einen Raum vorstellen, in dem nichts weiter als Stahl ist.")
 
 dialog_addDialog("ufo-m-ne", "en", "", "font_small", "My goodness? What should I do in such a level?")
 dialog_addDialog("ufo-m-ne", "cs", "sound/"..codename.."/cs/ufo-m-ne.ogg", "font_small", "To snad ne. Co bych v takové místnosti dělala?")
 dialog_addDialog("ufo-m-ne", "fr", "", "font_small", "Mon dieu ? Que pourrais-je faire dans un tel niveau ?")
+dialog_addDialog("ufo-m-ne", "de", "", "font_small", "Meine Güte! Was würde ich in solch einer Ebene tun?")
 
 dialog_addDialog("ufo-v-vpredu", "en", "", "font_big", "You would let me go in front, for a change.")
 dialog_addDialog("ufo-v-vpredu", "cs", "sound/"..codename.."/cs/ufo-v-vpredu.ogg", "font_big", "Konečně bys mě nechala jít vpředu.")
 dialog_addDialog("ufo-v-vpredu", "fr", "", "font_big", "Tu devras me laisser passer devant pour une fois.")
+dialog_addDialog("ufo-v-vpredu", "de", "", "font_big", "Du würdest mich zu Abwechslung mal vorlassen.")
 
 dialog_addDialog("ufo-m-zvlastni", "en", "", "font_small", "It`s interesting that this tangled steel forestalls our entry into that UFO more effectively than any security system.")
 dialog_addDialog("ufo-m-zvlastni", "cs", "sound/"..codename.."/cs/ufo-m-zvlastni.ogg", "font_small", "Zvláštní je, že ta změť oceli funguje jako účinnější překážka než bezpečnostní systém toho UFA.")
 dialog_addDialog("ufo-m-zvlastni", "fr", "", "font_small", "C'est intéressant comme cet enchevêtrement d'acier nous interdit l'accès de l'OVNI plus efficacement qu'un système de sécurité.")
+dialog_addDialog("ufo-m-zvlastni", "de", "", "font_small", "Interessant, dass dieses Stahlgewühl uns den Zugang zum UFO effektiver verhindert, als jedes Sicherheitssystem.")
 
 dialog_addDialog("ufo-v-rikam", "en", "", "font_big", "I keep telling you that our life is controlled by some higher, strange consciousness.")
 dialog_addDialog("ufo-v-rikam", "cs", "sound/"..codename.."/cs/ufo-v-rikam.ogg", "font_big", "Pořád ti říkám, že náš život je řízen jakousi vyšší, cizí inteligencí.")
 dialog_addDialog("ufo-v-rikam", "fr", "", "font_big", "Je continue à dire que notre vie est dirigée par quelque conscience supérieure.")
+dialog_addDialog("ufo-v-rikam", "de", "", "font_big", "Ich sag` dir doch, dass unser Leben durch ein höheres, fremdartiges Bewusstsein gesteuert wird.")
 
 dialog_addDialog("ufo-m-vidim", "en", "", "font_small", "I can see that, too. But I am not that sure it`s higher.")
 dialog_addDialog("ufo-m-vidim", "cs", "sound/"..codename.."/cs/ufo-m-vidim.ogg", "font_small", "To vidím taky. Jenom si nejsem jistá, že je vyšší.")
 dialog_addDialog("ufo-m-vidim", "fr", "", "font_small", "Je vois. Mais je ne suis pas sûr qu'elle soit supérieure.")
+dialog_addDialog("ufo-m-vidim", "de", "", "font_small", "Das sehe ich auch. Aber ich bin mir nicht sicher, dass es höher ist.")
 
 dialog_addDialog("ufo-v-dovnitr", "en", "", "font_big", "What can we find inside, once we manage to get there?")
 dialog_addDialog("ufo-v-dovnitr", "cs", "sound/"..codename.."/cs/ufo-v-dovnitr.ogg", "font_big", "Co tam tak můžeme najít, jestli se dostaneme dovnitř?")
 dialog_addDialog("ufo-v-dovnitr", "fr", "", "font_big", "Que trouverons à l'intérieur, quand nous y serons ?")
+dialog_addDialog("ufo-v-dovnitr", "de", "", "font_big", "Was werden wir drinnen finden, wenn wir es schaffen, reinzukommen?")
 
 dialog_addDialog("ufo-m-tajemstvi", "en", "", "font_small", "Secrets of another civilization, new technologies, new sources of energy...")
 dialog_addDialog("ufo-m-tajemstvi", "cs", "sound/"..codename.."/cs/ufo-m-tajemstvi.ogg", "font_small", "Tajemství cizí civilizace, nové technologie, nové zdroje energie...")
 dialog_addDialog("ufo-m-tajemstvi", "fr", "", "font_small", "Les secrets d'une autre civilisation, de nouvelles technologies, de nouvelles sources d'énergie...")
+dialog_addDialog("ufo-m-tajemstvi", "de", "", "font_small", "Geheimnisse einer anderen Zivilisation, neue Technologien, neue Energiequellen...")
 
 dialog_addDialog("ufo-v-zjistit0", "en", "", "font_big", "Most importantly, we should try to find out why they came at all.")
 dialog_addDialog("ufo-v-zjistit0", "cs", "sound/"..codename.."/cs/ufo-v-zjistit0.ogg", "font_big", "Hlavně bychom měli zjistit, proč sem vlastně přiletěli.")
 dialog_addDialog("ufo-v-zjistit0", "fr", "", "font_big", "Le plus important, essayons de découvrir pourquoi ils sont venus.")
+dialog_addDialog("ufo-v-zjistit0", "de", "", "font_big", "Zuallererst sollten wir herausfinden, warum sie überhaupt gekommen sind.")
 
 dialog_addDialog("ufo-v-zjistit1", "en", "", "font_big", "You know, most of all I wonder why have they crashed just here.")
 dialog_addDialog("ufo-v-zjistit1", "cs", "sound/"..codename.."/cs/ufo-v-zjistit1.ogg", "font_big", "Víš, nejvíc by mě zajímalo, proč ztroskotali právě tady.")
 dialog_addDialog("ufo-v-zjistit1", "fr", "", "font_big", "Tu sais, je me demande juste pourquoi ils se sont écrasés ici.")
+dialog_addDialog("ufo-v-zjistit1", "de", "", "font_big", "Weißt du, am meisten frage ich mich, warum sie genau hier abgestürzt sind.")
+dialog_addDialog("ufo-v-zjistit1", "de_CH", "", "font_big", "Weisst du, am meisten frage ich mich, warum sie genau hier abgestürzt sind.")
 
 dialog_addDialog("ufo-m-tady", "en", "", "font_small", "Didn`t you see it? Everything happens just here.")
 dialog_addDialog("ufo-m-tady", "cs", "sound/"..codename.."/cs/ufo-m-tady.ogg", "font_small", "Však jsi to viděl. Všechno se děje právě tady.")
 dialog_addDialog("ufo-m-tady", "fr", "", "font_small", "Tu n'as pas remarqué ? Simplement Tout se passe ici.")
+dialog_addDialog("ufo-m-tady", "de", "", "font_small", "Hast du nicht bemerkt? Alles passiert genau hier.")
 
 dialog_addDialog("ufo-m-nevim", "en", "", "font_small", "I don`t know why the UFOs keep crashing at our house, either.")
 dialog_addDialog("ufo-m-nevim", "cs", "sound/"..codename.."/cs/ufo-m-nevim.ogg", "font_small", "Taky nevím, proč každé UFO padá právě na náš domeček.")
 dialog_addDialog("ufo-m-nevim", "fr", "", "font_small", "Je ne sais toujours pas pourquoi l'OVNI s'est écrasé sur notre maison.")
+dialog_addDialog("ufo-m-nevim", "de", "", "font_small", "Ich weiß auch nicht, warum die UFOs weiter auf unser Haus abstürzen.")
+dialog_addDialog("ufo-m-nevim", "de_CH", "", "font_small", "Ich weiss auch nicht, warum die UFOs weiter auf unser Haus abstürzen.")
 
