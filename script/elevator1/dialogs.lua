b7b1fcf1c020f7821312509f3679bcf400c3c40a
ronnystandtke <ronnystandtke>
2004-10-06 13:10:15 +0000

    added German translation

--- script/elevator1/dialogs.lua
+++ script/elevator1/dialogs.lua
@@ -1,73 +1,92 @@
 dialog_addDialog("zd1-v-civil", "en", "", "font_big", "What a glorious civilization. So many centuries under water and their elevators still work.")
 dialog_addDialog("zd1-v-civil", "cs", "sound/"..codename.."/cs/zd1-v-civil.ogg", "font_big", "Jaká to byla vyspělá civilizace. Staletí pod vodou a jejich výtahy ještě jezdí.")
 dialog_addDialog("zd1-v-civil", "fr", "", "font_big", "Quelle glorieuse civilisation. Plusieurs siècles sous l'eau et les ascenseurs fonctionnent toujours.")
+dialog_addDialog("zd1-v-civil", "de", "", "font_big", "Welch glorreiche Zivilisation. So viele Jahrhunderte unter Wasser und ihre Fahrstühle funktionieren immer noch.")
 
 dialog_addDialog("zd1-m-tlac", "en", "", "font_small", "Especially, when we push them.")
 dialog_addDialog("zd1-m-tlac", "cs", "sound/"..codename.."/cs/zd1-m-tlac.ogg", "font_small", "Zvláště, když zatlačíme.")
 dialog_addDialog("zd1-m-tlac", "fr", "", "font_small", "Surtout quand nous les poussons.")
+dialog_addDialog("zd1-m-tlac", "de", "", "font_small", "Besonders, wenn wir sie schieben.")
 
 dialog_addDialog("zd1-m-dolu", "en", "", "font_small", "But only down.")
 dialog_addDialog("zd1-m-dolu", "cs", "sound/"..codename.."/cs/zd1-m-dolu.ogg", "font_small", "Ale jenom dolů.")
 dialog_addDialog("zd1-m-dolu", "fr", "", "font_small", "Mais seulement vers le bas.")
+dialog_addDialog("zd1-m-dolu", "de", "", "font_small", "Aber nur nach unten.")
 
 dialog_addDialog("zd1-m-last", "en", "", "font_small", "Do you see that shell?")
 dialog_addDialog("zd1-m-last", "cs", "sound/"..codename.."/cs/zd1-m-last.ogg", "font_small", "Vidíš tu lasturu?")
 dialog_addDialog("zd1-m-last", "fr", "", "font_small", "Tu vois ce coquillage.")
+dialog_addDialog("zd1-m-last", "de", "", "font_small", "Siehst du diese Muschel?")
 
 dialog_addDialog("zd1-v-talis", "en", "", "font_big", "It`s an age-old talisman for elevator builders who always said 'Build well. Build with Shell`.")
 dialog_addDialog("zd1-v-talis", "cs", "sound/"..codename.."/cs/zd1-v-talis.ogg", "font_big", "To je prastarý talisman stavitelů zdviží. Aby jim výtah skvěle Shell.")
 dialog_addDialog("zd1-v-talis", "fr", "", "font_big", "C'est un talisman ancien pour les constructeurs d'ascenseurs qui disent toujours 'Construire bien. Construire avec des coquillages'.")
+dialog_addDialog("zd1-v-talis", "de", "", "font_big", "Das ist ein uralter Glücksbringer für Fahrstuhlbauer, die immer gesagt haben \"Baue schnell, bau` mit Shell\".")
 
 dialog_addDialog("zd1-m-poved", "en", "", "font_small", "Isn`t that shell a little bit familiar?")
 dialog_addDialog("zd1-m-poved", "cs", "sound/"..codename.."/cs/zd1-m-poved.ogg", "font_small", "Není ti ta lastura nějak povědomá?")
 dialog_addDialog("zd1-m-poved", "fr", "", "font_small", "Ce coquillage, n'est-il pas un petit peu familier.")
+dialog_addDialog("zd1-m-poved", "de", "", "font_small", "Kommt dir die Muschel bekannt vor?")
 
 dialog_addDialog("zd1-v-styd", "en", "", "font_big", "They should be ashamed of themselves. This is subliminal advertising.")
 dialog_addDialog("zd1-v-styd", "cs", "sound/"..codename.."/cs/zd1-v-styd.ogg", "font_big", "Že se nestydí. Taková zjevná skrytá reklama!")
 dialog_addDialog("zd1-v-styd", "fr", "", "font_big", "Ils doivent avoir honte d'eux-mêmes. C'est une publicité subliminale.")
+dialog_addDialog("zd1-v-styd", "de", "", "font_big", "Die sollten sich schämen. Das ist unterschwellige Werbung.")
 
 dialog_addDialog("zd1-v-lebka", "en", "", "font_big", "That skull looks familiar.")
 dialog_addDialog("zd1-v-lebka", "cs", "sound/"..codename.."/cs/zd1-v-lebka.ogg", "font_big", "Ta lebka je mi nějak povědomá.")
 dialog_addDialog("zd1-v-lebka", "fr", "", "font_big", "Ce crâne semble familier.")
+dialog_addDialog("zd1-v-lebka", "de", "", "font_big", "Den Schädel kenn` ich irgendwoher.")
 
 dialog_addDialog("zd1-m-stejne", "en", "", "font_small", "All human skulls look the same... Unlike fish skeletons.")
 dialog_addDialog("zd1-m-stejne", "cs", "sound/"..codename.."/cs/zd1-m-stejne.ogg", "font_small", "Všechny lidské lebky vypadají stejně... Na rozdíl od rybích kostřiček.")
 dialog_addDialog("zd1-m-stejne", "fr", "", "font_small", "Tous les crânes humains se ressemblent... Contrairement aux squelettes de poissons.")
+dialog_addDialog("zd1-m-stejne", "de", "", "font_small", "Alle menschlichen Schädel sehen gleich aus... Im Gegensatz zu Fischskeletten.")
 
 dialog_addDialog("zd1-m-cesta", "en", "", "font_small", "This is a tough path.")
 dialog_addDialog("zd1-m-cesta", "cs", "sound/"..codename.."/cs/zd1-m-cesta.ogg", "font_small", "To je ale cesta.")
 dialog_addDialog("zd1-m-cesta", "fr", "", "font_small", "C'est un large chemin.")
+dialog_addDialog("zd1-m-cesta", "de", "", "font_small", "Das ist ein schwerer Weg.")
 
 dialog_addDialog("zd1-v-krecek", "en", "", "font_big", "I feel like a mouse on a treadmill.")
 dialog_addDialog("zd1-v-krecek", "cs", "sound/"..codename.."/cs/zd1-v-krecek.ogg", "font_big", "Připadám si jako křeček ve šlapacím kole.")
 dialog_addDialog("zd1-v-krecek", "fr", "", "font_big", "Je me sens comme une souris dans une meule.")
+dialog_addDialog("zd1-v-krecek", "de", "", "font_big", "Ich fühle mich wie eine Maus in der Tretmühle .")
 
 dialog_addDialog("zd1-m-slap", "en", "", "font_small", "A treadmill for fish? But we only have fins.")
 dialog_addDialog("zd1-m-slap", "cs", "sound/"..codename.."/cs/zd1-m-slap.ogg", "font_small", "Šlapací kolo na ryby? Vždyť máme jen ploutve.")
 dialog_addDialog("zd1-m-slap", "fr", "", "font_small", "Une meule pour poisson ? Nous n'avons que des nageoires.")
+dialog_addDialog("zd1-m-slap", "de", "", "font_small", "Eine Tretmühle für Fische? Aber wir haben doch nur Flossen.")
 
 dialog_addDialog("zd1-x-huhu1", "en", "", "font_lightgrey", "Hoo, hooooo, hoooo... I`m over heeeere...")
 dialog_addDialog("zd1-x-huhu1", "cs", "sound/"..codename.."/cs/zd1-x-huhu1.ogg", "font_lightgrey", "Hu húúú húúúúú... Já jsem táádýýý...")
 dialog_addDialog("zd1-x-huhu1", "fr", "", "font_lightgrey", "Hoo, hooooo, hoooo... Je suis iciii...")
+dialog_addDialog("zd1-x-huhu1", "de", "", "font_lightgrey", "Huhu, Huhuuu... Ich bin hier drüüüüben...")
 
 dialog_addDialog("zd1-x-huhu2", "en", "", "font_lightgrey", "Hoooo, if you want I`ll tell you how to solve all the levels...")
 dialog_addDialog("zd1-x-huhu2", "cs", "sound/"..codename.."/cs/zd1-x-huhu2.ogg", "font_lightgrey", "Húúú, jestli chcete, prozradím vám řešení všech místností...")
 dialog_addDialog("zd1-x-huhu2", "fr", "", "font_lightgrey", "Hoooo, Si vous voulez je vous dirais comment résoudre tous les niveaux...")
+dialog_addDialog("zd1-x-huhu2", "de", "", "font_lightgrey", "Huhu, wenn ihr wollt, werde ich euch sagen, wie ihr alle Ebenen löst...")
 
 dialog_addDialog("zd1-x-huhu3", "en", "", "font_lightgrey", "Hooo, ho, hooo, I know why the city sank!")
 dialog_addDialog("zd1-x-huhu3", "cs", "sound/"..codename.."/cs/zd1-x-huhu3.ogg", "font_lightgrey", "Húúúú hu húúúú, já vím, proč se potopilo město!")
 dialog_addDialog("zd1-x-huhu3", "fr", "", "font_lightgrey", "Hooo, ho, hooo, Je sais pourquoi la cité a sombré !")
+dialog_addDialog("zd1-x-huhu3", "de", "", "font_lightgrey", "Huhu, Huhuuu, ich weiß, warum die Stadt versunken ist!")
+dialog_addDialog("zd1-x-huhu3", "de_CH", "", "font_lightgrey", "Huhu, Huhuuu, ich weiss, warum die Stadt versunken ist!")
 
 dialog_addDialog("zd1-x-huhu4", "en", "", "font_lightgrey", "Hoooo, why isn`t anyone paying attention to me?")
 dialog_addDialog("zd1-x-huhu4", "cs", "sound/"..codename.."/cs/zd1-x-huhu4.ogg", "font_lightgrey", "Húúúú, proč si mě nikdo nevšímá?")
 dialog_addDialog("zd1-x-huhu4", "fr", "", "font_lightgrey", "Hoooo, Pourquoi personne ne fait attention à moi ?")
+dialog_addDialog("zd1-x-huhu4", "de", "", "font_lightgrey", "Huhuuu, warum beachtet mich niemand?")
 
 dialog_addDialog("zd1-x-huhu5", "en", "", "font_lightgrey", "Hoooo ho hooo! Why are you ignoring me?")
 dialog_addDialog("zd1-x-huhu5", "cs", "sound/"..codename.."/cs/zd1-x-huhu5.ogg", "font_lightgrey", "Húúúú hu húúú húúú! Proč mě ignorujete?")
 dialog_addDialog("zd1-x-huhu5", "fr", "", "font_lightgrey", "Hoooo ho hooo ! Pourquoi vous m'ignorez ?")
+dialog_addDialog("zd1-x-huhu5", "de", "", "font_lightgrey", "Huhuuu! Warum ignoriert ihr mich?")
 
 dialog_addDialog("zd1-x-huhu6", "en", "", "font_lightgrey", "Hooooo, hooooo, fish! Pay attention to me!")
 dialog_addDialog("zd1-x-huhu6", "cs", "sound/"..codename.."/cs/zd1-x-huhu6.ogg", "font_lightgrey", "Hahúúú, holahúúúú, ryby! Všimněte si mě!")
 dialog_addDialog("zd1-x-huhu6", "fr", "", "font_lightgrey", "Hooooo, hooooo, les poissons ! Je suis là moi !")
+dialog_addDialog("zd1-x-huhu6", "de", "", "font_lightgrey", "Huhu, haaaaloo, Fische! Hört mir doch mal zu!")
 
 
