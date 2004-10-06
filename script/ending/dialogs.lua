a218f7aef6b1a0f14cfa3adb8d8ad2efc909f1fa
ronnystandtke <ronnystandtke>
2004-10-06 13:57:11 +0000

    added German translation

--- script/ending/dialogs.lua
+++ script/ending/dialogs.lua
@@ -1,46 +1,60 @@
 dialog_addDialog("z-v-doma", "en", "", "font_big", "I am glad to be home again.")
 dialog_addDialog("z-v-doma", "cs", "sound/"..codename.."/cs/z-v-doma.ogg", "font_big", "Jsem rád, že jsme zase doma.")
 dialog_addDialog("z-v-doma", "fr", "", "font_big", "Je suis content de rentrer à la maison.")
+dialog_addDialog("z-v-doma", "de", "", "font_big", "Ich bin froh, wieder zuhause zu sein.")
 
 dialog_addDialog("z-m-pocit", "en", "", "font_small", "I have such a good feeling of the work well done.")
 dialog_addDialog("z-m-pocit", "cs", "sound/"..codename.."/cs/z-m-pocit.ogg", "font_small", "Mám takový pěkný pocit dobře odvedené práce.")
 dialog_addDialog("z-m-pocit", "fr", "", "font_small", "J'ai vraiment le sentiment d'un travail bien fait.")
+dialog_addDialog("z-m-pocit", "de", "", "font_small", "Ich hab so ein gutes Gefühl wegen der gut erledigten Arbeit.")
 
 dialog_addDialog("z-v-sef", "en", "", "font_big", "The boss called a moment ago. He was very satisfied with our performance.")
 dialog_addDialog("z-v-sef", "cs", "sound/"..codename.."/cs/z-v-sef.ogg", "font_big", "Před chvílí volal šéf. Byl s námi moc spokojen.")
 dialog_addDialog("z-v-sef", "fr", "", "font_big", "Le patron vient d'appeler. Il est parfaitement satisfait de nous.")
+dialog_addDialog("z-v-sef", "de", "", "font_big", "Der Chef hat gerade angerufen. Er war sehr zufrieden mit unserer Leistung.")
 
 dialog_addDialog("z-m-nemluv", "en", "", "font_small", "Don`t mention the boss to me. He sits somewhere in his cozy office and we have to do all the dirty work.")
 dialog_addDialog("z-m-nemluv", "cs", "sound/"..codename.."/cs/z-m-nemluv.ogg", "font_small", "O šéfovi mi ani nemluv. On si sedí někde v teploučku, zatím co my jsme se dřely.")
 dialog_addDialog("z-m-nemluv", "fr", "", "font_small", "Ne me parle pas du patron. Il est assis quelque part dans un bureau douillet et nous faisons tout le sale boulot.")
+dialog_addDialog("z-m-nemluv", "de", "", "font_small", "Komm mir nicht mit dem Chef. Der sitzt irgendwo in seinem gemütlichen Büro und wir müssen die ganze Drecksarbeit erledigen.")
 
 dialog_addDialog("z-v-slyset", "en", "", "font_big", "Shhh, he could hear us.")
 dialog_addDialog("z-v-slyset", "cs", "sound/"..codename.."/cs/z-v-slyset.ogg", "font_big", "Pssst, může nás slyšet.")
 dialog_addDialog("z-v-slyset", "fr", "", "font_big", "Chut, il pourrait nous entendre.")
+dialog_addDialog("z-v-slyset", "de", "", "font_big", "Psst, er könnte uns hören.")
 
 dialog_addDialog("z-m-netusi", "en", "", "font_small", "How could he? He surely doesn`t even know how long it took us.")
 dialog_addDialog("z-m-netusi", "cs", "sound/"..codename.."/cs/z-m-netusi.ogg", "font_small", "Jak by nás mohl slyšet. Určitě ani netuší, jak dlouho nám to trvalo.")
 dialog_addDialog("z-m-netusi", "fr", "", "font_small", "Comment le pourrait-il ? Il ne sait même pas combien de temps ça nous a pris.")
+dialog_addDialog("z-m-netusi", "de", "", "font_small", "Wie könnte er? Er weiß sicher nicht einmal, wie lange wir gebraucht haben.")
+dialog_addDialog("z-m-netusi", "de_CH", "", "font_small", "Wie könnte er? Er weiss sicher nicht einmal, wie lange wir gebraucht haben.")
 
 dialog_addDialog("z-m-dlouho", "en", "", "font_small", "Really? It didn`t seem that long to me.")
 dialog_addDialog("z-m-dlouho", "cs", "sound/"..codename.."/cs/z-m-dlouho.ogg", "font_small", "Jé, tak dlouho mi to ani nepřipadlo.")
 dialog_addDialog("z-m-dlouho", "fr", "", "font_small", "Vraiment ? Ca ne m'a pas paru si long que ça.")
+dialog_addDialog("z-m-dlouho", "de", "", "font_small", "Wirklich? Mir kam es gar nicht so lang vor.")
 
 dialog_addDialog("z-v-pozdrav", "en", "", "font_big", "Dear player. We would like to communicate to you the high regards of the authors of this game. If you didn`t solve it by some cheat, you are really very, very good. It`s a pity we cannot see you from here, but...")
 dialog_addDialog("z-v-pozdrav", "cs", "sound/"..codename.."/cs/z-v-pozdrav.ogg", "font_big", "Hráči, máme ti vyřídit pozdrav a uznání od autorů hry. Jestli jsi to neřešil nějakým podvodem, tak jsi opravdu dobrej. Nebo dokonce dobrá? My tě tady odsud moc nevidíme.")
 dialog_addDialog("z-v-pozdrav", "fr", "", "font_big", "Chère joueuse ou cher joueur. Nous aimerions vous communiquer le grand respect des auteurs de ce jeu. Si vous ne l'avez pas fini à l'aide de tricherie, vous ètes très très fort(e). C'est vraiment dommage que nous puissions pas vous voir d'ici, mais...")
+dialog_addDialog("z-v-pozdrav", "de", "", "font_big", "Werter Spieler, wir möchten dir die besten Grüße der Autoren dieses Spiels über mitteln. Wenn du nicht geschummelt hast, bist du wirklich sehr, sehr gut. Schade, dass wir dich nicht von hier sehen können, aber...")
+dialog_addDialog("z-v-pozdrav", "de_CH", "", "font_big", "Werter Spieler, wir möchten dir die besten Grüsse der Autoren dieses Spiels über mitteln. Wenn du nicht geschummelt hast, bist du wirklich sehr, sehr gut. Schade, dass wir dich nicht von hier sehen können, aber...")
 
 dialog_addDialog("z-m-oblicej", "en", "", "font_small", "Could you please put your face in front of the hard disk LED? You might not know it but it is a kind of computer`s eye through which...")
 dialog_addDialog("z-m-oblicej", "cs", "sound/"..codename.."/cs/z-m-oblicej.ogg", "font_small", "Mohl bys dát obličej před kontrolku harddisku? Možná to nevíš, ale to je takové oko počítače, kterým tě...")
 dialog_addDialog("z-m-oblicej", "fr", "", "font_small", "Pouvez-vous s'il vous plaît faire face à la led du disque dur ? Vous ne savez probablement pas mais c'est comme un oeil pour l'ordinateur à travers lequel...")
+dialog_addDialog("z-m-oblicej", "de", "", "font_small", "Kannst du bitte dein Gesicht vor die Festplatten-LED halten? Vielleicht weißt du es nicht, aber das ist so eine Art Rechnerauge, durch das...")
+dialog_addDialog("z-m-oblicej", "de_CH", "", "font_small", "Kannst du bitte dein Gesicht vor die Festplatten-LED halten? Vielleicht weisst du es nicht, aber das ist so eine Art Rechnerauge, durch das...")
 
 dialog_addDialog("z-v-forky", "en", "", "font_big", "Stop this jokes of yours. This is a very special moment. So:")
 dialog_addDialog("z-v-forky", "cs", "sound/"..codename.."/cs/z-v-forky.ogg", "font_big", "Nech už těch fórků. Teď je slavnostní chvíle. Takže:")
 dialog_addDialog("z-v-forky", "fr", "", "font_big", "Arrête ta plaisanterie. C'est un moment très spécial. alors :")
+dialog_addDialog("z-v-forky", "de", "", "font_big", "Lass deine Witze. Das ist ein sehr besonderer Augenblick. Also:")
 
 dialog_addDialog("z-o-blahoprejeme", "en", "", "font_both", "CONGRATULATIONS!!!")
 dialog_addDialog("z-o-blahoprejeme", "cs", "sound/"..codename.."/cs/z-o-blahoprejeme.ogg", "font_both", "BLAHOPŘEJEME!!!")
 dialog_addDialog("z-o-blahoprejeme", "fr", "", "font_both", "FELICITATIONS!!!")
+dialog_addDialog("z-o-blahoprejeme", "de", "", "font_both", "HERZLICHEN GLÜCKWUNSCH!!!")
 
 dialog_addDialog("z-c-1", "en", "")
 dialog_addDialog("z-c-1", "cs", "sound/"..codename.."/cs/z-c-1.ogg")
@@ -144,6 +158,7 @@ dialog_addDialog("z-c-tisice", "cs", "sound/"..codename.."/cs/z-c-tisice.ogg")
 dialog_addDialog("z-c-hodin", "en", "", "font_white", "To be specific, it took you %1 hours!")
 dialog_addDialog("z-c-hodin", "cs", "sound/"..codename.."/cs/z-c-hodin.ogg", "font_white", "Konkrétně %1 hodin!")
 dialog_addDialog("z-c-hodin", "fr", "", "font_white", "Pour être précis, vous avez mis %1 heures !")
+dialog_addDialog("z-c-hodin", "de", "", "font_white", "Um genau zu sein, du hast %1 Stunden gebraucht!")
 
 dialog_addDialog("z-c-konkretne", "en", "")
 dialog_addDialog("z-c-konkretne", "cs", "sound/"..codename.."/cs/z-c-konkretne.ogg")
