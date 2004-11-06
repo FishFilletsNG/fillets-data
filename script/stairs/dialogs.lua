8d60686c7ffb91afd792cf8faf9d7d9d135b7a77
coch <coch>
2004-11-06 13:48:17 +0000

    Added Italian translation

--- script/stairs/dialogs.lua
+++ script/stairs/dialogs.lua
@@ -2,30 +2,36 @@ dialog_addDialog("sch-m-spadlo", "en", "", "font_small", "Something fell here ag
 dialog_addDialog("sch-m-spadlo", "cs", "sound/"..codename.."/cs/sch-m-spadlo.ogg", "font_small", "Co nám to sem zase spadlo?")
 dialog_addDialog("sch-m-spadlo", "fr", "", "font_small", "Quelque chose est tombé ici aussi.")
 dialog_addDialog("sch-m-spadlo", "de", "", "font_small", "Hier ist wieder etwas runtergefallen.")
+dialog_addDialog("sch-m-spadlo", "it", "", "font_small", "E` di nuovo caduta della roba.")
 
 dialog_addDialog("sch-v-lastura", "en", "", "font_big", "It`s some kind of shell. We`ll probably have to push it upstairs again.")
 dialog_addDialog("sch-v-lastura", "cs", "sound/"..codename.."/cs/sch-v-lastura.ogg", "font_big", "Nějaká lastura. Asi ji budem muset vystrkat nahoru.")
 dialog_addDialog("sch-v-lastura", "fr", "", "font_big", "C'est une sorte de coquillage. Nous devons probablement lui faire monter les escaliers.")
 dialog_addDialog("sch-v-lastura", "de", "", "font_big", "Es ist eine Art Muschel. Wir müssen sie wahrscheinlich wieder die Treppe hochschieben.")
+dialog_addDialog("sch-v-lastura", "it", "", "font_big", "E` una specie di conchiglia. Mi sa che dovremo di nuovo portarla su.")
 
 dialog_addDialog("sch-v-setkani", "en", "", "font_big", "I can see it now: 'Shell Meets Flowerpot`. That`s almost like an iron and a sewing machine meeting on an operating table.")
 dialog_addDialog("sch-v-setkani", "cs", "sound/"..codename.."/cs/sch-v-setkani.ogg", "font_big", "Setkání lastury s květináčem. To je skoro jako setkání žehličky s šicím strojem na operačním stole.")
 dialog_addDialog("sch-v-setkani", "fr", "", "font_big", "Je comprends maintenant : 'le coquillage rencontre la plante'. Un peu comme la rencontre d'un fer à repasser et d'une machine à coudre sur une table d'opération.")
 dialog_addDialog("sch-v-setkani", "de", "", "font_big", "Das ist es: \"Muschel trifft Blumentopf\". Das ist ja fast so, als wenn sich ein Bügeleisen und eine Nähmaschine auf einem Operationstisch treffen.")
+dialog_addDialog("sch-v-setkani", "it", "", "font_big", "Ora vedo: 'La conchiglia incontra il vaso'. E` più o meno come un ferro da stiro e una sega elettrica che si incontrano su un tavolo operatorio.")
 
 dialog_addDialog("sch-m-moc0", "en", "", "font_small", "I`m sorry, I got a bit carried away.")
 dialog_addDialog("sch-m-moc0", "cs", "sound/"..codename.."/cs/sch-m-moc0.ogg", "font_small", "Nějak jsem se do toho moc zabrala.")
 dialog_addDialog("sch-m-moc0", "fr", "", "font_small", "Je suis désolée, j'ai un truc à pousser.")
 dialog_addDialog("sch-m-moc0", "de", "", "font_small", "Tschuldigung, ich habe mich ein wenig mitreißen lassen.")
 dialog_addDialog("sch-m-moc0", "de_CH", "", "font_small", "Tschuldigung, ich habe mich ein wenig mitreissen lassen.")
+dialog_addDialog("sch-m-moc0", "it", "", "font_small", "Scusa, mi sono lasciata trasportare.")
 
 dialog_addDialog("sch-m-moc1", "en", "", "font_small", "Sorry, I was momentarily distracted.")
 dialog_addDialog("sch-m-moc1", "cs", "sound/"..codename.."/cs/sch-m-moc1.ogg", "font_small", "Na chvíli jsem se zamyslela.")
 dialog_addDialog("sch-m-moc1", "fr", "", "font_small", "Désolée, j'étais ailleurs.")
 dialog_addDialog("sch-m-moc1", "de", "", "font_small", "Tschuldigung, ich war für einen Moment abgelenkt.")
+dialog_addDialog("sch-m-moc1", "it", "", "font_small", "Scusa, mi sono distratta un attimo.")
 
 dialog_addDialog("sch-m-moc2", "en", "", "font_small", "Where I am going with this shell?")
 dialog_addDialog("sch-m-moc2", "cs", "sound/"..codename.."/cs/sch-m-moc2.ogg", "font_small", "Kam to vlastně strkám?")
 dialog_addDialog("sch-m-moc2", "fr", "", "font_small", "Où vais-je avec ce coquillage ?")
 dialog_addDialog("sch-m-moc2", "de", "", "font_small", "Wohin soll ich mit der Muschel?")
+dialog_addDialog("sch-m-moc2", "it", "", "font_small", "Dove vado con 'sta conchiglia?")
 
