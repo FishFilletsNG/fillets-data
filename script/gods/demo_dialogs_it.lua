0ab70ec9310da3764843f4a9b1af29ce2cdbe766
coch <coch>
2005-02-26 12:23:21 +0000

    Italian translation

---
+++ script/gods/demo_dialogs_it.lua
@@ -0,0 +1,9 @@
+
+dialogId("dlg-x-poster1", "font_poster", "L'occupazione abituale del prigioniero che vi stiamo mandando è fare la divinità oceanica. Oltre alle sparizioni di navi ed aerei (il cosiddetto caso Battaglia Navale), è responsabile di altri crimini, della deriva dei continenti (nome in codice Corri, continente, corri) e del meteorite della Tunguska (nome in codice Saltapicchio) tra gli altri.")
+dialogStr("")
+
+dialogId("dlg-x-poster2", "font_poster", "Siamo riusciti ad intervenire appena in tempo: nell'alloggio del prigioniero abbiamo trovato una scatola nuova non ancora aperta contenente un gioco da tavolo chiamato GUERRE STELLARI.")
+dialogStr("")
+
+dialogId("dlg-x-poster3", "font_poster", "Troverete il rapporto completo delle sue battaglie navali in allegato.")
+dialogStr("")
