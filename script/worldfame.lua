6d29953d9bd28b078809f7d711013423db303007
mrpaja <mrpaja>
2005-04-20 21:28:20 +0000

    halloffame update

--- script/worldfame.lua
+++ script/worldfame.lua
@@ -16,7 +16,7 @@ node_bestSolution("viking1", 119, "Pavel Danihelka")
 node_bestSolution("airplane", 257, "Mark Byers")
 node_bestSolution("bathyscaph", 82, "Pavel Danihelka")
 node_bestSolution("tank", 402, "Josef Peterka")
-node_bestSolution("viking2", 359, "Jan Danihelka")
+node_bestSolution("viking2", 349, "Gabor Braun")
 node_bestSolution("party2", 285, "Josef Peterka")
 node_bestSolution("gods", 943, "Josef Peterka")
  
