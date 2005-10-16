6f022d05190039f06483c78e2f9f4dbedbcc821a
dbi <dbi>
2005-10-16 20:48:12 +0000

    One more file with Bulgarian translation of dialogs

---
+++ script/floppy/dialogs_bg.lua
@@ -0,0 +1,70 @@
+
+dialogId("disk-v-tady", "font_big", "Изглежда най-сетне пристигнахме.")
+
+dialogId("disk-m-tady", "font_small", "Мислиш ли, че е това?")
+
+dialogId("disk-m-vejit", "font_small", "Как може всичко да се събере на едно дърво и половининчова дискета?")
+
+dialogId("disk-v-metrova", "font_big", "Погледни го колко по-голямо е от мен. Това е дърво и половинметрова дискета.")
+
+dialogId("disk-m-velka", "font_small", "Така е по-добре. Голям план — голяма дискета. Има смисъл.")
+
+dialogId("disk-m-zvednem", "font_small", "Мислиш ли, че можем да я повдигнем?")
+
+dialogId("disk-v-tezko", "font_big", "Трудно ще е.")
+
+dialogId("disk-v-nejde", "font_big", "Не мисля, че е възможно. Дори и да можем, защо биха го крили тук?")
+
+dialogId("disk-v-ulamu", "font_big", "Млъкни или ще ти счупя краката!")
+
+dialogId("disk-v-kriz", "font_big", "Виждаш ли онзи стоманен кръст? Там са погребани надеждите ни.")
+
+dialogId("disk-m-depres", "font_small", "Не бъди винаги толкова черноглед. Не ми понася.")
+
+dialogId("disk-m-nahrat", "font_small", "Няма ли да е достатъчно ако изтеглим плана от флопито на нашия ноутбук?")
+
+dialogId("disk-v-mas", "font_big", "Защо не? Вади го.")
+
+dialogId("disk-m-sakra", "font_small", "О, по дяволите! Оставих го у дома.")
+
+dialogId("disk-v-vratime", "font_big", "Да се върнем ли да го вземем?")
+
+dialogId("disk-v-naano", "font_big", "Никога. Може да ти е смешно, но перките ми са възпалени.")
+
+dialogId("disk-v-nane", "font_big", "Както искаш. Любопитен съм как ще се справиш сега.")
+
+dialogId("disk-m-zmatlo", "font_small", "Ами ако във флопито няма план? Ако е тук само за да ни обърка?")
+
+dialogId("disk-v-neverim", "font_big", "Не ми се вярва. Има ли обект, който да е по-труден за изваждане?")
+
+dialogId("disk-m-tvorecci", "font_small", "Що за създания са тези малките?")
+
+dialogId("disk-v-viry", "font_big", "Трябва да са компютърни вируси.")
+
+dialogId("disk-m-potvory", "font_small", "Гадост. Пак изчадия.")
+
+dialogId("disk-v-pozor", "font_big", "Преди всичко трябва да внимаваме да не ги вземем заедно с диска. Играчът няма да е много доволен ако ги разсипем по работния му плот.")
+
+dialogId("disk-m-ukol", "font_small", "Целта ни е да избутаме този диск навън.")
+
+dialogId("disk-x-nepohnes", "font_darkgrey", "Няма да ме преместиш лесно!")
+
+dialogId("disk-x-mazany", "font_darkgrey", "Това беше хитро!")
+
+dialogId("disk-x-jejda0", "font_darkgrey", "Опала!")
+
+dialogId("disk-x-jejda1", "font_darkgrey", "Проклятие!")
+
+dialogId("disk-x-au0", "font_darkgrey", "Ох.")
+
+dialogId("disk-x-au1", "font_darkgrey", "Ох!.")
+
+dialogId("disk-x-au2", "font_darkgrey", "Уф.")
+
+dialogId("disk-x-uzne", "font_darkgrey", "Остави ме на мира!")
+
+dialogId("disk-x-vir0", "font_green", "1060D0A2A050B508C34D590923763F6B5B466C0E6033A9")
+
+dialogId("disk-x-vir1", "font_green", "033455D394F938E624A68CBCA301923784F35892")
+
+dialogId("disk-x-vir2", "font_green", "1A566837C0323B0413239DD2374E9F0036C56B23074")
