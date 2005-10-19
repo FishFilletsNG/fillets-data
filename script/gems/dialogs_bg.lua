0da7abb4b0a5418a2d7681ba69ec2d5394c4b224
dbi <dbi>
2005-10-19 16:26:01 +0000

    Three more dialogs translated in Bulgarian

---
+++ script/gems/dialogs_bg.lua
@@ -0,0 +1,26 @@
+
+dialogId("zav-v-sto", "font_big", "Забележете, че това ниво съдържа сто и девет обекта.")
+
+dialogId("zav-v-trpyt", "font_big", "И всички те са толкова бляскави.")
+
+dialogId("zav-m-pohnout", "font_small", "Забележете, че не мога да помръдна дори и перка.")
+
+dialogId("zav-m-krasa", "font_small", "Красота! Всичко е толкова бляскаво...")
+
+dialogId("zav-v-venku", "font_big", "Ще е много по-красиво когато се измъкнем.")
+
+dialogId("zav-v-zaval", "font_big", "Тази пещера е доста опасна. Късмет, че сме още живи.")
+
+dialogId("zav-m-hopskok", "font_small", "Не бързай. Все още не сме се измъкнали.")
+
+dialogId("zav-m-kameny", "font_small", "Не мислиш ли, че тези камъни бяха подредени другояче?")
+
+dialogId("zav-v-zeleny", "font_big", "Права си. Онзи зеления със сигурност беше там горе!")
+
+dialogId("zav-v-restart", "font_big", "Струва ми се, че тези скъпоценни камъни се подреждат различно след всяко рестартиране.")
+
+dialogId("zav-m-pravda", "font_small", "Да, прав си. Може би понякога дори образуват смислени фигури.")
+
+dialogId("zav-m-hrac", "font_small", "Играчът определено утолява жаждата си за цветове.")
+
+dialogId("zav-v-zachranit", "font_big", "Може би сега ще се опита да ни спаси.")
---
+++ script/gods/dialogs_bg.lua
@@ -0,0 +1,216 @@
+
+dialogId("lod-v-silenost0", "font_big", "Подозирам, че ни предстои да открием нещо ужасно.")
+
+dialogId("lod-v-silenost1", "font_big", "Винаги съм знаел, че боговете сигурно са полудели.")
+
+dialogId("lod-v-silenost2", "font_big", "Бог е луд и целият свят е негова играчка.")
+
+dialogId("lod-m-pravda0", "font_small", "Мисля, че си прав.")
+
+dialogId("lod-m-pravda1", "font_small", "Боя се, че си прав.")
+
+dialogId("lod-m-pravda2", "font_small", "Шокиращо е наистина.")
+
+dialogId("lod-m-costim", "font_small", "Какво ще правим?")
+
+dialogId("lod-v-internovat", "font_big", "Не може да ги оставим така. Трябва да ги вкараме в затвора.")
+
+dialogId("lod-m-co", "font_small", "Как ще го направим?")
+
+dialogId("lod-v-cvok", "font_big", "Ще ги вкараме в лидни... имам предвид в ареста на ООРД.")
+
+dialogId("lod-m-oba", "font_small", "Прав си. И двамата ли ще вземем?")
+
+dialogId("lod-v-golf", "font_big", "Разбира се. Ако оставим единия от тях тук, може да му хрумнат всякакви луди идеи. Може да започне да играе голф, например.")
+
+dialogId("lod-m-jednoho", "font_small", "С този стик — едва ли. Мисля, че дори да вземем само единия, пак ще е от полза. Другият ще е съвсем безобиден сам.")
+
+dialogId("lod-v-koho", "font_big", "Добре. Кой да бъде?")
+
+dialogId("lod-m-zluty", "font_small", "Жълтия.")
+
+dialogId("lod-m-modry", "font_small", "Синия.")
+
+dialogId("lod-v-zluty", "font_big", "Онзи синия.")
+
+dialogId("lod-v-modry", "font_big", "Онзи жълтия.")
+
+dialogId("lod-m-hrac", "font_small", "Ще оставим играча да реши.")
+
+dialogId("lod-v-hrac", "font_big", "Да оставим играча да реши.")
+
+dialogId("lod-v-hul", "font_big", "Погледни този стик — нещо ужасно му се е случило.")
+
+dialogId("lod-m-ozizlana", "font_small", "Сигурно е минал през водовъртеж.")
+
+dialogId("lod-v-hravost", "font_big", "Мама е права. Ненормалната игривост е корен на всяко зло.")
+
+dialogId("lod-m-palka", "font_small", "Вярно е. Щеше да ни е много по-лесно ако тази палка за тенис на масе беше някъде другаде.")
+
+dialogId("lod-v-micky", "font_big", "Между другото, забеляза ли, че истинската топка за голф е в горния ляв ъгъл, докато онова в долния десен ъгъл е топка за крикет?")
+
+dialogId("lod-m-vyznam", "font_small", "Дали има скрит символизъм?")
+
+dialogId("lod-v-kdovi", "font_big", "Кой знае.")
+
+dialogId("lod-m-micek", "font_small", "Мислех, че това годе е топче за тенис на маса.")
+
+dialogId("lod-v-rozliseni", "font_big", "Трудно е да се каже при тази разделителна способност на екрана.")
+
+dialogId("lod-m-bohove", "font_small", "Този път целта ни е да се отървем от някой от боговете.")
+
+dialogId("b1-voda1", "font_yellow", "Пропуск!")
+
+dialogId("b1-voda2", "font_yellow", "Пропуск!")
+
+dialogId("b1-voda3", "font_yellow", "Пропуск!")
+
+dialogId("b1-voda4", "font_yellow", "Пропуск!")
+
+dialogId("b1-voda5", "font_yellow", "Пропуск!")
+
+dialogId("b1-zasah1", "font_yellow", "Попадение!")
+
+dialogId("b1-zasah2", "font_yellow", "Попадение!")
+
+dialogId("b1-zasah3", "font_yellow", "Попадение!")
+
+dialogId("b1-zasah4", "font_yellow", "Попадение!")
+
+dialogId("b1-potop1", "font_yellow", "В пясъка!")
+
+dialogId("b1-potop2", "font_yellow", "В пясъка!")
+
+dialogId("b1-potop3", "font_yellow", "В пясъка!")
+
+dialogId("b1-vyhral", "font_yellow", "Победих, сестричке!!!")
+
+dialogId("b1-zacinam", "font_yellow", "Започвам!")
+
+dialogId("b1-dobre", "font_yellow", "Добре!")
+
+dialogId("b1-znovu", "font_yellow", "Ще изиграем ли още една игра?")
+
+dialogId("b1-nepodvadim", "font_yellow", "Не мамя.")
+
+dialogId("b1-spletl", "font_yellow", "Това май беше грешка...")
+
+dialogId("b2-voda1", "font_cyan", "Пропуск!")
+
+dialogId("b2-voda2", "font_cyan", "Пропуск!")
+
+dialogId("b2-voda3", "font_cyan", "Пропуск!")
+
+dialogId("b2-voda4", "font_cyan", "Пропуск!")
+
+dialogId("b2-voda5", "font_cyan", "Пропуск!")
+
+dialogId("b2-zasah1", "font_cyan", "Удар!")
+
+dialogId("b2-zasah2", "font_cyan", "Удар!")
+
+dialogId("b2-zasah3", "font_cyan", "Удар!")
+
+dialogId("b2-zasah4", "font_cyan", "Удар!")
+
+dialogId("b2-potop1", "font_cyan", "В пясъка!")
+
+dialogId("b2-potop2", "font_cyan", "В пясъка!")
+
+dialogId("b2-potop3", "font_cyan", "В пясъка!")
+
+dialogId("b2-vyhral", "font_cyan", "Ха, ха, ха... Печеля!")
+
+dialogId("b2-dobre", "font_cyan", "Добре!")
+
+dialogId("b2-znovu", "font_cyan", "Да опитаме ли отново?")
+
+dialogId("b2-rikal1", "font_cyan", "Това вече го опита!")
+
+dialogId("b2-rikal2", "font_cyan", "Това вече го каза!")
+
+dialogId("b2-nemuze", "font_cyan", "Не може да пропусна от това положение.")
+
+dialogId("b2-podvadis", "font_cyan", "Мамиш!!!")
+
+dialogId("b2-spatne", "font_cyan", "Опитах и ти каза „пропуск“!")
+
+dialogId("b1-1", "", "")
+
+dialogId("b1-2", "", "")
+
+dialogId("b1-3", "", "")
+
+dialogId("b1-4", "", "")
+
+dialogId("b1-5", "", "")
+
+dialogId("b1-6", "", "")
+
+dialogId("b1-7", "", "")
+
+dialogId("b1-8", "", "")
+
+dialogId("b1-9", "", "")
+
+dialogId("b1-10", "", "")
+
+dialogId("b1-a", "font_yellow", "%1.")
+
+dialogId("b1-b", "font_yellow", "%1.")
+
+dialogId("b1-c", "font_yellow", "%1.")
+
+dialogId("b1-d", "font_yellow", "%1.")
+
+dialogId("b1-e", "font_yellow", "%1.")
+
+dialogId("b1-f", "font_yellow", "%1.")
+
+dialogId("b1-g", "font_yellow", "%1.")
+
+dialogId("b1-h", "font_yellow", "%1.")
+
+dialogId("b1-i", "font_yellow", "%1.")
+
+dialogId("b1-j", "font_yellow", "%1.")
+
+dialogId("b2-1", "", "")
+
+dialogId("b2-2", "", "")
+
+dialogId("b2-3", "", "")
+
+dialogId("b2-4", "", "")
+
+dialogId("b2-5", "", "")
+
+dialogId("b2-6", "", "")
+
+dialogId("b2-7", "", "")
+
+dialogId("b2-8", "", "")
+
+dialogId("b2-9", "", "")
+
+dialogId("b2-10", "", "")
+
+dialogId("b2-a", "font_cyan", "%1.")
+
+dialogId("b2-b", "font_cyan", "%1.")
+
+dialogId("b2-c", "font_cyan", "%1.")
+
+dialogId("b2-d", "font_cyan", "%1.")
+
+dialogId("b2-e", "font_cyan", "%1.")
+
+dialogId("b2-f", "font_cyan", "%1.")
+
+dialogId("b2-g", "font_cyan", "%1.")
+
+dialogId("b2-h", "font_cyan", "%1.")
+
+dialogId("b2-i", "font_cyan", "%1.")
+
+dialogId("b2-j", "font_cyan", "%1.")
