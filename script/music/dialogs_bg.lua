934cee525a9488c3c588e051ddad80f9423746ef
dbi <dbi>
2005-11-24 22:21:49 +0000

    Two more files translated to Bulgarian

---
+++ script/music/dialogs_bg.lua
@@ -0,0 +1,22 @@
+
+dialogId("ves-v-veci", "font_big", "Я, какви интересни неща имало в града!")
+
+dialogId("ves-m-krab", "font_small", "Горкото раче!")
+
+dialogId("ves-v-vyp", "font_big", "Трябва да го изключим!")
+
+dialogId("ves-m-uz", "font_small", "О, неее...")
+
+dialogId("ves-v-pokoj", "font_big", "Иии край.")
+
+dialogId("ves-m-dik", "font_small", "Благодаря ти играчо. Повече не можех да търпя.")
+
+dialogId("ves-v-stejne", "font_big", "Не все пак се налага да го избутаме долу.")
+
+dialogId("ves-hs-hrajeme", "font_statue", "А сега да послушаме музика!")
+
+dialogId("ves-ampliony", "", "")
+
+dialogId("ves-hs-papa", "font_statue", "Та тада та тааада та тада та тааа...")
+
+dialogId("sp-smrt", "", "")
---
+++ script/noground/dialogs_bg.lua
@@ -0,0 +1,10 @@
+
+dialogId("nog-v-zvlastni", "font_big", "Много странна стая.")
+
+dialogId("nog-m-uvedom0", "font_small", "Трябва да осъзнаеш, че стоманеният цилиндър около нас е просто обект.")
+
+dialogId("nog-m-uvedom1", "font_small", "Тук има всичко на всичко само четири тухлички.")
+
+dialogId("nog-v-smetiste0", "font_big", "Този куп боклуци е странен...")
+
+dialogId("nog-v-smetiste1", "font_big", "Тази купчина боклуци все пак изглежда странно...")
