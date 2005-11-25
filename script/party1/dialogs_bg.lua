4aa6b0090f1024d2584ca393985c6b19c2ad3650
dbi <dbi>
2005-11-25 18:29:21 +0000

    Two more files translated to Bulgarian

---
+++ script/party1/dialogs_bg.lua
@@ -0,0 +1,24 @@
+
+dialogId("pt1-m-parnicek", "font_small", "Виж, увеселителната лодка.")
+
+dialogId("pt1-v-predtucha", "font_big", "Имам чувството, че това няма да е просто пикник.")
+
+dialogId("pt1-m-predtucha", "font_small", "Ти и твоите чувства. Сигурно е пълно с мили и весели хора.")
+
+dialogId("pt1-m-kostlivec", "font_small", "Може би си прав. Този пикник е малко странен.")
+
+dialogId("pt1-m-vylezt0", "font_small", "Мислиш ли, че могат да излязат от лодката?")
+
+dialogId("pt1-m-vylezt1", "font_small", "Надявам се, че не могат да излязат от лодката.")
+
+dialogId("pt1-m-vylezt2", "font_small", "Ами ако започнат да ни преследват?")
+
+dialogId("pt1-v-pryc0", "font_big", "Едва ли ще ни хванат. Скелетите не плуват много добре.")
+
+dialogId("pt1-v-pryc1", "font_big", "Стига си размишлявала и си размърдай перките. Трябва да се измъкнем.")
+
+dialogId("pt1-v-valec", "font_big", "Искаш да подпреш стоманената тръба с чаша?")
+
+dialogId("pt1-m-nemuzu", "font_small", "Нищо повече не мога да направя за теб.")
+
+dialogId("pt1-v-pozor", "font_big", "Внимавай да не разлееш!")
---
+++ script/party2/dialogs_bg.lua
@@ -0,0 +1,26 @@
+
+dialogId("pt2-m-parnik", "font_small", "Отново тази призрачна лодка.")
+
+dialogId("pt2-v-zmena", "font_big", "Виждаш ли? Нещо се е променило докато не бяхме тук. Казвам ти, наблюдават ни.")
+
+dialogId("pt2-m-hrac", "font_small", "Защо продължаваш да ни гледаш? Не виждаш ли, че това ни изнервя?")
+
+dialogId("pt2-m-piknik0", "font_small", "Изглежда пикника е приключил.")
+
+dialogId("pt2-m-piknik1", "font_small", "Пикникът е свършил.")
+
+dialogId("pt2-m-piknik2", "font_small", "Това е всичко, което е останало след пикника.")
+
+dialogId("pt2-m-piknik3", "font_small", "Пикник изотдолу.")
+
+dialogId("pt2-v-unaveni0", "font_big", "Мисля, че тези туристи са малко уморени.")
+
+dialogId("pt2-v-unaveni1", "font_big", "Да, май им стига толкова забавление.")
+
+dialogId("pt2-v-nemohou0", "font_big", "Май вече не мислят да ни нараняват.")
+
+dialogId("pt2-v-nemohou1", "font_big", "Мисля, че вече са напълно безопасни.")
+
+dialogId("pt2-v-minule0", "font_big", "Този път няма да е лесно като преди.")
+
+dialogId("pt2-v-minule1", "font_big", "Изглежда, че този път ще е по-трудно, а?")
