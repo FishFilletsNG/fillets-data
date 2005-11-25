1800934d54891ac95b2c2e5f9aed7a4dd807bf80
dbi <dbi>
2005-11-25 21:45:12 +0000

    Translation to Bulgarian

---
+++ script/pavement/dialogs_bg.lua
@@ -0,0 +1,58 @@
+
+dialogId("dir-m-rada0", "font_small", "Задръж си съвета.")
+
+dialogId("dir-m-rada1", "font_small", "Това си го знаем много добре.")
+
+dialogId("dir-m-rada2", "font_small", "Изчезвай.")
+
+dialogId("dir-m-rada3", "font_small", "Търпението ми се изчерпва.")
+
+dialogId("dir-m-rada4", "font_small", "Арргх... Ще го натроша на парчета.")
+
+dialogId("dir-v-rada0", "font_big", "Остави го, нека си говори.")
+
+dialogId("dir-v-rada1", "font_big", "Да помислим.")
+
+dialogId("dir-v-rada2", "font_big", "Посъветваха ни. Отново.")
+
+dialogId("dir-v-rada3", "font_big", "Срамота е, че нямам уши. Можех да ги запуша.")
+
+dialogId("dir-v-rada4", "font_big", "Задръстих се до хрилете с тези безсмислици.")
+
+dialogId("dir-hs-uvod0", "font_statue", "Най-лесно би било ако")
+
+dialogId("dir-hs-uvod1", "font_statue", "По-добре ще е ако")
+
+dialogId("dir-hs-uvod2", "font_statue", "Би ви помогнало ако")
+
+dialogId("dir-hs-uvod3", "font_statue", "Имам идея. Ами ако")
+
+dialogId("dir-hs-uvod4", "font_statue", "Извинете, че Ви прекъсван, но какво ще стане ако")
+
+dialogId("dir-hs-konec0", "font_statue", "съборите някоя стена.")
+
+dialogId("dir-hs-konec1", "font_statue", "извадите няколко камъка от стените и запълните дупките с тях.")
+
+dialogId("dir-hs-konec2", "font_statue", "запълните дупките с нещо, за да не се закача стоманената тръба.")
+
+dialogId("dir-hs-konec3", "font_statue", "разбиете колоната до изхода.")
+
+dialogId("dir-hs-konec4", "font_statue", "разрежете с ножовка стоманените тръби.")
+
+dialogId("dir-hs-konec5", "font_statue", "разместите обектите така, че да можете да излезете.")
+
+dialogId("dir-hs-konec6", "font_statue", "намерите някакво решение и стигнете до следващото ниво.")
+
+dialogId("dir-hs-konec7", "font_statue", "помислите наистина сериозно.")
+
+dialogId("dir-hs-konec8", "font_statue", "изплувате през онази дупка отляво.")
+
+dialogId("k1-chob-1", "", "")
+
+dialogId("k1-chob-2", "", "")
+
+dialogId("k1-chob-3", "", "")
+
+dialogId("k1-chob-p", "", "")
+
+dialogId("k1-x-vrz", "", "")
---
+++ script/pearls/dialogs_bg.lua
@@ -0,0 +1,38 @@
+
+dialogId("jed-m-libi", "font_small", "Хубаво е тук.")
+
+dialogId("jed-m-perly0", "font_small", "Колко много перли.")
+
+dialogId("jed-m-perly1", "font_small", "Тук има толкова много перли.")
+
+dialogId("jed-v-poslani0", "font_big", "Слушай... Не забравяй за мисията!")
+
+dialogId("jed-v-poslani1", "font_big", "Не сме ловци на перли.")
+
+dialogId("jed-v-poslani2", "font_big", "Помни! Имаме мисия.")
+
+dialogId("jed-m-flakas", "font_small", "Ти само стоиш там горе, а аз трябва да свърша цялата работа!")
+
+dialogId("jed-v-uzivat0", "font_big", "Не спомена ли, че обичаш перли?")
+
+dialogId("jed-v-uzivat1", "font_big", "Не си ли щастлива сред толкова перли?")
+
+dialogId("jed-m-perlorodka0", "font_small", "Трябва ми още една перла.")
+
+dialogId("jed-x-nedam", "font_parrot", "Няма.")
+
+dialogId("jed-m-perlorodka1", "font_small", "Още една перла, моля.")
+
+dialogId("jed-m-perlorodka2", "font_small", "Скъпи приятелю, не би ли могъл да ми заемеш само една мъничка перла?")
+
+dialogId("jed-m-trubka", "font_small", "Чудя се дали няма да е по-лесно да подкастрим онази стоманена тръба?")
+
+dialogId("jed-v-ocet", "font_big", "Чувал съм, че Клеопатра разтваряла перли в оцет.")
+
+dialogId("jed-m-moc", "font_small", "Мислиш ли, че ще са ни много?")
+
+dialogId("jed-v-vzdelat", "font_big", "Ами, аз само исках да ти дам поука.")
+
+dialogId("jed-v-poter", "font_big", "Като те гледам и си спомних как играхме на топчета с поповите лъжички.")
+
+dialogId("jed-m-kulicka", "font_small", "Нямаш резервно топче в джоба, нали?")
