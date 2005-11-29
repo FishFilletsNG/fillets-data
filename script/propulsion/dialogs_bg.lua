fda433d5987c61ae6d77c00a0e741586c65f156e
dbi <dbi>
2005-11-29 21:03:38 +0000

    Translation to Bulgarian

---
+++ script/propulsion/dialogs_bg.lua
@@ -0,0 +1,44 @@
+
+dialogId("poh-v-takhle", "font_big", "Това било значи!")
+
+dialogId("poh-m-tosnadne", "font_small", "О, не!")
+
+dialogId("poh-v-biosila", "font_big", "Биологична енергия. Трябваше да се досетим по-рано. Всичко води към нея.")
+
+dialogId("poh-m-reaktor", "font_small", "А ядреният реактор за какво беше?")
+
+dialogId("poh-v-automat", "font_big", "Не виждаш ли? Захранва машината за Кока-Кола ей там.")
+
+dialogId("poh-m-motor", "font_small", "Ами мотора?")
+
+dialogId("poh-v-tocit", "font_big", "Кара нивото да обикаля по екрана.")
+
+dialogId("poh-v-neuveri", "font_big", "Никой няма да ни повярва.")
+
+dialogId("poh-m-projekt", "font_small", "Може би. ОТРД работи над проект за използване на биоенергия.")
+
+dialogId("poh-v-zarizeni", "font_big", "Трябва да се измъкнем от това нещо.")
+
+dialogId("poh-m-sest", "font_small", "Ако правилно виждам, състои се от шест части.")
+
+dialogId("poh-v-klec", "font_big", "Клетката в средата вероятно е най-важната.")
+
+dialogId("poh-m-dobre", "font_small", "Добре, това трябва да го вземем.")
+
+dialogId("poh-v-forma", "font_big", "Погледни, там има някаква странна форма на живот. Не трябва ли да се опитаме да осъществим контакт?")
+
+dialogId("poh-m-princip", "font_small", "Задачата ни е съвсем ясна - принципи и устройство на междъзвесдната тяга. И нито дума за контакт.")
+
+dialogId("poh-v-pomoct", "font_big", "Да. Но те могат да ни помогнат.")
+
+dialogId("poh-m-dobryden0", "font_small", "Добър де. Извинете, моля, не може ли да забършем двигателя Ви?")
+
+dialogId("poh-m-dobryden1", "font_small", "Хм, добър ден, господине, мога ли да ви помоля да ни помогнете да демонтираме устройството за междузвездна тяга, моля?")
+
+dialogId("poh-v-ukol", "font_big", "Задачата ни в това ниво е да се измъкнем от клетката с животното.")
+
+dialogId("poh-m-pohadali", "font_small", "Имам чувството, че между тези двамата е имало спор.")
+
+dialogId("poh-v-setkani", "font_big", "Не съм си представял, че срещата с извънземни ще изглежда така.")
+
+dialogId("poh-m-sestra", "font_small", "Ти какво очакваш? Да отвлекат сестра ти?")
---
+++ script/puzzle/dialogs_bg.lua
@@ -0,0 +1,30 @@
+
+dialogId("puc-m-koukej", "font_small", "Я какви неща могат да се намерят на сметището.")
+
+dialogId("puc-v-podivej", "font_big", "Интересно какви неща изхвърлят хората.")
+
+dialogId("puc-v-videl", "font_big", "Сигурен съм, че съм виждал тази жена някъде.")
+
+dialogId("puc-m-oblicej", "font_small", "Сигурно грешиш. Има типична физиономия.")
+
+dialogId("puc-m-pld0", "font_small", "Погледни това нещо.")
+
+dialogId("puc-m-pld1", "font_small", "Погледни това. Какво чудовище.")
+
+dialogId("puc-m-hele", "font_small", "О, това е отблъскващо.")
+
+dialogId("puc-m-slizka", "font_small", "Отблъскващо, лигаво, мръсно и пречи.")
+
+dialogId("puc-v-fuska0", "font_big", "Данъците!.")
+
+dialogId("puc-v-fuska1", "font_big", "Тоста труд си трябва.")
+
+dialogId("puc-v-fuska2", "font_big", "Бая работа трябваше.")
+
+dialogId("puc-m-stalo", "font_small", "Но си заслужаваше, нали?")
+
+dialogId("puc-m-obraz", "font_small", "Представи си само, че беше някоя ценна картина и ние сме я спасили!")
+
+dialogId("puc-v-nesmysl", "font_big", "Ама че глупост!")
+
+dialogId("puc-x-pldik", "font_pink", "МУАХ... УАХ... УХ...")
