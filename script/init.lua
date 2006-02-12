ea359bc1c4efb77d2eaabc46ad28e379723f6f83
fidlej <fidlej>
2006-02-12 12:18:37 +0000

    Added score() function to print global score.

--- script/init.lua
+++ script/init.lua
@@ -12,7 +12,7 @@ local winCodes = {
     Polis = "pl",
     Spani = "es",
     Dutch = "nl",
-	Bulga = "bg",
+    Bulga = "bg",
 }
 
 if winCodes[lang] then
@@ -22,5 +22,29 @@ end
 --NOTE: default speech is 'cs' (there are cs/*.ogg files)
 if getParam("speech") == nil then
     setParam("speech", "cs")
+end
+
+--- Prints global score.
+-- Usable from debug console.
+function score()
+    local solvedCounter = 0
+    local userSteps = 0
+    local bestSteps = 0
+    local oldImpl = node_bestSolution
+    function node_bestSolution(codename, steps, solver)
+        local solution = "solved/"..codename..".lua"
+        if file_exists(solution) then
+            solvedCounter = solvedCounter + 1
+            bestSteps = bestSteps + steps
+            file_include(solution)
+            userSteps = userSteps + string.len(saved_moves)
+        end
+    end
+
+    file_include("script/worldfame.lua")
+    node_bestSolution = oldImpl
+    print("Solved:", solvedCounter)
+    print("Steps:", userSteps .."/".. bestSteps,
+        "(loss: ".. 100 * (userSteps - bestSteps) / bestSteps .."%)")
 end
 
