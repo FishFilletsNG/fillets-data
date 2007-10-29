9989279014af49eb166dd590c3455f4f5f6924b7
asmbsd <asmbsd>
2007-10-29 19:23:06 +0000

    Russian level dialog translate.

---
+++ script/party1/dialogs_ru.lua
@@ -0,0 +1,36 @@
+dialogId("pt1-m-parnicek", "font_small", "Look. The party boat.")
+dialogStr("Посмотри, вечеринка на лодке.")
+
+dialogId("pt1-v-predtucha", "font_big", "I have a feeling that this is not going to be a normal picnic.")
+dialogStr("У меня ощущение, что это будет не обычный пикник.")
+
+dialogId("pt1-m-predtucha", "font_small", "You and your feelings. Surely it’s full of kind and friendly people.")
+dialogStr("Это твои ощущения. Здесь полно добрых и дружественных людей")
+
+dialogId("pt1-m-kostlivec", "font_small", "Maybe, you were right. This is a strange picnic.")
+dialogStr("Может быть, ты прав. Это странный пикник")
+
+dialogId("pt1-m-vylezt0", "font_small", "Do you think they could get out of this boat?")
+dialogStr("Ты думаешь, они смогут сойти с этой лодки?")
+
+dialogId("pt1-m-vylezt1", "font_small", "I hope they can’t get out of that boat.")
+dialogStr("Надеюсь, они не согут сойти с нее.")
+
+dialogId("pt1-m-vylezt2", "font_small", "What if they start to pursue us?")
+dialogStr("Что, если они начнут гнаться за нами?")
+
+dialogId("pt1-v-pryc0", "font_big", "I don’t think they could get us. Skeletons can’t swim very well.")
+dialogStr("Я не думаю, что смогут это сделать. Скелеты не могут хорошо плавать.")
+
+dialogId("pt1-v-pryc1", "font_big", "Stop thinking about it and move your fins so that we can get out of here.")
+dialogStr("Хватит думать об этом. Перемести fins так, чтобы мы могли выйти отсюда.")
+
+dialogId("pt1-v-valec", "font_big", "You want to support that steel tube with a glass?")
+dialogStr("Ты хочешь поддержать эту стальную трубу стаканом?")
+
+dialogId("pt1-m-nemuzu", "font_small", "I can’t do anything else for you.")
+dialogStr("Я не могу сделать что-нибудь еще для тебя.")
+
+dialogId("pt1-v-pozor", "font_big", "Be careful not to spill it!")
+dialogStr("Смотри, не пролей это!")
+
---
+++ script/viking1/dialogs_ru.lua
@@ -0,0 +1,84 @@
+dialogId("d1-z-b1", "", "")
+dialogStr("")
+
+dialogId("d1-z-b2", "", "")
+dialogStr("")
+
+dialogId("d1-z-b3", "", "")
+dialogStr("")
+
+dialogId("d1-z-p1", "", "")
+dialogStr("")
+
+dialogId("d1-z-p2", "", "")
+dialogStr("")
+
+dialogId("d1-z-v0", "", "")
+dialogStr("")
+
+dialogId("d1-z-v1", "", "")
+dialogStr("")
+
+dialogId("d1-z-v2", "", "")
+dialogStr("")
+
+dialogId("dr-m-tojesnad", "font_small", "This must be a Viking ship!")
+dialogStr("Это, дожно быть, корабль викингов!")
+
+dialogId("dr-v-jiste", "font_big", "Sure, it’s a drakkar.")
+dialogStr("Да, это драккар.")
+
+dialogId("dr-m-musela", "font_small", "She must have sunk fast. The oarsmen are still in their places.")
+dialogStr("Он,наверно, быстро затонул. Гребцы находятся все еще на своих местах.")
+
+dialogId("dr-v-mozna", "font_big", "Maybe they sank her on purpose. Warriors would always accompany their leader on his way to Valhalla.")
+dialogStr("Возможно, они специально потопили его. Воины всегда сопровождают своего лидера на пути в Валхаллу.")
+
+dialogId("dr-m-hruza", "font_small", "That's awful. They look like they're still alive.")
+dialogStr("Это ужасно. Они выглядят как живые.")
+
+dialogId("d1-1-hudba0", "font_viking1", "The music is gone!")
+dialogStr("Музыка ушла!")
+
+dialogId("d1-1-hudba1", "font_viking1", "They stopped playing!")
+dialogStr("Они перестали играть!")
+
+dialogId("d1-1-hudba2", "font_viking1", "The music stopped again!")
+dialogStr("Музыка остановилась снова!")
+
+dialogId("d1-2-brb0", "font_viking2", "Oh, no!")
+dialogStr("О, нет!")
+
+dialogId("d1-2-brb1", "font_viking2", "Again?")
+dialogStr("Снова?")
+
+dialogId("d1-2-brb2", "font_viking2", "Oh, not again!")
+dialogStr("О, не снова!")
+
+dialogId("d1-3-brb0", "font_viking3", "What happened?")
+dialogStr("Что случилось?")
+
+dialogId("d1-3-brb1", "font_viking3", "Again?")
+dialogStr("Снова?")
+
+dialogId("d1-3-brb2", "font_viking3", "Oh, no!")
+dialogStr("О, нет!")
+
+dialogId("d1-4-brb0", "font_viking4", "The bloody music stopped!")
+dialogStr("Кровавая музыка остановилась!")
+
+dialogId("d1-4-brb1", "font_viking4", "What are they doing up there?")
+dialogStr("Что они там делают?")
+
+dialogId("d1-4-brb2", "font_viking4", "How could it be?")
+dialogStr("Как это может быть?")
+
+dialogId("d1-5-nevadi0", "font_viking5", "Take it easy, boys. Solo for Olaf!")
+dialogStr("Успокойтесь, мальчики. Соло для Олафа!")
+
+dialogId("d1-5-nevadi1", "font_viking5", "Never mind that, boys. We can sing ourself!")
+dialogStr("Не обращайте внимания, мальчики. Мы можем петь про себя!")
+
+dialogId("d1-5-nevadi2", "font_viking5", "Never mind, boys. We can sing ourself, like in the old times!")
+dialogStr("Не обращайте внимания, мальчики. Мы можем петь про себя как в былые времена!")
+
