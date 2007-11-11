b0c1513c88cfe4d9f0f297b2663f742fecf0ae35
asmbsd <asmbsd>
2007-11-11 17:21:48 +0000

    Russian translate, emulator (my spell fix)

--- script/emulator/dialogs_ru.lua
+++ script/emulator/dialogs_ru.lua
@@ -1,45 +1,45 @@
 dialogId("zx-m-pametnici", "font_small", "So this is a level for real connoisseurs.")
-dialogStr("Итак, этот уровень для знатоков.")
+dialogStr("Итак, этот уровень для ценителей.")
 
 dialogId("zx-v-osmibit", "font_big", "Yeah, I feel like I can feel the eight bit wind of history.")
-dialogStr("Да, мне нравиться, я чувствую веяние восьми битвой истории.")
+dialogStr("Да, мне нравиться, я чувствую веяние восьми битной истории.")
 
 dialogId("zx-m-pixel", "font_small", "Can you see that pixel graphic all around us? I must confess I feel nostalgic.")
-dialogStr("Ты видишь эти пискли вокруг нас? Ты должен признать, ты чувствуешь ностальгию.")
+dialogStr("Ты видишь эти пискли вокруг нас? Ты должен признать, чувствуешь ностальгию.")
 
 dialogId("zx-v-hry", "font_big", "This is an opportunity to play the merry old games of ZX Spectrum.")
-dialogStr("Это хорошая возможность радоваться старой игре на ZX Spectrum.")
+dialogStr("Хорошая возможность радоваться старой игре на ПК Дубна.")
 
 dialogId("zx-m-premyslis", "font_small", "What are you thinking about now?")
-dialogStr("Что ты думаешь об этом(сейчас)?")
+dialogStr("О чем ты думаешь сейчас?")
 
 dialogId("zx-v-pamet", "font_big", "Well, I just realized that the animations of us two alone would fill up the RAM of a ZX Spectrum.")
 dialogStr("Я пытаюсь реализовать анимацию в двух отдельных местах оперативной памяти на ПК Дубна.")
 
 dialogId("zx-v-otazka", "font_big", "Do you know that your question alone in WAV format would overfill the RAM of ZX Spectrum? And imagine how many such sounds are in this game.")
-dialogStr("Знаешь что некоторые твой вопрос в OGG формате могут переполнить РАМ на ПК Дубна.")
+dialogStr("Знаешь, что некоторые твои вопросы, в OGG формате, могут переполнить ОЗУ на ПК Дубна.")
 
 dialogId("zx-m-necodosebe", "font_small", "But still, these games had something to them, didn’t they?")
-dialogStr("Но спокойно, эта игра имеет что то другое, не считаешь?")
+dialogStr("Спокойно, эта игра немного другая, не считаешь?")
 
 dialogId("zx-m-knight", "font_small", "Can you see that Knight Lorer?")
-dialogStr("Ты видешь Knight Lorer.")
+dialogStr("Это Knight Lorer?")
 
 dialogId("zx-v-manicminer", "font_big", "And that must be the Manic Miner.")
-dialogStr("А это должно быть „Manic Miner\".")
+dialogStr("А это должно быть \"Manic Miner\".")
 
 dialogId("zx-m-jetpack", "font_small", "I really didn’t know I’d ever be putting the Jetpack together again.")
-dialogStr("Я несерьёзно не знаю, смогу ли я сложить Jetpack вместе.")
+dialogStr("Даже не знаю, смогу ли я сложить \"Jetpack\" снова.")
 
 dialogId("zx-v-roboti", "font_big", "This line of robots looks familiar.")
 dialogStr("Эта линия из роботов выглядит привычно.")
 
 dialogId("zx-m-highway", "font_small", "Oh sure, they are from the Highway Encounter.")
-dialogStr("Конечно же, это из „Highway Encounter\".")
+dialogStr("Конечно же, это из \"Highway Encounter\".")
 
 dialogId("zx-v-nahravani", "font_big", "I wonder what is loading right now.")
 dialogStr("Интересно, что это сейчас загружается.")
 
 dialogId("zx-m-ocel", "font_small", "The single biggest advantage of this room is that it contains no steel.")
-dialogStr("Единственное большое преимущество в этой комнате, то, что тут нет стали.")
+dialogStr("Одно большое преимущество в этой комнате в том, что в ней нет стали.")
 
