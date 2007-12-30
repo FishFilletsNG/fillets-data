3fb40d7894b0677a652cca054dbf8b442c7ca467
asmbsd <asmbsd>
2007-12-30 10:40:52 +0000

    Russian translate. WarCraft

---
+++ script/warcraft/dialogs_ru.lua
@@ -0,0 +1,73 @@
+-- FIXME. Spell
+dialogId("war-m-hrad", "font_small", "I am imprisoned under the castle.")
+dialogStr("Я заключена в замок.")
+
+dialogId("war-m-ocel", "font_small", "How did the steel get into this castle?")
+dialogStr("Откуда взялась сталь в замке?")
+
+dialogId("war-v-vesnicane", "font_big", "Hold on, I’ll bring some peasants to help you.")
+dialogStr("Замри, я постараюсь помочь тебе крестьянином.")
+
+dialogId("war-m-peoni", "font_small", "These are not peasants but peons.")
+dialogStr("Он не крестьянин, а пехотинец.")
+
+dialogId("war-v-doly", "font_big", "Do you think that these mines contain captain Silver’s treasure?")
+dialogStr("Как думаешь, эти минералы дороже сокровищ капитана Сильвера?")
+
+dialogId("war-m-povazuji", "font_small", "I consider this highly unlikely.")
+dialogStr("Я выгляжу не очень невезучий.")
+
+dialogId("war-m-kam", "font_small", "Where did we get us, again?")
+dialogStr("Где мы возьмем это снова?")
+
+dialogId("war-v-povedome", "font_big", "I find it oddly familiar here.")
+dialogStr("Я нашёл в этом что-то близкое.")
+
+dialogId("war-v-pohadka", "font_big", "When MS Word or another boring application is running on this machine, we, the computer game characters, get together in the C:\\WINDOWS\\CONFIG directory and talk.")
+dialogStr("Когда-то MS Word или другие надоедливые приложения запускалась на этой машине, мы, компьютерные игры, собирались в директории C:\\WINDOWS\\CONFIG и общались.")
+
+dialogId("war-m-pichat", "font_small", "When we don’t know what to do, we play hide-and-seek in the registry. That’s why your Windows sometimes crashes if you don’t play any games for a long period of time.")
+dialogStr("Когда мы не знали что делать, мы играли с реестром. По этому windows иногда ломался, если никто в течении долгого времени не запускал компьютерные игры.")
+
+dialogId("war-v-prozradit", "font_big", "You shouldn’t give that away. I only wanted to say that it looks very similar to this level. So many computer game characters.")
+dialogStr("Ты должен найти другой пуст. Я так думаю, потому что уровень выглядит очень похожим. Много компьютерных игр имеют отличительные признаки.")
+
+dialogId("war-m-aznato", "font_small", "But it’s not so difficult to get out of there.")
+dialogStr("Но это не очень сложно взять в другом месте.")
+
+dialogId("war-v-blizzard", "font_big", "The makers of this game would like to express their admiration for the Blizzard Entertainment company and wish them many successful games.")
+dialogStr("Создатели этой игры любили восхищаться Blizzard Entertainment компанией и хотели делать много удачных игр.")
+
+dialogId("war-m-hodiny", "font_small", "And they would also like to thank them for so many hours of neatly killed time.")
+dialogStr("И они любили благодарить их, за столь большое количество приятно убитого времени.")
+
+dialogId("war-k-ready0", "", "")
+dialogStr("")
+
+dialogId("war-k-ready1", "", "")
+dialogStr("")
+
+dialogId("war-k-ready2", "", "")
+dialogStr("")
+
+dialogId("war-k-move0", "", "")
+dialogStr("")
+
+dialogId("war-k-move1", "", "")
+dialogStr("")
+
+dialogId("war-k-move2", "", "")
+dialogStr("")
+
+dialogId("war-a-ready0", "", "")
+dialogStr("")
+
+dialogId("war-a-ready1", "", "")
+dialogStr("")
+
+dialogId("war-a-move0", "", "")
+dialogStr("")
+
+dialogId("war-a-move1", "", "")
+dialogStr("")
+
