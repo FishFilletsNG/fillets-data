4c1d0aeb2958836a77391b6ad50fd5f44e5c8eef
asmbsd <asmbsd>
2008-01-02 07:07:31 +0000

    Russian translate. warcraft (mini fix)

--- script/warcraft/dialogs_ru.lua
+++ script/warcraft/dialogs_ru.lua
@@ -6,34 +6,34 @@ dialogId("war-m-ocel", "font_small", "How did the steel get into this castle?")
 dialogStr("Откуда взялась сталь в замке?")
 
 dialogId("war-v-vesnicane", "font_big", "Hold on, I’ll bring some peasants to help you.")
-dialogStr("Замри, я постараюсь помочь тебе крестьянином.")
+dialogStr("Держись, я приведу крестьян тебе на помощь.")
 
 dialogId("war-m-peoni", "font_small", "These are not peasants but peons.")
-dialogStr("Он не крестьянин, а пехотинец.")
+dialogStr("Это не крестьяне, а батраки.")
 
 dialogId("war-v-doly", "font_big", "Do you think that these mines contain captain Silver’s treasure?")
-dialogStr("Как думаешь, эти минералы дороже сокровищ капитана Сильвера?")
+dialogStr("Как пологешь, в этих рудниках спрятаны сокровища капитана Сильвера?")
 
 dialogId("war-m-povazuji", "font_small", "I consider this highly unlikely.")
-dialogStr("Я выгляжу не очень невезучий.")
+dialogStr("Я пологаю, это мало вероятно.")
 
 dialogId("war-m-kam", "font_small", "Where did we get us, again?")
-dialogStr("Где мы возьмем это снова?")
+dialogStr("Где это мы находимся?")
 
 dialogId("war-v-povedome", "font_big", "I find it oddly familiar here.")
-dialogStr("Я нашёл в этом что-то близкое.")
+dialogStr("Это выглядит очень знакомым.")
 
 dialogId("war-v-pohadka", "font_big", "When MS Word or another boring application is running on this machine, we, the computer game characters, get together in the C:\\WINDOWS\\CONFIG directory and talk.")
-dialogStr("Когда-то MS Word или другие надоедливые приложения запускалась на этой машине, мы, компьютерные игры, собирались в директории C:\\WINDOWS\\CONFIG и общались.")
+dialogStr("Когда-то MS Word или другие скучные приложения запускаются на этой машине, мы, герои компьютерной игры, собираемся в директории C:\\WINDOWS\\CONFIG и болтаем.")
 
 dialogId("war-m-pichat", "font_small", "When we don’t know what to do, we play hide-and-seek in the registry. That’s why your Windows sometimes crashes if you don’t play any games for a long period of time.")
-dialogStr("Когда мы не знали что делать, мы играли с реестром. По этому windows иногда ломался, если никто в течении долгого времени не запускал компьютерные игры.")
+dialogStr("Когда мы не знаем чем заняться, мы играем в прятки в реестре. Поэтому windows иногда ломается, если вы долго не играете в компьютерные игры.")
 
 dialogId("war-v-prozradit", "font_big", "You shouldn’t give that away. I only wanted to say that it looks very similar to this level. So many computer game characters.")
-dialogStr("Ты должен найти другой пуст. Я так думаю, потому что уровень выглядит очень похожим. Много компьютерных игр имеют отличительные признаки.")
+dialogStr("Ты должна найти другое развлечение. Я лишь хотел сказать, что уровень выглядит очень похоже. Так много компьютерных героев.")
 
 dialogId("war-m-aznato", "font_small", "But it’s not so difficult to get out of there.")
-dialogStr("Но это не очень сложно взять в другом месте.")
+dialogStr("Но это не так сложно выбраться отсуда.")
 
 dialogId("war-v-blizzard", "font_big", "The makers of this game would like to express their admiration for the Blizzard Entertainment company and wish them many successful games.")
 dialogStr("Создатели этой игры любили восхищаться Blizzard Entertainment компанией и хотели делать много удачных игр.")
