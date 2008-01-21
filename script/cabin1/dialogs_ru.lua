b4436cf3741d87d128ce5dd80580b54731281561
asmbsd <asmbsd>
2008-01-21 16:57:48 +0000

    Russian translate (5 levels)

---
+++ script/cabin1/dialogs_ru.lua
@@ -0,0 +1,127 @@
+-- FIXME. spell
+dialogId("k1-pap-3xkruty", "font_parrot", "Crrruel, crrruel, crrruel Captain Silverrrr...")
+dialogStr("Жжжесток, жжжесток, жжжесток капитан Силверррр...")
+
+dialogId("k1-pap-karamba", "font_parrot", "Ay... caramba!!!")
+dialogStr("Ей... карамба!!!")
+
+dialogId("k1-pap-kruty", "font_parrot", "Crrruel Captain Silverrrr...")
+dialogStr("Жжжесток капитан Силверррр...")
+
+dialogId("k1-pap-drahokamy", "font_parrot", "Trrreasurre, you foools...")
+dialogStr("Сокрррровище, выыы глубцииии...")
+
+dialogId("k1-pap-kruci", "font_parrot", "Good grrrief!")
+dialogStr("Удачных бед!")
+
+dialogId("k1-pap-drahousek", "font_parrot", "That’s rrrrrridiculous! Polly is his masterrr’s darrrling.")
+dialogStr("Это нелепо! Поли он госппподддиин.")
+
+dialogId("k1-pap-nestrkej", "font_parrot", "Don’t ppppush me!")
+dialogStr("Не троггггай меня!")
+
+dialogId("k1-pap-noproto", "font_parrot", "That’s betterrr!")
+dialogStr("Так то луччччше!")
+
+dialogId("k1-pap-prcice", "font_parrot", "And it’s currrtains forrr you!")
+dialogStr("И это ззззановес для тебя!")
+
+dialogId("k1-pap-prekazet", "font_parrot", "I’m going to be in yourrr way herrre.")
+dialogStr("Я пуду на тттвоем ппусттти.")
+
+dialogId("k1-pap-problem", "font_parrot", "Trrricky prrroblem, isn’t it?")
+dialogStr("Трудддная проблеммма, да?")
+
+dialogId("k1-pap-sakris", "font_parrot", "Dang it!")
+dialogStr("Черт побери!")
+
+dialogId("k1-pap-sestlustej", "font_parrot", "You’rrre too fat, you can’t make it thrrrrough.")
+dialogStr("Ты слижжжжжком толстттттый, ты можжжжешь это испрррравить.")
+
+dialogId("k1-pap-sucharek", "font_parrot", "Polly wants a crrracker!")
+dialogStr("Полллли хочит печеньку!")
+
+dialogId("k1-pap-trhnisi", "font_parrot", "Beat it, sirrr...")
+dialogStr("Ломай это серрр...")
+
+dialogId("k1-pap-vodprejskni", "font_parrot", "Leave me alone and get lost!")
+dialogStr("Оставте меня в покое и уберайтесь!")
+
+dialogId("k1-m-copak", "font_small", "What could be inside?")
+dialogStr("Что случилось внутри?")
+
+dialogId("k1-v-kdovi", "font_big", "Who knows...")
+dialogStr("Кто знает...")
+
+dialogId("k1-m-myslis", "font_small", "Could this be the legendary Silver’s treasure?")
+dialogStr("Может быть это сокровиша легендарного капитана Сильвира?")
+
+dialogId("k1-v-bedna", "font_big", "In such a small chest? The boss surely wouldn’t contact us for such a little case.")
+dialogStr("Этот маленький суднук? Главный возможно хранил его себе на всякий маленьки случай.")
+
+dialogId("k1-m-fuj", "font_small", "Yuck!")
+dialogStr("Гадость!")
+
+dialogId("k1-m-diky", "font_small", "Thanks.")
+dialogStr("Спасибо.")
+
+dialogId("k1-v-radose", "font_big", "No problem.")
+dialogStr("Не стоит.")
+
+dialogId("k1-m-codelas", "font_small", "What are you doing?!")
+dialogStr("Что ты делаешь?!")
+
+dialogId("k1-v-promin", "font_big", "I’m sorry. I’ll be more careful next time.")
+dialogStr("Прости. Я постораюсь быть оккуратнее в следующий раз.")
+
+dialogId("k1-v-opatrne", "font_big", "Be careful! Don’t hurt it!")
+dialogStr("Окуратнее! Не пораньcя этим!")
+
+dialogId("k1-m-tospisona", "font_small", "It’s more the other way around!")
+dialogStr("Скорее наоборт!")
+
+dialogId("k1-m-chobotnice", "font_small", "That’s some octopus! I wonder who’s her owner?")
+dialogStr("Это какой-то осминог! Я хочу знать, кто к ней относиться?")
+
+dialogId("k1-v-patrila", "font_big", "Maybe it belonged to the person who lived in this cabin.")
+dialogStr("Может быть это пренадлежит персоне, которая любила эту кабину.")
+
+dialogId("k1-m-podivin", "font_small", "Whoever lived here sure was weird.")
+dialogStr("Кто жил тут, был достаточно странным.")
+
+dialogId("k1-v-proc", "font_big", "Why?")
+dialogStr("Почему?")
+
+dialogId("k1-m-lebku", "font_small", "Look, he had a human skull here!")
+dialogStr("Посмотри, тут человеческий череп!")
+
+dialogId("k1-v-jejeho", "font_big", "Well, maybe it’s his own skull.")
+dialogStr("Хорошо, может быть это он и есть.")
+
+dialogId("k1-m-mysli", "font_small", "You think so?")
+dialogStr("Ты так думаешь?")
+
+dialogId("k1-v-citis", "font_big", "Can you sense the atmosphere of rot and decay? The sunken wreck, the parrot’s skeleton, the human skull, and the broken cradle...")
+dialogStr("Очущаешь атмосферу грязи и гнили? Утонвышение останки, скилет повторяющий как попугай, человеческий скилет и разбитая колыбель...")
+
+dialogId("k1-m-kolebku", "font_small", "Where do you see the cradle?")
+dialogStr("Ты видешь колыбель?")
+
+dialogId("k1-v-cit", "font_big", "Oh, you don’t have any feeling for poetry at all...")
+dialogStr("Ох, у тебя нет чуства поэзии...")
+
+dialogId("k1-chob-1", "", "")
+dialogStr("")
+
+dialogId("k1-chob-2", "", "")
+dialogStr("")
+
+dialogId("k1-chob-3", "", "")
+dialogStr("")
+
+dialogId("k1-chob-p", "", "")
+dialogStr("")
+
+dialogId("k1-x-vrz", "", "")
+dialogStr("")
+
