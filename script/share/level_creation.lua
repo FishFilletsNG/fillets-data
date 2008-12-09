2e72eb72e1068a9e72a7e4836504e5d3e2949c46
fidlej <fidlej>
2008-12-09 16:15:47 +0000

    Reverted unneeded code.

--- script/share/level_creation.lua
+++ script/share/level_creation.lua
@@ -157,18 +157,7 @@ function addItemAnim(model, picture_00)
 
     local lang = string.sub(options_getParam("lang") or "", 1, 2)
     for index, filename in ipairs(imgList(picture_00)) do
-    
-    		-- NOTE: uses select_lang.lua to determine avaiable languages
-    		local langs = {}
-    		local oldfunc = select_addFlag
-    		function select_addFlag(lang, flag)
-        	table.insert(langs, lang)
-    		end
-				file_include("script/select_lang.lua")
-    		select_addFlag = oldfunc
-        
-				local localized = string.gsub(filename, "(.*)(%.[^.]*)$", "%1_"..lang.."%2", 1)
-				--print("LANG: "..lang)
+        local localized = string.gsub(filename, "(.*)(%.[^.]*)$", "%1_"..lang.."%2", 1)
         if file_exists(localized) then
             print(string.format("DEBUG: including localized image"..
                     "; lang=%q; file=%q", lang, localized))
