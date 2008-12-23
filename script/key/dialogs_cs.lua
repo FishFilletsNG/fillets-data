318de05ef6c8a27c2bd4eec50117e641f5d15c7e
fidlej <fidlej>
2008-12-23 15:53:32 +0000

    Added Czech dialogs for key.

---
+++ script/key/dialogs_cs.lua
@@ -0,0 +1,142 @@
+dialogId("start-0", "font_small", "Look, someone has built a lock in the stone.")
+dialogStr("Podívej, někdo vybudoval zámek v kameni.")
+
+dialogId("start-1", "font_big", "Where do you see a lock? I only see that I cannot leave through the door.")
+dialogStr("Kde vidíš nějaký zámek? Já vidím jenom zavřené dveře, kterými neodpluji.")
+
+dialogId("start-2", "font_small", "Here is a lock. Down there is the key.")
+dialogStr("Zde je zámek. Dole leží klíč.")
+
+dialogId("start-3", "font_big", "I cannot leave anyway. It is closed all over there.")
+dialogStr("Stejně nemohu odplout, je zamčeno.")
+
+dialogId("start-4", "font_small", "That is why we will need the key.")
+dialogStr("Kvůli tomu potřebujeme ten klíč.")
+
+
+dialogId("rd-0-0", "font_big", "Can you please get me the key, I want to leave.")
+dialogStr("Můžeš mi podat ten klíč? Chtěl bych odplout.")
+
+dialogId("rd-0-1", "font_small", "I would if I could, but I think it is stuck.")
+dialogStr("Podala bych ti ho, kdybych mohla. Myslím, že je přilepený.")
+
+
+dialogId("rd-1-0", "font_big", "Look, a broom.")
+dialogStr("Podívej, koště.")
+
+dialogId("rd-1-1", "font_small", "I don't want to clean here. It was enough in the boiler room.")
+dialogStr("Nebudu zde uklízet. Stačilo mi to uklízení v kotelně.")
+
+dialogId("rd-1-2", "font_big", "Why you. I have cleaned the heavy things there.")
+dialogStr("Tobě? Já jsem tam uklidil všechny těžké předměty.")
+
+dialogId("rd-1-3", "font_small", "Without me, you would be still there.")
+dialogStr("Nebýt mě, byl by jsi tam ještě teď.")
+
+
+dialogId("rd-2-0", "font_small", "We'll just swim out. Do you see how big the holes in the net are?")
+dialogStr("Proč prostě neodplaveme pryč? Vidíš jak velké jsou díry jsou v té síti?")
+
+dialogId("rd-2-1", "font_big", "Yes, too small for me.")
+dialogStr("Ano, příliš malé pro mě.")
+
+
+dialogId("rd-3-0", "font_small", "I think, we should really unlock with the key.")
+dialogStr("Opravdu myslím, že bychom měli odemknout tím klíčem.")
+
+dialogId("rd-3-1", "font_big", "Then get it, big mouth.")
+dialogStr("Tak ho vem, když jsi tak chytrá.")
+
+dialogId("rd-3-2", "font_small", "Be nice to me, otherwise I'll swim out alone.")
+dialogStr("Nebuď hubatý, nebo odpluji sama.")
+
+dialogId("rd-3-3", "font_big", "OK, I wouldn't say such things to you.")
+dialogStr("OK, nebudu ti tak říkat.")
+
+dialogId("rd-3-4", "font_small", "Don't even think them.")
+dialogStr("Ani si to nemysli.")
+
+dialogId("rd-3-5", "font_big", "Hmm... OK.")
+dialogStr("Hmm... OK.")
+
+
+dialogId("rd-4-0", "font_big", "Do you think the key will unfix if we move it around a bit?")
+dialogStr("Myslíš, že by se ten klíč mohl uvolnit, když s ním budeme šoupat?")
+
+dialogId("rd-4-1", "font_small", "I don't think so, but we can try.")
+dialogStr("Nemyslím, ale můžeme to zkusit.")
+
+
+dialogId("rdopt-0-0", "font_big", "We will never get the key. It's been there so long that it is grown over by a coral.")
+dialogStr("Nikdy ten klíč nedostaneme. Byl tady tak dlouho, že obrostl korálem.")
+
+dialogId("rdopt-0-1", "font_small", "Don't loose your nerves.")
+dialogStr("Neztrácej nervy.")
+
+
+dialogId("rdopt-1-0", "font_big", "Might the icicle over there being helpful?")
+dialogStr("Nehodil by se ten rampouch co je nahoře?")
+
+dialogId("rdopt-1-1", "font_small", "We wouldn't get it anyway.")
+dialogStr("Stejně ho neuvolníme.")
+
+dialogId("rdopt-1-2", "font_big", "We'll get it with enough force.")
+dialogStr("S dostatečnou silou dostaneme ho.")
+
+dialogId("rdopt-1-3", "font_small", "Perhaps if you take the hammer?")
+dialogStr("Třeba, když vezmeš to kladivo?")
+
+
+dialogId("cactus-0small", "font_small", "Outch, that cactus has spikes.")
+dialogStr("Au, ten kaktus má ostny.")
+
+dialogId("cactus-1small", "font_big", "Of course, it is a cactus.")
+dialogStr("Vždyť je to taky kaktus.")
+
+dialogId("cactus-2small", "font_small", "What does it do here?")
+dialogStr("Co tady dělá?")
+
+
+dialogId("cactus-0big", "font_big", "Outch, that cactus has spikes.")
+dialogStr("Au, ten kaktus má ostny.")
+
+dialogId("cactus-1big", "font_small", "Of course, it is a cactus.")
+dialogStr("Vždyť je to taky kaktus.")
+
+dialogId("cactus-2big", "font_big", "What does it do here?")
+dialogStr("Co tady dělá?")
+
+
+dialogId("up-0-0", "font_big", "We got it. Now hold the key until I have left.")
+dialogStr("A máme to. Podrž ten klíč než odplavu.")
+
+dialogId("up-0-1", "font_small", "No problem.")
+dialogStr("Jasně.")
+
+dialogId("up-0-2", "font_small", "As soon I have learned to carry steel.")
+dialogStr("Hned jak se naučím nosit ocel.")
+
+dialogId("up-0-3", "font_big", "Then learn it immediately.")
+dialogStr("Tak se to nauč okamžitě.")
+
+
+dialogId("up-1-0", "font_big", "Please hold the key this time.")
+dialogStr("Prosím, drž ten klíč tentokrát.")
+
+dialogId("up-1-1", "font_small", "I'll try it.")
+dialogStr("Pokusím se.")
+
+
+dialogId("down-0-0", "font_big", "No! The door is closed again.")
+dialogStr("Ne! Ty dveře jsou zase zavřené.")
+
+dialogId("down-0-1", "font_small", "So open it again.")
+dialogStr("Tak je zase otevři.")
+
+
+dialogId("down-1-0", "font_big", "Grmbl...")
+dialogStr("Grmbl...")
+
+dialogId("down-1-1", "font_small", "Keep calm. We'll have it solved soon.")
+dialogStr("Klid. Vyřešíme to brzy.")
+
