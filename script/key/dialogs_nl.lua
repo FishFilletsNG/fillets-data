c7498b26db35a21b3f661cbc552fcd088b4d899b
fidlej <fidlej>
2008-12-27 17:37:14 +0000

    Used the improved English.

--- script/key/dialogs_nl.lua
+++ script/key/dialogs_nl.lua
@@ -1,133 +1,133 @@
-dialogId("start-0", "font_small", "Look, someone has built a lock in the stone.")
+dialogId("start-0", "font_small", "Look, someone’s built a lock in the stone.")
 dialogStr("Moet je zien, iemand heeft een slot gemaakt in die steen.")
 
-dialogId("start-1", "font_big", "Where do you see a lock? I only see that I cannot leave through the door.")
+dialogId("start-1", "font_big", "Where do you see a lock? I only see I can’t leave through the door.")
 dialogStr("Waar zie je een slot? Ik zie alleen maar dat ik door de deuren hier niet uit kom.")
 
-dialogId("start-2", "font_small", "Here is a lock. Down there is the key.")
+dialogId("start-2", "font_small", "There is a lock here somewhere. The key is down there.")
 dialogStr("Natuurlijk is hier een slot. Daar beneden ligt de sleutel.")
 
-dialogId("start-3", "font_big", "I cannot leave anyway. It is closed all over there.")
+dialogId("start-3", "font_big", "I can’t leave anyway. Everything is blocked over there.")
 dialogStr("Ik kom hier toch niet weg, want daar is alles afgesloten.")
 
-dialogId("start-4", "font_small", "That is why we will need the key.")
+dialogId("start-4", "font_small", "That’s why we need the key.")
 dialogStr("Daarom hebben we die sleutel ook nodig.")
 
 
-dialogId("rd-0-0", "font_big", "Can you please get me the key, I want to leave.")
+dialogId("rd-0-0", "font_big", "Could you please get me the key, I would like to leave.")
 dialogStr("Kun je die sleutel voor me halen?  Ik wil hier weg.")
 
-dialogId("rd-0-1", "font_small", "I would if I could, but I think it is stuck.")
+dialogId("rd-0-1", "font_small", "I would if I could, but I think it’s stuck.")
 dialogStr("Zou ik graag doen, maar ik denk dat hij vast zit.")
 
 
 dialogId("rd-1-0", "font_big", "Look, a broom.")
 dialogStr("Kijk nou, een bezem.")
 
-dialogId("rd-1-1", "font_small", "I don't want to clean here. It was enough in the boiler room.")
+dialogId("rd-1-1", "font_small", "I don’t want to clean up here. It was bad enough in the boiler room.")
 dialogStr("Ik heb geen zin meer in opruimen. De troep in het boilerhok was wel genoeg.")
 
-dialogId("rd-1-2", "font_big", "Why you. I have cleaned the heavy things there.")
+dialogId("rd-1-2", "font_big", "Why are you complaining? I cleared away all the heavy things there.")
 dialogStr("Wat zeur je nou? Ik heb al het zware werk gedaan.")
 
-dialogId("rd-1-3", "font_small", "Without me, you would be still there.")
+dialogId("rd-1-3", "font_small", "Without me, you would still be stuck there.")
 dialogStr("Zonder mij zat je daar nog.")
 
 
-dialogId("rd-2-0", "font_small", "We'll just swim out. Do you see how big the holes in the net are?")
+dialogId("rd-2-0", "font_small", "We could just swim out. Do you see how big the holes in the net are?")
 dialogStr("Waarom zwemmen we hier niet gewoon weg? Zie je hoe groot die gaten in het net zijn?")
 
 dialogId("rd-2-1", "font_big", "Yes, too small for me.")
 dialogStr("Ja, te klein voor mij.")
 
 
-dialogId("rd-3-0", "font_small", "I think, we should really unlock with the key.")
+dialogId("rd-3-0", "font_small", "I think we should really use the key to open the lock.")
 dialogStr("Ik denk dat we de boel met de sleutel open kunnen krijgen.")
 
-dialogId("rd-3-1", "font_big", "Then get it, big mouth.")
+dialogId("rd-3-1", "font_big", "Then go get it, smarty-pants.")
 dialogStr("Ga die sleutel dan halen, slimmert")
 
-dialogId("rd-3-2", "font_small", "Be nice to me, otherwise I'll swim out alone.")
+dialogId("rd-3-2", "font_small", "Be nice to me, or I might swim out alone.")
 dialogStr("Doe wat aardiger tegen mij, anders zwem ik hier in mijn eentje weg.")
 
-dialogId("rd-3-3", "font_big", "OK, I wouldn't say such things to you.")
+dialogId("rd-3-3", "font_big", "OK, I won’t say such things to you anymore.")
 dialogStr("Nou goed, dan hou ik dat soort dingen voor me.")
 
-dialogId("rd-3-4", "font_small", "Don't even think them.")
+dialogId("rd-3-4", "font_small", "Don’t even think them.")
 dialogStr("Denk ze ook niet.")
 
 dialogId("rd-3-5", "font_big", "Hmm... OK.")
 dialogStr("Hmm... vooruit.")
 
 
-dialogId("rd-4-0", "font_big", "Do you think the key will unfix if we move it around a bit?")
+dialogId("rd-4-0", "font_big", "Do you think the key will get loose if we move it around a bit?")
 dialogStr("Denk je dat de sleutel los komt als we hem een beetje op en neer bewegen?")
 
-dialogId("rd-4-1", "font_small", "I don't think so, but we can try.")
+dialogId("rd-4-1", "font_small", "I don’t think so, but we can try.")
 dialogStr("Ik denk van niet, maar we kunnen het proberen.")
 
 
-dialogId("rdopt-0-0", "font_big", "We will never get the key. It's been there so long that it is grown over by a coral.")
+dialogId("rdopt-0-0", "font_big", "We’ll never get the key out. It’s been stuck there so long that it’s grown over with coral.")
 dialogStr("We krijgen de sleutel nooit los. Die zit daar al zo lang dat hij met koraal bedekt is.")
 
-dialogId("rdopt-0-1", "font_small", "Don't loose your nerves.")
+dialogId("rdopt-0-1", "font_small", "Don’t give up.")
 dialogStr("Geef niet op!")
 
 
-dialogId("rdopt-1-0", "font_big", "Might the icicle over there being helpful?")
+dialogId("rdopt-1-0", "font_big", "Could the icicle over there help?")
 dialogStr("Zou die ijspegel daar boven handig zijn?")
 
-dialogId("rdopt-1-1", "font_small", "We wouldn't get it anyway.")
+dialogId("rdopt-1-1", "font_small", "We can’t get it out anyway.")
 dialogStr("Daar komen we ook niet bij.")
 
-dialogId("rdopt-1-2", "font_big", "We'll get it with enough force.")
+dialogId("rdopt-1-2", "font_big", "We just have to apply enough force.")
 dialogStr("Met genoeg geweld vast wel.")
 
-dialogId("rdopt-1-3", "font_small", "Perhaps if you take the hammer?")
+dialogId("rdopt-1-3", "font_small", "Perhaps if you use the hammer?")
 dialogStr("Misschien als je de hamer gebruikt?")
 
 
 dialogId("cactus-0small", "font_small", "Outch, that cactus has spikes.")
 dialogStr("Auw, die cactus heeft stekels.")
 
-dialogId("cactus-1small", "font_big", "Of course, it is a cactus.")
+dialogId("cactus-1small", "font_big", "Of course, it’s a cactus.")
 dialogStr("Het is dan ook een cactus.")
 
-dialogId("cactus-2small", "font_small", "What does it do here?")
+dialogId("cactus-2small", "font_small", "What’s it doing here?")
 dialogStr("Wat doet hij hier?")
 
 
 dialogId("cactus-0big", "font_big", "Outch, that cactus has spikes.")
 dialogStr("Auw, die cactus heeft stekels.")
 
-dialogId("cactus-1big", "font_small", "Of course, it is a cactus.")
+dialogId("cactus-1big", "font_small", "Of course, it’s a cactus.")
 dialogStr("Het is dan ook een cactus.")
 
-dialogId("cactus-2big", "font_big", "What does it do here?")
+dialogId("cactus-2big", "font_big", "What’s it doing here?")
 dialogStr("Wat doet hij hier?")
 
 
-dialogId("up-0-0", "font_big", "We got it. Now hold the key until I have left.")
+dialogId("up-0-0", "font_big", "We’ve got it. Now hold the key until I’ve left.")
 dialogStr("Het is gelukt. Hou alsjeblieft even de sleutel vast zodat ik weg kan zwemmen.")
 
 dialogId("up-0-1", "font_small", "No problem.")
 dialogStr("Natuurlijk.")
 
-dialogId("up-0-2", "font_small", "As soon I have learned to carry steel.")
+dialogId("up-0-2", "font_small", "As soon I’ve learned to carry steel.")
 dialogStr("Zodra ik staal kan dragen.")
 
-dialogId("up-0-3", "font_big", "Then learn it immediately.")
+dialogId("up-0-3", "font_big", "You’d better learn quickly.")
 dialogStr("Leer dat dan snel.")
 
 
 dialogId("up-1-0", "font_big", "Please hold the key this time.")
 dialogStr("Hou deze keer de sleutel alsjeblieft goed vast.")
 
-dialogId("up-1-1", "font_small", "I'll try it.")
+dialogId("up-1-1", "font_small", "I’ll try.")
 dialogStr("Ik doe mijn best.")
 
 
-dialogId("down-0-0", "font_big", "No! The door is closed again.")
+dialogId("down-0-0", "font_big", "No! The door closed again.")
 dialogStr("Nee! De deur is weer dicht.")
 
 dialogId("down-0-1", "font_small", "So open it again.")
@@ -137,6 +137,6 @@ dialogStr("Maak hem dan weer open.")
 dialogId("down-1-0", "font_big", "Grmbl...")
 dialogStr("Grmbl...")
 
-dialogId("down-1-1", "font_small", "Keep calm. We'll have it solved soon.")
+dialogId("down-1-1", "font_small", "Stay calm. We’ll get it soon.")
 dialogStr("Wind je niet op. We zijn er bijna.")
 
