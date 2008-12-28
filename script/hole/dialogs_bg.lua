440f442b3d246888c5481605027d8d3ebf5bd5fc
dbi <dbi>
2008-12-28 14:04:47 +0000

    add Bulgarian translation for "hole" level

---
+++ script/hole/dialogs_bg.lua
@@ -0,0 +1,49 @@
+dialogId("m-strasidelna", "font_small", "Тази стая е обитавана от духове.")
+
+dialogId("v-bojim", "font_big",
+        "Боя се, че ще е трудно да пренесем нещо над тази дупка.")
+
+dialogId("s-prejete", "font_green", "Ето, както пожела.")
+
+dialogId("l-halo0", "font_lightgrey", "Има ли някой?")
+
+dialogId("l-halo1", "font_lightgrey", "Кой е там?")
+
+dialogId("l-halo2", "font_lightgrey", "Мисля, че приемникът не работи.")
+
+dialogId("l-dejte0", "font_lightgrey",
+         "Върни го веднага обратно! Индианското украшение си е мое.")
+
+dialogId("l-dejte1", "font_lightgrey", "Веднага го върни обратно, мое е!")
+
+dialogId("l-dejte2", "font_lightgrey",
+         "О О О О, Аз съм индианец. Върни го веднага обратно!")
+
+dialogId("l-dejte3", "font_lightgrey", "О О О О, веднага го върни обратно!")
+
+dialogId("l-vinnetou", "font_lightgrey",
+         "Накъде ме буташ? Аз съм известният Винету!")
+
+dialogId("m-nedame0", "font_small",
+         "Съжалявам, имаме нужда от това.")
+
+dialogId("v-nedame0", "font_big",
+        "Как може да е твое като не си индианец?")
+
+dialogId("m-nedame1", "font_small",
+         "Не може да си индианец - не си червенокож.")
+
+dialogId("v-nedame1", "font_big", "Не мога да отрека.")
+
+dialogId("v-neber", "font_big",
+"Молим играчът да не приема действието в играта като проява на дискриминация.")
+
+dialogId("m-sluchatko", "font_small", "Това може да е вълшебен приемник.")
+
+dialogId("v-zkus", "font_big", "Опитай да го задействаш.")
+
+dialogId("m-zmizet", "font_small",
+         "Ехо, пожелавам си стоманеното парче до изхода да изчезне.")
+
+dialogId("v-vratte", "font_big",
+         "Върни го обратно. Решението трябва да дойде от играча, не от нас.")
