cc10453a8a74b8a27ece69b88dc6494cbe3f1e3a
dbi <dbi>
2008-12-28 12:51:35 +0000

    translate hanoi dialogs to Bulgarian

---
+++ script/hanoi/dialogs_bg.lua
@@ -0,0 +1,67 @@
+dialogId("v-tady", "font_big", "Вече е тук!")
+
+dialogId("m-co", "font_small", "Кое?")
+
+dialogId("v-jacity", "font_big", "Или аз излизам, или ти!")
+ 
+dialogId("m-bude", "font_small", "И кой от нас ще излезе?")
+
+dialogId("v-mamja", "font_big", "Би трябвало да съм аз,")
+
+dialogId("v-alehrac", "font_big", "но решението е на играча.")
+
+dialogId("m-predstavujes", "font_small",
+"Как мислиш? Освобождаваш великана и ме оставяш тук. Той може да мести железа, но какво от това?")
+
+dialogId("m-restartuj", "font_small",
+"Ще чака да му помогна в следващата зала. Рестартирай я веднага!")
+
+dialogId("v-nenifer", "font_big",
+"Не е честно, пътят ми беше свободен. Само преместих желязото в неправилна посока.")
+
+dialogId("v-restartovat", "font_big",
+"Малките рибки се губят в огромния океан. Няма да отиде далеч без мен. Трябва да рестартираш незабавно.")
+
+dialogId("v-orechove", "font_big", "Докторът поръча друго.")
+
+dialogId("m-hazet", "font_small",
+"Не трябва да хвърлям малкото парче върху голямото.")
+
+dialogId("v-kopie", "font_big",
+"Някои биха помислили, че тази стая е просто копие от fish fillets 2.")
+
+dialogId("m-inspiroval", "font_small",
+"Всъщност авторът само е вдъхновен от стаята с хардуерния проблем.")
+
+dialogId("m-rekurzivni", "font_small",
+"Защо ли трябва да подреждам всичките парчета, при положение, че проста рекурсивна програма може да го направи?")
+
+dialogId("v-bavit", "font_big", "Дали би било приятно за играча?")
+
+dialogId("m-tesise", "font_small", "Играчът може би очаква следващите нива.")
+
+dialogId("v-budou", "font_big", "Но те са неизвестни.")
+
+dialogId("m-vyhecoval", "font_small", "Може да е жертва на внушение.")
+
+dialogId("v-looser", "font_big",
+"Уф, ако не можеш да решиш загадката си голям загубеняк.")
+
+dialogId("m-nepomahas", "font_small",
+"Няма да стане по-малко досадно ако не ми помогнеш.")
+
+dialogId("m-trikrat", "font_small", "Вече три пъти го каза.")
+
+dialogId("v-jineho", "font_big",
+"Ако трябва да казвам различно нещо всеки път, това би напълнило твърдия диск.")
+
+dialogId("m-vicedat", "font_small",
+"В днешни дни дисковете са по-големи.")
+
+dialogId("v-plny", "font_big",
+"Големината няма значение, дисковете винаги са пълни.")
+
+dialogId("m-citovat", "font_small", "Засрами се, измамник такъв!")
+
+dialogId("v-pochvalil", "font_big",
+"Поздравление, намери по-дългото решение")
