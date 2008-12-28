ffff720ffdf6bb7b8b84e7d7b2568265de94b770
dbi <dbi>
2008-12-28 14:29:32 +0000

    add Bulgarian dialogs for "nowall" level

---
+++ script/nowall/dialogs_bg.lua
@@ -0,0 +1,29 @@
+dialogId("m-zvlastni", "font_small", "Тази стая е много странна.")
+
+dialogId("v-zadne", "font_big", "Няма стени.")
+
+dialogId("m-zeme", "font_small", "Или пък изрезки от Земята.")
+
+dialogId("m-uvedomit", "font_small",
+"Разбери, че стоманеният цилиндър, който е около нас")
+
+dialogId("v-nad", "font_big", "... по-скоро над мен ...")
+
+dialogId("m-predmet", "font_small", "е просто обект.")
+
+dialogId("v-krehci", "font_big",
+"Точно затова съм по-внимателен от обикновено.")
+
+dialogId("m-otazka0", "font_small", "В НЛО ли съм?")
+dialogId("m-otazka1", "font_small", "Защо фонът е звездно небе?")
+dialogId("m-otazka2", "font_small", "А защо звездите се местят?")
+dialogId("m-otazka3", "font_small", "Доста бързо се върти.")
+dialogId("m-otazka4", "font_small", "Мислех, че сме в космоса.")
+
+dialogId("v-odpoved0", "font_big",
+"Не, това е просто още един асансьор.")
+dialogId("v-odpoved1", "font_big", "Защото е нощ.")
+dialogId("v-odpoved2", "font_big",
+"Защото земята се върти около оста си.")
+dialogId("v-odpoved3", "font_big",
+"За нас няма значение. Във водата сме.")
