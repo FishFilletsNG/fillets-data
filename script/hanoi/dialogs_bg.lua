0d5c1e37fc4a23052e460010bfd40787c5db9ae5
dbi <dbi>
2008-12-28 17:48:15 +0000

    fix Bulgarian translation of "hanoi" level to use the proper translation convention

--- script/hanoi/dialogs_bg.lua
+++ script/hanoi/dialogs_bg.lua
@@ -1,67 +1,94 @@
-dialogId("v-tady", "font_big", "Вече е тук!")
+dialogId("v-tady", "font_big", "It is already there!")
+dialogStr("Вече е тук!")
 
-dialogId("m-co", "font_small", "Кое?")
+dialogId("m-co", "font_small", "What?")
+dialogStr("Кое?")
 
-dialogId("v-jacity", "font_big", "Или аз излизам, или ти!")
- 
-dialogId("m-bude", "font_small", "И кой от нас ще излезе?")
+dialogId("v-jacity", "font_big", "Either I get out or you!")
+dialogStr("Или аз излизам, или ти!")
 
-dialogId("v-mamja", "font_big", "Би трябвало да съм аз,")
+dialogId("m-bude", "font_small", "And who will it be?")
+dialogStr("И кой от нас ще излезе?")
 
-dialogId("v-alehrac", "font_big", "но решението е на играча.")
+dialogId("v-mamja", "font_big", "It should be me,")
+dialogStr("Би трябвало да съм аз,")
+
+dialogId("v-alehrac", "font_big", "but player decides it.")
+dialogStr("но решението е на играча.")
 
 dialogId("m-predstavujes", "font_small",
-"Как мислиш? Освобождаваш великана и ме оставяш тук. Той може да мести железа, но какво от това?")
+"What are you thinking of? You set the giant free and you leave me here. OK, he can move the steel, so what?")
+dialogStr("Как мислиш? Освобождаваш великана и ме оставяш тук. Той може да мести железа, но какво от това?")
 
 dialogId("m-restartuj", "font_small",
-"Ще чака да му помогна в следващата зала. Рестартирай я веднага!")
+"He will be wating for my help in the next room. Restart it, right now!")
+dialogStr("Ще чака да му помогна в следващата зала. Рестартирай я веднага!")
 
 dialogId("v-nenifer", "font_big",
-"Не е честно, пътят ми беше свободен. Само преместих желязото в неправилна посока.")
+"It isn't fair, my way was free. I only pushed to the steel the wrong way.")
+dialogStr("Не е честно, пътят ми беше свободен. Само преместих желязото в неправилна посока.")
 
 dialogId("v-restartovat", "font_big",
-"Малките рибки се губят в огромния океан. Няма да отиде далеч без мен. Трябва да рестартираш незабавно.")
+"The small fish gets lost in the open ocean. Whithout me she won't get far away. You should restart the room immediatelly.")
+dialogStr("Малките рибки се губят в огромния океан. Няма да отиде далеч без мен. Трябва да рестартираш незабавно.")
 
-dialogId("v-orechove", "font_big", "Докторът поръча друго.")
+dialogId("v-orechove", "font_big", "It isn't what the doctor ordered.")
+dialogStr("Докторът поръча друго.")
 
 dialogId("m-hazet", "font_small",
-"Не трябва да хвърлям малкото парче върху голямото.")
+"I should not throw the smaller piece onto the bigger one.")
+dialogStr("Не трябва да поставям малкото парче върху голямото.")
 
 dialogId("v-kopie", "font_big",
-"Някои биха помислили, че тази стая е просто копие от fish fillets 2.")
+"Somebody could think this room is only a copy of one room from fish fillets 2.")
+dialogStr("Някои биха помислили, че тази стая е просто копие от fish fillets 2.")
 
 dialogId("m-inspiroval", "font_small",
-"Всъщност авторът само е вдъхновен от стаята с хардуерния проблем.")
+"But in fact the author was only inspired by the hardware problem room.")
+dialogStr("Всъщност авторът само е вдъхновен от стаята с хардуерния проблем.")
 
 dialogId("m-rekurzivni", "font_small",
-"Защо ли трябва да подреждам всичките парчета, при положение, че проста рекурсивна програма може да го направи?")
+"Why do I have to fold up all the pieces while a simple recursive program could do it?")
+dialogStr("Защо ли трябва да подреждам всичките парчета, при положение, че проста рекурсивна програма може да го направи?")
 
-dialogId("v-bavit", "font_big", "Дали би било приятно за играча?")
+dialogId("v-bavit", "font_big", "How can the player find a pleasure in this?")
+dialogStr("Дали би било приятно за играча?")
 
-dialogId("m-tesise", "font_small", "Играчът може би очаква следващите нива.")
+dialogId("m-tesise", "font_small", "He may look foward for next rooms.")
+dialogStr("Играчът може би очаква следващите нива.")
 
-dialogId("v-budou", "font_big", "Но те са неизвестни.")
+dialogId("v-budou", "font_big", "He doesn't know, what they are like.")
+dialogStr("Но те са неизвестни.")
 
-dialogId("m-vyhecoval", "font_small", "Може да е жертва на внушение.")
+dialogId("m-vyhecoval", "font_small", "Possibly somebody psyched him up.")
+dialogStr("Може да е жертва на внушение.")
 
 dialogId("v-looser", "font_big",
-"Уф, ако не можеш да решиш загадката си голям загубеняк.")
+"Oh well, player, if you don't solve it, you are a big loser.")
+dialogStr("Уф, ако не можеш да решиш загадката си голям загубеняк.")
 
 dialogId("m-nepomahas", "font_small",
-"Няма да стане по-малко досадно ако не ми помогнеш.")
+"Of course, you are not entertained, when you almost don't help me.")
+dialogStr("Няма да стане по-малко досадно ако не ми помагаш.")
 
-dialogId("m-trikrat", "font_small", "Вече три пъти го каза.")
+dialogId("m-trikrat", "font_small", "You said it already three times.")
+dialogStr("Вече три пъти го каза.")
 
 dialogId("v-jineho", "font_big",
-"Ако трябва да казвам различно нещо всеки път, това би напълнило твърдия диск.")
+"If I should say always something different then we fill player's disc up.")
+dialogStr("Ако трябва да казвам нещо различно нещо всеки път, това би напълнило твърдия диск.")
 
 dialogId("m-vicedat", "font_small",
-"В днешни дни дисковете са по-големи.")
+"Maybe nowadays player's disc can accommodate more data then in past.")
+dialogStr("В днешни дни дисковете са по-големи.")
 
 dialogId("v-plny", "font_big",
-"Големината няма значение, дисковете винаги са пълни.")
+"I think what ever size the disc is, it's always full.")
+dialogStr("Големината няма значение, дисковете винаги са пълни.")
 
-dialogId("m-citovat", "font_small", "Засрами се, измамник такъв!")
+dialogId("m-citovat", "font_small", "Be ashamed, cheater.")
+dialogStr("Засрами се, измамник такъв!")
 
 dialogId("v-pochvalil", "font_big",
-"Поздравление, намери по-дългото решение")
+"Finally I praise the player, that he solved it by the longer way.")
+dialogStr("Поздравления, намери по-дългото решение!")
