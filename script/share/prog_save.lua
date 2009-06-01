05f99798674c578c6bc4eb0e1d1490e477751740
fidlej <fidlej>
2009-06-01 22:07:08 +0000

    Preserved the first known state on the undo stack.

--- script/share/prog_save.lua
+++ script/share/prog_save.lua
@@ -47,7 +47,7 @@ end
 local UNDO_MAX_OVERWRITES = 10
 if not undo_stack then
     undo_clear_table = false
-    undo_num_overwrites = 0
+    undo_num_overwrites = -1
     undo_stack = {}
 end
 function script_saveUndo(moves, forceSave)
@@ -60,8 +60,11 @@ function script_saveUndo(moves, forceSave)
     local movesLen = string.len(moves)
     if movesLen <= 1 or undo_clear_table then
         undo_clear_table = false
-        undo_num_overwrites = 0
         undo_stack = {}
+    end
+    if table.getn(undo_stack) == 0 then
+        -- The first known state should be preserved.
+        undo_num_overwrites = -1
     end
 
     if undo_num_overwrites > 0 then
@@ -83,14 +86,14 @@ end
 
 function script_loadUndo()
     local saved = table.remove(undo_stack)
+    if not saved then
+        return
+    end
+
     -- The undo overwrites will be postponed.
     -- They should not overwrite the previous undo and this position.
     undo_num_overwrites = -1
     undo_clear_table = false
-
-    if not saved then
-        return
-    end
     level_load(saved.moves)
 
     local saved_table = unpickle_string(saved.serialized)
