487459390f2f5b702cd20390213c64bbd2782ccc
fidlej <fidlej>
2009-06-04 20:02:09 +0000

    Implemented redo on key +.

--- script/share/prog_save.lua
+++ script/share/prog_save.lua
@@ -33,7 +33,7 @@ local function assignModelAttributes(saved_table)
 end
 
 function script_loadState()
-    undo_clear_table = true
+    undo.seen_restarts = nil
     if not saved_models then
         error("global variable 'saved_models' is not set")
     end
@@ -45,58 +45,19 @@ end
 -- Functions to enable undo
 --
 local UNDO_MAX_OVERWRITES = 10
-if not undo_stack then
-    undo_clear_table = false
-    undo_num_overwrites = -1
-    undo_stack = {}
-end
-function script_saveUndo(moves, forceSave)
-    if forceSave then
-        undo_num_overwrites = 0
-    end
-
-    -- A discontinuity in the moves will clear the undo stack.
-    -- That limits the memory usage.
-    local movesLen = string.len(moves)
-    if movesLen <= 1 or undo_clear_table then
-        undo_clear_table = false
-        undo_stack = {}
-    end
-    if table.getn(undo_stack) == 0 then
-        -- The first known state should be preserved.
-        undo_num_overwrites = -1
-    end
-
-    if undo_num_overwrites > 0 then
-        undo_num_overwrites = undo_num_overwrites - 1
-        table.remove(undo_stack)
-    elseif undo_num_overwrites == 0 then
-        undo_num_overwrites = UNDO_MAX_OVERWRITES
-    else
-        undo_num_overwrites = undo_num_overwrites + 1
-    end
-
-    -- saving the actual model position (after room.prepareRound())
-    for index, model in pairs(getModelsTable()) do
-        model.X, model.Y = model:getLoc()
-    end
-    local serialized = pickle(getModelsTable())
-    table.insert(undo_stack, {moves=moves, serialized=serialized})
+if not undo then
+    undo = {}
+    undo.stack = {}
+    undo.seen_restarts = getRestartCount()
+    undo.num_overwrites = -1
+    -- The undo.index points where to save the next undo.
+    undo.index = 0
 end
 
-function script_loadUndo()
-    local saved = table.remove(undo_stack)
-    if not saved then
-        return
-    end
+local function applyUndoState(state)
+    level_load(state.moves)
 
-    -- The undo overwrites will be postponed.
-    -- They should not overwrite the previous undo and this position.
-    undo_num_overwrites = -1
-    undo_clear_table = false
-    level_load(saved.moves)
-
-    local saved_table = unpickle_string(saved.serialized)
+    local saved_table = unpickle_string(state.serialized)
     assignModelAttributes(saved_table)
     for index, model in pairs(getModelsTable()) do
         model_change_setLocation(model.index, model.X, model.Y)
@@ -107,3 +68,73 @@ function script_loadUndo()
         end
     end
 end
+
+local function collectUndoState(moves)
+    -- Saving the actual model position (after room.prepareRound())
+    for index, model in pairs(getModelsTable()) do
+        model.X, model.Y = model:getLoc()
+    end
+    local serialized = pickle(getModelsTable())
+    return {moves=moves, serialized=serialized}
+end
+
+local function setupOverwrites(forceSave)
+    -- Sets undo.index to do an overwrite or not.
+    if forceSave then
+        undo.num_overwrites = 0
+    end
+
+    -- Any level restart or load will clear the undo stack.
+    -- That limits the memory usage.
+    if undo.seen_restarts ~= getRestartCount() then
+        undo.seen_restarts = getRestartCount()
+        undo.num_overwrites = -1
+        undo.index = 0
+        undo.stack = {}
+    end
+    if undo.index == 0 then
+        -- The first known state should be preserved.
+        undo.num_overwrites = -1
+    end
+
+    if undo.num_overwrites > 0 then
+        undo.index = undo.index - 1
+        undo.num_overwrites = undo.num_overwrites - 1
+    else
+        if undo.num_overwrites == 0 then
+            undo.num_overwrites = UNDO_MAX_OVERWRITES
+        else
+            undo.num_overwrites = undo.num_overwrites + 1
+        end
+    end
+end
+
+local function preventRedo()
+    undo.stack[undo.index + 1] = nil
+end
+
+function script_saveUndo(moves, forceSave)
+    setupOverwrites(forceSave)
+
+    undo.stack[undo.index] = collectUndoState(moves)
+    undo.index = undo.index + 1
+    preventRedo()
+end
+
+function script_loadUndo(steps)
+    -- Undo has steps == 1
+    -- Redo has steps == -1
+    local saved = undo.stack[undo.index - steps]
+    if not saved then
+        return
+    end
+    -- The next saved undo will overwrite the just loaded undo.
+    -- And it itself will not be overwritten,
+    -- to keep the max distance between undos.
+    undo.index = undo.index - steps
+    undo.num_overwrites = -1
+    undo.seen_restarts = getRestartCount()
+
+    applyUndoState(saved)
+end
+
