daf4580fbd54bf0ed56ba9c099247581d7397a6f
mrpaja <mrpaja>
2009-08-02 15:09:22 +0000

    halloffame update

--- script/worldfame.lua
+++ script/worldfame.lua
@@ -86,5 +86,6 @@ node_bestSolution("nowall", 418, "Masaaki Irie")
 node_bestSolution("hole", 499, "Miroslav Olšák")
 node_bestSolution("key", 826, "David McGrath")
 node_bestSolution("keys", 283, "Masaaki Irie")
+node_bestSolution("linux", 694, "Masaaki Irie")
 
 node_bestSolution("electromagnet", 482, "Josef Peterka")
