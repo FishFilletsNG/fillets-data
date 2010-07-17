c84d22e6f27cb2b0057f88c1c82a7f0a1a165037
fidlej <fidlej>
2010-07-17 15:17:10 +0000

    Reused the flags.

--- script/select_speech.lua
+++ script/select_speech.lua
@@ -1,4 +1,4 @@
 
-select_addFlag("cs", "images/menu/subtitles/speech_cs.png")
-select_addFlag("nl", "images/menu/subtitles/speech_nl.png")
+select_addFlag("cs", "images/menu/flags/cs.png")
+select_addFlag("nl", "images/menu/flags/nl.png")
 select_addFlag("en", "images/menu/subtitles/no.png")
