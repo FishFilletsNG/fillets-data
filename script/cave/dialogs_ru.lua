7daaf56dc85500135588037c0bbbb0870b92ec6d
Levshinovskaya Maria <masa.levs@gmail.com>
2012-04-22 22:47:30 +0400

    Russian translation fixed (cave, grail).

--- script/cave/dialogs_ru.lua
+++ script/cave/dialogs_ru.lua
@@ -1,39 +1,38 @@
--- FIXME. asm
 dialogId("jes-m-netopyr", "font_small", "That bat must be terribly strong.")
-dialogStr("Эта летучая мышь должно быть ужасно сильная.")
+dialogStr("Эта летучая мышь, должно быть, ужасно сильная.")
 
 dialogId("jes-v-tojo", "font_big", "Well, yes.")
-dialogStr("Итак, да.")
+dialogStr("Пожалуй, да.")
 
 dialogId("jes-m-tvor", "font_small", "That red creature is a little bit strange.")
-dialogStr("Это красное творение совсем немного странное.")
+dialogStr("Это красное существо немного странное.")
 
 dialogId("jes-v-nestezuj", "font_big", "That may be true, but I think we are going to need it.")
-dialogStr("Возможноо, но я думаю, нам это не нужно.")
+dialogStr("Может и так, но, я думаю, оно нам понадобится.")
 
 dialogId("jes-v-uzke", "font_big", "It’s too narrow for me. You have to do it on your own down there.")
-dialogStr("Тут слишком узко для меня. Попробуй сама здесь спуститься.") --FIXME
+dialogStr("Тут слишком узко для меня. Ты должна справиться самостоятельно там внизу.")
 
 dialogId("jes-m-ryba", "font_small", "That white fish is a terrible obstacle.")
-dialogStr("Белая рыба страшная проблема.")
+dialogStr("Эта белая рыба является страшной помехой.")
 
 dialogId("jes-v-kamen", "font_big", "Fish? I thought it was only a stone.")
-dialogStr("Рыба? Мне кажется, что это просто камень.")
+dialogStr("Рыба? Я думал, что это просто камень.")
 
 dialogId("jes-v-gral", "font_big", "I think that we should be looking for the Grail somewhere else...")
-dialogStr("Я думаю, что мы должны смотреть для Гнала где нибудь ещё...") --FIXME
+dialogStr("Я думаю, что мы должны поискать Грааль где-нибудь ещё...") 
 
 dialogId("jes-m-deprese", "font_small", "Don’t be depressive. We will solve this in no time.")
-dialogStr("Нибудь так депрессивным. Нам нужно решить это без учета времени.")
+dialogStr("Не будь таким депрессивным. Мы решим эту проблему в ближайшее время.")
 
 dialogId("jes-v-nevim", "font_big", "I am not so sure.")
-dialogStr("Я не уверен.")
+dialogStr("Я не так уж уверен в этом.")
 
 dialogId("jes-m-takvidis", "font_small", "See? You are here.")
-dialogStr("Видишь? Ты где.")
+dialogStr("Вот видишь? Ты здесь.")
 
 dialogId("jes-m-potvora0", "font_small", "What kind of color-shifting monster is this?")
-dialogStr("Что за монстр здесь, цвета хамелеона?")
+dialogStr("Что это за монстр здесь, постоянно меняющий свой цвет?")
 
 dialogId("jes-m-potvora1", "font_small", "This is an abomination.")
 dialogStr("Это отвратительно.")
@@ -42,28 +41,28 @@ dialogId("jes-m-potvora2", "font_small", "This is an offense of Nature.")
 dialogStr("Это ошибка природы.")
 
 dialogId("jes-v-potvora0", "font_big", "It is called the Flashy hammerhead.")
-dialogStr("Это назвают Фальшь рыба-молот.")
+dialogStr("Это Ослепительная Рыба-молот.")
 
 dialogId("jes-v-potvora1", "font_big", "It is called the Glossy repulsor.")
-dialogStr("Это называют глянцевым отвратном.")
+dialogStr("Это Глянцевая Мерзость.")
 
 dialogId("jes-v-potvora2", "font_big", "It is called the Politician fish.")
-dialogStr("Это называют рыба-политик.")
+dialogStr("Это Рыба-политик.")
 
 dialogId("jes-m-netopyr0", "font_small", "This bat is kind of strange.")
-dialogStr("Странная разновидность эта летучая мышь.")
+dialogStr("Какая странная разновидность летучих мышей.")
 
 dialogId("jes-v-netopyr0", "font_big", "This is very phlegmatic bat.")
 dialogStr("Эта летучая мышь очень флегматичная.")
 
 dialogId("jes-m-netopyr1", "font_small", "This is a stuffed bat.")
-dialogStr("Это закормленная летучая мышь.")
+dialogStr("Это откормленная летучая мышь.")
 
 dialogId("jes-v-netopyr1", "font_big", "It’s a sculpture of bat.")
 dialogStr("Это скульптура летучий мыши.")
 
 dialogId("jes-m-netopyr2", "font_small", "It’s a stalagmite shaped as a bat.")
-dialogStr("Это сталагмит похожий на летучую мышь.")
+dialogStr("Это сталагмит, похожий на летучую мышь.")
 
 dialogId("jes-v-netopyr2", "font_big", "It’s a plain stalagmite.")
 dialogStr("Это обычный сталагмит.")
@@ -72,8 +71,8 @@ dialogId("jes-m-netopyr3", "font_small", "It’s just an ordinary piece of rock.
 dialogStr("Это обычный кусок горной породы.")
 
 dialogId("jes-v-nechut0", "font_big", "I think you are going to need that ‘monster’.")
-dialogStr("Я думаю нам ничего не нужно от этого „чудовища“.")
+dialogStr("Я думаю, нам пригодится это „чудовище“.")
 
 dialogId("jes-v-nechut1", "font_big", "I think you will have to overcome your dislike of that ‘abomination’.")
-dialogStr("Я думаю ты поборола твою нелюбовь к „отвратному“.")
+dialogStr("Я думаю, тебе придётся побороть свою неприязнь к „отвратному“.")
 
--- script/grail/dialogs_ru.lua
+++ script/grail/dialogs_ru.lua
@@ -1,42 +1,41 @@
--- FIXME adascor
 dialogId("gr-m-gral", "font_small", "That thing over there must be Holy Grail!")
-dialogStr("Это должно быть чаша Святого Грааля!")
+dialogStr("Это, должно быть, и есть Святой Грааль!")
 
 dialogId("gr-v-jiste", "font_big", "How can you be so sure?")
 dialogStr("Как ты можешь быть уверен в этом?")
 
 dialogId("gr-m-zare0", "font_small", "Can’t you see that halo?")
-dialogStr("Разве ты не видишь этот ореол?")
+dialogStr("Разве ты не видишь это сияние?")
 
 dialogId("gr-m-zare1", "font_small", "Can’t you see how it glows among the others?")
-dialogStr("Ты не видишь, как он горит среди остальных?")
+dialogStr("Ты не видишь, как он выделяется среди остальных?")
 
 dialogId("gr-m-zare2", "font_small", "Can’t you see how conspicuously is it placed in the center of the cave?")
-dialogStr("Ты не видишь, как он выделяется в центре пещеры?")
+dialogStr("Ты не видишь, как сильно он выделяется в центре пещеры?")
 
 dialogId("gr-v-nic0", "font_big", "The halo means nothing. The Holy Grail could look quite plain.")
 dialogStr("Ореол ничего не означает. Чаша Святого Грааля может выглядеть совсем обыкновенно.")
 
 dialogId("gr-v-nic1", "font_big", "All that glitters is not gold. No one ever said that Holy Grail must glow.")
-dialogStr("Не все, что блестит - золото. Никто не говорил, что чаша Святого Грааля должна светиться.")
+dialogStr("Не всё то золото, что блестит. Никто не говорил, что чаша Святого Грааля должна светиться.")
 
 dialogId("gr-v-nic2", "font_big", "Hmm... Placing a plain Grail in a conspicuous place will not turn it into a Holy one.")
-dialogStr("Хм... Размещение простой чаши Грааля в заметном месте не сделает ее Святой.")
+dialogStr("Хм... Размещение простой чаши Грааля в заметном месте не сделает её Святой.")
 
 dialogId("gr-v-tuseni", "font_big", "I feel it will not be as it seems.")
-dialogStr("Чувчтвую, что это будет не так, как кажется.")
+dialogStr("Чувствую, это будет не так, как кажется.")
 
 dialogId("gr-m-tuseni", "font_small", "You and your feelings.")
-dialogStr("Ты и твои чувства.")
+dialogStr("Опять ты со своими чувствами.")
 
 dialogId("gr-m-svaty0", "font_small", "Let’s go, we shall take that central glowing Grail with us.")
-dialogStr("Пойдем, нам надо взять с собой ту светящуюся в центре чашу Грааля.")
+dialogStr("Пойдём, нам надо взять ту светящуюся чашу Грааля с собой.")
 
 dialogId("gr-v-vsechny0", "font_big", "I’d rather take all of them.")
-dialogStr("Я взял бы всех их.")
+dialogStr("Я бы предпочёл взять их все.")
 
 dialogId("gr-m-vsechny0", "font_small", "Hmm... It seems you were right. We’ll take them all.")
-dialogStr("Хм... Кажется, ты прав. Мы возьмем их всех.")
+dialogStr("Хм... Кажется, ты прав. Мы возьмём все чаши.")
 
 dialogId("gr-m-svaty1", "font_small", "Our goal in this level is to push out the Holy Grail.")
 dialogStr("Наша цель на этом уровне - вытащить чашу Святого Грааля.")
@@ -51,8 +50,8 @@ dialogId("gr-m-vsechny1", "font_small", "Our goal is to push all the Grails out 
 dialogStr("Наша цель - вытащить все чаши Грааля из этой комнаты.")
 
 dialogId("gr-v-skoro0", "font_big", "Almost all of them are gone, now!")
-dialogStr("Почти все чаши ушли, сейчас!")          ---??
+dialogStr("Почти все чаши убраны!") 
 
 dialogId("gr-v-skoro1", "font_big", "One more Grail and we are done!")
-dialogStr("Еще одна чаша и мы все сделали!")
+dialogStr("Ещё одна чаша, и дело сделано!")
 
